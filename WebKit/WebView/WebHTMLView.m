night-codes/WebKit

commit d7093dabca40dfcd0891577cf32200123e9a1359

Reviewed by Adam and Oliver
        While working on http://bugs.webkit.org/show_bug.cgi?id=11675 I
        decided to fix much of the null-deref problems that creeped in via
        the loader refactoring.  This isn't changing behavior, just reintroducing
        the free nil checking we used to have with pure ObjC

        * WebView/WebHTMLView.m:
        (-[NSArray menuForEvent:]):  Explicitly check for null frames
        (-[NSArray mouseDown:]):  Ditto
        (-[NSArray mouseDragged:]):  Ditto
        (-[NSArray mouseUp:]):  Ditto
        (-[NSArray performKeyEquivalent:]):  Ditto
        (-[WebHTMLView elementAtPoint:allowShadowContent:]):  Ditto



Identifier: 15143@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18046 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/WebView/WebHTMLView.m
@@ -2657,12 +2657,15 @@ static WebHTMLView *lastHitView = nil;
     [_private->compController endRevertingChange:NO moveLeft:NO];
 
     _private->handlingMouseDownEvent = YES;
-    BOOL handledEvent = core([self _frame])->eventHandler()->sendContextMenuEvent(event);
+    BOOL handledEvent = NO;
+    Frame* coreframe = core([self _frame]);
+    if (coreframe)
+        handledEvent = coreframe->eventHandler()->sendContextMenuEvent(event);
     _private->handlingMouseDownEvent = NO;
     
     if (handledEvent) {
 #ifdef WEBCORE_CONTEXT_MENUS
-        if (Page* page = core([self _frame])->page()) {
+        if (coreframe && Page* page = coreframe->page()) {
             NSArray* menuItems = page->contextMenuController()->contextMenu()->platformDescription();
             NSMenu* menu = nil;
             if (menuItems && [menuItems count] > 0) {
@@ -2942,7 +2945,8 @@ static WebHTMLView *lastHitView = nil;
 
         // Let WebCore get a chance to deal with the event. This will call back to us
         // to start the autoscroll timer if appropriate.
-        core([self _frame])->eventHandler()->mouseDown(event);
+        if (Frame* coreframe = core([self _frame]))
+            coreframe->eventHandler()->mouseDown(event);
     }
 
 done:
@@ -2995,7 +2999,8 @@ done:
     [self retain];
 
     if (!_private->ignoringMouseDraggedEvents)
-        core([self _frame])->eventHandler()->mouseDragged(event);
+        if (Frame* coreframe = core([self _frame]))
+            coreframe->eventHandler()->mouseDragged(event);
 
     [self release];
 }
@@ -3078,7 +3083,8 @@ done:
     [self retain];
 
     [self _stopAutoscrollTimer];
-    core([self _frame])->eventHandler()->mouseUp(event);
+    if (Frame* coreframe = core([self _frame]))
+        coreframe->eventHandler()->mouseUp(event);
     [self _updateMouseoverWithFakeEvent];
 
     [self release];
@@ -4045,6 +4051,7 @@ done:
     if (!eventWasSentToWebCore 
             && event == [NSApp currentEvent]    // Pressing Esc results in a fake event being sent - don't pass it to WebCore
             && [self _web_firstResponderIsSelfOrDescendantView]
+            && core([self _frame])
             && core([self _frame])->eventHandler()->keyEvent(event))
         ret = YES;
     else
@@ -6156,8 +6163,10 @@ static DOMRange *unionDOMRanges(DOMRange *a, DOMRange *b)
 
 - (NSDictionary *)elementAtPoint:(NSPoint)point allowShadowContent:(BOOL)allow;
 {
-    return [[[WebElementDictionary alloc] initWithHitTestResult:
-        core([self _frame])->eventHandler()->hitTestResultAtPoint(IntPoint(point), allow)] autorelease];
+    Frame* coreframe = core([self _frame]);
+    if (coreframe) 
+        return [[[WebElementDictionary alloc] initWithHitTestResult:coreframe->eventHandler()->hitTestResultAtPoint(IntPoint(point), allow)] autorelease];
+    return nil;
 }
 
 @end

commit 0ea135bd603399fe4e6a4919abccb36ba34ecd91

Reviewed by Brady.
        Build fix for WebCore ContextMenus. It got broken by r18046.

        * WebView/WebHTMLView.m:
        (-[NSArray menuForEvent:]):


Identifier: 15154@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@18059 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/WebView/WebHTMLView.m
@@ -2663,9 +2663,9 @@ static WebHTMLView *lastHitView = nil;
         handledEvent = coreframe->eventHandler()->sendContextMenuEvent(event);
     _private->handlingMouseDownEvent = NO;
     
-    if (handledEvent) {
+    if (handledEvent && coreframe) {
 #ifdef WEBCORE_CONTEXT_MENUS
-        if (coreframe && Page* page = coreframe->page()) {
+        if (Page* page = coreframe->page()) {
             NSArray* menuItems = page->contextMenuController()->contextMenu()->platformDescription();
             NSMenu* menu = nil;
             if (menuItems && [menuItems count] > 0) {