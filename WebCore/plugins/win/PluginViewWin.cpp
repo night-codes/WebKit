night-codes/WebKit

commit 5d33323fcddafe9d0e6890bdf2828a19277e555b

Reviewed by Oliver.
        <rdar://problem/5253950>
        When Shockwave plugin has been loaded, exiting crashes Safari

        Add a "load count" for plugin packages. When the load count is zero, unload the plugin.

        * plugins/win/PluginPackageWin.cpp:
        (WebCore::PluginPackageWin::PluginPackageWin):
        (WebCore::PluginPackageWin::load):
        (WebCore::PluginPackageWin::unload):
        (WebCore::PluginPackageWin::unloadWithoutShutdown):
        * plugins/win/PluginPackageWin.h:

        * plugins/win/PluginViewWin.cpp:
        (WebCore::PluginViewWin::~PluginViewWin):
        Make sure to unload the plugin package and clear any references to plugin script objects.



Identifier: 18603@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@23827 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/plugins/win/PluginViewWin.cpp
@@ -1155,6 +1155,9 @@ PluginViewWin::~PluginViewWin()
 
     if (m_window)
         DestroyWindow(m_window);
+
+    m_parentFrame->cleanupScriptObjectsForPlugin(this);
+    m_plugin->unload();
 }
 
 void PluginViewWin::disconnectStream(PluginStreamWin* stream)

WebCore/plugins/win/PluginPackageWin.cpp
@@ -85,6 +85,7 @@ PluginPackageWin::PluginPackageWin(const String& path, const FILETIME& lastModif
     , m_module(0)
     , m_lastModified(lastModified)
     , m_isLoaded(false)
+    , m_loadCount(0)
 {
     int pos = m_path.deprecatedString().findRev('\\');
 
@@ -142,8 +143,10 @@ bool PluginPackageWin::fetchInfo()
 
 bool PluginPackageWin::load()
 {
-    if (m_isLoaded)
+    if (m_isLoaded) {
+        m_loadCount++;
         return true;
+    }
 
     WCHAR currentPath[MAX_PATH];
 
@@ -240,6 +243,7 @@ bool PluginPackageWin::load()
     if (npErr != NPERR_NO_ERROR)
         goto abort;
 
+    m_loadCount++;
     return true;
 abort:
     unloadWithoutShutdown();
@@ -249,6 +253,9 @@ abort:
 void PluginPackageWin::unload()
 {
     if (!m_isLoaded)
+        return;
+
+    if (--m_loadCount > 0)
         return;
 
     m_NPP_Shutdown();
@@ -261,6 +268,7 @@ void PluginPackageWin::unloadWithoutShutdown()
     if (!m_isLoaded)
         return;
 
+    ASSERT(m_loadCount == 0);
     ASSERT(m_module);
 
     FreeLibrary(m_module);

WebCore/plugins/win/PluginPackageWin.h
@@ -64,6 +64,7 @@ namespace WebCore {
         bool fetchInfo();
 
         bool m_isLoaded;
+        int m_loadCount;
 
         String m_description;
         String m_path;

commit 0e7c085929e752b1a7b6b438a00283112dafc1c8

Fix a crash in ~PluginViewWin()
This crash was introduced in r23827.

Reviewed by Anders.

Fixes dom/html/level2/html/HTMLAnchorElement01.html

* plugins/win/PluginViewWin.cpp:
(WebCore::PluginViewWin::~PluginViewWin): Allow m_plugin to be null.


Identifier: 18680@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@23920 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/plugins/win/PluginViewWin.cpp
@@ -1157,7 +1157,9 @@ PluginViewWin::~PluginViewWin()
         DestroyWindow(m_window);
 
     m_parentFrame->cleanupScriptObjectsForPlugin(this);
-    m_plugin->unload();
+
+    if (m_plugin)
+        m_plugin->unload();
 }
 
 void PluginViewWin::disconnectStream(PluginStreamWin* stream)