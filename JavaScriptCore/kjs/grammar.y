night-codes/WebKit

commit fb33ed04d419eda3733dcd92a17ce19aa5a28f2c

JavaScriptCore:
        Reviewed by Darin Adler.

        Fixed http://bugs.webkit.org/show_bug.cgi?id=10880 | <rdar://problem/5335694>
        REGRESSION: JavaScript menu doesn't appear on pricepoint.com (14595)

        Though the ECMA spec says auto-semicolon insertion should not occur
        without a newline or '}', Firefox treats do-while specially, and the
        library used by pricepoint.com requires that special treatment.

        * JavaScriptCore.xcodeproj/project.pbxproj:
        * kjs/grammar.y:

LayoutTests:

        Reviewed by Darin Adler.

        Test for http://bugs.webkit.org/show_bug.cgi?id=10880 | <rdar://problem/5335694>
        REGRESSION: JavaScript menu doesn't appear on pricepoint.com (14595)

        * fast/js/do-while-without-semicolon-expected.txt: Added.
        * fast/js/do-while-without-semicolon.html: Added.



Identifier: 19105@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@24457 268f45cc-cd09-0410-ab3c-d52691b4dbfc

JavaScriptCore/kjs/grammar.y
@@ -722,7 +722,7 @@ IfStatement:
 
 IterationStatement:
     DO Statement WHILE '(' Expr ')' ';'    { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3);}
-  | DO Statement WHILE '(' Expr ')' error    { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3); AUTO_SEMICOLON;}
+  | DO Statement WHILE '(' Expr ')'     { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3);}
   | WHILE '(' Expr ')' Statement        { $$ = new WhileNode($3, $5); DBG($$, @1, @4); }
   | FOR '(' ExprNoInOpt ';' ExprOpt ';' ExprOpt ')' Statement
                                         { $$ = new ForNode($3, $5, $7, $9); DBG($$, @1, @8); }

JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj
@@ -1352,7 +1352,6 @@
 		0867D690FE84028FC02AAC07 /* Project object */ = {
 			isa = PBXProject;
 			buildConfigurationList = 149C277108902AFE008A9EFC /* Build configuration list for PBXProject "JavaScriptCore" */;
-			compatibilityVersion = "Xcode 2.4";
 			hasScannedForEncodings = 1;
 			mainGroup = 0867D691FE84028FC02AAC07 /* JavaScriptCore */;
 			productRefGroup = 034768DFFF38A50411DB9C8B /* Products */;

LayoutTests/fast/js/do-while-without-semicolon-expected.txt
@@ -1,0 +1,3 @@
+This page verifies that a statement following a do/while statement will execute even if a semi-colon does not separate the two. See bug 10880.
+
+PASS: Script following do/while statement executed.

LayoutTests/fast/js/do-while-without-semicolon.html
@@ -1,0 +1,15 @@
+<p>This page verifies that a statement following a do/while statement will execute
+even if a semi-colon does not separate the two. See 
+<a href="http://bugs.webkit.org/show_bug.cgi?id=10880">bug 10880</a>.</p>
+<p id="console">FAIL: Script following do/while statement did not execute.</p>
+<script>
+function pass()
+{
+    document.getElementById("console").innerHTML = "PASS: Script following do/while statement executed.";
+}
+
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+do{}while(false)pass();
+</script>

commit 1259610fe60147b8aa5b5c505453e52620612650

Reviewed by Darin Adler.
        Fixed <rdar://problem/5567504> shift/reduce conflict introduced in r24457

        JS tests, including

            ecma_2/Statements/dowhile-001.js
            ecma_2/Statements/dowhile-002.js
            ecma_2/Statements/dowhile-003.js
            ecma_2/Statements/dowhile-004.js
            ecma_2/Statements/dowhile-005.js
            ecma_2/Statements/dowhile-006.js
            ecma_2/Statements/dowhile-007.js
            js1_2/statements/do_while.js

        and layout tests, including

            do-while-expression-value.html
            do-while-semicolon.html
            do-while-without-semicolon.html

        pass.

        * kjs/grammar.y: Use the explicit "error" production, as we do with other
        automatic semicolon insertions, to disambiguate "do { } while();" from
        "do { } while()" followed by ";" (the empty statement).



Identifier: 21191@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@27270 268f45cc-cd09-0410-ab3c-d52691b4dbfc

JavaScriptCore/kjs/grammar.y
@@ -729,8 +729,8 @@ IfStatement:
 ;
 
 IterationStatement:
-    DO Statement WHILE '(' Expr ')' ';'    { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3);}
-  | DO Statement WHILE '(' Expr ')'     { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3);}
+    DO Statement WHILE '(' Expr ')' ';'    { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3); }
+  | DO Statement WHILE '(' Expr ')' error  { $$ = new DoWhileNode($2, $5); DBG($$, @1, @3); } // Always performs automatic semicolon insertion.
   | WHILE '(' Expr ')' Statement        { $$ = new WhileNode($3, $5); DBG($$, @1, @4); }
   | FOR '(' ExprNoInOpt ';' ExprOpt ';' ExprOpt ')' Statement
                                         { $$ = new ForNode($3, $5, $7, $9); DBG($$, @1, @8); }