night-codes/WebKit

commit b76331c164d0533837027c5519fe4f245f65ec15

WebCore:
        Reviewed by John Sullivan.

        - fix <rdar://problem/5644324> Delegate-less WebKit clients should have no databases

        - remove the default database quota setting

        - simplify the quota-related client calls by merging the one for a new database with
          the one for an existing database, adding a feature where you can get details about
          a database being created using the same functions that work on the other databases,
          and removing the parameters and return values, instead having clients use a function
          to set the quota

        - fix unsafe multi-thread access to the database tracker's quota map
        - fix bug in deleteAllDatabases where it would iterate a HashMap while modifying it

        The tracker database is now only created when we set the quota for a database origin.
        Thus asking for info about databases won't cause anything to be written to disk.

        * WebCore.base.exp: Updated.

        * bindings/js/GCController.cpp: Added an #if to get rid of an unused function warning.

        * page/Chrome.cpp: Removed database-related functions. There's no problem having the
        code deal directly with the client.
        * page/Chrome.h: Ditto. Also made Chrome inherit from Noncopyable.

        * page/ChromeClient.h: Replaced the two quota-related functions with a single one.
        The details about the state of databases in the origin are now available by asking
        for database details. There's also no need to pass the security origin, since
        it's easy to get that from the frame's document.

        * page/Settings.cpp: Removed the default quota setting.
        * page/Settings.h: Ditto.

        * platform/FileSystem.h: Added deleteEmptyDirectory.
        * platform/gtk/FileSystemGtk.cpp:
        (WebCore::deleteEmptyDirectory): Added.
        * platform/posix/FileSystemPOSIX.cpp:
        (WebCore::deleteEmptyDirectory): Added.

WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
@@ -93,8 +93,7 @@ namespace WebKit {
 
         virtual void print(WebCore::Frame*);
 
-        virtual unsigned long long requestQuotaIncreaseForNewDatabase(WebCore::Frame*, WebCore::SecurityOrigin*, const WebCore::String&, unsigned long long);
-        virtual unsigned long long requestQuotaIncreaseForDatabaseOperation(WebCore::Frame*, WebCore::SecurityOrigin*, const WebCore::String&, unsigned long long);
+        virtual void exceededDatabaseQuota(Frame*, const String&);
 
     private:
         WebKitWebView* m_webView;

WebCore/WebCore.base.exp
@@ -294,14 +294,14 @@ __ZN7WebCore15BackForwardList9goForwardEv
 __ZN7WebCore15BackForwardListC1EPNS_4PageE
 __ZN7WebCore15BackForwardListD1Ev
 __ZN7WebCore15ContextMenuItem26releasePlatformDescriptionEv
+__ZN7WebCore15DatabaseTracker12deleteOriginEPNS_14SecurityOriginE
 __ZN7WebCore15DatabaseTracker14deleteDatabaseEPNS_14SecurityOriginERKNS_6StringE
 __ZN7WebCore15DatabaseTracker14quotaForOriginEPNS_14SecurityOriginE
 __ZN7WebCore15DatabaseTracker14usageForOriginEPNS_14SecurityOriginE
-__ZN7WebCore15DatabaseTracker15setDatabasePathERKNS_6StringE
 __ZN7WebCore15DatabaseTracker18deleteAllDatabasesEv
 __ZN7WebCore15DatabaseTracker22databaseNamesForOriginEPNS_14SecurityOriginERN3WTF6VectorINS_6StringELm0EEE
 __ZN7WebCore15DatabaseTracker23detailsForNameAndOriginERKNS_6StringEPNS_14SecurityOriginE
-__ZN7WebCore15DatabaseTracker25deleteDatabasesWithOriginEPNS_14SecurityOriginE
+__ZN7WebCore15DatabaseTracker24setDatabaseDirectoryPathERKNS_6StringE
 __ZN7WebCore15DatabaseTracker7originsERN3WTF6VectorINS1_6RefPtrINS_14SecurityOriginEEELm0EEE
 __ZN7WebCore15DatabaseTracker7trackerEv
 __ZN7WebCore15DatabaseTracker8setQuotaEPNS_14SecurityOriginEy
@@ -491,7 +491,6 @@ __ZN7WebCore8Settings27setFTPDirectoryTemplatePathERKNS_6StringE
 __ZN7WebCore8Settings27setLoadsImagesAutomaticallyEb
 __ZN7WebCore8Settings28setForceFTPDirectoryListingsEb
 __ZN7WebCore8Settings29setAuthorAndUserStylesEnabledEb
-__ZN7WebCore8Settings29setDefaultDatabaseOriginQuotaEy
 __ZN7WebCore8Settings31setShrinksStandaloneImagesToFitEb
 __ZN7WebCore8Settings32setNeedsAdobeFrameReloadingQuirkEb
 __ZN7WebCore8Settings40setJavaScriptCanOpenWindowsAutomaticallyEb

WebCore/bindings/js/GCController.cpp
@@ -37,12 +37,16 @@ using namespace KJS;
 
 namespace WebCore {
 
+#if USE(PTHREADS)
+
 static void* collect(void*)
 {
     JSLock lock;
     Collector::collect();
     return 0;
 }
+
+#endif
 
 GCController& gcController()
 {

WebCore/page/Chrome.cpp
@@ -343,16 +343,6 @@ void Chrome::print(Frame* frame)
     m_client->print(frame);
 }
 
-unsigned long long Chrome::requestQuotaIncreaseForNewDatabase(Frame* frame, SecurityOrigin* origin, const String& databaseDisplayName, unsigned long long estimatedSize)
-{
-    return m_client->requestQuotaIncreaseForNewDatabase(frame, origin, databaseDisplayName, estimatedSize);
-}
-
-unsigned long long Chrome::requestQuotaIncreaseForDatabaseOperation(Frame* frame, SecurityOrigin* origin, const String& databaseIdentifier, unsigned long long proposedNewQuota)
-{
-    return m_client->requestQuotaIncreaseForDatabaseOperation(frame, origin, databaseIdentifier, proposedNewQuota);
-}
-
 PageGroupLoadDeferrer::PageGroupLoadDeferrer(Page* page, bool deferSelf)
 {
     const HashSet<Page*>* group = page->frameNamespace();

WebCore/page/Chrome.h
@@ -1,6 +1,6 @@
 // -*- mode: c++; c-basic-offset: 4 -*-
 /*
- * Copyright (C) 2006, 2007 Apple Inc. All rights reserved.
+ * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -23,6 +23,7 @@
 
 #include "FocusDirection.h"
 #include <wtf/Forward.h>
+#include <wtf/Noncopyable.h>
 #include <wtf/RefPtr.h>
 
 #if PLATFORM(MAC)
@@ -40,7 +41,6 @@ namespace WebCore {
     class HitTestResult;
     class IntRect;
     class Page;
-    class SecurityOrigin;
     class String;
     
     struct FrameLoadRequest;
@@ -61,7 +61,7 @@ namespace WebCore {
         ErrorMessageLevel
     };
 
-    class Chrome {
+    class Chrome : Noncopyable {
     public:
         Chrome(Page*, ChromeClient*);
         ~Chrome();
@@ -125,9 +125,6 @@ namespace WebCore {
         void setToolTip(const HitTestResult&);
 
         void print(Frame*);
-
-        unsigned long long requestQuotaIncreaseForNewDatabase(Frame*, SecurityOrigin*, const String& databaseDisplayName, unsigned long long estimatedSize);
-        unsigned long long requestQuotaIncreaseForDatabaseOperation(Frame*, SecurityOrigin* origin, const String& databaseIdentifier, unsigned long long proposedNewQuota);
         
 #if PLATFORM(MAC)
         void focusNSView(NSView*);

WebCore/page/ChromeClient.h
@@ -1,6 +1,6 @@
 // -*- mode: c++; c-basic-offset: 4 -*-
 /*
- * Copyright (C) 2006-2007 Apple, Inc.
+ * Copyright (C) 2006, 2007, 2008 Apple, Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -30,7 +30,6 @@ namespace WebCore {
     class HitTestResult;
     class IntRect;
     class Page;
-    class SecurityOrigin;
     class String;
     
     struct FrameLoadRequest;
@@ -38,7 +37,6 @@ namespace WebCore {
     
     class ChromeClient {
     public:
-        virtual ~ChromeClient() {  }
         virtual void chromeDestroyed() = 0;
         
         virtual void setWindowRect(const FloatRect&) = 0;
@@ -104,9 +102,11 @@ namespace WebCore {
 
         virtual void print(Frame*) = 0;
 
-        virtual unsigned long long requestQuotaIncreaseForNewDatabase(Frame*, SecurityOrigin* origin, const String& databaseDisplayName, unsigned long long estimatedSize) = 0;
-        virtual unsigned long long requestQuotaIncreaseForDatabaseOperation(Frame*, SecurityOrigin* origin, const String& databaseIdentifier, unsigned long long proposedNewQuota) = 0;
-};
+        virtual void exceededDatabaseQuota(Frame*, const String& databaseName) = 0;
+
+    protected:
+        virtual ~ChromeClient() { }
+    };
 
 }
 

WebCore/page/Settings.cpp
@@ -301,22 +301,6 @@ void Settings::setAuthorAndUserStylesEnabled(bool authorAndUserStylesEnabled)
     setNeedsReapplyStylesInAllFrames(m_page);
 }
 
-void Settings::setDefaultDatabaseOriginQuota(unsigned long long quota)
-{
-#if ENABLE(DATABASE)
-    DatabaseTracker::tracker().setDefaultOriginQuota(quota);
-#endif
-}
-
-unsigned long long Settings::defaultDatabaseOriginQuota() const
-{
-#if ENABLE(DATABASE)
-    return DatabaseTracker::tracker().defaultOriginQuota();
-#else
-    return 0;
-#endif
-}
-
 void Settings::setFontRenderingMode(FontRenderingMode mode)
 {
     if (fontRenderingMode() == mode)

WebCore/page/Settings.h
@@ -144,9 +144,6 @@ namespace WebCore {
         void setAuthorAndUserStylesEnabled(bool);
         bool authorAndUserStylesEnabled() const { return m_authorAndUserStylesEnabled; }
         
-        void setDefaultDatabaseOriginQuota(unsigned long long);
-        unsigned long long defaultDatabaseOriginQuota() const;
-        
         void setFontRenderingMode(FontRenderingMode mode);
         FontRenderingMode fontRenderingMode() const;
 

commit 6d630ab9ad8da2f9bd3992fe0fdbbf4a9f2da77c

2008-01-21  Alp Toker  <alp@atoker.com>
        GTK+ build fix for breakage introduced in r29698.

        * WebCoreSupport/ChromeClientGtk.h:


Identifier: 23526@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@29699 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/gtk/WebCoreSupport/ChromeClientGtk.h
@@ -93,7 +93,7 @@ namespace WebKit {
 
         virtual void print(WebCore::Frame*);
 
-        virtual void exceededDatabaseQuota(Frame*, const String&);
+        virtual void exceededDatabaseQuota(WebCore::Frame*, const WebCore::String&);
 
     private:
         WebKitWebView* m_webView;