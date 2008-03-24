night-codes/WebKit

commit 692598f37e77ec5133bacb792749b0786a55ca4c

WebCore:
2008-03-18  Brent Fulgham  <bfulgham@gmail.com>

        Reviewed by Adam Roben.

        Provide some stub implementations for things that WebKit
        uses for performing authentication/challenge activities.  This
        is in support of http://bugs.webkit.org/show_bug.cgi?id=17837

        * platform/network/ResourceHandle.h:
        * platform/network/curl/AuthenticationChallenge.h:
        (WebCore::AuthenticationChallenge::AuthenticationChallenge):
        (WebCore::AuthenticationChallenge::sourceHandle):

WebKit/win:

2008-03-18  Brent Fulgham  <bfulgham@gmail.com>

        Reviewed by Adam Roben.

        Provide some stub implementations for things that WebKit
        uses for performing authentication/challenge activities.  This
        is in support of http://bugs.webkit.org/show_bug.cgi?id=17837

        * WebDataSource.cpp:
        * WebError.cpp:  Conditionalize CFNetwork-specific logic
        * WebURLAuthenticationChallenge.cpp:  Conditionalize constructor
          for authentication/challenge member.
        (WebURLAuthenticationChallenge::initWithAuthenticationChallenge):
        * WebURLResponse.cpp:  Remove CFNetwork-specific logic.
        * WebURLResponse.h: Conditionalize CFNetwork-specific member.
        * WebView.cpp:  Conditionalize CFNetwork-specific network protocol test.
        (WebView::canHandleRequest):



Identifier: 24820@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@31141 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/platform/network/ResourceHandle.h
@@ -118,8 +118,14 @@ public:
     static CFRunLoopRef loaderRunLoop();
     CFURLConnectionRef connection() const;
     CFURLConnectionRef releaseConnectionForDownload();
-
     static void setHostAllowsAnyHTTPSCertificate(const String&);
+    static void setClientCertificate(const String& host, CFDataRef);
+#endif
+
+#if PLATFORM(WIN) && USE(CURL)
+    static void setHostAllowsAnyHTTPSCertificate(const String&);
+#endif
+#if PLATFORM(WIN) && USE(CURL) && PLATFORM(CF)
     static void setClientCertificate(const String& host, CFDataRef);
 #endif
 
@@ -137,7 +143,7 @@ public:
     friend LRESULT __stdcall ResourceHandleWndProc(HWND, unsigned message, WPARAM, LPARAM);
 #endif
 
-#if PLATFORM(GTK) || PLATFORM(QT) || PLATFORM(WX)
+#if PLATFORM(QT) || USE(CURL)
     ResourceHandleInternal* getInternal() { return d.get(); }
 #endif
 

WebCore/platform/network/curl/AuthenticationChallenge.h
@@ -39,6 +39,10 @@ public:
         : AuthenticationChallengeBase(protectionSpace, proposedCredential, previousFailureCount, response, error)
     {
     }
+
+    ResourceHandle* sourceHandle() const { return m_sourceHandle.get(); }
+
+    RefPtr<ResourceHandle> m_sourceHandle;    
 };
 
 }

WebCore/platform/network/curl/ResourceHandleCurl.cpp
@@ -78,6 +78,13 @@ void WebCoreSynchronousLoader::didFail(ResourceHandle*, const ResourceError& err
 }
 
 
+static HashSet<String>& allowsAnyHTTPSCertificateHosts()
+{
+    static HashSet<String> hosts;
+
+    return hosts;
+}
+
 ResourceHandleInternal::~ResourceHandleInternal()
 {
     free(m_url);
@@ -114,6 +121,26 @@ bool ResourceHandle::supportsBufferedData()
 {
     return false;
 }
+
+void ResourceHandle::setHostAllowsAnyHTTPSCertificate(const String& host)
+{
+    allowsAnyHTTPSCertificateHosts().add(host.lower());
+}
+
+#if PLATFORM(WIN) && PLATFORM(CF)
+// FIXME:  The CFDataRef will need to be something else when
+// building without 
+static HashMap<String, RetainPtr<CFDataRef> >& clientCerts()
+{
+    static HashMap<String, RetainPtr<CFDataRef> > certs;
+    return certs;
+}
+
+void ResourceHandle::setClientCertificate(const String& host, CFDataRef cert)
+{
+    clientCerts().set(host.lower(), cert);
+}
+#endif
 
 void ResourceHandle::setDefersLoading(bool defers)
 {

WebKit/win/WebError.cpp
@@ -28,10 +28,13 @@
 #include "WebError.h"
 #include "WebKit.h"
 
-#include <WebKitSystemInterface/WebKitSystemInterface.h>
 #pragma warning(push, 0)
 #include <WebCore/BString.h>
 #pragma warning(pop)
+
+#if USE(CFNETWORK)
+#include <WebKitSystemInterface/WebKitSystemInterface.h>
+#endif
 
 using namespace WebCore;
 
@@ -213,10 +216,12 @@ HRESULT STDMETHODCALLTYPE WebError::sslPeerCertificate(
     if (!m_cfErrorUserInfoDict)
         return E_FAIL;
 
+#if USE(CFNETWORK)
     void* data = wkGetSSLPeerCertificateData(m_cfErrorUserInfoDict.get());
     if (!data)
         return E_FAIL;
     *result = (OLE_HANDLE)(ULONG64)data;
+#endif
     return *result ? S_OK : E_FAIL;
 }
 

WebKit/win/WebURLAuthenticationChallenge.cpp
@@ -168,9 +168,14 @@ HRESULT STDMETHODCALLTYPE WebURLAuthenticationChallenge::initWithAuthenticationC
     if (!webSender)
         return E_NOINTERFACE;
 
+#if USE(CFNETWORK)
     m_authenticationChallenge = AuthenticationChallenge(webChallenge->authenticationChallenge().cfURLAuthChallengeRef(), webSender->resourceHandle());
 
     return S_OK;
+#else
+
+    return E_FAIL;
+#endif
 }
 
 HRESULT STDMETHODCALLTYPE WebURLAuthenticationChallenge::error(

WebKit/win/WebURLResponse.cpp
@@ -397,6 +397,8 @@ HRESULT STDMETHODCALLTYPE WebURLResponse::sslPeerCertificate(
     if (!result)
         return E_POINTER;
     *result = 0;
+
+#if USE(CFNETWORK)
     CFDictionaryRef dict = certificateDictionary();
     if (!dict)
         return E_FAIL;
@@ -404,6 +406,8 @@ HRESULT STDMETHODCALLTYPE WebURLResponse::sslPeerCertificate(
     if (!data)
         return E_FAIL;
     *result = (OLE_HANDLE)(ULONG64)data;
+#endif
+
     return *result ? S_OK : E_FAIL;
 }
 
@@ -463,6 +467,7 @@ const ResourceResponse& WebURLResponse::resourceResponse() const
     return m_response;
 }
 
+#if USE(CFNETWORK)
 CFDictionaryRef WebURLResponse::certificateDictionary() const
 {
     if (m_SSLCertificateInfo)
@@ -474,3 +479,4 @@ CFDictionaryRef WebURLResponse::certificateDictionary() const
     m_SSLCertificateInfo = wkGetSSLCertificateInfo(cfResponse);
     return m_SSLCertificateInfo.get();
 }
+#endif

WebKit/win/WebURLResponse.h
@@ -91,12 +91,18 @@ public:
 
 protected:
     HRESULT suggestedFileExtension(BSTR* result);
+
+#if USE(CFNETWORK)
     CFDictionaryRef certificateDictionary() const;
+#endif
 
 protected:
     ULONG m_refCount;
     WebCore::ResourceResponse m_response;
+
+#if USE(CFNETWORK)
     mutable RetainPtr<CFDictionaryRef> m_SSLCertificateInfo;    // this ensures certificate contexts are valid for the lifetime of this WebURLResponse.
+#endif
 };
 
 #endif

WebKit/win/WebView.cpp
@@ -962,11 +962,15 @@ bool WebView::canHandleRequest(const WebCore::ResourceRequest& request)
     if (equalIgnoringCase(String(request.url().protocol()), "about"))
         return true;
 
+#if USE(CFNETWORK)
     if (CFURLProtocolCanHandleRequest(request.cfURLRequest()))
         return true;
 
     // FIXME: Mac WebKit calls _representationExistsForURLScheme here
     return false;
+#else
+    return true;
+#endif
 }
 
 Page* WebView::page()

commit 23c95834f22f6e886fcf1f955c00dcfba1c9d4f1

2008-03-24  Alp Toker  <alp@atoker.com>
        GTK+/soup http backend build fix for breakage introduced in r31141.
        Fix suggested by Xan.

        * platform/network/ResourceHandle.h:


Identifier: 24926@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@31249 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/platform/network/ResourceHandle.h
@@ -143,7 +143,7 @@ public:
     friend LRESULT __stdcall ResourceHandleWndProc(HWND, unsigned message, WPARAM, LPARAM);
 #endif
 
-#if PLATFORM(QT) || USE(CURL)
+#if PLATFORM(QT) || USE(CURL) || USE(SOUP)
     ResourceHandleInternal* getInternal() { return d.get(); }
 #endif
 