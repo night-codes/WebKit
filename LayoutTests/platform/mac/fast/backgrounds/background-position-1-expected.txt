night-codes/WebKit

commit 9a8d89ccca262c73f71c0cdab2556786570ef1f2

2008-03-28  David Hyatt  <hyatt@apple.com>
        Fix a bug where background-position truncates instead of rounding when it evaluates to fractional
        pixel values.  This matches other browsers.

        Reviewed by maciej

        Added fast/backgrounds/background-position-rounding.html

        * rendering/Length.h:
        (WebCore::Length::calcValue):
        (WebCore::Length::calcMinValue):
        * rendering/RenderBox.cpp:
        (WebCore::RenderBox::calculateBackgroundImageGeometry):



Identifier: 25042@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@31389 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/rendering/Length.h
@@ -103,12 +103,14 @@ namespace WebCore {
         }
 
         // note: works only for certain types, returns undefinedLength otherwise
-        int calcValue(int maxValue) const
+        int calcValue(int maxValue, bool roundPercentages = false) const
         {
             switch (type()) {
                 case Fixed:
                     return value();
                 case Percent:
+                    if (roundPercentages)
+                        return static_cast<int>(round(maxValue * percent()));
                     return maxValue * rawValue() / (100 * percentScaleFactor);
                 case Auto:
                     return maxValue;
@@ -117,12 +119,14 @@ namespace WebCore {
             }
         }
 
-        int calcMinValue(int maxValue) const
+        int calcMinValue(int maxValue, bool roundPercentages = false) const
         {
             switch (type()) {
                 case Fixed:
                     return value();
                 case Percent:
+                    if (roundPercentages)
+                        return static_cast<int>(round(maxValue * percent()));
                     return maxValue * rawValue() / (100 * percentScaleFactor);
                 case Auto:
                 default:

WebCore/rendering/RenderBox.cpp
@@ -577,7 +577,7 @@ void RenderBox::calculateBackgroundImageGeometry(const BackgroundLayer* bgLayer,
 
     EBackgroundRepeat backgroundRepeat = bgLayer->backgroundRepeat();
     
-    int xPosition = bgLayer->backgroundXPosition().calcMinValue(pw - scaledImageWidth);
+    int xPosition = bgLayer->backgroundXPosition().calcMinValue(pw - scaledImageWidth, true);
     if (backgroundRepeat == REPEAT || backgroundRepeat == REPEAT_X) {
         cw = pw + left + right;
         sx = scaledImageWidth ? scaledImageWidth - (xPosition + left) % scaledImageWidth : 0;
@@ -587,7 +587,7 @@ void RenderBox::calculateBackgroundImageGeometry(const BackgroundLayer* bgLayer,
         cw = scaledImageWidth + min(xPosition + left, 0);
     }
 
-    int yPosition = bgLayer->backgroundYPosition().calcMinValue(ph - scaledImageHeight);
+    int yPosition = bgLayer->backgroundYPosition().calcMinValue(ph - scaledImageHeight, true);
     if (backgroundRepeat == REPEAT || backgroundRepeat == REPEAT_Y) {
         ch = ph + top + bottom;
         sy = scaledImageHeight ? scaledImageHeight - (yPosition + top) % scaledImageHeight : 0;

commit 0e980c95e53cc02e6996a1a630ff746fcdb3de52

WebCore:
        Reviewed by Dave Hyatt.

        - fix a percentage background position regression from r31389

        Test: fast/backgrounds/background-position-1.html

        * rendering/Length.h:
        (WebCore::Length::calcValue): Divide by 100.
        (WebCore::Length::calcMinValue): Ditto.

LayoutTests:

        Reviewed by Dave Hyatt.

        - pixel test for a percentage background position regression from r31389

        * fast/backgrounds/background-position-1.html: Added.
        * platform/mac/fast/backgrounds/background-position-1-expected.checksum: Added.
        * platform/mac/fast/backgrounds/background-position-1-expected.png: Added.
        * platform/mac/fast/backgrounds/background-position-1-expected.txt: Added.



Identifier: 25072@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@31426 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/rendering/Length.h
@@ -111,7 +111,7 @@ namespace WebCore {
                     return value();
                 case Percent:
                     if (roundPercentages)
-                        return static_cast<int>(round(maxValue * percent()));
+                        return static_cast<int>(round(maxValue * percent() / 100.0));
                     return maxValue * rawValue() / (100 * percentScaleFactor);
                 case Auto:
                     return maxValue;
@@ -127,7 +127,7 @@ namespace WebCore {
                     return value();
                 case Percent:
                     if (roundPercentages)
-                        return static_cast<int>(round(maxValue * percent()));
+                        return static_cast<int>(round(maxValue * percent() / 100.0));
                     return maxValue * rawValue() / (100 * percentScaleFactor);
                 case Auto:
                 default:

LayoutTests/fast/backgrounds/background-position-1.html
@@ -1,0 +1,7 @@
+<style>
+ div { height: 100px; width: 101px; background: url(../../http/tests/multipart/resources/green-100x100.png) no-repeat top 100%; }
+</style>
+<p>
+    This should be a green square:
+</p>
+<div></div>

LayoutTests/platform/mac/fast/backgrounds/background-position-1-expected.checksum
@@ -1,0 +1,1 @@
+2c038121b8fd9a265510ec136888ca78

LayoutTests/platform/mac/fast/backgrounds/background-position-1-expected.txt
@@ -1,0 +1,9 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x18
+        RenderText {#text} at (0,0) size 192x18
+          text run at (0,0) width 192: "This should be a green square:"
+      RenderBlock {DIV} at (0,34) size 101x100