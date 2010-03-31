night-codes/WebKit

commit e69439703541387a2f4726f5bfcf3c82d1996e27

https://bugs.webkit.org/show_bug.cgi?id=36745
Patch by Thomas Zander <t.zander@nokia.com> on 2010-03-29
Reviewed by Simon Hausmann.

[Qt] Fix def file builds with Qt 4.7.

Switched to using DEF_FILE, which is the official variable.

* WebCore.pro: Use DEF_FILE

Identifier: 48008@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56714 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/WebCore.pro
@@ -2932,6 +2932,8 @@ SOURCES += \
 symbian {
     shared {
         contains(CONFIG, def_files) {
+            DEF_FILE=../WebKit/qt/symbian
+            ; defFilePath is for Qt4.6 compatibility
             defFilePath=../WebKit/qt/symbian
         } else {
             MMP_RULES += EXPORTUNFROZEN

commit 1d8253caa0153ea0a6c079be8f44bdccc7628ae4

2010-03-30  Stanislav Paltis  <Stanislav.Paltis@nokia.com>
        Reviewed by Laszlo Gombos.

        [Qt] qmake/symbian build failure
        https://bugs.webkit.org/show_bug.cgi?id=36745

        Fix build break in Symbian build introduced in r56714,
        by modifying comment escape character from ";" to "#".

        * WebCore.pro:



Identifier: 48115@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@56822 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/WebCore.pro
@@ -2946,7 +2946,7 @@ symbian {
     shared {
         contains(CONFIG, def_files) {
             DEF_FILE=../WebKit/qt/symbian
-            ; defFilePath is for Qt4.6 compatibility
+            # defFilePath is for Qt4.6 compatibility
             defFilePath=../WebKit/qt/symbian
         } else {
             MMP_RULES += EXPORTUNFROZEN