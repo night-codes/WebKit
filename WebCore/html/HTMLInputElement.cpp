night-codes/WebKit

commit 765b4d16ab4295696b4a473395bbadd1c06a9cb5

2010-07-07  John Gregg  <johnnyg@google.com>
        Reviewed by Jian Li.

        Experimental directory upload feature.
        https://bugs.webkit.org/show_bug.cgi?id=40872

        This patch adds a new HTML attribute webkitdirectory which applies to
        <input type="file"> tags and allows the user to specify a folder
        which is recursively enumerated so that all the files in that folder
        are added to the file list.

        The files chosen in that way have a .webkitRelativePath attribute which contains
        the relative path starting from the chosen folder.  The relative path is
        also appended to each item in the FormData when uploaded.

        All the code is behind an ENABLE_DIRECTORY_UPLOAD flag.

        Test: fast/forms/input-file-directory-upload.html

        * html/Blob.cpp:
        (WebCore::Blob::Blob):
        * html/Blob.h:
        * html/File.cpp:
        (WebCore::File::File):
        (WebCore::File::Init):
        (WebCore::File::webkitRelativePath):
        * html/File.h:
        (WebCore::File::create):
        * html/File.idl:
        * html/HTMLAttributeNames.in: add webkitdirectory attribute
        * html/HTMLInputElement.cpp:
        (WebCore::HTMLInputElement::setFileListFromRenderer):
        (WebCore::HTMLInputElement::webkitdirectory):
        * html/HTMLInputElement.h:
        * html/HTMLInputElement.idl:
        * platform/BlobItem.cpp:
        (WebCore::FileBlobItem::create):
        (WebCore::FileBlobItem::FileBlobItem):
        * platform/BlobItem.h:
        (WebCore::FileBlobItem::relativePath):
        * platform/FileChooser.h:
        (WebCore::FileChooser::allowsDirectoryUpload):
        * platform/network/FormData.cpp:
        (WebCore::FormData::appendKeyValuePairItems):
        * rendering/RenderFileUploadControl.cpp:
        (WebCore::RenderFileUploadControl::allowsMultipleFiles):
        (WebCore::RenderFileUploadControl::allowsDirectoryUpload):
        * rendering/RenderFileUploadControl.h:

WebCore/html/HTMLInputElement.cpp
@@ -40,6 +40,7 @@
 #include "ExceptionCode.h"
 #include "File.h"
 #include "FileList.h"
+#include "FileSystem.h"
 #include "FocusController.h"
 #include "FormDataList.h"
 #include "Frame.h"
@@ -1967,8 +1968,30 @@ void HTMLInputElement::setFileListFromRenderer(const Vector<String>& paths)
 {
     m_fileList->clear();
     int size = paths.size();
+
+#if ENABLE(DIRECTORY_UPLOAD)
+    // If a directory is being selected, the UI allows a directory to be chosen
+    // and the paths provided here share a root directory somewhere up the tree;
+    // we want to store only the relative paths from that point.
+    if (webkitdirectory() && size > 0) {
+        String rootPath = directoryName(paths[0]);
+        // Find the common root path.
+        for (int i = 1; i < size; i++) {
+            while (!paths[i].startsWith(rootPath))
+                rootPath = directoryName(rootPath);
+        }
+        rootPath = directoryName(rootPath);
+        ASSERT(rootPath.length());
+        for (int i = 0; i < size; i++)
+            m_fileList->append(File::create(paths[i].substring(1 + rootPath.length()), paths[i]));
+    } else {
+        for (int i = 0; i < size; i++)
+            m_fileList->append(File::create(paths[i]));
+    }
+#else
     for (int i = 0; i < size; i++)
         m_fileList->append(File::create(paths[i]));
+#endif
 
     setFormControlValueMatchesRenderer(true);
     InputElement::notifyFormStateChanged(this);
@@ -2447,6 +2470,13 @@ bool HTMLInputElement::multiple() const
 {
     return !getAttribute(multipleAttr).isNull();
 }
+
+#if ENABLE(DIRECTORY_UPLOAD)
+bool HTMLInputElement::webkitdirectory() const
+{
+    return !getAttribute(webkitdirectoryAttr).isNull();
+}
+#endif
 
 void HTMLInputElement::setSize(unsigned size)
 {

LayoutTests/fast/forms/input-file-directory-upload-expected.txt
@@ -1,0 +1,19 @@
+
+PASS: name is file1; path is dirupload/path1/file1
+PASS: name is file2; path is dirupload/path1/file2
+PASS: name is file1; path is dirupload/path1/subpath1/file1
+PASS: name is file1; path is dirupload/path2/file1
+PASS: name is file1; path is dirupload/path2/subpath1/subpath2/file1
+PASS: name is file2; path is dirupload/path2/file2
+PASS: name is file1; path is dirupload/path3/file1
+PASS: name is file1; path is dirupload/file1
+
+PASS: name is file1; path is path1/file1
+PASS: name is file2; path is path1/file2
+
+PASS: name is file1; path is dirupload/path1/file1
+PASS: name is file1; path is dirupload/path2/file1
+
+PASS: name is file1; path is path1/file1
+PASS: name is file1; path is path1/subpath1/file1
+

LayoutTests/fast/forms/input-file-directory-upload.html
@@ -1,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<body>
+<input type="file" name="files" id="files" webkitdirectory>
+<pre id='output'></pre>
+
+<script>
+var testFileList1 = [
+    {'path': 'resources/dirupload/path1/file1',                   'expect-relpath': 'dirupload/path1/file1'},
+    {'path': 'resources/dirupload/path1/file2',                   'expect-relpath': 'dirupload/path1/file2' },
+    {'path': 'resources/dirupload/path1/subpath1/file1',          'expect-relpath': 'dirupload/path1/subpath1/file1'},
+    {'path': 'resources/dirupload/path2/file1',                   'expect-relpath': 'dirupload/path2/file1'},
+    {'path': 'resources/dirupload/path2/subpath1/subpath2/file1', 'expect-relpath': 'dirupload/path2/subpath1/subpath2/file1'},
+    {'path': 'resources/dirupload/path2/file2',                   'expect-relpath': 'dirupload/path2/file2'},
+    {'path': 'resources/dirupload/path3/file1',                   'expect-relpath': 'dirupload/path3/file1'},
+    {'path': 'resources/dirupload/file1',                         'expect-relpath': 'dirupload/file1'},
+];
+
+var testFileList2 = [
+    {'path': 'resources/dirupload/path1/file1',                   'expect-relpath': 'path1/file1'},
+    {'path': 'resources/dirupload/path1/file2',                   'expect-relpath': 'path1/file2' },
+];
+
+var testFileList3 = [
+    {'path': 'resources/dirupload/path1/file1',                   'expect-relpath': 'dirupload/path1/file1'},
+    {'path': 'resources/dirupload/path2/file1',                   'expect-relpath': 'dirupload/path2/file1' },
+];
+
+var testFileList4 = [
+    {'path': 'resources/dirupload/path1/file1',                   'expect-relpath': 'path1/file1'},
+    {'path': 'resources/dirupload/path1/subpath1/file1',          'expect-relpath': 'path1/subpath1/file1' },
+];
+
+function log(message)
+{
+    document.getElementById('output').appendChild(document.createTextNode(message + "\n"));
+}
+
+function onInputFileChange(currentFileList, lastTest)
+{
+    var files = document.getElementById("files").files;
+    for (var i = 0; i < files.length; i++) {
+        if (files[i].webkitRelativePath != currentFileList[i]['expect-relpath'])
+            log("FAIL: name is " + files[i].name + "; path is " + files[i].webkitRelativePath + " but should be " + currentFileList[i]['expect-relpath']);
+        else
+            log("PASS: name is " + files[i].name + "; path is " + files[i].webkitRelativePath);
+    }
+
+   if (lastTest && layoutTestController)
+        layoutTestController.notifyDone();
+   else
+        log("");
+}
+
+function doTest(fileList, last)
+{
+    var pathsOnly = fileList.map(function(item) { return item['path'];});
+    var f = function() { onInputFileChange(fileList, last); };
+    document.getElementById("files").onchange = f;
+    eventSender.beginDragWithFiles(pathsOnly);
+    eventSender.mouseMoveTo(10, 10);
+    eventSender.mouseUp();
+}
+
+if (layoutTestController) {
+    layoutTestController.dumpAsText();
+    layoutTestController.waitUntilDone();
+}
+
+if (window.eventSender) {
+    doTest(testFileList1);
+    doTest(testFileList2);
+    doTest(testFileList3);
+    doTest(testFileList4, true);
+}
+</script>
+</body>
+</html>

LayoutTests/platform/gtk/Skipped
@@ -5858,3 +5858,6 @@ editing/selection/extend-forward-after-set-base-and-extent.html
 
 # Missing DRT API.
 http/tests/loading/preload-slow-loading.php
+
+# Directory upload is not enabled.
+fast/forms/input-file-directory-upload.html

LayoutTests/platform/mac/Skipped
@@ -288,3 +288,5 @@ fast/forms/input-appearance-speechbutton.html
 # Needs to be fixed at image decoder layer for Safari.
 fast/images/large-size-image-crash.html
 
+# Directory upload is not enabled.
+fast/forms/input-file-directory-upload.html

LayoutTests/platform/qt/Skipped
@@ -5460,3 +5460,6 @@ fast/dom/beforeload/remove-video-in-beforeload-listener.html
 inspector/timeline-layout.html
 inspector/timeline-parse-html.html
 inspector/timeline-recalculate-styles.html
+
+# Directory upload is not enabled.
+fast/forms/input-file-directory-upload.html

LayoutTests/platform/win/Skipped
@@ -953,3 +953,6 @@ userscripts/user-script-all-frames.html
 userscripts/user-script-top-frame-only.html
 userscripts/user-style-all-frames.html
 userscripts/user-style-top-frame-only.html
+
+# Directory upload is not enabled.
+fast/forms/input-file-directory-upload.html

WebCore/html/Blob.cpp
@@ -43,6 +43,11 @@ Blob::Blob(const String& type, const BlobItemList& items)
         m_items.append(items[i]);
 }
 
+Blob::Blob(const PassRefPtr<BlobItem>& item)
+{
+    m_items.append(item);
+}
+
 Blob::Blob(const String& path)
 {
     // Note: this doesn't initialize the type unlike File(path).

commit 088d72eb150e142ba84231bdf802ea527fd42b60

2010-07-22  John Gregg  <johnnyg@google.com>
        Reviewed by Kent Tamura.

        [chromium] fast/forms/input-file-directory-upload.html fails on win after r63454
        https://bugs.webkit.org/show_bug.cgi?id=42768

        Normalize path separators for the script API by convering \'s to /'s.

        Covered by input-file-directory-upload.html

        * html/HTMLInputElement.cpp:
        (WebCore::HTMLInputElement::setFileListFromRenderer):

Identifier: 54751@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@63914 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/html/HTMLInputElement.cpp
@@ -1996,8 +1996,11 @@ void HTMLInputElement::setFileListFromRenderer(const Vector<String>& paths)
         }
         rootPath = directoryName(rootPath);
         ASSERT(rootPath.length());
-        for (int i = 0; i < size; i++)
-            m_fileList->append(File::create(paths[i].substring(1 + rootPath.length()), paths[i]));
+        for (int i = 0; i < size; i++) {
+            // Normalize backslashes to slashes before exposing the relative path to script.
+            String relativePath = paths[i].substring(1 + rootPath.length()).replace('\\','/');
+            m_fileList->append(File::create(relativePath, paths[i]));
+        }
     } else {
         for (int i = 0; i < size; i++)
             m_fileList->append(File::create(paths[i]));