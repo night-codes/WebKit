night-codes/WebKit

commit 61daec01d8869807ca66513a8f80dfd4381e668a

2009-10-19  Jens Alfke  <jens@mooseyard.com>
        Reviewed by Darin Adler.

        Optimize string upper/lowercasing
        https://bugs.webkit.org/show_bug.cgi?id=30261

        - Added AtomicString::upper() and lower()
        - Further optimized StringImpl::lower()
        - Removed StringImpl::isLower()
        - Added QualifiedName::localNameUpper(), which is cached, thereby saving
          thousands of upper() calls and string allocations.

         * dom/Element.cpp:
         (WebCore::Element::setAttribute): Call AtomicString::lower()
         * dom/QualifiedName.cpp:
         (WebCore::QualifiedName::localNameUpper): New method
         * dom/QualifiedName.h: Added localNameUpper() method
         * dom/StyledElement.cpp:
         (WebCore::StyledElement::parseMappedAttribute):  Call AtomicString::lower()
         * html/HTMLDocument.cpp:
         (WebCore::HTMLDocument::createElement): Call AtomicString::lower()
         * html/HTMLElement.cpp:
         (WebCore::HTMLElement::nodeName): Call localNameUpper()
         * platform/text/AtomicString.cpp:
         (WebCore::AtomicString::lower): New method
         (WebCore::AtomicString::upper): New method
         * platform/text/AtomicString.h: Added lower() and upper()
         * platform/text/StringImpl.cpp: Removed isLower()
         (WebCore::StringImpl::lower): Further optimization of initial loop
         * platform/text/StringImpl.h: Removed isLower()

Identifier: 41252@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@49798 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/platform/text/AtomicString.cpp
@@ -228,6 +228,16 @@ void AtomicString::remove(StringImpl* r)
 {
     stringTable().remove(r);
 }
+    
+AtomicString AtomicString::lower() const
+{
+    // Note: This is a hot function in the Dromaeo benchmark.
+    StringImpl* impl = this->impl();
+    RefPtr<StringImpl> newImpl = impl->lower();
+    if (LIKELY(newImpl == impl))
+        return *this;
+    return AtomicString(newImpl);
+}
 
 #if USE(JSC)
 PassRefPtr<StringImpl> AtomicString::add(const JSC::Identifier& identifier)

WebCore/dom/Element.cpp
@@ -516,7 +516,7 @@ void Element::setAttribute(const AtomicString& name, const AtomicString& value, 
         return;
     }
 
-    const AtomicString& localName = (shouldIgnoreAttributeCase(this) && !name.string().impl()->isLower()) ? AtomicString(name.string().lower()) : name;
+    const AtomicString& localName = shouldIgnoreAttributeCase(this) ? name.lower() : name;
 
     // allocate attributemap if necessary
     Attribute* old = attributes(false)->getAttributeItem(localName, false);

WebCore/dom/QualifiedName.cpp
@@ -97,4 +97,11 @@ void QualifiedName::init()
     }
 }
 
+const AtomicString& QualifiedName::localNameUpper() const
+{
+    if (!m_impl->m_localNameUpper)
+        m_impl->m_localNameUpper = m_impl->m_localName.upper();
+    return m_impl->m_localNameUpper;
+}
+
 }

WebCore/dom/QualifiedName.h
@@ -41,9 +41,10 @@ public:
             return adoptRef(new QualifiedNameImpl(prefix, localName, namespaceURI));
         }
 
-        AtomicString m_prefix;
-        AtomicString m_localName;
-        AtomicString m_namespace;
+        const AtomicString m_prefix;
+        const AtomicString m_localName;
+        const AtomicString m_namespace;
+        mutable AtomicString m_localNameUpper;
 
     private:
         QualifiedNameImpl(const AtomicString& prefix, const AtomicString& localName, const AtomicString& namespaceURI)
@@ -75,6 +76,9 @@ public:
     const AtomicString& prefix() const { return m_impl->m_prefix; }
     const AtomicString& localName() const { return m_impl->m_localName; }
     const AtomicString& namespaceURI() const { return m_impl->m_namespace; }
+
+    // Uppercased localName, cached for efficiency
+    const AtomicString& localNameUpper() const;
 
     String toString() const;
 

WebCore/dom/StyledElement.cpp
@@ -240,8 +240,8 @@ void StyledElement::parseMappedAttribute(MappedAttribute *attr)
         if (namedAttrMap) {
             if (attr->isNull())
                 namedAttrMap->setID(nullAtom);
-            else if (document()->inCompatMode() && !attr->value().impl()->isLower())
-                namedAttrMap->setID(AtomicString(attr->value().string().lower()));
+            else if (document()->inCompatMode())
+                namedAttrMap->setID(attr->value().lower());
             else
                 namedAttrMap->setID(attr->value());
         }

WebCore/html/HTMLDocument.cpp
@@ -307,8 +307,7 @@ PassRefPtr<Element> HTMLDocument::createElement(const AtomicString& name, Except
         ec = INVALID_CHARACTER_ERR;
         return 0;
     }
-    AtomicString lowerName = name.string().impl()->isLower() ? name : AtomicString(name.string().lower());
-    return HTMLElementFactory::createHTMLElement(QualifiedName(nullAtom, lowerName, xhtmlNamespaceURI), this, 0, false);
+    return HTMLElementFactory::createHTMLElement(QualifiedName(nullAtom, name.lower(), xhtmlNamespaceURI), this, 0, false);
 }
 
 static void addItemToMap(HashCountedSet<AtomicStringImpl*>& map, const AtomicString& name)

WebCore/html/HTMLElement.cpp
@@ -67,7 +67,7 @@ String HTMLElement::nodeName() const
     // the string on a hit in the hash.
     // FIXME: We should have a way to detect XHTML elements and replace the hasPrefix() check with it.
     if (document()->isHTMLDocument() && !tagQName().hasPrefix())
-        return tagQName().localName().string().upper();
+        return tagQName().localNameUpper();
     return Element::nodeName();
 }
     

WebCore/platform/text/AtomicString.h
@@ -83,6 +83,9 @@ public:
     bool endsWith(const String& s, bool caseSensitive = true) const
         { return m_string.endsWith(s, caseSensitive); }
     
+    AtomicString lower() const;
+    AtomicString upper() const { return AtomicString(impl()->upper()); }
+    
     int toInt(bool* ok = 0) const { return m_string.toInt(ok); }
     double toDouble(bool* ok = 0) const { return m_string.toDouble(ok); }
     float toFloat(bool* ok = 0) const { return m_string.toFloat(ok); }

commit 1e779ef8c09e2329e70a9c602df3e45ac5f998c8

2010-11-04  Eric Seidel  <eric@webkit.org>
        Reviewed by Gavin Barraclough.

        REGRESSION(49798): Crash in HTMLObjectElement::parseMappedAttribute
        https://bugs.webkit.org/show_bug.cgi?id=48789

        The contract for all String/AtomicString methods seems to be that it's
        safe to call them, even when the String is null (impl() returns 0).
        This contract was broken by r49798 (unintentionally) when optimizing
        for dromeo.
        This patch adds a null check to AtomicString::lower() fixing this
        crash and preventing future confusion.

        * wtf/text/AtomicString.cpp:
        (WTF::AtomicString::lower):
2010-11-04  Eric Seidel  <eric@webkit.org>

        Reviewed by Gavin Barraclough.

        REGRESSION(49798): Crash in HTMLObjectElement::parseMappedAttribute
        https://bugs.webkit.org/show_bug.cgi?id=48789

        * fast/dom/HTMLObjectElement/set-type-to-null-crash-expected.txt: Added.
        * fast/dom/HTMLObjectElement/set-type-to-null-crash.html: Added.

Identifier: 61834@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@71345 268f45cc-cd09-0410-ab3c-d52691b4dbfc

JavaScriptCore/wtf/text/AtomicString.cpp
@@ -295,6 +295,8 @@ AtomicString AtomicString::lower() const
 {
     // Note: This is a hot function in the Dromaeo benchmark.
     StringImpl* impl = this->impl();
+    if (UNLIKELY(!impl))
+        return *this;
     RefPtr<StringImpl> newImpl = impl->lower();
     if (LIKELY(newImpl == impl))
         return *this;

LayoutTests/fast/dom/HTMLObjectElement/set-type-to-null-crash-expected.txt
@@ -1,0 +1,1 @@
+ PASS - The test didn't crash.

LayoutTests/fast/dom/HTMLObjectElement/set-type-to-null-crash.html
@@ -1,0 +1,8 @@
+<body onload="crash.type=null">
+<object type="" id="crash"></object>
+
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+</script>
+PASS - The test didn't crash.