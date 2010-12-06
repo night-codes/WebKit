night-codes/WebKit

commit 8ed4b1dc2d03a8c0d216666ecb4d8674a10f759b

2010-12-06  Joone Hur  <joone@kldp.org>
        Reviewed by Xan Lopez.

        [GTK] The webkit cache model needs to be set when WebFrameLoaderClient::didPerformFirstNavigation() is called
        https://bugs.webkit.org/show_bug.cgi?id=50430

        * WebCoreSupport/FrameLoaderClientGtk.cpp:
        (WebKit::FrameLoaderClient::didPerformFirstNavigation): Set the webview cache model.
        * webkit/webkitprivate.cpp:
        (webkit_init): Removed the code of setting the webview cache model.

Identifier: 63775@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73353 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/gtk/webkit/webkitwebview.h
@@ -50,8 +50,21 @@ typedef enum {
     WEBKIT_NAVIGATION_RESPONSE_DOWNLOAD
 } WebKitNavigationResponse;
 
+/*  
+ * WebKitCacheModel:
+ * @WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER: Disable the cache completely, which 
+ *   substantially reduces memory usage. Useful for applications that only
+ *   access local files.
+ * @WEBKIT_CACHE_MODEL_WEB_BROWSER: Improve document load speed substantially
+ *   by caching previously viewed content. This is the default setting.
+ * 
+ * Enum values used for determining the webview cache model.
+ */
 typedef enum {
-    WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER = 1,
+    /*< private >*/
+    WEBKIT_CACHE_MODEL_DEFAULT,
+    /*< public >*/
+    WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER,
     WEBKIT_CACHE_MODEL_WEB_BROWSER
 } WebKitCacheModel;
 

WebKit/gtk/WebCoreSupport/FrameLoaderClientGtk.cpp
@@ -690,6 +690,10 @@ void FrameLoaderClient::documentElementAvailable()
 
 void FrameLoaderClient::didPerformFirstNavigation() const
 {
+    WebKitCacheModel cacheModel = webkit_get_cache_model();
+    // If user agents do not determine the cache model, we use WEBKIT_CACHE_MODEL_WEB_BROWSER by default.
+    if (cacheModel == WEBKIT_CACHE_MODEL_DEFAULT)
+        webkit_set_cache_model(WEBKIT_CACHE_MODEL_WEB_BROWSER);
 }
 
 void FrameLoaderClient::registerForIconNotification(bool shouldRegister)

WebKit/gtk/webkit/webkitprivate.cpp
@@ -283,11 +283,6 @@ void webkit_init()
     // that may only be done by the main thread.
     atomicCanonicalTextEncodingName("UTF-8");
 
-    // Page cache capacity (in pages). Comment from Mac port:
-    // (Research indicates that value / page drops substantially after 3 pages.)
-    // FIXME: Expose this with an API and/or calculate based on available resources
-    webkit_set_cache_model(WEBKIT_CACHE_MODEL_WEB_BROWSER);
-
 #if ENABLE(DATABASE)
     gchar* databaseDirectory = g_build_filename(g_get_user_data_dir(), "webkit", "databases", NULL);
     webkit_set_web_database_directory_path(databaseDirectory);

WebKit/gtk/webkit/webkitwebview.cpp
@@ -125,7 +125,7 @@
  */
 
 static const double defaultDPI = 96.0;
-static WebKitCacheModel cacheModel;
+static WebKitCacheModel cacheModel = WEBKIT_CACHE_MODEL_DEFAULT;
 static IntPoint globalPointForClientPoint(GdkWindow* window, const IntPoint& clientPoint);
 
 using namespace WebKit;
@@ -4994,6 +4994,8 @@ void webkit_set_cache_model(WebKitCacheModel model)
         deadDecodedDataDeletionInterval = 0;
         break;
     case WEBKIT_CACHE_MODEL_WEB_BROWSER:
+        // Page cache capacity (in pages). Comment from Mac port:
+        // (Research indicates that value / page drops substantially after 3 pages.)
         pageCacheCapacity = 3;
         cacheTotalCapacity = 32 * 1024 * 1024;
         cacheMinDeadCapacity = cacheTotalCapacity / 4;

commit 09a4dab219fd6f1f82ad8ff7acd69bf850d30506

2010-12-06  Martin Robinson  <mrobinson@igalia.com>
        Reviewed by Xan Lopez.

        [GTK] Build is broken after r73353
        https://bugs.webkit.org/show_bug.cgi?id=50568

        Fix the gtk-doc build by making all members of the cache model
        enum public. gtk-doc does not support making only some enum
        members private. We should also expose it so that users querying
        the cache model do not get mysterious, undocumented results.

        * webkit/webkitwebview.h: Fix gtk-doc for the cache model enum.

Identifier: 63789@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@73368 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/gtk/webkit/webkitwebview.h
@@ -52,18 +52,18 @@ typedef enum {
 
 /*  
  * WebKitCacheModel:
+ * @WEBKIT_CACHE_MODEL_DEFAULT: The default cache model. This is
+ *   WEBKIT_CACHE_MODEL_WEB_BROWSER.
  * @WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER: Disable the cache completely, which 
  *   substantially reduces memory usage. Useful for applications that only
  *   access local files.
  * @WEBKIT_CACHE_MODEL_WEB_BROWSER: Improve document load speed substantially
- *   by caching previously viewed content. This is the default setting.
+ *   by caching previously viewed content.
  * 
  * Enum values used for determining the webview cache model.
  */
 typedef enum {
-    /*< private >*/
     WEBKIT_CACHE_MODEL_DEFAULT,
-    /*< public >*/
     WEBKIT_CACHE_MODEL_DOCUMENT_VIEWER,
     WEBKIT_CACHE_MODEL_WEB_BROWSER
 } WebKitCacheModel;