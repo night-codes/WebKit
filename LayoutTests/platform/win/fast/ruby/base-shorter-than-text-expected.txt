night-codes/WebKit

commit 35728cffa47c19a561a93d0c8509991a627ae67e

<rdar://problem/8902740> Expand ruby base when it is shorter than the ruby text https://bugs.webkit.org/show_bug.cgi?id=55183
Reviewed by Dave Hyatt.

Source/WebCore:

Test: fast/ruby/base-shorter-than-text.html

* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::availableLogicalWidthForLine):
* rendering/RenderBlock.h:
(WebCore::RenderBlock::adjustInlineDirectionLineBounds): Added. The base clase implementation does nothing.
* rendering/RenderBlockLineLayout.cpp:
(WebCore::RenderBlock::textAlignmentForLine): Added. Implements the logic that changes "justify" to "auto" for
the last line or a line that ends with a hard break.
(WebCore::RenderBlock::computeInlineDirectionPositionsForLine): Use textAlignmentForLine(), compute the available
width more efficiently, and call adjustInlineDirectionLineBounds() for justified lines.
* rendering/RenderRubyBase.cpp:
(WebCore::RenderRubyBase::rubyRun): Added.
(WebCore::RenderRubyBase::textAlignmentForLine): Added. Alwyas returns "justify".
(WebCore::RenderRubyBase::adjustInlineDirectionLineBounds): Added. Insets the line such that the inset is
half the width of a single intra-line expansion.
* rendering/RenderRubyBase.h:
* rendering/RenderRubyRun.cpp:
(WebCore::RenderRubyRun::addChild):

LayoutTests:

* fast/ruby/base-shorter-than-text.html: Added.
* platform/mac/fast/ruby/base-shorter-than-text-expected.checksum: Added.
* platform/mac/fast/ruby/base-shorter-than-text-expected.png: Added.
* platform/mac/fast/ruby/base-shorter-than-text-expected.txt: Added.



Identifier: 69548@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@79629 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac/fast/ruby/base-shorter-than-text-expected.checksum
@@ -1,0 +1,1 @@
+2d81af0f1d1ab94e5b3fde5e29f26d5e

LayoutTests/platform/mac/fast/ruby/base-shorter-than-text-expected.txt
@@ -1,0 +1,46 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x84
+        RenderRuby (inline) {RUBY} at (0,0) size 144x48
+          RenderRubyRun (anonymous) at (0,12) size 144x72
+            RenderRubyText {RT} at (0,-18) size 144x36
+              RenderText {#text} at (0,6) size 144x24
+                text run at (0,6) width 144: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}"
+            RenderRubyBase (anonymous) at (0,0) size 144x72
+              RenderText {#text} at (12,12) size 120x48
+                text run at (12,12) width 120: "\x{653B}\x{6BBB}"
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {P} at (0,100) size 784x84
+        RenderRuby (inline) {RUBY} at (0,0) size 192x48
+          RenderRubyRun (anonymous) at (0,12) size 192x72
+            RenderRubyText {RT} at (0,-18) size 192x36
+              RenderText {#text} at (0,6) size 192x24
+                text run at (0,6) width 192: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}\x{3069}\x{3046}"
+            RenderRubyBase (anonymous) at (0,0) size 192x72
+              RenderText {#text} at (8,12) size 176x48
+                text run at (8,12) width 176: "\x{6A5F}\x{52D5}\x{968A}"
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {DIV} at (0,200) size 216x300
+        RenderBlock {P} at (16,0) size 84x300
+          RenderRuby (inline) {RUBY} at (0,0) size 48x146
+            RenderRubyRun (anonymous) at (12,0) size 72x146
+              RenderRubyText {RT} at (-18,0) size 36x146
+                RenderText {#text} at (6,0) size 24x146
+                  text run at (6,0) width 146: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}"
+              RenderRubyBase (anonymous) at (0,0) size 72x146
+                RenderText {#text} at (12,12) size 48x121
+                  text run at (12,12) width 121: "\x{653B}\x{6BBB}"
+          RenderText {#text} at (0,0) size 0x0
+        RenderBlock {P} at (116,0) size 84x300
+          RenderRuby (inline) {RUBY} at (0,0) size 48x194
+            RenderRubyRun (anonymous) at (12,0) size 72x194
+              RenderRubyText {RT} at (-18,0) size 36x194
+                RenderText {#text} at (6,0) size 24x194
+                  text run at (6,0) width 194: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}\x{3069}\x{3046}"
+              RenderRubyBase (anonymous) at (0,0) size 72x194
+                RenderText {#text} at (12,8) size 48x178
+                  text run at (12,8) width 178: "\x{6A5F}\x{52D5}\x{968A}"
+          RenderText {#text} at (0,0) size 0x0

LayoutTests/fast/ruby/base-shorter-than-text.html
@@ -1,0 +1,30 @@
+<meta charset="utf-8">
+<style>
+    ruby { font-size: 48px; font-family: HiraMinProN-W3;}
+</style>
+<p>
+    <ruby>
+        攻殻
+        <rt>きどうたいき</rt>
+    </ruby>
+</p>
+<p>
+    <ruby>
+        機動隊
+        <rt>きどうたいきどう</rt>
+    </ruby>
+</p>
+<div style="-webkit-writing-mode: vertical-rl; height: 50%;">
+<p>
+    <ruby>
+        攻殻
+        <rt>きどうたいき</rt>
+    </ruby>
+</p>
+<p>
+    <ruby>
+        機動隊
+        <rt>きどうたいきどう</rt>
+    </ruby>
+</p>
+</div>

Source/WebCore/rendering/RenderBlock.cpp
@@ -3427,8 +3427,7 @@ int RenderBlock::logicalRightOffsetForLine(int logicalTop, int fixedOffset, bool
     return right;
 }
 
-int
-RenderBlock::availableLogicalWidthForLine(int position, bool firstLine) const
+int RenderBlock::availableLogicalWidthForLine(int position, bool firstLine) const
 {
     int result = logicalRightOffsetForLine(position, firstLine) - logicalLeftOffsetForLine(position, firstLine);
     return (result < 0) ? 0 : result;

Source/WebCore/rendering/RenderBlock.h
@@ -489,6 +489,10 @@ private:
     InlineIterator findNextLineBreak(InlineBidiResolver&, bool firstLine, bool& isLineEmpty, bool& previousLineBrokeCleanly, bool& hyphenated, EClear*, FloatingObject* lastFloatFromPreviousLine);
     RootInlineBox* constructLine(unsigned runCount, BidiRun* firstRun, BidiRun* lastRun, bool firstLine, bool lastLine, RenderObject* endObject);
     InlineFlowBox* createLineBoxes(RenderObject*, bool firstLine);
+
+    virtual ETextAlign textAlignmentForLine(bool endsWithSoftBreak) const;
+    virtual void adjustInlineDirectionLineBounds(int /* expansionOpportunityCount */, float& /* logicalLeft */, float& /* logicalWidth */) const { }
+
     void computeInlineDirectionPositionsForLine(RootInlineBox*, bool firstLine, BidiRun* firstRun, BidiRun* trailingSpaceRun, bool reachedEnd, GlyphOverflowAndFallbackFontsMap&);
     void computeBlockDirectionPositionsForLine(RootInlineBox*, BidiRun*, GlyphOverflowAndFallbackFontsMap&, VerticalPositionCache&);
     void deleteEllipsisLineBoxes();

Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -315,16 +315,26 @@ RootInlineBox* RenderBlock::constructLine(unsigned runCount, BidiRun* firstRun, 
     return lastRootBox();
 }
 
+ETextAlign RenderBlock::textAlignmentForLine(bool endsWithSoftBreak) const
+{
+    ETextAlign alignment = style()->textAlign();
+    if (!endsWithSoftBreak && alignment == JUSTIFY)
+        alignment = TAAUTO;
+
+    return alignment;
+}
+
 void RenderBlock::computeInlineDirectionPositionsForLine(RootInlineBox* lineBox, bool firstLine, BidiRun* firstRun, BidiRun* trailingSpaceRun, bool reachedEnd, GlyphOverflowAndFallbackFontsMap& textBoxDataMap)
 {
-    // First determine our total logical width.
-    float availableLogicalWidth = availableLogicalWidthForLine(logicalHeight(), firstLine);
-    float totalLogicalWidth = lineBox->getFlowSpacingLogicalWidth();
+    ETextAlign textAlign = textAlignmentForLine(!reachedEnd && !lineBox->endsWithBreak());
+    float logicalLeft = logicalLeftOffsetForLine(logicalHeight(), firstLine);
+    float availableLogicalWidth = logicalRightOffsetForLine(logicalHeight(), firstLine) - logicalLeft;
+
     bool needsWordSpacing = false;
+    float totalLogicalWidth = lineBox->getFlowSpacingLogicalWidth();
     unsigned expansionOpportunityCount = 0;
     bool isAfterExpansion = true;
     Vector<unsigned, 16> expansionOpportunities;
-    ETextAlign textAlign = style()->textAlign();
 
     for (BidiRun* r = firstRun; r; r = r->next()) {
         if (!r->m_box || r->m_object->isPositioned() || r->m_box->isLineBreak())
@@ -386,7 +396,6 @@ void RenderBlock::computeInlineDirectionPositionsForLine(RootInlineBox* lineBox,
     // we now examine our text-align property in order to determine where to position the
     // objects horizontally.  The total width of the line can be increased if we end up
     // justifying text.
-    float logicalLeft = logicalLeftOffsetForLine(logicalHeight(), firstLine);
     switch (textAlign) {
         case LEFT:
         case WEBKIT_LEFT:
@@ -403,7 +412,8 @@ void RenderBlock::computeInlineDirectionPositionsForLine(RootInlineBox* lineBox,
             }
             break;
         case JUSTIFY:
-            if (expansionOpportunityCount && !reachedEnd && !lineBox->endsWithBreak()) {
+            if (expansionOpportunityCount) {
+                adjustInlineDirectionLineBounds(expansionOpportunityCount, logicalLeft, availableLogicalWidth);
                 if (trailingSpaceRun) {
                     totalLogicalWidth -= trailingSpaceRun->m_box->logicalWidth();
                     trailingSpaceRun->m_box->setLogicalWidth(0);

Source/WebCore/rendering/RenderRubyBase.cpp
@@ -31,6 +31,10 @@
 #include "config.h"
 
 #include "RenderRubyBase.h"
+#include "RenderRubyRun.h"
+#include "RenderRubyText.h"
+
+using namespace std;
 
 namespace WebCore {
 
@@ -180,6 +184,32 @@ void RenderRubyBase::mergeBlockChildren(RenderRubyBase* toBase, RenderObject* fr
     }
     // Move all remaining children normally.
     moveChildrenTo(toBase, firstChild(), fromBeforeChild);
+}
+
+RenderRubyRun* RenderRubyBase::rubyRun() const
+{
+    ASSERT(parent());
+    ASSERT(parent()->isRubyRun());
+
+    return static_cast<RenderRubyRun*>(parent());
+}
+
+ETextAlign RenderRubyBase::textAlignmentForLine(bool /* endsWithSoftBreak */) const
+{
+    return JUSTIFY;
+}
+
+void RenderRubyBase::adjustInlineDirectionLineBounds(int expansionOpportunityCount, float& logicalLeft, float& logicalWidth) const
+{
+    int maxPreferredLogicalWidth = this->maxPreferredLogicalWidth();
+    if (maxPreferredLogicalWidth >= logicalWidth)
+        return;
+
+    // Inset the ruby base by half the inter-ideograph expansion amount.
+    float inset = (logicalWidth - maxPreferredLogicalWidth) / (expansionOpportunityCount + 1);
+
+    logicalLeft += inset / 2;
+    logicalWidth -= inset;
 }
 
 } // namespace WebCore

commit 2511cf86a72e7d972d3219c18c5a82f092478124

[Windows 7 Release Tests] fast/ruby/base-shorter-than-text.html failing since introduction in r79629. https://bugs.webkit.org/show_bug.cgi?id=55230
Add Windows-specific expected results. Unreviewed.

* platform/win/fast/ruby/base-shorter-than-text-expected.checksum: Added.
* platform/win/fast/ruby/base-shorter-than-text-expected.png: Added.
* platform/win/fast/ruby/base-shorter-than-text-expected.txt: Added.



Identifier: 69612@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@79693 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/win/fast/ruby/base-shorter-than-text-expected.checksum
@@ -1,0 +1,1 @@
+652d5abfd940f5ad60df620fd871131f

LayoutTests/platform/win/fast/ruby/base-shorter-than-text-expected.txt
@@ -1,0 +1,46 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x83
+        RenderRuby (inline) {RUBY} at (0,0) size 123x55
+          RenderRubyRun (anonymous) at (0,28) size 123x55
+            RenderRubyText {RT} at (0,-28) size 123x28
+              RenderText {#text} at (0,0) size 123x28
+                text run at (0,0) width 123: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}"
+            RenderRubyBase (anonymous) at (0,0) size 123x55
+              RenderText {#text} at (13,0) size 97x55
+                text run at (13,0) width 97: "\x{653B}\x{6BBB}"
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {P} at (0,99) size 784x83
+        RenderRuby (inline) {RUBY} at (0,0) size 161x55
+          RenderRubyRun (anonymous) at (0,28) size 161x55
+            RenderRubyText {RT} at (0,-28) size 161x28
+              RenderText {#text} at (0,0) size 161x28
+                text run at (0,0) width 161: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}\x{3069}\x{3046}"
+            RenderRubyBase (anonymous) at (0,0) size 161x55
+              RenderText {#text} at (8,0) size 145x55
+                text run at (8,0) width 145: "\x{6A5F}\x{52D5}\x{968A}"
+        RenderText {#text} at (0,0) size 0x0
+      RenderBlock {DIV} at (0,198) size 214x300
+        RenderBlock {P} at (16,0) size 83x300
+          RenderRuby (inline) {RUBY} at (0,0) size 55x123
+            RenderRubyRun (anonymous) at (28,0) size 55x123
+              RenderRubyText {RT} at (-28,0) size 28x123
+                RenderText {#text} at (0,0) size 28x123
+                  text run at (0,0) width 123: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}"
+              RenderRubyBase (anonymous) at (0,0) size 55x123
+                RenderText {#text} at (0,13) size 55x97
+                  text run at (0,13) width 96: "\x{653B}\x{6BBB}"
+          RenderText {#text} at (0,0) size 0x0
+        RenderBlock {P} at (115,0) size 83x300
+          RenderRuby (inline) {RUBY} at (0,0) size 55x161
+            RenderRubyRun (anonymous) at (28,0) size 55x161
+              RenderRubyText {RT} at (-28,0) size 28x161
+                RenderText {#text} at (0,0) size 28x161
+                  text run at (0,0) width 161: "\x{304D}\x{3069}\x{3046}\x{305F}\x{3044}\x{304D}\x{3069}\x{3046}"
+              RenderRubyBase (anonymous) at (0,0) size 55x161
+                RenderText {#text} at (0,8) size 55x145
+                  text run at (0,8) width 144: "\x{6A5F}\x{52D5}\x{968A}"
+          RenderText {#text} at (0,0) size 0x0