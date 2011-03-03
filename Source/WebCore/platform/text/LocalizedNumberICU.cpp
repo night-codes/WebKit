night-codes/WebKit

commit b6367fb7371ad053cfbfab5b74031edc1d760a6f

Caching number formatter instances in LocalizedNumber* implementations https://bugs.webkit.org/show_bug.cgi?id=55629
Reviewed by Darin Adler.

No new tests. This change doesn't change existing behavior, and is
covered by existing tests.

* platform/text/LocalizedNumberICU.cpp:
(WebCore::createFormatterForCurrentLocale):
(WebCore::numberFormatter): Introduce a function to return a static
  instance of NumberFormat.
(WebCore::parseLocalizedNumber): Use numberFormatter().
(WebCore::formatLocalizedNumber): Use numberFormatter().
* platform/text/mac/LocalizedNumberMac.mm:
(WebCore::createFormatterForCurrentLocale):
(WebCore::numberFormatter): Introduce a function to return a static
  instance of NSNumberFormatter.
(WebCore::parseLocalizedNumber): Use numberFormatter().
(WebCore::formatLocalizedNumber): Use numberFormatter().

Identifier: 70093@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@80198 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/text/LocalizedNumberICU.cpp
@@ -40,17 +40,26 @@ using namespace std;
 
 namespace WebCore {
 
-static inline PassOwnPtr<NumberFormat> createFormatterForCurrentLocale()
+static PassOwnPtr<NumberFormat> createFormatterForCurrentLocale()
 {
     UErrorCode status = U_ZERO_ERROR;
-    return adoptPtr(NumberFormat::createInstance(status));
+    OwnPtr<NumberFormat> formatter = adoptPtr(NumberFormat::createInstance(status));
+    return satus == U_ZERO_ERROR ? formatter.release() : 0;
+}
+
+// This might return 0.
+static NumberFormat* numberFormatter()
+{
+    ASSERT(isMainThread());
+    static NumberFormat* formatter = createFormatterForCurrentLocale().leakPtr();
+    return formatter;
 }
 
 double parseLocalizedNumber(const String& numberString)
 {
     if (numberString.isEmpty())
         return numeric_limits<double>::quiet_NaN();
-    OwnPtr<NumberFormat> formatter = createFormatterForCurrentLocale();
+    NumberFormat* formatter = numberFormatter();
     if (!formatter)
         return numeric_limits<double>::quiet_NaN();
     UnicodeString numberUnicodeString(numberString.characters(), numberString.length());
@@ -66,7 +75,7 @@ double parseLocalizedNumber(const String& numberString)
 
 String formatLocalizedNumber(double number)
 {
-    OwnPtr<NumberFormat> formatter = createFormatterForCurrentLocale();
+    NumberFormat* formatter = numberFormatter();
     if (!formatter)
         return String();
     UnicodeString result;

Source/WebCore/platform/text/mac/LocalizedNumberMac.mm
@@ -40,14 +40,26 @@ using namespace std;
 
 namespace WebCore {
 
+static RetainPtr<NSNumberFormatter> createFormatterForCurrentLocale()
+{
+    RetainPtr<NSNumberFormatter> formatter(AdoptNS, [[NSNumberFormatter alloc] init]);
+    [formatter.get() setLocalizesFormat:YES];
+    [formatter.get() setNumberStyle:NSNumberFormatterDecimalStyle];
+    return formatter;
+}
+
+static NSNumberFormatter *numberFormatter()
+{
+    ASSERT(isMainThread());
+    static NSNumberFormatter *formatter = createFormatterForCurrentLocale().leakRef();
+    return formatter;
+}
+
 double parseLocalizedNumber(const String& numberString)
 {
     if (numberString.isEmpty())
         return numeric_limits<double>::quiet_NaN();
-    RetainPtr<NSNumberFormatter> formatter(AdoptNS, [[NSNumberFormatter alloc] init]);
-    [formatter.get() setLocalizesFormat:YES];
-    [formatter.get() setNumberStyle:NSNumberFormatterDecimalStyle];
-    NSNumber *number = [formatter.get() numberFromString:numberString];
+    NSNumber *number = [numberFormatter() numberFromString:numberString];
     if (!number)
         return numeric_limits<double>::quiet_NaN();
     return [number doubleValue];
@@ -56,10 +68,7 @@ double parseLocalizedNumber(const String& numberString)
 String formatLocalizedNumber(double inputNumber)
 {
     RetainPtr<NSNumber> number(AdoptNS, [[NSNumber alloc] initWithDouble:inputNumber]);
-    RetainPtr<NSNumberFormatter> formatter(AdoptNS, [[NSNumberFormatter alloc] init]);
-    [formatter.get() setLocalizesFormat:YES];
-    [formatter.get() setNumberStyle:NSNumberFormatterDecimalStyle];
-    return String([formatter.get() stringFromNumber:number.get()]);
+    return String([numberFormatter() stringFromNumber:number.get()]);
 }
 
 } // namespace WebCore

commit 313f98431f755c1a2f3f82cd9cb35a046cfbb7c5

2011-03-02  Kent Tamura  <tkent@chromium.org>
        Unreviewed, a trivial regression fix.

        Fix LocalizedNumberICU regression by r80198 and r80199.
        https://bugs.webkit.org/show_bug.cgi?id=55629

        * platform/text/LocalizedNumberICU.cpp:
          We should use U_SUCCESS().
        (WebCore::createFormatterForCurrentLocale):
        (WebCore::parseLocalizedNumber):

Identifier: 70098@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@80203 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/text/LocalizedNumberICU.cpp
@@ -44,7 +44,7 @@ static PassOwnPtr<NumberFormat> createFormatterForCurrentLocale()
 {
     UErrorCode status = U_ZERO_ERROR;
     OwnPtr<NumberFormat> formatter = adoptPtr(NumberFormat::createInstance(status));
-    return status == U_ZERO_ERROR ? formatter.release() : 0;
+    return U_SUCCESS(status) ? formatter.release() : 0;
 }
 
 // This might return 0.
@@ -70,7 +70,7 @@ double parseLocalizedNumber(const String& numberString)
         return numeric_limits<double>::quiet_NaN();
     UErrorCode status = U_ZERO_ERROR;
     double numericResult = result.getDouble(status);
-    return status == U_ZERO_ERROR ? numericResult : numeric_limits<double>::quiet_NaN();
+    return U_SUCCESS(status) ? numericResult : numeric_limits<double>::quiet_NaN();
 }
 
 String formatLocalizedNumber(double number)