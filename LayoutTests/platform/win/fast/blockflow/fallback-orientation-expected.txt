night-codes/WebKit

commit 14829b1ec72f40a990e085ca11f2249844c1de02

<rdar://problem/9180716> REGRESSION (r80582): Angle bracket rendered upright in vertical mode https://bugs.webkit.org/show_bug.cgi?id=57169
Reviewed by Maciej Stachowiak.

Source/WebCore:

Test: fast/blockflow/fallback-orientation.html

* platform/graphics/FontFastPath.cpp:
(WebCore::Font::glyphDataForCharacter): If the font has vertical glyphs, use it, rather
than continuing down the fallback list.

LayoutTests:

* fast/blockflow/fallback-orientation.html: Added.
* platform/mac/fast/blockflow/fallback-orientation-expected.checksum: Added.
* platform/mac/fast/blockflow/fallback-orientation-expected.png: Added.
* platform/mac/fast/blockflow/fallback-orientation-expected.txt: Added.



Identifier: 71861@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82056 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac/fast/blockflow/fallback-orientation-expected.checksum
@@ -1,0 +1,1 @@
+7240cc2ea9f191173261de9bcdf00124

LayoutTests/platform/mac/fast/blockflow/fallback-orientation-expected.txt
@@ -1,0 +1,11 @@
+layer at (0,0) size 785x650
+  RenderView at (0,0) size 785x600
+layer at (0,0) size 785x650
+  RenderBlock {HTML} at (0,0) size 785x650
+    RenderBody {BODY} at (8,8) size 769x634
+      RenderBlock {P} at (0,0) size 769x18
+        RenderText {#text} at (0,0) size 211x18
+          text run at (0,0) width 211: "This should be pointing upwards:"
+      RenderBlock {DIV} at (0,34) size 120x600
+        RenderText {#text} at (20,0) size 80x81
+          text run at (20,0) width 81: "\x{3008}"

LayoutTests/fast/blockflow/fallback-orientation.html
@@ -1,0 +1,7 @@
+<p>
+    This should be pointing upwards:
+</p>
+<div style="font-family: 'hiragino mincho pro'; font-size: 80px; -webkit-writing-mode: vertical-rl;">
+    &#x3008;
+</div>
+</html>

Source/WebCore/platform/graphics/FontFastPath.cpp
@@ -96,7 +96,6 @@ GlyphData Font::glyphDataForCharacter(UChar32 c, bool mirror, FontDataVariant va
                             
                             // Shouldn't be possible to even reach this point.
                             ASSERT_NOT_REACHED();
-                            return data;
                         }
                     } else {
                         if (m_fontDescription.textOrientation() == TextOrientationVerticalRight) {
@@ -131,8 +130,8 @@ GlyphData Font::glyphDataForCharacter(UChar32 c, bool mirror, FontDataVariant va
 
                         // Shouldn't be possible to even reach this point.
                         ASSERT_NOT_REACHED();
-                        return data;
                     }
+                    return data;
                 }
 
                 if (node->isSystemFallback())

commit 0bbc0b03dea8cc56e75398ec1708cafd57f126d5

[Windows 7 Release Tests] fast/blockflow/fallback-orientation.html failing since introduction in r82056. https://bugs.webkit.org/show_bug.cgi?id=57238
Unreviewed, add expected failing results, since vertical text is not yet implemented on
Windows, to get the bots green.

* platform/win/fast/blockflow/fallback-orientation-expected.checksum: Added.
* platform/win/fast/blockflow/fallback-orientation-expected.png: Added.
* platform/win/fast/blockflow/fallback-orientation-expected.txt: Added.



Identifier: 71928@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@82126 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/win/fast/blockflow/fallback-orientation-expected.checksum
@@ -1,0 +1,1 @@
+dc02c06e73184f425aa0940daeaa4bd8

LayoutTests/platform/win/fast/blockflow/fallback-orientation-expected.txt
@@ -1,0 +1,11 @@
+layer at (0,0) size 785x650
+  RenderView at (0,0) size 785x600
+layer at (0,0) size 785x650
+  RenderBlock {HTML} at (0,0) size 785x650
+    RenderBody {BODY} at (8,8) size 769x634
+      RenderBlock {P} at (0,0) size 769x18
+        RenderText {#text} at (0,0) size 211x18
+          text run at (0,0) width 211: "This should be pointing upwards:"
+      RenderBlock {DIV} at (0,34) size 92x600
+        RenderText {#text} at (0,0) size 92x40
+          text run at (0,0) width 40: "\x{3008}"