night-codes/WebKit

commit ff9b6538bbc59306a22c7bb5cb599bcfb9666d49

2011-05-02  Adam Barth  <abarth@webkit.org>
        Reviewed by David Levin.

        PLATFORM(MAC) should (almost!) build with strict PassOwnPtr
        https://bugs.webkit.org/show_bug.cgi?id=59924

        * css/CSSGrammar.y:
        * platform/graphics/mac/SimpleFontDataMac.mm:
            - Memory management for the font cache is somewhat... complext.
              This will require some careful thought to sort out.
2011-05-02  Adam Barth  <abarth@webkit.org>

        Reviewed by David Levin.

        PLATFORM(MAC) should (almost!) build with strict PassOwnPtr
        https://bugs.webkit.org/show_bug.cgi?id=59924

        * History/WebHistory.mm:
        (-[WebHistory _visitedURL:withTitle:method:wasFailure:increaseVisitCount:]):
        * Plugins/WebBaseNetscapePluginView.mm:
        (-[WebBaseNetscapePluginView initWithFrame:pluginPackage:URL:baseURL:MIMEType:attributeKeys:attributeValues:loadManually:element:WebCore::]):
        * WebCoreSupport/WebFrameLoaderClient.mm:
        (WebFrameLoaderClient::savePlatformDataToCachedFrame):
        * WebView/WebView.mm:
        (-[WebView _injectMailQuirksScript]):
        (-[WebView _injectOutlookQuirksScript]):
        (toStringVector):
2011-05-02  Adam Barth  <abarth@webkit.org>

        Reviewed by David Levin.

        PLATFORM(MAC) should (almost!) build with strict PassOwnPtr
        https://bugs.webkit.org/show_bug.cgi?id=59924

        * Platform/CoreIPC/Connection.cpp:
        (CoreIPC::Connection::sendSyncMessage):
        (CoreIPC::Connection::waitForSyncReply):
        (CoreIPC::Connection::dispatchSyncMessage):
        * Platform/CoreIPC/Connection.h:
        (CoreIPC::Connection::Message::releaseArguments):
            - The memory management for this class is very strange.  I wrote a
              large comment.  Hopefully a kind soul will come by and fix it.
        * Shared/Plugins/Netscape/NetscapePluginModule.cpp:
        (WebKit::NetscapePluginModule::unload):

Source/WebKit2/Platform/CoreIPC/Connection.cpp
@@ -387,7 +387,7 @@ PassOwnPtr<ArgumentDecoder> Connection::sendSyncMessage(MessageID messageID, uin
 
     if (!isValid()) {
         didFailToSendSyncMessage();
-        return 0;
+        return PassOwnPtr<ArgumentDecoder>();
     }
 
     // Push the pending sync reply information on our stack.
@@ -395,7 +395,7 @@ PassOwnPtr<ArgumentDecoder> Connection::sendSyncMessage(MessageID messageID, uin
         MutexLocker locker(m_syncReplyStateMutex);
         if (!m_shouldWaitForSyncReplies) {
             didFailToSendSyncMessage();
-            return 0;
+            return PassOwnPtr<ArgumentDecoder>();
         }
 
         m_pendingSyncReplies.append(PendingSyncReply(syncRequestID));
@@ -457,7 +457,7 @@ PassOwnPtr<ArgumentDecoder> Connection::waitForSyncReply(uint64_t syncRequestID,
         // If that happens, we need to stop waiting, or we'll hang since we won't get
         // any more incoming messages.
         if (!isValid())
-            return 0;
+            return PassOwnPtr<ArgumentDecoder>();
 
         // We didn't find a sync reply yet, keep waiting.
 #if PLATFORM(WIN)
@@ -471,7 +471,7 @@ PassOwnPtr<ArgumentDecoder> Connection::waitForSyncReply(uint64_t syncRequestID,
     if (m_client)
         m_client->syncMessageSendTimedOut(this);
 
-    return 0;
+    return PassOwnPtr<ArgumentDecoder>();
 }
 
 void Connection::processIncomingSyncReply(PassOwnPtr<ArgumentDecoder> arguments)
@@ -626,10 +626,10 @@ void Connection::dispatchSyncMessage(MessageID messageID, ArgumentDecoder* argum
     }
 
     // Create our reply encoder.
-    ArgumentEncoder* replyEncoder = ArgumentEncoder::create(syncRequestID).leakPtr();
+    OwnPtr<ArgumentEncoder> replyEncoder = ArgumentEncoder::create(syncRequestID);
     
-    // Hand off both the decoder and encoder to the client..
-    SyncReplyMode syncReplyMode = m_client->didReceiveSyncMessage(this, messageID, arguments, replyEncoder);
+    // Hand off both the decoder and encoder to the client.
+    SyncReplyMode syncReplyMode = m_client->didReceiveSyncMessage(this, messageID, arguments, replyEncoder.get());
 
     // FIXME: If the message was invalid, we should send back a SyncMessageError.
     ASSERT(!arguments->isInvalid());
@@ -641,7 +641,7 @@ void Connection::dispatchSyncMessage(MessageID messageID, ArgumentDecoder* argum
     }
 
     // Send the reply.
-    sendSyncReply(replyEncoder);
+    sendSyncReply(replyEncoder.release());
 }
 
 void Connection::didFailToSendSyncMessage()

Source/WebCore/css/CSSGrammar.y
@@ -1450,7 +1450,7 @@ function:
         CSSParser* p = static_cast<CSSParser*>(parser);
         CSSParserFunction* f = p->createFloatingFunction();
         f->name = $1;
-        f->args = p->sinkFloatingValueList($3);
+        f->args = adoptPtr(p->sinkFloatingValueList($3));
         $$.id = 0;
         $$.unit = CSSParserValue::Function;
         $$.function = f;
@@ -1459,7 +1459,7 @@ function:
         CSSParser* p = static_cast<CSSParser*>(parser);
         CSSParserFunction* f = p->createFloatingFunction();
         f->name = $1;
-        f->args = 0;
+        f->args = nullptr;
         $$.id = 0;
         $$.unit = CSSParserValue::Function;
         $$.function = f;
@@ -1568,7 +1568,7 @@ calc_function:
         CSSParser* p = static_cast<CSSParser*>(parser);
         CSSParserFunction* f = p->createFloatingFunction();
         f->name = $1;
-        f->args = p->sinkFloatingValueList($3);
+        f->args = adoptPtr(p->sinkFloatingValueList($3));
         $$.id = 0;
         $$.unit = CSSParserValue::Function;
         $$.function = f;
@@ -1593,7 +1593,7 @@ min_or_max_function:
         CSSParser* p = static_cast<CSSParser*>(parser);
         CSSParserFunction* f = p->createFloatingFunction();
         f->name = $1;
-        f->args = p->sinkFloatingValueList($3);
+        f->args = adoptPtr(p->sinkFloatingValueList($3));
         $$.id = 0;
         $$.unit = CSSParserValue::Function;
         $$.function = f;

Source/WebCore/platform/graphics/mac/SimpleFontDataMac.mm
@@ -28,6 +28,10 @@
  */
 
 #import "config.h"
+
+// FIXME: Remove this define!
+#define LOOSE_PASS_OWN_PTR
+
 #import "SimpleFontData.h"
 
 #import "BlockExceptions.h"

Source/WebKit/mac/History/WebHistory.mm
@@ -826,7 +826,7 @@ static inline WebHistoryDateKey dateKey(NSTimeInterval date)
     if ([method length])
         item->setLastVisitWasHTTPNonGet([method caseInsensitiveCompare:@"GET"] && (![[url scheme] caseInsensitiveCompare:@"http"] || ![[url scheme] caseInsensitiveCompare:@"https"]));
 
-    item->setRedirectURLs(0);
+    item->setRedirectURLs(PassOwnPtr<Vector<String> >());
 
     NSArray *entries = [[NSArray alloc] initWithObjects:entry, nil];
     [self _sendNotification:WebHistoryItemsAddedNotification entries:entries];

Source/WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
@@ -162,7 +162,7 @@ String WebHaltablePlugin::pluginName() const
         _mode = NP_EMBED;
     
     _loadManually = loadManually;
-    _haltable = new WebHaltablePlugin(self);
+    _haltable = adoptPtr(new WebHaltablePlugin(self));
     return self;
 }
 

Source/WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
@@ -1205,8 +1205,8 @@ void WebFrameLoaderClient::setTitle(const StringWithDirection& title, const KURL
 
 void WebFrameLoaderClient::savePlatformDataToCachedFrame(CachedFrame* cachedFrame)
 {
-    WebCachedFramePlatformData* webPlatformData = new WebCachedFramePlatformData([m_webFrame->_private->webFrameView documentView]);
-    cachedFrame->setCachedFramePlatformData(webPlatformData);
+    OwnPtr<WebCachedFramePlatformData> webPlatformData = adoptPtr(new WebCachedFramePlatformData([m_webFrame->_private->webFrameView documentView]));
+    cachedFrame->setCachedFramePlatformData(webPlatformData.release());
 }
 
 void WebFrameLoaderClient::transitionToCommittedFromCachedFrame(CachedFrame* cachedFrame)

Source/WebKit/mac/WebView/WebView.mm
@@ -642,7 +642,7 @@ static NSString *leakMailQuirksUserScriptContents()
 {
     static NSString *mailQuirksScriptContents = leakMailQuirksUserScriptContents();
     core(self)->group().addUserScriptToWorld(core([WebScriptWorld world]),
-        mailQuirksScriptContents, KURL(), 0, 0, InjectAtDocumentEnd, InjectInAllFrames);
+        mailQuirksScriptContents, KURL(), PassOwnPtr<Vector<String> >(), PassOwnPtr<Vector<String> >(), InjectAtDocumentEnd, InjectInAllFrames);
 }
 
 static bool needsOutlookQuirksScript()
@@ -663,7 +663,7 @@ static NSString *leakOutlookQuirksUserScriptContents()
 {
     static NSString *outlookQuirksScriptContents = leakOutlookQuirksUserScriptContents();
     core(self)->group().addUserScriptToWorld(core([WebScriptWorld world]),
-        outlookQuirksScriptContents, KURL(), 0, 0, InjectAtDocumentEnd, InjectInAllFrames);
+        outlookQuirksScriptContents, KURL(), PassOwnPtr<Vector<String> >(), PassOwnPtr<Vector<String> >(), InjectAtDocumentEnd, InjectInAllFrames);
 }
 
 - (void)_commonInitializationWithFrameName:(NSString *)frameName groupName:(NSString *)groupName usesDocumentViews:(BOOL)usesDocumentViews
@@ -2563,14 +2563,14 @@ static PassOwnPtr<Vector<String> > toStringVector(NSArray* patterns)
     // Convert the patterns into Vectors.
     NSUInteger count = [patterns count];
     if (count == 0)
-        return 0;
-    Vector<String>* patternsVector = new Vector<String>;
+        return PassOwnPtr<Vector<String> >();
+    OwnPtr<Vector<String> > patternsVector = adoptPtr(new Vector<String>);
     for (NSUInteger i = 0; i < count; ++i) {
         id entry = [patterns objectAtIndex:i];
         if ([entry isKindOfClass:[NSString class]])
             patternsVector->append(String((NSString*)entry));
     }
-    return patternsVector;
+    return patternsVector.release();
 }
 
 + (void)_addUserScriptToGroup:(NSString *)groupName world:(WebScriptWorld *)world source:(NSString *)source url:(NSURL *)url

Source/WebKit2/Platform/CoreIPC/Connection.h
@@ -185,14 +185,19 @@ private:
         
         PassOwnPtr<T> releaseArguments()
         {
-            T* arguments = m_arguments;
+            OwnPtr<T> arguments = adoptPtr(m_arguments);
             m_arguments = 0;
 
-            return arguments;
+            return arguments.release();
         }
         
     private:
         MessageID m_messageID;
+        // The memory management of this class is very unusual. The class acts
+        // as if it has an owning reference to m_arguments (e.g., accepting a
+        // PassOwnPtr in its constructor) in all ways except that it does not
+        // deallocate m_arguments on destruction.
+        // FIXME: Does this leak m_arguments on destruction?
         T* m_arguments;
     };
 

commit 0c0fe354b32cd95f792052ab266a151c6a3b1e81

Fix crash that was introduced in r85515.
* Platform/CoreIPC/Connection.cpp:
(CoreIPC::Connection::dispatchSyncMessage):
If didReceiveSyncMessage returns ManualReply, the client took ownership of the reply
encoder so we must leak it to avoid a double-free.



Identifier: 75266@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@85576 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Platform/CoreIPC/Connection.cpp
@@ -641,7 +641,9 @@ void Connection::dispatchSyncMessage(MessageID messageID, ArgumentDecoder* argum
 
     if (syncReplyMode == ManualReply) {
         // The client will take ownership of the reply encoder and send it at some point in the future.
-        // We won't do anything here.
+        ArgumentEncoder *encoder = replyEncoder.leakPtr();
+        (void)encoder;
+
         return;
     }
 