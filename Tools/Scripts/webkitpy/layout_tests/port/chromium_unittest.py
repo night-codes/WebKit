night-codes/WebKit

commit 4bc6cb048b051b5e14d138c6bd4ff1ce2b16aedd

Make NRWT find crash logs for the crashed process, which may not necessarily be the driver process
Fixes <http://webkit.org/b/72526> REGRESSION (NRWT): WebKitTestRunner crash log gets saved
when web process crashes, but WebProcess crash log should get saved instead

Reviewed by Eric Seidel.

* Scripts/webkitpy/layout_tests/controllers/test_result_writer.py:
(write_test_result): Simplified a little to share more code between the case where we
crashed when running the test and where we crashed when checking the expected rendering for
a reftest. Changed to pass the crashed process name down to write_crash_report.
(TestResultWriter.write_crash_report): Added crashed_process_name parameter, which we pass
along to CrashLogs.find_newest_log rather than always using the driver name.

* Scripts/webkitpy/layout_tests/port/chromium.py:
(ChromiumDriver.run_test): Pass the driver name as the name of the crashed process if we
crashed.

* Scripts/webkitpy/layout_tests/port/test.py:
(TestInstance.__init__): Initialize new web_process_crash attribute.
(unit_test_list): Added new web-process-crash-with-stderr test, which is used by the new
test this patch adds.
(TestDriver.run_test): Pass a crashed process name to DriverOutput based on whether the test
specifies that the driver crashed or the web process crashed.

* Scripts/webkitpy/layout_tests/run_webkit_tests_integrationtest.py:
(MainTest.test_web_process_crash_log): Added. Similar to test_crash_log, but uses a test for
which we pretend the web process crashed rather than the driver process.

(MainTest.test_run_singly_actually_runs_tests):
(MainTest.test_unexpected_failures):
(EndToEndTest.test_end_to_end):
Updated magic numbers to account for new unexpected failing test added to unit_test_list.

Identifier: 89154@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100648 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -588,14 +588,16 @@ class ChromiumDriver(Driver):
                 text = None
 
         error = ''.join(error)
+        crashed_process_name = None
         # Currently the stacktrace is in the text output, not error, so append the two together so
         # that we can see stack in the output. See http://webkit.org/b/66806
         # FIXME: We really should properly handle the stderr output separately.
         if crash:
             error = error + str(text)
+            crashed_process_name = self.driver_name()
 
         return DriverOutput(text, output_image, actual_checksum, audio=audio_bytes,
-            crash=crash, test_time=run_time, timeout=timeout, error=error)
+            crash=crash, crashed_process_name=crashed_process_name, test_time=run_time, timeout=timeout, error=error)
 
     def stop(self):
         if not self._proc:

Tools/Scripts/webkitpy/layout_tests/controllers/test_result_writer.py
@@ -63,10 +63,8 @@ def write_test_result(port, test_name, driver_output,
                                   test_failures.FailureMissingAudio)):
             writer.write_audio_files(driver_output.audio, expected_driver_output.audio)
         elif isinstance(failure, test_failures.FailureCrash):
-            if failure.is_reftest:
-                writer.write_crash_report(expected_driver_output.error)
-            else:
-                writer.write_crash_report(driver_output.error)
+            crashed_driver_output = expected_driver_output if failure.is_reftest else driver_output
+            writer.write_crash_report(crashed_driver_output.crashed_process_name, crashed_driver_output.error)
         elif isinstance(failure, test_failures.FailureReftestMismatch):
             writer.write_image_files(driver_output.image, expected_driver_output.image)
             # FIXME: This work should be done earlier in the pipeline (e.g., when we compare images for non-ref tests).
@@ -156,13 +154,13 @@ class TestResultWriter(object):
         fs.maybe_make_directory(fs.dirname(filename))
         fs.write_binary_file(filename, error)
 
-    def write_crash_report(self, error):
+    def write_crash_report(self, crashed_process_name, error):
         fs = self._port._filesystem
         filename = self.output_filename(self.FILENAME_SUFFIX_CRASH_LOG + ".txt")
         fs.maybe_make_directory(fs.dirname(filename))
         # FIXME: We shouldn't be grabbing private members of port.
         crash_logs = CrashLogs(fs)
-        log = crash_logs.find_newest_log(self._port.driver_name())
+        log = crash_logs.find_newest_log(crashed_process_name)
         # CrashLogs doesn't support every platform, so we fall back to
         # including the stderr output, which is admittedly somewhat redundant.
         fs.write_text_file(filename, log if log else error)

Tools/Scripts/webkitpy/layout_tests/port/test.py
@@ -46,6 +46,7 @@ class TestInstance(object):
         self.name = name
         self.base = name[(name.rfind("/") + 1):name.rfind(".html")]
         self.crash = False
+        self.web_process_crash = False
         self.exception = False
         self.hang = False
         self.keyboard = False
@@ -149,6 +150,8 @@ layer at (0,0) size 800x34
 """, expected_text=None)
     tests.add('failures/unexpected/crash.html', crash=True)
     tests.add('failures/unexpected/crash-with-stderr.html', crash=True,
+              error="mock-std-error-output")
+    tests.add('failures/unexpected/web-process-crash-with-stderr.html', web_process_crash=True,
               error="mock-std-error-output")
     tests.add('failures/unexpected/text-image-checksum.html',
               actual_text='text-image-checksum_fail-txt',
@@ -502,8 +505,14 @@ class TestDriver(Driver):
         audio = None
         if test.actual_audio:
             audio = base64.b64decode(test.actual_audio)
+        crashed_process_name = None
+        if test.crash:
+            crashed_process_name = self._port.driver_name()
+        elif test.web_process_crash:
+            crashed_process_name = 'WebProcess'
         return DriverOutput(test.actual_text, test.actual_image,
-            test.actual_checksum, audio, crash=test.crash,
+            test.actual_checksum, audio, crash=test.crash or test.web_process_crash,
+            crashed_process_name=crashed_process_name,
             test_time=time.time() - start_time, timeout=test.timeout, error=test.error)
 
     def stop(self):

Tools/Scripts/webkitpy/layout_tests/run_webkit_tests_integrationtest.py
@@ -376,7 +376,7 @@ class MainTest(unittest.TestCase):
 
     def test_run_singly_actually_runs_tests(self):
         res, _, _, _ = logging_run(['--run-singly', 'failures/unexpected'])
-        self.assertEquals(res, 6)
+        self.assertEquals(res, 7)
 
     def test_single_file(self):
         # FIXME: We should consider replacing more of the get_tests_run()-style tests
@@ -426,7 +426,7 @@ class MainTest(unittest.TestCase):
 
         # Update this magic number if you add an unexpected test to webkitpy.layout_tests.port.test
         # FIXME: It's nice to have a routine in port/test.py that returns this number.
-        unexpected_tests_count = 6
+        unexpected_tests_count = 7
 
         self.assertEqual(res, unexpected_tests_count)
         self.assertFalse(out.empty())
@@ -505,6 +505,22 @@ class MainTest(unittest.TestCase):
         if sys.platform != "darwin":
             expected_crash_log = "mock-std-error-output"
         self.assertEquals(fs.read_text_file('/tmp/layout-test-results/failures/unexpected/crash-with-stderr-crash-log.txt'), expected_crash_log)
+
+    def test_web_process_crash_log(self):
+        mock_crash_report = 'mock-crash-report'
+        fs = unit_test_filesystem()
+        fs.write_text_file('/Users/mock/Library/Logs/DiagnosticReports/WebProcess_2011-06-13-150719_quadzen.crash', mock_crash_report)
+        res, buildbot_output, regular_output, user = logging_run([
+                'failures/unexpected/web-process-crash-with-stderr.html',
+            ],
+            tests_included=True,
+            record_results=True,
+            filesystem=fs)
+        expected_crash_log = mock_crash_report
+        # Currently CrashLog uploading only works on Darwin.
+        if sys.platform != "darwin":
+            expected_crash_log = "mock-std-error-output"
+        self.assertEquals(fs.read_text_file('/tmp/layout-test-results/failures/unexpected/web-process-crash-with-stderr-crash-log.txt'), expected_crash_log)
 
     def test_exit_after_n_failures_upload(self):
         fs = unit_test_filesystem()
@@ -741,8 +757,8 @@ class EndToEndTest(unittest.TestCase):
         fs = unit_test_filesystem()
         res, out, err, user = logging_run(record_results=True, tests_included=True, filesystem=fs)
 
-        # Six tests should fail, so the return code should be 6.
-        self.assertEquals(res, 6)
+        # Seven tests should fail, so the return code should be 7.
+        self.assertEquals(res, 7)
         results = self.parse_full_results(fs.files['/tmp/layout-test-results/full_results.json'])
 
         # Check to ensure we're passing back image diff %age correctly.

commit 21b949af3b7bdad3c71aa1ab06c553cc6a63bf61

Fix a crash-inducing typo I introduced in r100648
Fixes <http://webkit.org/b/72642> REGRESSION (r100648): NRWT crashes on Chromium when
handling a crashing test

Reviewed by Adam Barth.

* Scripts/webkitpy/layout_tests/port/chromium.py:
(ChromiumDriver.run_test): Get the driver name from the port.

* Scripts/webkitpy/layout_tests/port/chromium_unittest.py:
(ChromiumDriverTest.test_crashed_process_name): Simulate a crash and check that we got the
right crashed process name.

Identifier: 89159@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@100657 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -594,7 +594,7 @@ class ChromiumDriver(Driver):
         # FIXME: We really should properly handle the stderr output separately.
         if crash:
             error = error + str(text)
-            crashed_process_name = self.driver_name()
+            crashed_process_name = self._port.driver_name()
 
         return DriverOutput(text, output_image, actual_checksum, audio=audio_bytes,
             crash=crash, crashed_process_name=crashed_process_name, test_time=run_time, timeout=timeout, error=error)

Tools/Scripts/webkitpy/layout_tests/port/chromium_unittest.py
@@ -45,6 +45,7 @@ import chromium_win
 
 from webkitpy.layout_tests.models.test_configuration import TestConfiguration
 from webkitpy.layout_tests.port import port_testcase
+from webkitpy.layout_tests.port.driver import DriverInput
 
 
 class ChromiumDriverTest(unittest.TestCase):
@@ -88,6 +89,18 @@ class ChromiumDriverTest(unittest.TestCase):
             raise IOError
         self.driver._proc.stdout.readline = mock_readline
         self._assert_write_command_and_read_line(expected_crash=True)
+
+    def test_crashed_process_name(self):
+        self.driver._proc = Mock()
+
+        # Simulate a crash by having stdout close unexpectedly.
+        def mock_readline():
+            raise IOError
+        self.driver._proc.stdout.readline = mock_readline
+
+        self.driver._port.test_to_uri = lambda test: 'mocktesturi'
+        driver_output = self.driver.run_test(DriverInput(test_name='some/test.html', timeout=1, image_hash=None, is_reftest=False))
+        self.assertEqual(self.driver._port.driver_name(), driver_output.crashed_process_name)
 
     def test_stop(self):
         self.pid = None