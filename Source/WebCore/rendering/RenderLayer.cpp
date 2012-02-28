night-codes/WebKit

commit 7e21e9ab8f86c3fabac41edc91f20bfee54e81cc

Microsoft IE fishtank demo causes assertion in RenderLayer::convertToLayerCoords https://bugs.webkit.org/show_bug.cgi?id=61964
Reviewed by James Robinson.

The assertion is caused with the following callstack:
WebCore::RenderLayer::convertToLayerCoords
WebCore::RenderLayerCompositor::layerWillBeRemoved
WebCore::RenderLayer::removeChild
WebCore::RenderLayer::removeOnlyThisLayer

WebCore::RenderLayer::removeOnlyThisLayer removes itself from the parent
before moving its children to its parent. When WebCore::RenderLayer::convertToLayerCoords
is called for one of the children, it tries to walk to root only to stop at the immediate
parent which was disconnected from the tree in WebCore::RenderLayer::removeOnlyThisLayer.
If removal of layer is delayed until the children has been moved, the ASSERT is avoided.

* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::removeOnlyThisLayer):


Identifier: 96441@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@108659 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderLayer.cpp
@@ -1193,22 +1193,16 @@ void RenderLayer::removeOnlyThisLayer()
     // Dirty the clip rects.
     clearClipRectsIncludingDescendants();
 
-    // Remove us from the parent.
-    RenderLayer* parent = m_parent;
     RenderLayer* nextSib = nextSibling();
     bool hasLayerOffset;
     const LayoutPoint offsetFromRootBeforeMove = computeOffsetFromRoot(hasLayerOffset);
-    parent->removeChild(this);
-    
-    if (reflection())
-        removeChild(reflectionLayer());
 
     // Now walk our kids and reattach them to our parent.
     RenderLayer* current = m_first;
     while (current) {
         RenderLayer* next = current->nextSibling();
         removeChild(current);
-        parent->addChild(current, nextSib);
+        m_parent->addChild(current, nextSib);
         current->setRepaintStatus(NeedsFullRepaint);
         LayoutPoint offsetFromRoot = offsetFromRootBeforeMove;
         // updateLayerPositions depends on hasLayer() already being false for proper layout.
@@ -1216,6 +1210,11 @@ void RenderLayer::removeOnlyThisLayer()
         current->updateLayerPositions(hasLayerOffset ? &offsetFromRoot : 0);
         current = next;
     }
+
+    // Remove us from the parent.
+    if (reflection())
+        removeChild(reflectionLayer());
+    m_parent->removeChild(this);
 
     m_renderer->destroyLayer();
 }

commit 15afefa7eb94327ee7577a243f56c9c0cc04c5aa

Heap-use-after-free in WebCore::RenderLayer::addChild https://bugs.webkit.org/show_bug.cgi?id=79698
Reviewed by Simon Fraser.

Source/WebCore:

This patch fixes a regression introduced in r108659.
The reflection layer was moved to the parent by mistake. It was then
deleted and the parent was left holding on to a deleted pointer. This
patch restores the location where reflection layer is removed - before
moving the child layers.

Test: fast/reflections/toggle-reflection-crash.html

* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::removeOnlyThisLayer):

LayoutTests:

* fast/reflections/toggle-reflection-crash-expected.txt: Added.
* fast/reflections/toggle-reflection-crash.html: Added.


Identifier: 96885@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@109125 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderLayer.cpp
@@ -1197,6 +1197,11 @@ void RenderLayer::removeOnlyThisLayer()
     bool hasLayerOffset;
     const LayoutPoint offsetFromRootBeforeMove = computeOffsetFromRoot(hasLayerOffset);
 
+    // Remove the child reflection layer before moving other child layers.
+    // The reflection layer should not be moved to the parent.
+    if (reflection())
+        removeChild(reflectionLayer());
+
     // Now walk our kids and reattach them to our parent.
     RenderLayer* current = m_first;
     while (current) {
@@ -1212,10 +1217,7 @@ void RenderLayer::removeOnlyThisLayer()
     }
 
     // Remove us from the parent.
-    if (reflection())
-        removeChild(reflectionLayer());
     m_parent->removeChild(this);
-
     m_renderer->destroyLayer();
 }
 

LayoutTests/fast/reflections/toggle-reflection-crash-expected.txt
@@ -1,0 +1,3 @@
+This test should not crash when run with Address Sanitizer.
+
+

LayoutTests/fast/reflections/toggle-reflection-crash.html
@@ -1,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<script>
+  var reflectionOn = true;
+  function toggleReflection() {
+    reflectionOn = !reflectionOn;
+    var box = document.getElementById('box');
+    box.style.webkitBoxReflect = reflectionOn ? 'below' : 'none';
+  };
+
+  function finishTest() {
+    toggleReflection();
+    if (window.layoutTestController)
+      layoutTestController.notifyDone();
+  };
+
+  function startTest() {
+    if (window.layoutTestController) {
+      layoutTestController.waitUntilDone();
+      layoutTestController.dumpAsText();
+    }
+    toggleReflection();
+    window.setTimeout(function() { finishTest(); }, 0);
+  };
+
+  window.addEventListener('load', startTest, false);
+
+</script>
+</head>
+
+<body>
+  <p>This test should not crash when run with Address Sanitizer.</p>
+  <div id="box" style="-webkit-box-reflect: below;"</div>
+</body>
+</html>