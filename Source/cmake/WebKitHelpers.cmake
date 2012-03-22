night-codes/WebKit

commit dfe283dd11d8a8f91c085b66214b97dc63182f96

[CMake] Unreviewed, fix build on 64-bit platforms with SHARED_CORE=ON after r109197.
This partially reverts r66714, in that whether -fPIC is passed to
the compiler does not depend on the value of SHARED_CORE anymore:
since we always build WTF as a static library, it will always need
-fPIC because our shared libraries are built with it.

* Source/cmake/WebKitHelpers.cmake: Always pass -fPIC to
STATIC_LIBRARY targets.

Identifier: 97045@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@109292 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/cmake/WebKitHelpers.cmake
@@ -22,7 +22,12 @@ MACRO(WEBKIT_SET_EXTRA_COMPILER_FLAGS _target)
     ENDIF ()
 
     IF (NOT SHARED_CORE)
-        SET(OLD_COMPILE_FLAGS "-fPIC -fvisibility=hidden ${OLD_COMPILE_FLAGS}")
+        SET(OLD_COMPILE_FLAGS "-fvisibility=hidden ${OLD_COMPILE_FLAGS}")
+    ENDIF ()
+
+    GET_TARGET_PROPERTY(TARGET_TYPE ${_target} TYPE)
+    IF (${TARGET_TYPE} STREQUAL "STATIC_LIBRARY") # -fPIC is automatically added to shared libraries
+        SET(OLD_COMPILE_FLAGS "-fPIC")
     ENDIF ()
 
     SET(OLD_COMPILE_FLAGS "-fno-exceptions -fno-strict-aliasing ${OLD_COMPILE_FLAGS}")

commit 0d89a4545c7297089966f20bd9633793ac08a5b8

OLD_COMPILE_FLAGS was set incorrectly in WebKitHelpers.cmake https://bugs.webkit.org/show_bug.cgi?id=81961
Patch by Ming Xie <mxie@rim.com> on 2012-03-22
Reviewed by Rob Buis.

This fixes the regression introduced by r109292

* Source/cmake/WebKitHelpers.cmake:

Identifier: 99257@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@111769 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/cmake/WebKitHelpers.cmake
@@ -27,7 +27,7 @@ MACRO(WEBKIT_SET_EXTRA_COMPILER_FLAGS _target)
 
     GET_TARGET_PROPERTY(TARGET_TYPE ${_target} TYPE)
     IF (${TARGET_TYPE} STREQUAL "STATIC_LIBRARY") # -fPIC is automatically added to shared libraries
-        SET(OLD_COMPILE_FLAGS "-fPIC")
+        SET(OLD_COMPILE_FLAGS "-fPIC ${OLD_COMPILE_FLAGS}")
     ENDIF ()
 
     SET(OLD_COMPILE_FLAGS "-fno-exceptions -fno-strict-aliasing ${OLD_COMPILE_FLAGS}")