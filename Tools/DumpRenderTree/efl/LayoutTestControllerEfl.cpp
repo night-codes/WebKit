night-codes/WebKit

commit 8fc33a0447878ddc21b44e16f6d740c04214deb5

[EFL][DRT] Normalize file:///tmp/LayoutTests in LayoutTestController::pathToLocalResource() https://bugs.webkit.org/show_bug.cgi?id=67255
Patch by Joshua Lock <joshua.lock@intel.com> on 2012-06-07
Reviewed by Dirk Pranke.

Tools:

Implement pathToLocalResource to redirect file:///tmp URL's to
DUMPRENDERTREE_TEMP and file:///tmp/LayoutTests to point to the
LayoutTests sub-directory of the WebKit source tree.

* DumpRenderTree/efl/LayoutTestControllerEfl.cpp:
(LayoutTestController::pathToLocalResource):

LayoutTests:

* platform/efl/TestExpectations: unskip
fast/dom/frame-loading-via-document-write.html now that
LayoutTestController::pathToLocalResource() is implemented for EFL.

Identifier: 106426@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119788 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/DumpRenderTree/efl/LayoutTestControllerEfl.cpp
@@ -7,6 +7,7 @@
  * Copyright (C) 2010 Joone Hur <joone@kldp.org>
  * Copyright (C) 2011 ProFUSION Embedded Systems
  * Copyright (C) 2011 Samsung Electronics
+ * Copyright (C) 2012 Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -177,8 +178,28 @@ void LayoutTestController::notifyDone()
 
 JSStringRef LayoutTestController::pathToLocalResource(JSContextRef context, JSStringRef url)
 {
-    // Function introduced in r28690. This may need special-casing on Windows.
-    return JSStringRetain(url); // Do nothing on Unix.
+    String requestedUrl(url->characters());
+    String resourceRoot;
+    String requestedRoot;
+
+    if (requestedUrl.find("LayoutTests") != notFound) {
+        // If the URL contains LayoutTests we need to remap that to
+        // LOCAL_RESOURCE_ROOT which is the path of the LayoutTests directory
+        // within the WebKit source tree.
+        requestedRoot = "/tmp/LayoutTests";
+        resourceRoot = getenv("LOCAL_RESOURCE_ROOT");
+    } else if (requestedUrl.find("tmp") != notFound) {
+        // If the URL is a child of /tmp we need to convert it to be a child
+        // DUMPRENDERTREE_TEMP replace tmp with DUMPRENDERTREE_TEMP
+        requestedRoot = "/tmp";
+        resourceRoot = getenv("DUMPRENDERTREE_TEMP");
+    }
+
+    size_t indexOfRootStart = requestedUrl.reverseFind(requestedRoot);
+    size_t indexOfSeparatorAfterRoot = indexOfRootStart + requestedRoot.length();
+    String fullPathToUrl = "file://" + resourceRoot + requestedUrl.substring(indexOfSeparatorAfterRoot);
+
+    return JSStringCreateWithUTF8CString(fullPathToUrl.utf8().data());
 }
 
 void LayoutTestController::queueLoad(JSStringRef url, JSStringRef target)

LayoutTests/platform/efl/TestExpectations
@@ -224,9 +224,6 @@ BUGWK84333 : http/tests/local/fileapi/send-sliced-dragged-file.html = TEXT
 BUGWK84333 : http/tests/local/formdata = TEXT
 BUGWK84333 : http/tests/security/clipboard/clipboard-file-access.html = TEXT
 
-// LayoutTestController.pathToLocalResource needs special handling of file URLs to /tmp.
-BUGWK84336 : fast/dom/frame-loading-via-document-write.html = TIMEOUT TEXT
-
 // Missing execCommand('copy') and execCommand('paste').
 BUGWK84338 : editing/deleting/paste-with-transparent-background-color.html = TEXT
 BUGWK84338 : editing/execCommand/find-after-replace.html = TEXT MISSING

commit 13b23988844fe79be88e4a9fae26e543486a994c

[EFL] REGRESSION (r119788): tests rely on pathToLocalResource are failing after r119788 https://bugs.webkit.org/show_bug.cgi?id=88661
Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-06-11
Reviewed by Dirk Pranke.

Tools:

Pass string length explicitly when creating String object from
non-null-terminated UChar* returned by characters().

* DumpRenderTree/efl/LayoutTestControllerEfl.cpp:
(LayoutTestController::pathToLocalResource):

LayoutTests:

Unskip test cases which rely on pathToLocalResource().

* platform/efl/TestExpectations:

Identifier: 106632@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@119998 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/DumpRenderTree/efl/LayoutTestControllerEfl.cpp
@@ -178,7 +178,7 @@ void LayoutTestController::notifyDone()
 
 JSStringRef LayoutTestController::pathToLocalResource(JSContextRef context, JSStringRef url)
 {
-    String requestedUrl(url->characters());
+    String requestedUrl(url->characters(), url->length());
     String resourceRoot;
     String requestedRoot;
 

LayoutTests/platform/efl/TestExpectations
@@ -684,24 +684,6 @@ BUGWK86633 : editing/spelling/spelling-insert-html.html = TEXT
 BUGWK86633 : editing/spelling/spelling-linebreak.html = TEXT
 BUGWK86633 : editing/spelling/spelling-unified-emulation.html = TEXT
 
-// Tests that rely on pathToLocalResource are failing after r119788
-BUGWK88661 SKIP : fast/dom/frame-loading-via-document-write.html = FAIL
-BUGWK88661 SKIP : fast/loader/local-CSS-from-local.html = FAIL
-BUGWK88661 SKIP : fast/loader/local-JavaScript-from-local.html = FAIL
-BUGWK88661 SKIP : fast/loader/local-iFrame-source-from-local.html = FAIL
-BUGWK88661 SKIP : fast/loader/local-image-from-local.html = FAIL
-BUGWK88661 SKIP : http/tests/plugins/post-url-file.html = FAIL
-BUGWK88661 SKIP : http/tests/security/frame-loading-via-document-write.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-CSS-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-JavaScript-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-iFrame-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-image-from-remote-whitelisted.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-image-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-user-CSS-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-video-poster-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-video-source-from-remote.html = FAIL
-BUGWK88661 SKIP : http/tests/security/local-video-src-from-remote.html = FAIL
-
 // Failing after r119268 on GTK and EFL
 BUGWK88138 : http/tests/media/video-buffered.html = TEXT
 