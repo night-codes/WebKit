night-codes/WebKit

commit 6ece9f4857bac557293f3ed3f99f0ce9d617d572

A couple of UI nits about the flagging feature in results.html https://bugs.webkit.org/show_bug.cgi?id=86798
Reviewed by Dirk Pranke.

-Clicking the flag unflags it now
-There's a flag all link for each test list
-Make the flag list always show up at the bottom of the viewport
when there are flagged tests. This makes it both less annoying
and easier to understand what's going on when you flag a test.
This required wrapping all the content in a div so that we could
make the body be a flexbox.
* fast/harness/resources/results-test.js:
(runSingleRowTest):
* fast/harness/results.html:

Identifier: 109341@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@122880 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/harness/resources/results-test.js
@@ -105,7 +105,7 @@ function runSingleRowTest(results, isExpected, textResults, imageResults)
         else
             assertTrue(document.querySelector('tbody').className.indexOf('expected') == -1);
 
-        assertTrue(document.querySelector('tbody td:nth-child(1)').textContent == '+' + test);
+        assertTrue(document.querySelector('tbody td:nth-child(1)').textContent == '+' + test + ' \u2691');
         assertTrue(document.querySelector('tbody td:nth-child(2)').textContent == textResults);
         assertTrue(document.querySelector('tbody td:nth-child(3)').textContent == imageResults);
         assertTrue(document.querySelector('tbody td:nth-child(4)').textContent == actual);
@@ -684,22 +684,22 @@ function runTests()
     subtree['bar-missing.html'].is_missing_text = true;
     runTest(results, function() {
         var titles = document.getElementsByTagName('h1');
-        assertTrue(titles[0].textContent == 'Tests that crashed (1):');
-        assertTrue(titles[1].textContent == 'Tests that failed text/pixel/audio diff (3):');
-        assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1):');
-        assertTrue(titles[3].textContent == 'Tests that timed out (0):');
-        assertTrue(titles[4].textContent == 'Tests that had stderr output (1):');
-        assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1):');
+        assertTrue(titles[0].textContent == 'Tests that crashed (1): flag all');
+        assertTrue(titles[1].textContent == 'Tests that failed text/pixel/audio diff (3): flag all');
+        assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1): flag all');
+        assertTrue(titles[3].textContent == 'Tests that timed out (0): flag all');
+        assertTrue(titles[4].textContent == 'Tests that had stderr output (1): flag all');
+        assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1): flag all');
 
         document.getElementById('unexpected-results').checked = false;
         document.getElementById('unexpected-results').onchange();
 
-        assertTrue(titles[0].textContent == 'Tests that crashed (2):');
-        assertTrue(titles[1].textContent == 'Tests that failed text/pixel/audio diff (5):');
-        assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1):');
-        assertTrue(titles[3].textContent == 'Tests that timed out (1):');
-        assertTrue(titles[4].textContent == 'Tests that had stderr output (1):');
-        assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1):');
+        assertTrue(titles[0].textContent == 'Tests that crashed (2): flag all');
+        assertTrue(titles[1].textContent == 'Tests that failed text/pixel/audio diff (5): flag all');
+        assertTrue(titles[2].textContent == 'Tests that had no expected results (probably new) (1): flag all');
+        assertTrue(titles[3].textContent == 'Tests that timed out (1): flag all');
+        assertTrue(titles[4].textContent == 'Tests that had stderr output (1): flag all');
+        assertTrue(titles[5].textContent == 'Tests expected to fail but passed (1): flag all');
     });
 
     results = mockResults();
@@ -710,11 +710,32 @@ function runTests()
     subtree['bar-3.html'] = mockExpectation('PASS TEXT', 'TEXT PASS');
     runTest(results, function() {
         var titles = document.getElementsByTagName('h1');
-        assertTrue(titles[0].textContent == 'Tests that failed text/pixel/audio diff (1):');
-        assertTrue(titles[1].textContent =='Flaky tests (failed the first run and passed on retry) (1):');
+        assertTrue(titles[0].textContent == 'Tests that failed text/pixel/audio diff (1): flag all');
+        assertTrue(titles[1].textContent =='Flaky tests (failed the first run and passed on retry) (1): flag all');
 
         assertTrue(document.querySelectorAll('#results-table tbody').length == 2);
         assertTrue(document.querySelectorAll('#flaky-tests-table tbody').length == 2);
+    });
+
+    results = mockResults();
+    var subtree = results.tests['foo'] = {}
+    subtree['bar.html'] = mockExpectation('TEXT', 'IMAGE');
+    subtree['bar1.html'] = mockExpectation('TEXT', 'TEXT');
+    subtree['bar2.html'] = mockExpectation('TEXT', 'TEXT');
+    runTest(results, function() {
+        var flaggedTestsTextbox = document.getElementById('flagged-tests');
+
+        flagAll(document.querySelector('.flag-all'));
+        assertTrue(flaggedTestsTextbox.innerText == 'foo/bar.html');
+
+        document.getElementById('unexpected-results').checked = false;
+        document.getElementById('unexpected-results').onchange();
+
+        flagAll(document.querySelector('.flag-all'));
+        assertTrue(flaggedTestsTextbox.innerText == 'foo/bar.html\nfoo/bar1.html\nfoo/bar2.html');
+
+        unflag(document.querySelector('.flag'));
+        assertTrue(flaggedTestsTextbox.innerText == 'foo/bar1.html\nfoo/bar2.html');
     });
 
     document.body.innerHTML = '<pre>' + g_log.join('\n') + '</pre>';

LayoutTests/fast/harness/results-expected.txt
@@ -221,3 +221,6 @@ TEST-38: PASS
 TEST-38: PASS
 TEST-38: PASS
 TEST-38: PASS
+TEST-39: PASS
+TEST-39: PASS
+TEST-39: PASS

LayoutTests/fast/harness/results.html
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <style>
+html {
+    height: 100%;
+}
 body {
-    margin: 4px;
+    margin: 0;
     font-family: Helvetica, sans-serif;
     font-size: 11pt;
+    display: -webkit-flex;
+    -webkit-flex-direction: column;
+    height: 100%;
 }
 
-body > p:first-of-type {
-    margin-top: 0;
+body > * {
+    margin-left: 4px;
+    margin-top: 4px;
 }
 
 h1 {
@@ -42,6 +49,12 @@ th:empty, td:empty {
 th {
     -webkit-user-select: none;
     -moz-user-select: none;
+}
+
+.content-container {
+    -webkit-flex: 1;
+    min-height: 0;
+    overflow: auto;
 }
 
 .note {
@@ -98,8 +111,12 @@ th {
     left: 1px;
 }
 
-.test-link.flagged:after {
-    content: ' \2691';
+tbody .flag {
+    display: none;
+}
+
+tbody.flagged .flag {
+    display: inline;
 }
 
 .stopped-running-early-message {
@@ -193,6 +210,7 @@ th {
 
 #flagged-tests {
     padding: 5px;
+    height: 100px;
 }
 </style>
 <style id="unexpected-style"></style>
@@ -252,8 +270,6 @@ function parentOfType(node, selector)
     }
     return null;
 }
-
-
 
 function remove(node)
 {
@@ -388,6 +404,14 @@ function async(func, args)
     setTimeout(function() { func.apply(null, args); }, 100);
 }
 
+function visibleTests(container)
+{
+    if (onlyShowUnexpectedFailures())
+        return container.querySelectorAll('tbody:not(.expected)');
+    else
+        return container.querySelectorAll('tbody');
+}
+
 function visibleExpandLinks()
 {
     if (onlyShowUnexpectedFailures())
@@ -428,9 +452,16 @@ function testLink(test)
     return '<a class=test-link href="' + basePath + test + '">' + test + '</a>';
 }
 
+function unflag(flag)
+{
+    var shouldFlag = false;
+    TestNavigator.flagTest(parentOfType(flag, 'tbody'), shouldFlag);
+}
+
 function testLinkWithExpandButton(test)
 {
-    return '<span class=expand-button onclick="toggleExpectations(this)"><span class=expand-button-text>+</span></span>' + testLink(test);
+    return '<span class=expand-button onclick="toggleExpectations(this)"><span class=expand-button-text>+</span></span>' + testLink(test) +
+        '<span class=flag onclick="unflag(this)"> \u2691</span>';
 }
 
 function resultLink(testPrefix, suffix, contents)
@@ -643,9 +674,18 @@ function updateTestlistCounts()
     })
 }
 
+function flagAll(headerLink)
+{
+    var tests = visibleTests(parentOfType(headerLink, 'div'));
+    forEach(tests, function(tests) {
+        var shouldFlag = true;
+        TestNavigator.flagTest(tests, shouldFlag);
+    })
+}
+
 function testListHeaderHtml(header)
 {
-    return '<h1>' + header + ' (<span class=test-list-count></span>):</h1>';
+    return '<h1>' + header + ' (<span class=test-list-count></span>): <a href="#" class=flag-all onclick="flagAll(this)">flag all</a></h1>';
 }
 
 function testList(tests, header, tableId)
@@ -1007,15 +1047,21 @@ TestNavigator._toggleCurrentTest = function()
 
 TestNavigator._toggleCurrentTestFlagged = function()
 {
-    var testLink = parentOfType(TestNavigator._currentTestLink(), 'tbody').querySelector('.test-link');
-    var testName = testLink.innerText;
+    var testLink = parentOfType(TestNavigator._currentTestLink(), 'tbody');
+    TestNavigator.flagTest(testLink, !testLink.classList.contains('flagged'));
+}
+
+// FIXME: Test navigator shouldn't know anything about flagging. It should probably call out to TestFlagger or something.
+TestNavigator.flagTest = function(testTbody, shouldFlag)
+{
+    var testName = testTbody.querySelector('.test-link').innerText;
     
-    if (testLink.classList.contains('flagged')) {
-        testLink.classList.remove('flagged');
-        delete TestNavigator.flaggedTests[testName];
-    } else {
-        testLink.classList.add('flagged');
+    if (shouldFlag) {
+        testTbody.classList.add('flagged');
         TestNavigator.flaggedTests[testName] = 1;
+    } else {
+        testTbody.classList.remove('flagged');
+        delete TestNavigator.flaggedTests[testName];
     }
 
     TestNavigator.updateFlaggedTests();
@@ -1258,7 +1304,7 @@ function generatePage()
 {
     forEachTest(processGlobalStateFor);
 
-    var html = '<div id=toolbar>' +
+    var html = '<div class=content-container><div id=toolbar>' +
         '<div class="note">Use the i, j, k and l keys to navigate, e, c to expand and collapse, and f to flag</div>' +
         '<a href="javascript:void()" onclick="expandAllExpectations()">expand all</a> ' +
         '<a href="javascript:void()" onclick="collapseAllExpectations()">collapse all</a> ' +
@@ -1298,7 +1344,7 @@ function generatePage()
             '<p>httpd error log: <a href="error_log.txt">error_log.txt</a></p>';
     }
 
-    document.body.innerHTML = html;
+    document.body.innerHTML = html + '</div>';
 
     if (document.getElementById('results-table')) {
         document.getElementById('results-table').addEventListener('click', TableSorter.handleClick, false);

commit 0575ab6443248386f870177cfbf4b0251813b64b

i and j keys no longer scroll in results.html https://bugs.webkit.org/show_bug.cgi?id=92584
Reviewed by Tony Chang.

Scrolling broke with r122880 since the scrolling element is no
longer the body.

Also, fix TestNavigator to allow navigating to tests in the
unexpected passes table. That table doesn't have expand links,
so we change TestNavigator to navigate the tbody's instead, which
also happens to remove a bunch of unnecessary tree-walking.

* fast/harness/resources/results-test.js:
* fast/harness/results-expected.txt:
* fast/harness/results.html:


Identifier: 110662@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@124287 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/harness/resources/results-test.js
@@ -552,7 +552,7 @@ function runTests()
     runTest(results, function() {
         if (window.eventSender) {
             eventSender.keyDown('k'); // previous
-            var testRows = document.querySelectorAll('#results-table tbody tr');
+            var testRows = document.querySelectorAll('#results-table tbody');
             assertTrue(!testRows[0].classList.contains('current'));
             assertTrue(!testRows[1].classList.contains('current'));
             assertTrue(testRows[2].classList.contains('current'));
@@ -562,12 +562,20 @@ function runTests()
     runTest(results, function() {
         if (window.eventSender) {
             eventSender.keyDown('j'); // next
-            var testRows = document.querySelectorAll('#results-table tbody tr');
+            var testRows = document.querySelectorAll('#results-table tbody');
             assertTrue(testRows[0].classList.contains('current'));
             assertTrue(!testRows[1].classList.contains('current'));
             assertTrue(!testRows[2].classList.contains('current'));
         }
     });
+
+    results = mockResults();
+    var subtree = results.tests['foo'] = {}
+    subtree['bar.html'] = mockExpectation('TEXT', 'FAIL');
+    subtree['bar1.html'] = mockExpectation('TEXT', 'FAIL');
+    subtree['bar2.html'] = mockExpectation('TEXT', 'FAIL');
+    subtree['bar3.html'] = mockExpectation('TEXT', 'PASS');
+    subtree['bar4.html'] = mockExpectation('IMAGE', 'PASS');
 
     runTest(results, function() {
         assertTrue(document.getElementById('results-table'));
@@ -577,7 +585,7 @@ function runTests()
             eventSender.keyDown('i', ["metaKey"]);
             eventSender.keyDown('i', ["shiftKey"]);
             eventSender.keyDown('i', ["ctrlKey"]);
-            var testRows = document.querySelectorAll('#results-table tbody tr');
+            var testRows = document.querySelectorAll('tbody');
             assertTrue(!testRows[0].classList.contains('current'));
             assertTrue(!testRows[1].classList.contains('current'));
             assertTrue(!testRows[2].classList.contains('current'));
@@ -621,7 +629,13 @@ function runTests()
             eventSender.keyDown('l'); // last
             assertTrue(!testRows[0].classList.contains('current'));
             assertTrue(!testRows[1].classList.contains('current'));
-            assertTrue(testRows[2].classList.contains('current'));
+            assertTrue(testRows[4].classList.contains('current'));
+
+            var flaggedTestsTextbox = document.getElementById('flagged-tests');
+
+            eventSender.keyDown('f'); // flag
+            assertTrue(flaggedTestsTextbox.innerText == 'foo/bar4.html');
+            eventSender.keyDown('f'); // unflag
 
             eventSender.keyDown('i'); // first
 
@@ -645,7 +659,6 @@ function runTests()
             eventSender.keyDown('f', ["metaKey"]);
             eventSender.keyDown('f', ["shiftKey"]);
             eventSender.keyDown('f', ["ctrlKey"]);
-            var flaggedTestsTextbox = document.getElementById('flagged-tests');
             assertTrue(flaggedTestsTextbox.innerText == '');
 
             eventSender.keyDown('f'); // flag

LayoutTests/fast/harness/results-expected.txt
@@ -205,6 +205,7 @@ TEST-36: PASS
 TEST-36: PASS
 TEST-36: PASS
 TEST-36: PASS
+TEST-36: PASS
 TEST-37: PASS
 TEST-37: PASS
 TEST-37: PASS

LayoutTests/fast/harness/results.html
@@ -404,8 +404,9 @@ function async(func, args)
     setTimeout(function() { func.apply(null, args); }, 100);
 }
 
-function visibleTests(container)
+function visibleTests(opt_container)
 {
+    var container = opt_container || document;
     if (onlyShowUnexpectedFailures())
         return container.querySelectorAll('tbody:not(.expected)');
     else
@@ -449,7 +450,7 @@ function testLink(test)
         basePath += '/trunk/LayoutTests/';
     } else
         basePath = globalState().results.layout_tests_dir + '/';
-    return '<a class=test-link href="' + basePath + test + '">' + test + '</a>';
+    return '<a class=test-link href="' + basePath + test + '">' + test + '</a><span class=flag onclick="unflag(this)"> \u2691</span>';
 }
 
 function unflag(flag)
@@ -460,8 +461,7 @@ function unflag(flag)
 
 function testLinkWithExpandButton(test)
 {
-    return '<span class=expand-button onclick="toggleExpectations(this)"><span class=expand-button-text>+</span></span>' + testLink(test) +
-        '<span class=flag onclick="unflag(this)"> \u2691</span>';
+    return '<span class=expand-button onclick="toggleExpectations(this)"><span class=expand-button-text>+</span></span>' + testLink(test);
 }
 
 function resultLink(testPrefix, suffix, contents)
@@ -1003,7 +1003,7 @@ TestNavigator._scrollToFirstTest = function()
 
 TestNavigator._scrollToLastTest = function()
 {
-    var links = visibleExpandLinks();
+    var links = visibleTests();
     if (TestNavigator._setCurrentTest(links.length - 1))
         TestNavigator._scrollToCurrentTest();
 }
@@ -1026,28 +1026,33 @@ TestNavigator._scrollToPreviousTest = function()
 
 TestNavigator._currentTestLink = function()
 {
-    var links = visibleExpandLinks();
+    var links = visibleTests();
     return links[TestNavigator.currentTestIndex];
+}
+
+TestNavigator._currentTestExpandLink = function()
+{
+    return TestNavigator._currentTestLink().querySelector('.expand-button-text');
 }
 
 TestNavigator._expandCurrentTest = function()
 {
-    expandExpectations(TestNavigator._currentTestLink());
+    expandExpectations(TestNavigator._currentTestExpandLink());
 }
 
 TestNavigator._collapseCurrentTest = function()
 {
-    collapseExpectations(TestNavigator._currentTestLink());
+    collapseExpectations(TestNavigator._currentTestExpandLink());
 }
 
 TestNavigator._toggleCurrentTest = function()
 {
-    toggleExpectations(TestNavigator._currentTestLink());
+    toggleExpectations(TestNavigator._currentTestExpandLink());
 }
 
 TestNavigator._toggleCurrentTestFlagged = function()
 {
-    var testLink = parentOfType(TestNavigator._currentTestLink(), 'tbody');
+    var testLink = TestNavigator._currentTestLink();
     TestNavigator.flagTest(testLink, !testLink.classList.contains('flagged'));
 }
 
@@ -1088,18 +1093,18 @@ TestNavigator.updateFlaggedTests = function()
 
 TestNavigator._setCurrentTest = function(testIndex)
 {
-    var links = visibleExpandLinks();
+    var links = visibleTests();
     if (testIndex < 0 || testIndex >= links.length)
         return false;
 
-    var currExpandLink = links[TestNavigator.currentTestIndex];
-    if (currExpandLink)
-        parentOfType(currExpandLink, 'tr').classList.remove('current');
+    var currentTest = links[TestNavigator.currentTestIndex];
+    if (currentTest)
+        currentTest.classList.remove('current');
 
     TestNavigator.currentTestIndex = testIndex;
 
-    currExpandLink = links[TestNavigator.currentTestIndex];
-    parentOfType(currExpandLink, 'tr').classList.add('current');
+    currentTest = links[TestNavigator.currentTestIndex];
+    currentTest.classList.add('current');
 
     return true;
 }
@@ -1111,23 +1116,24 @@ TestNavigator._scrollToCurrentTest = function()
         return;
 
     var rowRect = targetLink.getBoundingClientRect();
+    var container = document.querySelector('.content-container');
     // rowRect is in client coords (i.e. relative to viewport), so we just want to add its top to the current scroll position.
-    window.scrollTo(window.scrollX, window.scrollY + rowRect.top - 20);
+    container.scrollTop += rowRect.top - 20;
 }
 
 TestNavigator.onlyShowUnexpectedFailuresChanged = function()
 {
-    var currentTestLink = document.querySelector('.current .expand-button-text');
-    if (!currentTestLink)
+    var currentTest = document.querySelector('.current');
+    if (!currentTest)
         return;
 
     // If our currentTest became hidden, reset the currentTestIndex.
-    if (onlyShowUnexpectedFailures() && parentOfType(currentTestLink, 'tbody').classList.contains('expected'))
+    if (onlyShowUnexpectedFailures() && currentTest.classList.contains('expected'))
         TestNavigator._scrollToFirstTest();
     else {
         // Recompute TestNavigator.currentTestIndex
-        var links = visibleExpandLinks();
-        TestNavigator.currentTestIndex = links.indexOf(currentTestLink);
+        var links = visibleTests();
+        TestNavigator.currentTestIndex = links.indexOf(currentTest);
         window.console.log('TestNavigator.currentTestIndex is ', TestNavigator.currentTestIndex)
     }
 }