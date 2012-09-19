night-codes/WebKit

commit 6a0cbd354de3be85e20648e16f26bce4aac38bed

Enable AVCF hardware video decoding https://bugs.webkit.org/show_bug.cgi?id=90015 <rdar://problem/10770317>
Reviewed by Anders Carlsson.

Source/WebCore:

* html/HTMLMediaElement.cpp:
(WebCore):
(WebCore::HTMLMediaElement::mediaPlayerGraphicsDeviceAdapter): New, return the client's graphics
    device adapter.
* html/HTMLMediaElement.h:

* page/ChromeClient.h:
(WebCore::ChromeClient::graphicsDeviceAdapter): New.

* platform/graphics/MediaPlayer.cpp:
(WebCore::MediaPlayer::graphicsDeviceAdapter): New, ask the media element for the graphics
    device adapter.
* platform/graphics/MediaPlayer.h:

* platform/graphics/avfoundation/cf/AVFoundationCFSoftLinking.h: Soft-link AVCFPlayerSetDirect3DDevice
    and AVCFPlayerEnableHardwareAcceleratedVideoDecoderKey.

* platform/graphics/avfoundation/cf/MediaPlayerPrivateAVFoundationCF.cpp:
(WebCore::MediaPlayerPrivateAVFoundationCF::createAVAssetForURL): Pass the current d3d9
    device interface to the AVFWrapper.
(WebCore::AVFWrapper::createAssetForURL): If the d3d9 device implements IDirect3DDevice9Ex,
    tell the AVAsset to enable hardware video decoding.
(WebCore::AVFWrapper::createPlayer): Pass the d3d9 device to the player if it implements IDirect3DDevice9Ex.

* platform/graphics/ca/win/CACFLayerTreeHost.h:
(WebCore::CACFLayerTreeHost::graphicsDeviceAdapter): New, default implementation.

* platform/graphics/ca/win/LegacyCACFLayerTreeHost.h:
(WebCore::LegacyCACFLayerTreeHost::graphicsDeviceAdapter): New, default implementation.
* platform/graphics/ca/win/WKCACFViewLayerTreeHost.cpp:
(WebCore::WKCACFViewLayerTreeHost::graphicsDeviceAdapter): New.
* platform/graphics/ca/win/WKCACFViewLayerTreeHost.h:

* platform/win/SoftLinking.h: Define SOFT_LINK_DLL_IMPORT_OPTIONAL, SOFT_LINK_LOADED_LIBRARY,
    and SOFT_LINK_VARIABLE_DLL_IMPORT_OPTIONAL.

Source/WebKit/win:

* WebCoreSupport/WebChromeClient.cpp:
(WebChromeClient::graphicsDeviceAdapter): New, return the view's graphics adapter.
* WebCoreSupport/WebChromeClient.h:

Source/WebCore/platform/graphics/MediaPlayer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008, 2009, 2010, 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -208,7 +208,7 @@ static Vector<MediaPlayerFactory*>& installedMediaEngines()
         enginesQueried = true;
 
 #if USE(AVFOUNDATION)
-        if (Settings::isAVFoundationEnabled()) {
+        if (1 /* @@Settings::isAVFoundationEnabled() @@ */) {
 #if PLATFORM(MAC)
             MediaPlayerPrivateAVFoundationObjC::registerMediaEngine(addMediaEngine);
 #elif PLATFORM(WIN)
@@ -1067,6 +1067,16 @@ String MediaPlayer::userAgent() const
     
     return m_mediaPlayerClient->mediaPlayerUserAgent();
 }
+
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+GraphicsDeviceAdapter* MediaPlayer::graphicsDeviceAdapter() const
+{
+    if (!m_mediaPlayerClient)
+        return 0;
+    
+    return m_mediaPlayerClient->mediaPlayerGraphicsDeviceAdapter(this);
+}
+#endif
 
 }
 

Source/WebCore/html/HTMLMediaElement.cpp
@@ -3485,6 +3485,16 @@ void HTMLMediaElement::mediaPlayerRenderingModeChanged(MediaPlayer*)
 }
 #endif
 
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+GraphicsDeviceAdapter* HTMLMediaElement::mediaPlayerGraphicsDeviceAdapter(const MediaPlayer*) const
+{
+    if (!document() || !document()->page())
+        return 0;
+
+    return document()->page()->chrome()->client()->graphicsDeviceAdapter();
+}
+#endif
+
 void HTMLMediaElement::mediaPlayerEngineUpdated(MediaPlayer*)
 {
     LOG(Media, "HTMLMediaElement::mediaPlayerEngineUpdated");

Source/WebCore/html/HTMLMediaElement.h
@@ -432,6 +432,10 @@ private:
     virtual bool mediaPlayerNeedsSiteSpecificHacks() const OVERRIDE;
     virtual String mediaPlayerDocumentHost() const OVERRIDE;
 
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+    virtual GraphicsDeviceAdapter* mediaPlayerGraphicsDeviceAdapter(const MediaPlayer*) const OVERRIDE;
+#endif
+
     void loadTimerFired(Timer<HTMLMediaElement>*);
     void progressEventTimerFired(Timer<HTMLMediaElement>*);
     void playbackProgressTimerFired(Timer<HTMLMediaElement>*);

Source/WebCore/page/ChromeClient.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006, 2007, 2008, 2009 Apple, Inc. All rights reserved.
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 Apple, Inc. All rights reserved.
  * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
  * Copyright (C) 2012 Samsung Electronics. All rights reserved.
  *
@@ -80,6 +80,10 @@ namespace WebCore {
 #if ENABLE(INPUT_TYPE_COLOR)
     class ColorChooser;
     class ColorChooserClient;
+#endif
+
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+    struct GraphicsDeviceAdapter;
 #endif
 
     class ChromeClient {
@@ -275,6 +279,10 @@ namespace WebCore {
 
         // Returns a bitfield indicating conditions that can trigger the compositor.
         virtual CompositingTriggerFlags allowedCompositingTriggers() const { return static_cast<CompositingTriggerFlags>(AllTriggers); }
+#endif
+
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+        virtual GraphicsDeviceAdapter* graphicsDeviceAdapter() const { return 0; }
 #endif
 
         virtual bool supportsFullscreenForNode(const Node*) { return false; }

Source/WebCore/platform/graphics/MediaPlayer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007, 2008, 2009, 2010, 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -99,6 +99,10 @@ class MediaPlayer;
 struct MediaPlayerFactory;
 class TimeRanges;
 
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+struct GraphicsDeviceAdapter;
+#endif
+
 class MediaPlayerClient {
 public:
     enum CORSMode { Unspecified, Anonymous, UseCredentials };
@@ -163,6 +167,10 @@ public:
     // called when the media player's rendering mode changed, which indicates a change in the
     // availability of the platformLayer().
     virtual void mediaPlayerRenderingModeChanged(MediaPlayer*) { }
+#endif
+
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+    virtual GraphicsDeviceAdapter* mediaPlayerGraphicsDeviceAdapter(const MediaPlayer*) const { return 0; }
 #endif
 
 #if ENABLE(MEDIA_SOURCE)
@@ -354,6 +362,10 @@ public:
     bool supportsAcceleratedRendering() const;
     // called when the rendering system flips the into or out of accelerated rendering mode.
     void acceleratedRenderingStateChanged();
+#endif
+
+#if PLATFORM(WIN) && USE(AVFOUNDATION)
+    GraphicsDeviceAdapter* graphicsDeviceAdapter() const;
 #endif
 
     bool hasSingleSecurityOrigin() const;

Source/WebCore/platform/graphics/avfoundation/cf/AVFoundationCFSoftLinking.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -177,6 +177,8 @@ SOFT_LINK_DLL_IMPORT(AVFoundationCF, AVCFURLAssetCopyAudiovisualMIMETypes, CFArr
 SOFT_LINK_DLL_IMPORT(AVFoundationCF, AVCFURLAssetCreateWithURLAndOptions, AVCFURLAssetRef, __cdecl, (CFAllocatorRef allocator, CFURLRef URL, CFDictionaryRef options, dispatch_queue_t notificationQueue), (allocator, URL, options, notificationQueue))
 #define AVCFURLAssetCreateWithURLAndOptions softLink_AVCFURLAssetCreateWithURLAndOptions
 
+SOFT_LINK_DLL_IMPORT_OPTIONAL(AVFoundationCF, AVCFPlayerSetDirect3DDevice, void, __cdecl, (AVCFPlayerRef player, IDirect3DDevice9* d3dDevice))
+
 // Variables
 
 SOFT_LINK_VARIABLE_DLL_IMPORT(AVFoundationCF, AVCFAssetImageGeneratorApertureModeCleanAperture, const CFStringRef);
@@ -244,3 +246,6 @@ SOFT_LINK_VARIABLE_DLL_IMPORT(AVFoundationCF, AVCFPlayerItemTracksChangedNotific
 
 SOFT_LINK_VARIABLE_DLL_IMPORT(AVFoundationCF, AVCFPlayerRateChangedNotification, const CFStringRef);
 #define AVCFPlayerRateChangedNotification get_AVCFPlayerRateChangedNotification()
+
+SOFT_LINK_VARIABLE_DLL_IMPORT_OPTIONAL(AVFoundationCF, AVCFPlayerEnableHardwareAcceleratedVideoDecoderKey, const CFStringRef);
+#define AVCFPlayerEnableHardwareAcceleratedVideoDecoderKey get_AVCFPlayerEnableHardwareAcceleratedVideoDecoderKey()

Source/WebCore/platform/graphics/avfoundation/cf/MediaPlayerPrivateAVFoundationCF.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,6 +32,7 @@
 #include "MediaPlayerPrivateAVFoundationCF.h"
 
 #include "ApplicationCacheResource.h"
+#include "COMPtr.h"
 #include "FloatConversion.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
@@ -44,9 +45,9 @@
 #include <AVFoundationCF/AVCFPlayerLayer.h>
 #include <AVFoundationCF/AVFoundationCF.h>
 #include <CoreMedia/CoreMedia.h>
-
 #include <delayimp.h>
 #include <dispatch/dispatch.h>
+#include <WebKitQuartzCoreAdditions/WKCACFTypes.h>
 #include <wtf/HashMap.h>
 #include <wtf/Threading.h>
 #include <wtf/UnusedParam.h>
@@ -87,7 +88,7 @@ public:
     void createAssetForURL(const String& url);
     void setAsset(AVCFURLAssetRef);
     
-    void createPlayer();
+    void createPlayer(IDirect3DDevice9*);
     void createPlayerItem();
     
     void checkPlayability();
@@ -137,6 +138,7 @@ private:
     RefPtr<PlatformCALayer> m_videoLayerWrapper;
 
     OwnPtr<LayerClient> m_layerClient;
+    COMPtr<IDirect3DDevice9Ex> m_d3dDevice;
 };
 
 uintptr_t AVFWrapper::s_nextAVFWrapperObjectID;
@@ -339,7 +341,7 @@ void MediaPlayerPrivateAVFoundationCF::createAVPlayer()
     ASSERT(m_avfWrapper);
     
     setDelayCallbacks(true);
-    m_avfWrapper->createPlayer();
+    m_avfWrapper->createPlayer(reinterpret_cast<IDirect3DDevice9*>(player()->graphicsDeviceAdapter()));
     setDelayCallbacks(false);
 }
 
@@ -355,8 +357,9 @@ void MediaPlayerPrivateAVFoundationCF::createAVPlayerItem()
 void MediaPlayerPrivateAVFoundationCF::createAVAssetForURL(const String& url)
 {
     ASSERT(!m_avfWrapper);
-    
+
     setDelayCallbacks(true);
+
     m_avfWrapper = new AVFWrapper(this);
     m_avfWrapper->createAssetForURL(url);
     setDelayCallbacks(false);
@@ -955,19 +958,35 @@ void AVFWrapper::disconnectAndDeleteAVFWrapper(void* context)
 void AVFWrapper::createAssetForURL(const String& url)
 {
     ASSERT(!avAsset());
-    
+
     RetainPtr<CFURLRef> urlRef(AdoptCF, KURL(ParsedURLString, url).createCFURL());
+
     AVCFURLAssetRef assetRef = AVCFURLAssetCreateWithURLAndOptions(kCFAllocatorDefault, urlRef.get(), 0, m_notificationQueue);
     m_avAsset.adoptCF(assetRef);
 }
 
-void AVFWrapper::createPlayer()
+void AVFWrapper::createPlayer(IDirect3DDevice9* d3dDevice)
 {
     ASSERT(!avPlayer() && avPlayerItem());
 
+    RetainPtr<CFMutableDictionaryRef> optionsRef(AdoptCF, CFDictionaryCreateMutable(kCFAllocatorDefault, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks));
+
+    if (d3dDevice) {
+        // QI for an IDirect3DDevice9Ex interface, it is required to do HW video decoding.
+        COMPtr<IDirect3DDevice9Ex> d3dEx(Query, d3dDevice);
+        m_d3dDevice = d3dEx;
+    } else
+        m_d3dDevice = 0;
+
+    if (m_d3dDevice && AVCFPlayerEnableHardwareAcceleratedVideoDecoderKey)
+        CFDictionarySetValue(optionsRef.get(), AVCFPlayerEnableHardwareAcceleratedVideoDecoderKey, kCFBooleanTrue);
+
     // FIXME: We need a way to create a AVPlayer without an AVPlayerItem, see <rdar://problem/9877730>.
-    AVCFPlayerRef playerRef = AVCFPlayerCreateWithPlayerItemAndOptions(kCFAllocatorDefault, avPlayerItem(), 0, m_notificationQueue);
+    AVCFPlayerRef playerRef = AVCFPlayerCreateWithPlayerItemAndOptions(kCFAllocatorDefault, avPlayerItem(), optionsRef.get(), m_notificationQueue);
     m_avPlayer.adoptCF(playerRef);
+
+    if (m_d3dDevice && AVCFPlayerSetDirect3DDevicePtr())
+        AVCFPlayerSetDirect3DDevicePtr()(playerRef, m_d3dDevice.get());
 
     CFNotificationCenterRef center = CFNotificationCenterGetLocalCenter();
     ASSERT(center);

Source/WebCore/platform/graphics/ca/win/CACFLayerTreeHost.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2009, 2010, 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -46,6 +46,10 @@ struct WKCACFContext;
 
 typedef struct CGImage* CGImageRef;
 
+#if USE(AVFOUNDATION)
+struct GraphicsDeviceAdapter;
+#endif
+
 namespace WebCore {
 
 class CACFLayerTreeHostClient;
@@ -66,6 +70,9 @@ public:
     virtual void resize() = 0;
     void flushPendingGraphicsLayerChangesSoon();
     virtual void setShouldInvertColors(bool);
+#if USE(AVFOUNDATION)
+    virtual GraphicsDeviceAdapter* graphicsDeviceAdapter() const { return 0; }
+#endif
 
     // AbstractCACFLayerTreeHost
     virtual void flushPendingLayerChangesNow();

commit 3386bca6b39b6d131d0be78ef8982888976f0bc7

Check settings before registering AVFoundation media engine. https://bugs.webkit.org/show_bug.cgi?id=97048 <rdar://problem/12313594>
Reviewed by Dan Bernstein.

Fix the bug introduced in r122676.

* platform/graphics/MediaPlayer.cpp:
(WebCore::installedMediaEngines): Uncomment the call to check AVFoundation settings.


Identifier: 115020@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@128964 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/MediaPlayer.cpp
@@ -210,7 +210,7 @@ static Vector<MediaPlayerFactory*>& installedMediaEngines()
         enginesQueried = true;
 
 #if USE(AVFOUNDATION)
-        if (1 /* @@Settings::isAVFoundationEnabled() @@ */) {
+        if (Settings::isAVFoundationEnabled()) {
 #if PLATFORM(MAC)
             MediaPlayerPrivateAVFoundationObjC::registerMediaEngine(addMediaEngine);
 #elif PLATFORM(WIN)