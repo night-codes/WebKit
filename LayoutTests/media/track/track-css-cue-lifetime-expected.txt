night-codes/WebKit

commit 88febf3eada0622bbd2b1d3eeda7433a975e1cd1

[Track] Rendering crash https://bugs.webkit.org/show_bug.cgi?id=106199
Reviewed by Eric Carlson.

Source/WebCore:

The browser is crashing if a text track is added through JavaScript,
due to the fact that the containing block is not marked as inline at
the time RenderTextTrackCue::layout() is called. The fix consists
of ensuring that the pseudo id is set, even if the track isn't
yet visible.

Test: media/track/track-cue-inline-assertion-crash.html

* html/track/TextTrackCue.cpp:
(WebCore::TextTrackCue::updateDisplayTree): The pseudo-id should
be set when the display tree is generated.
(WebCore::TextTrackCue::getDisplayTree): Added extra check to not
do unneccessary logic if the track will never be displayed.

LayoutTests:

* media/track/track-cue-inline-assertion-crash-expected.txt: Added.
* media/track/track-cue-inline-assertion-crash.html: Added.


Identifier: 124429@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@138966 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/track/TextTrackCue.cpp
@@ -695,10 +695,8 @@ void TextTrackCue::updateDisplayTree(float movieTime)
     if (!track()->isRendered())
       return;
 
-
     // Clear the contents of the set.
     m_allDocumentNodes->removeChildren();
-    m_allDocumentNodes->setPseudo(allNodesShadowPseudoId());
 
     // Update the two sets containing past and future WebVTT objects.
     RefPtr<DocumentFragment> referenceTree = getCueAsHTML();
@@ -709,7 +707,7 @@ void TextTrackCue::updateDisplayTree(float movieTime)
 
 PassRefPtr<TextTrackCueBox> TextTrackCue::getDisplayTree()
 {
-    if (!m_displayTreeShouldChange)
+    if (!m_displayTreeShouldChange || !track()->isRendered())
         return m_displayTree;
 
     // 10.1 - 10.10
@@ -727,6 +725,7 @@ PassRefPtr<TextTrackCueBox> TextTrackCue::getDisplayTree()
     // background box.
 
     // Note: This is contained by default in m_allDocumentNodes.
+    m_allDocumentNodes->setPseudo(allNodesShadowPseudoId());
     m_displayTree->appendChild(m_allDocumentNodes, ASSERT_NO_EXCEPTION, true);
 
     // FIXME(BUG 79916): Runs of children of WebVTT Ruby Objects that are not

LayoutTests/media/track/track-cue-inline-assertion-crash-expected.txt
@@ -1,0 +1,10 @@
+Tests that adding a track and changing its mode through JS doesn't crash the browser.
+
+** Add a text track through JS to the video element **
+** Add cue to the text track **
+** Set the track mode to showing **
+
+No crash. PASS.
+
+END OF TEST
+

LayoutTests/media/track/track-cue-inline-assertion-crash.html
@@ -1,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <script src=../media-file.js></script>
+        <script src=../video-test.js></script>
+
+        <script>
+        function startTest()
+        {
+            findMediaElement();
+
+            consoleWrite("** Add a text track through JS to the video element **");
+            var t = video.addTextTrack('captions', 'English', 'en');
+
+            consoleWrite("** Add cue to the text track **");
+            t.addCue(new TextTrackCue(0.0, 10.0, 'wow wow'));
+
+            consoleWrite("** Set the track mode to showing **");
+            t.mode = "showing";
+
+            consoleWrite("");
+            consoleWrite("No crash. PASS.");
+            consoleWrite("");
+
+            endTest();
+        }
+        </script>
+    </head>
+
+    <body onload="startTest()">
+        <p>Tests that adding a track and changing its mode through JS doesn't crash the browser.</p>
+        <video controls>
+            <!-- If the src is specified through JS after the body load, the test doesn't crash anymore. -->
+            <source src="../content/test.mp4" type="video/mp4">
+            <source src="../content/test.ogv" type="video/ogg">
+        </video>
+    </body>
+</html>

commit 2ad5239796f684f9acedad810bc9b7f97feebdc0

Styling disappears from the cue that's being styled by ::cue pseudo element https://bugs.webkit.org/show_bug.cgi?id=106723
Patch by Dima Gorbik <dgorbik@apple.com> on 2013-01-13
Reviewed by Antti Koivisto.

Source/WebCore:

Fixes a regression caused by r138966. Setting a pseudoId in TextTrackCue::updateDisplayTree for m_allDocumentNodes
was triggering recalculating styles the next run loop. Now when this was removed we append the WebVTT tree after its children have
been marked by markFutureAndPastNodes so that correct styles are set within this append call.

Test: media/track/track-css-cue-lifetime.html

* html/track/TextTrackCue.cpp:
(WebCore::TextTrackCue::markFutureAndPastNodes): changing the type of the first argument because DocumentFragment is not a subclass
of Element.
(WebCore::TextTrackCue::updateDisplayTree): appending the WebVTT rendering tree after its children have been marked properly.
* html/track/TextTrackCue.h:
(TextTrackCue):

LayoutTests:

* media/track/captions-webvtt/styling-lifetime.vtt: Added.
* media/track/track-css-cue-lifetime-expected.txt: Added.
* media/track/track-css-cue-lifetime.html: Added.

Identifier: 124975@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@139562 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/track/TextTrackCue.cpp
@@ -658,7 +658,7 @@ void TextTrackCue::calculateDisplayParameters()
     m_computedLinePosition = calculateComputedLinePosition();
 }
     
-void TextTrackCue::markFutureAndPastNodes(Element* root, double previousTimestamp, double movieTime)
+void TextTrackCue::markFutureAndPastNodes(Node* root, double previousTimestamp, double movieTime)
 {
     DEFINE_STATIC_LOCAL(const String, timestampTag, (ASCIILiteral("timestamp")));
     
@@ -700,9 +700,8 @@ void TextTrackCue::updateDisplayTree(float movieTime)
 
     // Update the two sets containing past and future WebVTT objects.
     RefPtr<DocumentFragment> referenceTree = getCueAsHTML();
-
+    markFutureAndPastNodes(referenceTree.get(), startTime(), movieTime);
     m_allDocumentNodes->appendChild(referenceTree);
-    markFutureAndPastNodes(m_allDocumentNodes.get(), startTime(), movieTime);
 }
 
 PassRefPtr<TextTrackCueBox> TextTrackCue::getDisplayTree()

Source/WebCore/html/track/TextTrackCue.h
@@ -135,7 +135,7 @@ public:
     PassRefPtr<TextTrackCueBox> getDisplayTree();
     void updateDisplayTree(float);
     void removeDisplayTree();
-    void markFutureAndPastNodes(Element*, double, double);
+    void markFutureAndPastNodes(Node*, double, double);
 
     int calculateComputedLinePosition();
 

LayoutTests/media/track/captions-webvtt/styling-lifetime.vtt
@@ -1,0 +1,5 @@
+WEBVTT
+
+1
+00:00.500 --> 00:03.000
+<c.red>dolor sit </c><c.green>amet, consectetur </c><c.red2>adipiscing elit</c>

LayoutTests/media/track/track-css-cue-lifetime-expected.txt
@@ -1,0 +1,146 @@
+Test that the cue is styled properly throughout its lifetime.
+EVENT(canplaythrough)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+1
+RUN(video.currentTime = 0.7)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+2
+RUN(video.currentTime = 0.8)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+3
+RUN(video.currentTime = 0.9)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+4
+RUN(video.currentTime = 1)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+5
+RUN(video.currentTime = 1.1)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+6
+RUN(video.currentTime = 1.2000000000000002)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+7
+RUN(video.currentTime = 1.3)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+8
+RUN(video.currentTime = 1.4)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+9
+RUN(video.currentTime = 1.5)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+10
+RUN(video.currentTime = 1.6)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+11
+RUN(video.currentTime = 1.7000000000000002)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+12
+RUN(video.currentTime = 1.8000000000000003)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+13
+RUN(video.currentTime = 1.9)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+14
+RUN(video.currentTime = 2)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+15
+RUN(video.currentTime = 2.1)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+16
+RUN(video.currentTime = 2.2)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+17
+RUN(video.currentTime = 2.3000000000000003)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+18
+RUN(video.currentTime = 2.4)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+19
+RUN(video.currentTime = 2.5)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+20
+RUN(video.currentTime = 2.6)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+21
+RUN(video.currentTime = 2.7)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+22
+RUN(video.currentTime = 2.8000000000000003)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+23
+RUN(video.currentTime = 2.9000000000000004)
+EVENT(seeked)
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(0, 128, 0)') OK
+EXPECTED (getComputedStyle(cueNode).color == 'rgb(255, 0, 0)') OK
+END OF TEST
+

LayoutTests/media/track/track-css-cue-lifetime.html
@@ -1,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <script src=../media-file.js></script>
+        <script src=../video-test.js></script>
+        <script src=../media-controls.js></script>
+
+        <style>
+        video::cue(.red, .red2) { color:red }
+        video::cue(.green) { color:green }
+        </style>
+
+        <script>
+
+        var cueNode;
+        var seekedCount = 0;
+        var step = 0.1;
+        var initialTime = 0.6;
+        var endTime = 3.0
+
+        function skipNonElements(root)
+        {
+            nextElementSibling = root;
+            while (nextElementSibling.nodeType != 1) {
+                nextElementSibling = nextElementSibling.nextSibling;
+            }
+            return nextElementSibling;
+        }
+
+        function seeked()
+        {
+            if (testEnded)
+                return;
+
+            cueNode = skipNonElements(textTrackDisplayElement(video, 'all-nodes').firstChild);
+            skipNonElements(cueNode);
+            testExpected("getComputedStyle(cueNode).color", "rgb(255, 0, 0)");
+            cueNode = skipNonElements(cueNode.nextSibling);
+            testExpected("getComputedStyle(cueNode).color", "rgb(0, 128, 0)");
+            cueNode = skipNonElements(cueNode.nextSibling);
+            testExpected("getComputedStyle(cueNode).color", "rgb(255, 0, 0)");
+
+            if (++seekedCount * step + initialTime > endTime)
+                endTest();
+            else {
+                consoleWrite("" + seekedCount);
+                run("video.currentTime = " + (initialTime + seekedCount * step));
+            }
+        }
+
+        function loaded()
+        {
+            consoleWrite("Test that the cue is styled properly throughout its lifetime.");
+            findMediaElement();
+            video.src = findMediaFile('video', '../content/test');
+            video.id = "testvideo";
+            waitForEvent('seeked', seeked);
+            waitForEvent('canplaythrough', function() { video.currentTime = initialTime; });
+        }
+
+        </script>
+    </head>
+    <body onload="loaded()">
+        <video controls >
+            <track src="captions-webvtt/styling-lifetime.vtt" kind="captions" default>
+        </video>
+    </body>
+</html>