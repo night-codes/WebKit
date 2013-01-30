night-codes/WebKit

commit baf6570eef6dcffc5ad655e1aa75482719c9b3dc

Convert CSE phase to not rely too much on NodeIndex https://bugs.webkit.org/show_bug.cgi?id=107616
Reviewed by Geoffrey Garen.

- Instead of looping over the graph (which assumes that you can simply loop over all
  nodes without considering blocks first) to reset node.replacement, do that in the
  loop that sets up relevantToOSR, just before running CSE on the block.

- Instead of having a relevantToOSR bitvector indexed by NodeIndex, made
  NodeRelevantToOSR be a NodeFlag. We had exactly one bit left in NodeFlags, so I did
  some reshuffling to fit it in.

* dfg/DFGCSEPhase.cpp:
(JSC::DFG::CSEPhase::CSEPhase):
(JSC::DFG::CSEPhase::eliminateIrrelevantPhantomChildren):
(JSC::DFG::CSEPhase::performNodeCSE):
(JSC::DFG::CSEPhase::performBlockCSE):
(CSEPhase):
* dfg/DFGNodeFlags.h:
(DFG):
* dfg/DFGNodeType.h:
(DFG):



Identifier: 125848@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@140504 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/dfg/DFGNodeFlags.h
@@ -36,32 +36,34 @@ namespace JSC { namespace DFG {
 
 // Entries in the NodeType enum (below) are composed of an id, a result type (possibly none)
 // and some additional informative flags (must generate, is constant, etc).
-#define NodeResultMask              0xF
+#define NodeResultMask              0x7
 #define NodeResultJS                0x1
 #define NodeResultNumber            0x2
 #define NodeResultInt32             0x3
 #define NodeResultBoolean           0x4
 #define NodeResultStorage           0x5
                                 
-#define NodeMustGenerate           0x10 // set on nodes that have side effects, and may not trivially be removed by DCE.
-#define NodeHasVarArgs             0x20
-#define NodeClobbersWorld          0x40
-#define NodeMightClobber           0x80
+#define NodeMustGenerate           0x08 // set on nodes that have side effects, and may not trivially be removed by DCE.
+#define NodeHasVarArgs             0x10
+#define NodeClobbersWorld          0x20
+#define NodeMightClobber           0x40
                                 
-#define NodeBehaviorMask          0x300
-#define NodeMayOverflow           0x100
-#define NodeMayNegZero            0x200
+#define NodeBehaviorMask          0x180
+#define NodeMayOverflow           0x080
+#define NodeMayNegZero            0x100
                                 
-#define NodeBackPropMask         0x7C00
+#define NodeBackPropMask         0x3E00
 #define NodeUseBottom            0x0000
-#define NodeUsedAsNumber          0x400 // The result of this computation may be used in a context that observes fractional, or bigger-than-int32, results.
-#define NodeNeedsNegZero          0x800 // The result of this computation may be used in a context that observes -0.
-#define NodeUsedAsOther          0x1000 // The result of this computation may be used in a context that distinguishes between NaN and other things (like undefined).
+#define NodeUsedAsNumber         0x0200 // The result of this computation may be used in a context that observes fractional, or bigger-than-int32, results.
+#define NodeNeedsNegZero         0x0400 // The result of this computation may be used in a context that observes -0.
+#define NodeUsedAsOther          0x0800 // The result of this computation may be used in a context that distinguishes between NaN and other things (like undefined).
 #define NodeUsedAsValue          (NodeUsedAsNumber | NodeNeedsNegZero | NodeUsedAsOther)
-#define NodeUsedAsInt            0x2000 // The result of this computation is known to be used in a context that prefers, but does not require, integer values.
-#define NodeUsedAsIntLocally     0x4000 // Same as NodeUsedAsInt, but within the same basic block.
+#define NodeUsedAsInt            0x1000 // The result of this computation is known to be used in a context that prefers, but does not require, integer values.
+#define NodeUsedAsIntLocally     0x2000 // Same as NodeUsedAsInt, but within the same basic block.
 
-#define NodeDoesNotExit          0x8000 // This flag is negated to make it natural for the default to be that a node does exit.
+#define NodeDoesNotExit          0x4000 // This flag is negated to make it natural for the default to be that a node does exit.
+
+#define NodeRelevantToOSR        0x8000
 
 typedef uint16_t NodeFlags;
 

Source/JavaScriptCore/dfg/DFGCSEPhase.cpp
@@ -39,10 +39,6 @@ public:
     CSEPhase(Graph& graph)
         : Phase(graph, "common subexpression elimination")
     {
-        for (unsigned i = 0; i < m_graph.size(); ++i)
-            m_graph[i].replacement = NoNode;
-        
-        m_relevantToOSR.resize(m_graph.size());
     }
     
     bool run()
@@ -1023,7 +1019,7 @@ private:
             Edge edge = node.children.child(i);
             if (!edge)
                 continue;
-            if (m_relevantToOSR.get(edge.index()))
+            if (m_graph[edge].flags() & NodeRelevantToOSR)
                 continue;
             
 #if DFG_ENABLE(DEBUG_PROPAGATION_VERBOSE)
@@ -1108,7 +1104,7 @@ private:
         }
         
         if (node.op() == SetLocal)
-            m_relevantToOSR.set(node.child1().index());
+            m_graph[node.child1()].mergeFlags(NodeRelevantToOSR);
         
         if (!shouldGenerate)
             return;
@@ -1429,20 +1425,29 @@ private:
         for (unsigned i = 0; i < LastNodeType; ++i)
             m_lastSeen[i] = UINT_MAX;
         
-        // Make all of my Phi nodes relevant to OSR.
-        for (unsigned i = 0; i < block->phis.size(); ++i)
-            m_relevantToOSR.set(block->phis[i]);
+        // All Phis need to already be marked as relevant to OSR, and have their
+        // replacements cleared, so we don't get confused while doing substitutions on
+        // GetLocal's.
+        for (unsigned i = 0; i < block->phis.size(); ++i) {
+            ASSERT(m_graph[block->phis[i]].flags() & NodeRelevantToOSR);
+            m_graph[block->phis[i]].replacement = NoNode;
+        }
         
-        // Make all of my SetLocal and GetLocal nodes relevant to OSR.
+        // Make all of my SetLocal and GetLocal nodes relevant to OSR, and do some other
+        // necessary bookkeeping.
         for (unsigned i = 0; i < block->size(); ++i) {
             NodeIndex nodeIndex = block->at(i);
             Node& node = m_graph[nodeIndex];
+            
+            node.replacement = NoNode;
+            
             switch (node.op()) {
             case SetLocal:
             case GetLocal: // FIXME: The GetLocal case is only necessary until we do https://bugs.webkit.org/show_bug.cgi?id=106707.
-                m_relevantToOSR.set(nodeIndex);
+                node.mergeFlags(NodeRelevantToOSR);
                 break;
             default:
+                node.clearFlags(NodeRelevantToOSR);
                 break;
             }
         }
@@ -1457,7 +1462,6 @@ private:
     NodeIndex m_compileIndex;
     unsigned m_indexInBlock;
     FixedArray<unsigned, LastNodeType> m_lastSeen;
-    FastBitVector m_relevantToOSR;
     bool m_changed; // Only tracks changes that have a substantive effect on other optimizations.
 };
 

Source/JavaScriptCore/dfg/DFGNodeType.h
@@ -60,8 +60,8 @@ namespace JSC { namespace DFG {
     macro(GetLocal, NodeResultJS) \
     macro(SetLocal, 0) \
     macro(Phantom, NodeMustGenerate | NodeDoesNotExit) \
-    macro(Nop, 0 | NodeDoesNotExit) \
-    macro(Phi, 0 | NodeDoesNotExit) \
+    macro(Nop, NodeDoesNotExit) \
+    macro(Phi, NodeDoesNotExit | NodeRelevantToOSR) \
     macro(Flush, NodeMustGenerate | NodeDoesNotExit) \
     \
     /* Get the value of a local variable, without linking into the VariableAccessData */\

commit 0422d93b7210d60a005267c52d50576798ebf89d

REGRESSION(140504): pure CSE no longer matches things, 10% regression on Kraken https://bugs.webkit.org/show_bug.cgi?id=108366
Reviewed by Geoffrey Garen and Mark Hahnenberg.

This was a longstanding bug that was revealed by http://trac.webkit.org/changeset/140504.
Pure CSE requires that the Node::flags() that may affect the behavior of a node match,
when comparing a possibly redundant node to its possible replacement. It was doing this
by comparing Node::arithNodeFlags(), which as the name might appear to suggest, returns
just those flag bits that correspond to actual node behavior and not auxiliary things.
Unfortunately, Node::arithNodeFlags() wasn't actually masking off the irrelevant bits.
This worked prior to r140504 because CSE itself didn't mutate the flags, so there was a
very high probability that matching nodes would also have completely identical flag bits
(even the ones that aren't relevant to arithmetic behavior, like NodeDoesNotExit). But
r140504 moved one of CSE's side-tables (m_relevantToOSR) into a flag bit for quicker
access. These bits would be mutated as the CSE ran over a basic block, in such a way that
there was a very high probability that the possible replacement would already have the
bit set, while the redundant node did not have the bit set. Since Node::arithNodeFlags()
returned all of the bits, this would cause CSEPhase::pureCSE() to reject the match
almost every time.

The solution is to make Node::arithNodeFlags() do as its name suggests: only return those
flags that are relevant to arithmetic behavior. This patch introduces a new mask that
represents those bits, and includes NodeBehaviorMask and NodeBackPropMask, which are both
used for queries on Node::arithNodeFlags(), and both affect arithmetic code gen. None of
the other flags are relevant to Node::arithNodeFlags() since they either correspond to
information already conveyed by the opcode (like NodeResultMask, NodeMustGenerate,
NodeHasVarArgs, NodeClobbersWorld, NodeMightClobber) or information that doesn't affect
the result that the node will produce or any of the queries performed on the result of
Node::arithNodeFlags (NodeDoesNotExit and of course NodeRelevantToOSR).

This is a 10% speed-up on Kraken, undoing the regression from r140504.

Source/JavaScriptCore/dfg/DFGNode.h
@@ -442,7 +442,7 @@ struct Node {
     // to know if it can speculate on negative zero.
     NodeFlags arithNodeFlags()
     {
-        NodeFlags result = m_flags;
+        NodeFlags result = m_flags & NodeArithFlagsMask;
         if (op() == ArithMul || op() == ArithDiv || op() == ArithMod)
             return result;
         return result & ~NodeNeedsNegZero;

Source/JavaScriptCore/dfg/DFGNodeFlags.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Apple Inc. All rights reserved.
+ * Copyright (C) 2012, 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -60,6 +60,8 @@ namespace JSC { namespace DFG {
 #define NodeUsedAsValue          (NodeUsedAsNumber | NodeNeedsNegZero | NodeUsedAsOther)
 #define NodeUsedAsInt            0x1000 // The result of this computation is known to be used in a context that prefers, but does not require, integer values.
 #define NodeUsedAsIntLocally     0x2000 // Same as NodeUsedAsInt, but within the same basic block.
+
+#define NodeArithFlagsMask       (NodeBehaviorMask | NodeBackPropMask)
 
 #define NodeDoesNotExit          0x4000 // This flag is negated to make it natural for the default to be that a node does exit.
 