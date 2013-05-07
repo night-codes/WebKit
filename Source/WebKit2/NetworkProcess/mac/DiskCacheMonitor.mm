night-codes/WebKit

commit 4cb38681502ba0f677a20413c6c94bd1eceb3f6d

Remove some unnecessary soft linking in NetworkProcess. <rdar://problem/13821779> and https://bugs.webkit.org/show_bug.cgi?id=115683
Reviewed by Sam Weinig.

Replace some unneeded soft linking with forward declarations.

* NetworkProcess/mac/DiskCacheMonitor.mm:
(WebKit::DiskCacheMonitor::DiskCacheMonitor):
* NetworkProcess/mac/NetworkResourceLoaderMac.mm:
(WebKit::NetworkResourceLoader::tryGetShareableHandleFromCFURLCachedResponse):
(WebKit::NetworkResourceLoader::tryGetShareableHandleFromSharedBuffer):

Identifier: 134113@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149651 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/NetworkProcess/mac/DiskCacheMonitor.mm
@@ -30,20 +30,11 @@
 #import "NetworkProcessConnectionMessages.h"
 #import "NetworkResourceLoader.h"
 #import "WebCoreArgumentCoders.h"
-#import <WebCore/SoftLinking.h>
 
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
 
-SOFT_LINK_FRAMEWORK(CFNetwork)
-
 typedef void (^CFCachedURLResponseCallBackBlock)(CFCachedURLResponseRef);
-static void CFCachedURLResponseSetBecameFileBackedCallBackBlock(CFCachedURLResponseRef response, CFCachedURLResponseCallBackBlock callback, dispatch_queue_t queue)
-{
-    typedef void (*SetCallbackFunctionType)(CFCachedURLResponseRef, CFCachedURLResponseCallBackBlock, dispatch_queue_t);
-    static SetCallbackFunctionType softResponseSetBecameFileBackedCallBackBlock = (SetCallbackFunctionType) dlsym(CFNetworkLibrary(), "_CFCachedURLResponseSetBecameFileBackedCallBackBlock");
-    if (softResponseSetBecameFileBackedCallBackBlock)
-        return softResponseSetBecameFileBackedCallBackBlock(response, callback, queue);
-}
+extern "C" void _CFCachedURLResponseSetBecameFileBackedCallBackBlock(CFCachedURLResponseRef, CFCachedURLResponseCallBackBlock, dispatch_queue_t);
 
 using namespace WebCore;
 
@@ -94,7 +85,7 @@ DiskCacheMonitor::DiskCacheMonitor(CFCachedURLResponseRef cachedResponse, Networ
         monitor->send(Messages::NetworkProcessConnection::DidCacheResource(monitor->resourceRequest(), handle));
     };
 
-    CFCachedURLResponseSetBecameFileBackedCallBackBlock(cachedResponse, block, dispatch_get_main_queue());
+    _CFCachedURLResponseSetBecameFileBackedCallBackBlock(cachedResponse, block, dispatch_get_main_queue());
 }
 
 CoreIPC::Connection* DiskCacheMonitor::connection() const

Source/WebKit2/NetworkProcess/mac/NetworkResourceLoaderMac.mm
@@ -30,7 +30,6 @@
 #import "ShareableResource.h"
 #import <WebCore/ResourceHandle.h>
 #import <WebCore/SharedBuffer.h>
-#import <WebCore/SoftLinking.h>
 
 using namespace WebCore;
 
@@ -39,27 +38,8 @@ typedef const struct _CFURLCache* CFURLCacheRef;
 typedef const struct _CFCachedURLResponse* CFCachedURLResponseRef;
 extern "C" CFURLCacheRef CFURLCacheCopySharedURLCache();
 extern "C" CFCachedURLResponseRef CFURLCacheCopyResponseForRequest(CFURLCacheRef, CFURLRequestRef);
-
-SOFT_LINK_FRAMEWORK(CFNetwork)
-static CFDataRef CFCachedURLResponseGetMemMappedData(CFCachedURLResponseRef response)
-{
-    typedef CFDataRef (*functionType)(CFCachedURLResponseRef);
-    static functionType softGetMemMappedData = (functionType) dlsym(CFNetworkLibrary(), "_CFCachedURLResponseGetMemMappedData");
-    
-    if (softGetMemMappedData)
-        return softGetMemMappedData(response);
-    return NULL;
-}
-
-static bool CFURLCacheIsMemMappedData(CFURLCacheRef cache, CFDataRef data)
-{
-    typedef CFBooleanRef (*functionType)(CFURLCacheRef cache, CFDataRef data);
-    static functionType softLinkIsCacheMMAPedData = (functionType) dlsym(CFNetworkLibrary(), "_CFURLCacheIsResponseDataMemMapped");
-   
-    if (softLinkIsCacheMMAPedData)
-        return softLinkIsCacheMMAPedData(cache, data) == kCFBooleanTrue;
-    return false;
-}
+extern "C" CFDataRef _CFCachedURLResponseGetMemMappedData(CFCachedURLResponseRef);
+extern "C" bool _CFURLCacheIsResponseDataMemMapped(CFURLCacheRef, CFDataRef);
 #endif
 
 @interface NSCachedURLResponse (NSCachedURLResponseDetails)
@@ -88,7 +68,7 @@ static void tryGetShareableHandleFromCFData(ShareableResource::Handle& handle, C
 
 void NetworkResourceLoader::tryGetShareableHandleFromCFURLCachedResponse(ShareableResource::Handle& handle, CFCachedURLResponseRef cachedResponse)
 {
-    CFDataRef data = CFCachedURLResponseGetMemMappedData(cachedResponse);
+    CFDataRef data = _CFCachedURLResponseGetMemMappedData(cachedResponse);
 
     tryGetShareableHandleFromCFData(handle, data);
 }
@@ -100,7 +80,7 @@ void NetworkResourceLoader::tryGetShareableHandleFromSharedBuffer(ShareableResou
         return;
 
     RetainPtr<CFDataRef> data = adoptCF(buffer->createCFData());
-    if (!CFURLCacheIsMemMappedData(cache.get(), data.get()))
+    if (!_CFURLCacheIsResponseDataMemMapped(cache.get(), data.get()))
         return;
 
     tryGetShareableHandleFromCFData(handle, data.get());

commit 7de001598db75fa41be39e2b70c9580141f01265

Remove some unnecessary soft linking in NetworkProcess. (Take 2) <rdar://problem/13821779> and https://bugs.webkit.org/show_bug.cgi?id=115683
Reviewed by Dean Jackson and owned by Andreas Kling.

As a followup to r149651, include the private headers conditionally.
Also, fix a bug introduced with r149651 (bool vs CFBooleanRef).

* NetworkProcess/mac/DiskCacheMonitor.mm:
* NetworkProcess/mac/NetworkResourceLoaderMac.mm:
(WebKit::NetworkResourceLoader::tryGetShareableHandleFromSharedBuffer):

Identifier: 134139@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@149678 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/NetworkProcess/mac/DiskCacheMonitor.mm
@@ -31,6 +31,12 @@
 #import "NetworkResourceLoader.h"
 #import "WebCoreArgumentCoders.h"
 
+#ifdef __has_include
+#if __has_include(<CFNetwork/CFURLCachePriv.h>)
+#include <CFNetwork/CFURLCachePriv.h>
+#endif
+#endif
+
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
 
 typedef void (^CFCachedURLResponseCallBackBlock)(CFCachedURLResponseRef);

Source/WebKit2/NetworkProcess/mac/NetworkResourceLoaderMac.mm
@@ -33,13 +33,22 @@
 
 using namespace WebCore;
 
+#ifdef __has_include
+#if __has_include(<CFNetwork/CFURLCache.h>)
+#include <CFNetwork/CFURLCache.h>
+#endif
+#if __has_include(<CFNetwork/CFURLCachePriv.h>)
+#include <CFNetwork/CFURLCachePriv.h>
+#endif
+#endif
+
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
 typedef const struct _CFURLCache* CFURLCacheRef;
 typedef const struct _CFCachedURLResponse* CFCachedURLResponseRef;
 extern "C" CFURLCacheRef CFURLCacheCopySharedURLCache();
 extern "C" CFCachedURLResponseRef CFURLCacheCopyResponseForRequest(CFURLCacheRef, CFURLRequestRef);
 extern "C" CFDataRef _CFCachedURLResponseGetMemMappedData(CFCachedURLResponseRef);
-extern "C" bool _CFURLCacheIsResponseDataMemMapped(CFURLCacheRef, CFDataRef);
+extern "C" CFBooleanRef _CFURLCacheIsResponseDataMemMapped(CFURLCacheRef, CFDataRef);
 #endif
 
 @interface NSCachedURLResponse (NSCachedURLResponseDetails)
@@ -80,7 +89,7 @@ void NetworkResourceLoader::tryGetShareableHandleFromSharedBuffer(ShareableResou
         return;
 
     RetainPtr<CFDataRef> data = adoptCF(buffer->createCFData());
-    if (!_CFURLCacheIsResponseDataMemMapped(cache.get(), data.get()))
+    if (_CFURLCacheIsResponseDataMemMapped(cache.get(), data.get()) == kCFBooleanFalse)
         return;
 
     tryGetShareableHandleFromCFData(handle, data.get());