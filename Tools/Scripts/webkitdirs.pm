night-codes/WebKit

commit 1abd02c426f60a407481878c2926858b87fe2ccf

Remove uses of VS2005 scripts, executables and associated environment variables from scripts. <rdar://problem/14042021>.
Reviewed by Brent Fulgham.

* Scripts/build-api-tests:
* Scripts/build-dumprendertree:
* Scripts/build-webkit:
* Scripts/update-webkit-dependency:
* Scripts/update-webkit-support-libs:
* Scripts/webkitdirs.pm:
(determineBaseProductDir):
(setupAppleWinEnv):
(setupCygwinEnv):


Identifier: 135476@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151196 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitdirs.pm
@@ -183,7 +183,7 @@ sub determineBaseProductDir
     determineSourceDir();
 
     my $setSharedPrecompsDir;
-    $baseProductDir = $ENV{"WEBKITOUTPUTDIR"}; # FIXME: Switch to WEBKIT_OUTPUTDIR as part of https://bugs.webkit.org/show_bug.cgi?id=109472
+    $baseProductDir = $ENV{"WEBKIT_OUTPUTDIR"};
 
     if (!defined($baseProductDir) and isAppleMacWebKit()) {
         # Silently remove ~/Library/Preferences/xcodebuild.plist which can
@@ -247,7 +247,6 @@ sub determineBaseProductDir
     if (isCygwin()) {
         my $dosBuildPath = `cygpath --windows \"$baseProductDir\"`;
         chomp $dosBuildPath;
-        $ENV{"WEBKITOUTPUTDIR"} = $dosBuildPath;
         $ENV{"WEBKIT_OUTPUTDIR"} = $dosBuildPath;
         my $unixBuildPath = `cygpath --unix \"$baseProductDir\"`;
         chomp $unixBuildPath;
@@ -431,7 +430,7 @@ sub visualStudioInstallDir
         $vsInstallDir = $ENV{'VSINSTALLDIR'};
         $vsInstallDir =~ s|[\\/]$||;
     } else {
-        $vsInstallDir = File::Spec->catdir(programFilesPath(), "Microsoft Visual Studio 8");
+        $vsInstallDir = File::Spec->catdir(programFilesPath(), "Microsoft Visual Studio 10.0");
     }
     chomp($vsInstallDir = `cygpath "$vsInstallDir"`) if isCygwin();
 
@@ -1576,29 +1575,19 @@ sub setupAppleWinEnv()
         }
         
         # Those environment variables must be set to be able to build inside Visual Studio.
-        $variablesToSet{WEBKITLIBRARIESDIR} = windowsLibrariesDir() unless $ENV{WEBKITLIBRARIESDIR};
         $variablesToSet{WEBKIT_LIBRARIES} = windowsLibrariesDir() unless $ENV{WEBKIT_LIBRARIES};
-        $variablesToSet{WEBKITOUTPUTDIR} = windowsOutputDir() unless $ENV{WEBKITOUTPUTDIR};
         $variablesToSet{WEBKIT_OUTPUTDIR} = windowsOutputDir() unless $ENV{WEBKIT_OUTPUTDIR};
-        $variablesToSet{WEBKIT_SOURCE} = windowsSourceSourceDir() unless $ENV{WEBKIT_SOURCE};
 
         foreach my $variable (keys %variablesToSet) {
             print "Setting the Environment Variable '" . $variable . "' to '" . $variablesToSet{$variable} . "'\n\n";
             system qw(regtool -s set), '\\HKEY_CURRENT_USER\\Environment\\' . $variable, $variablesToSet{$variable};
-            $restartNeeded ||= $variable eq "WEBKITLIBRARIESDIR" || $variable eq "WEBKITOUTPUTDIR" || $variable eq "WEBKIT_LIBRARIES" || $variable eq "WEBKIT_OUTPUTDIR" || $variable eq "WEBKIT_SOURCE";
+            $restartNeeded ||=  $variable eq "WEBKIT_LIBRARIES" || $variable eq "WEBKIT_OUTPUTDIR";
         }
 
         if ($restartNeeded) {
             print "Please restart your computer before attempting to build inside Visual Studio.\n\n";
         }
     } else {
-        if (!$ENV{'WEBKITLIBRARIESDIR'}) {
-            # VS2005 version.  This will be removed as part of https://bugs.webkit.org/show_bug.cgi?id=109472.
-            print "Warning: You must set the 'WebKitLibrariesDir' environment variable\n";
-            print "         to be able build WebKit from within Visual Studio 2005.\n";
-            print "         Make sure that 'WebKitLibrariesDir' points to the\n";
-            print "         'WebKitLibraries/win' directory, not the 'WebKitLibraries/' directory.\n\n";
-        }
         if (!$ENV{'WEBKIT_LIBRARIES'}) {
             # VS2010 (and newer) version. This will replace the VS2005 version as part of
             # https://bugs.webkit.org/show_bug.cgi?id=109472. 
@@ -1607,19 +1596,10 @@ sub setupAppleWinEnv()
             print "         Make sure that 'WebKit_Libraries' points to the\n";
             print "         'WebKitLibraries/win' directory, not the 'WebKitLibraries/' directory.\n\n";
         }
-        if (!$ENV{'WEBKITOUTPUTDIR'}) {
-            # VS2005 version.  This will be removed as part of https://bugs.webkit.org/show_bug.cgi?id=109472.
-            print "Warning: You must set the 'WebKitOutputDir' environment variable\n";
-            print "         to be able build WebKit from within Visual Studio 2005.\n\n";
-        }
         if (!$ENV{'WEBKIT_OUTPUTDIR'}) {
             # VS2010 (and newer) version. This will replace the VS2005 version as part of
             # https://bugs.webkit.org/show_bug.cgi?id=109472. 
             print "Warning: You must set the 'WebKit_OutputDir' environment variable\n";
-            print "         to be able build WebKit from within Visual Studio 2010 and newer.\n\n";
-        }
-        if (!$ENV{'WEBKIT_SOURCE'}) {
-            print "Warning: You must set the 'WebKit_Source' environment variable\n";
             print "         to be able build WebKit from within Visual Studio 2010 and newer.\n\n";
         }
     }
@@ -1633,10 +1613,9 @@ sub setupCygwinEnv()
     my $programFilesPath = programFilesPath();
     $vcBuildPath = File::Spec->catfile(visualStudioInstallDir(), qw(Common7 IDE devenv.com));
     if (-e $vcBuildPath) {
-        # Visual Studio is installed; we can use pdevenv to build.
-        if (visualStudioVersion() eq "8") {
-            # FIXME: Make pdevenv work with non-Cygwin Perl.
-            $vcBuildPath = File::Spec->catfile(sourceDir(), qw(Tools Scripts pdevenv)) if isCygwin();
+        # Visual Studio is installed;
+        if (visualStudioVersion() eq "10") {
+            $vcBuildPath = File::Spec->catfile(visualStudioInstallDir(), qw(Common7 IDE devenv.exe));
         }
     } else {
         # Visual Studio not found, try VC++ Express
@@ -1662,20 +1641,9 @@ sub setupCygwinEnv()
         print "*************************************************************\n";
         die;
     }
-    
-    unless ($ENV{WEBKITLIBRARIESDIR}) {
-        $ENV{'WEBKITLIBRARIESDIR'} = File::Spec->catdir($sourceDir, "WebKitLibraries", "win");
-        chomp($ENV{WEBKITLIBRARIESDIR} = `cygpath -wa '$ENV{WEBKITLIBRARIESDIR}'`) if isCygwin();
-    }
-    unless ($ENV{WEBKIT_LIBRARIES}) {
-        $ENV{'WEBKIT_LIBRARIES'} = File::Spec->catdir($sourceDir, "WebKitLibraries", "win");
-        chomp($ENV{WEBKIT_LIBRARIES} = `cygpath -wa '$ENV{WEBKIT_LIBRARIES}'`) if isCygwin();
-    }
 
     print "Building results into: ", baseProductDir(), "\n";
-    print "WEBKITOUTPUTDIR is set to: ", $ENV{"WEBKITOUTPUTDIR"}, "\n";
     print "WEBKIT_OUTPUTDIR is set to: ", $ENV{"WEBKIT_OUTPUTDIR"}, "\n";
-    print "WEBKITLIBRARIESDIR is set to: ", $ENV{"WEBKITLIBRARIESDIR"}, "\n";
     print "WEBKIT_LIBRARIES is set to: ", $ENV{"WEBKIT_LIBRARIES"}, "\n";
 }
 
@@ -2528,15 +2496,6 @@ sub debugSafari
     if (isAppleMacWebKit()) {
         checkFrameworks();
         execMacWebKitAppForDebugging(safariPath());
-    }
-
-    if (isAppleWinWebKit()) {
-        setupCygwinEnv();
-        my $productDir = productDir();
-        chomp($ENV{WEBKITNIGHTLY} = `cygpath -wa "$productDir"`);
-        my $safariPath = safariPath();
-        chomp($safariPath = `cygpath -wa "$safariPath"`);
-        return system { $vcBuildPath } $vcBuildPath, "/debugexe", "\"$safariPath\"", @ARGV;
     }
 
     return 1; # Unsupported platform; can't debug Safari on this platform.

Tools/Scripts/build-api-tests
@@ -66,9 +66,6 @@ if (isAppleMacWebKit()) {
 } elsif (isAppleWinWebKit()) {
     chdir "Tools/TestWebKitAPI" or die;
     my $solutionPath = "TestWebKitAPI.vcxproj/TestWebKitAPI.sln";
-    if (visualStudioVersion() eq "8") {
-        $solutionPath = "win/TestWebKitAPI.sln";
-    }
     $result = buildVisualStudioProject($solutionPath, $clean);
 } else {
     die "TestWebKitAPI is not supported on this platform.\n";

Tools/Scripts/build-dumprendertree
@@ -69,9 +69,6 @@ if (isAppleMacWebKit()) {
     $result = buildXCodeProject("DumpRenderTree", $clean, XcodeOptions(), @ARGV);
 } elsif (isAppleWinWebKit()) {
     my $drtSolutionPath = "DumpRenderTree.vcxproj/DumpRenderTree.sln";
-    if (visualStudioVersion() eq "8") {
-        $drtSolutionPath = "DumpRenderTree.sln";
-    }
     $result = buildVisualStudioProject($drtSolutionPath, $clean);
 } elsif (isQt() || isGtk() || isEfl()) {
     # Qt, Gtk and EFL build everything in one shot. No need to build anything here.

Tools/Scripts/build-jsc
@@ -82,7 +82,7 @@ sub buildMyProject
     if (isAppleMacWebKit()) {
         $result = system "sh", "-c", ('xcodebuild -project ' . $projectName . '.xcodeproj "$@" | grep -v setenv && exit ${PIPESTATUS[0]}'), "xcodebuild",  @options, @ARGV, @coverageSupportOptions;
     } elsif (isAppleWinWebKit()) {
-        $result = buildVisualStudioProject("$projectName.vcproj/$projectName.sln");
+        $result = buildVisualStudioProject("$projectName.vcxproj/$projectName.submit.sln");
     } elsif (isGtk()) {
         checkForArgumentAndRemoveFromARGV("--gtk");
         $result = buildGtkProject($projectName, 0);

Tools/Scripts/build-webkit
@@ -339,9 +339,6 @@ for my $dir (@projects) {
     } elsif (isAppleWinWebKit()) {
         if ($project eq "WebKit") {
             my $webkitSolutionPath = "WebKit.vcxproj/WebKit.sln";
-            if (visualStudioVersion() eq "8") {
-                $webkitSolutionPath = "win/WebKit.vcproj/WebKit.sln";
-            }
             $result = buildVisualStudioProject($webkitSolutionPath, $clean);
             my $vsConfiguration = configurationForVisualStudio();
             if (usingVisualStudioExpress()) {

Tools/Scripts/update-webkit-dependency
@@ -67,7 +67,7 @@ my $prefixInZip = shift;
 my $sourceDir = sourceDir();
 my $file = getLibraryName($libsURL);
 my $zipFile = "$file.zip"; 
-my $webkitLibrariesDir = toUnixPath($ENV{'WEBKITLIBRARIESDIR'}) || "$sourceDir/WebKitLibraries/win";
+my $webkitLibrariesDir = toUnixPath($ENV{'WEBKIT_LIBRARIES'}) || "$sourceDir/WebKitLibraries/win";
 my $tmpRelativeDir = File::Temp::tempdir("webkitlibsXXXXXXX", TMPDIR => 1, CLEANUP => 1);
 my $tmpAbsDir = File::Spec->rel2abs($tmpRelativeDir);
 

Tools/Scripts/update-webkit-support-libs
@@ -46,7 +46,7 @@ my $file = "WebKitSupportLibrary";
 my $zipFile = "$file.zip"; 
 my $zipDirectory = toUnixPath($ENV{'WEBKITSUPPORTLIBRARIESZIPDIR'}) || $sourceDir;
 my $pathToZip = File::Spec->catfile($zipDirectory, $zipFile);
-my $webkitLibrariesDir = toUnixPath($ENV{'WEBKITLIBRARIESDIR'}) || "$sourceDir/WebKitLibraries/win";
+my $webkitLibrariesDir = toUnixPath($ENV{'WEBKIT_LIBRARIES'}) || "$sourceDir/WebKitLibraries/win";
 my $versionFile = $file . "Version";
 my $pathToVersionFile = File::Spec->catfile($webkitLibrariesDir, $versionFile);
 my $tmpRelativeDir = File::Temp::tempdir("webkitlibsXXXXXXX", TMPDIR => 1, CLEANUP => 1);

commit 0f88689361d5a6af89f26ca19405bf6e83823960

Use WEBKITOUTPUTDIR if WEBKIT_OUTPUTDIR is not present. https://bugs.webkit.org/show_bug.cgi?id=117244
Reviewed by Daniel Bates.

Fixed a regression introduced by r151196: not only does webkit.org
still mention WEBKITOUTPUTDIR instead of WEBKIT_OUTPUTDIR, but the
infrastructure used by ports such as GTK+ and EFL also use on it for
things such as jhbuild.

While everything is not fully transitioned to WEBKIT_OUTPUTDIR, read
both environment variables.

* Scripts/webkitdirs.pm:
(determineBaseProductDir):

Identifier: 135496@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@151216 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitdirs.pm
@@ -183,7 +183,12 @@ sub determineBaseProductDir
     determineSourceDir();
 
     my $setSharedPrecompsDir;
+
+    # FIXME: See https://bugs.webkit.org/show_bug.cgi?id=117249.
+    #        Once all ports have migrated to WEBKIT_OUTPUTDIR, we can stop
+    #        reading the WEBKITOUTPUTDIR environment variable.
     $baseProductDir = $ENV{"WEBKIT_OUTPUTDIR"};
+    $baseProductDir = $ENV{"WEBKITOUTPUTDIR"} if not $baseProductDir;
 
     if (!defined($baseProductDir) and isAppleMacWebKit()) {
         # Silently remove ~/Library/Preferences/xcodebuild.plist which can