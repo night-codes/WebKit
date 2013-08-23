night-codes/WebKit

commit 2beb09fdc283145863829c04fee90974f7b46d54

Move ElementTraversal to ElementTraversal.h https://bugs.webkit.org/show_bug.cgi?id=119678
Rubber-stamped by Andreas Kling.

Move ElementTraversal namespace from NodeTraversal.h to a correctly named file.

* GNUmakefile.list.am:
* Target.pri:
* WebCore.vcxproj/WebCore.vcxproj:
* WebCore.xcodeproj/project.pbxproj:
* accessibility/AccessibilityRenderObject.cpp:
* css/StyleInvalidationAnalysis.cpp:
* dom/Document.cpp:
* dom/DocumentOrderedMap.cpp:
* dom/Element.cpp:
* dom/ElementTraversal.h: Added.
(WebCore::ElementTraversal::firstElementWithinTemplate):
(WebCore::ElementTraversal::firstWithin):
(WebCore::ElementTraversal::traverseNextElementTemplate):
(WebCore::ElementTraversal::next):
(WebCore::ElementTraversal::traverseNextElementSkippingChildrenTemplate):
(WebCore::ElementTraversal::nextSkippingChildren):
(WebCore::ElementTraversal::previousIncludingPseudo):
(WebCore::ElementTraversal::nextIncludingPseudo):
(WebCore::ElementTraversal::nextIncludingPseudoSkippingChildren):
(WebCore::ElementTraversal::pseudoAwarePreviousSibling):
* dom/Node.cpp:
* dom/NodeTraversal.h:
* dom/SelectorQuery.cpp:
* dom/TreeScope.cpp:
* dom/VisitedLinkState.cpp:
* editing/FrameSelection.cpp:
* editing/markup.cpp:
* html/HTMLCollection.cpp:
* html/HTMLFieldSetElement.cpp:
* html/HTMLFormElement.cpp:
* html/HTMLLabelElement.cpp:
* html/HTMLLegendElement.cpp:
* html/HTMLMapElement.cpp:
* html/HTMLObjectElement.cpp:
* html/HTMLSelectElement.cpp:
* html/shadow/ContentDistributor.cpp:
* page/FocusController.cpp:
* rendering/RenderCounter.cpp:
* rendering/RenderListItem.cpp:
* svg/SVGSVGElement.cpp:
* svg/SVGStyledElement.cpp:
* svg/SVGUseElement.cpp:
* svg/animation/SMILTimeContainer.cpp:
* svg/graphics/SVGImage.cpp:



Identifier: 137629@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@153939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Target.pri
@@ -1609,6 +1609,7 @@ HEADERS += \
     dom/DatasetDOMStringMap.h \
     dom/Element.h \
     dom/ElementShadow.h \
+    dom/ElementTravesal.h \
     dom/Entity.h \
     dom/EntityReference.h \
     dom/Event.h \

Source/WebCore/GNUmakefile.list.am
@@ -2907,6 +2907,7 @@ webcore_sources += \
 	Source/WebCore/dom/ElementRareData.h \
 	Source/WebCore/dom/ElementShadow.cpp \
 	Source/WebCore/dom/ElementShadow.h \
+	Source/WebCore/dom/ElementTraversal.h \
 	Source/WebCore/dom/Entity.h \
 	Source/WebCore/dom/EntityReference.cpp \
 	Source/WebCore/dom/EntityReference.h \

Source/WebCore/WebCore.vcxproj/WebCore.vcxproj
@@ -20197,6 +20197,7 @@
     <ClInclude Include="..\dom\DOMStringMap.h" />
     <ClInclude Include="..\dom\DOMTimeStamp.h" />
     <ClInclude Include="..\dom\Element.h" />
+    <ClInclude Include="..\dom\ElementTraversal.h" />
     <ClInclude Include="..\dom\Entity.h" />
     <ClInclude Include="..\dom\EntityReference.h" />
     <ClInclude Include="..\dom\ErrorEvent.h" />

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -5549,6 +5549,7 @@
 		E4D58EB517B4DBDC00CBDCA8 /* StyleResolveForDocument.h in Headers */ = {isa = PBXBuildFile; fileRef = E4D58EB317B4DBDC00CBDCA8 /* StyleResolveForDocument.h */; };
 		E4D58EB817B4ED8900CBDCA8 /* StyleFontSizeFunctions.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4D58EB617B4ED8900CBDCA8 /* StyleFontSizeFunctions.cpp */; };
 		E4D58EB917B4ED8900CBDCA8 /* StyleFontSizeFunctions.h in Headers */ = {isa = PBXBuildFile; fileRef = E4D58EB717B4ED8900CBDCA8 /* StyleFontSizeFunctions.h */; };
+		E4D58EBB17B8F12800CBDCA8 /* ElementTraversal.h in Headers */ = {isa = PBXBuildFile; fileRef = E4D58EBA17B8F12800CBDCA8 /* ElementTraversal.h */; };
 		E4D687770ED7AE3D006EA978 /* PurgeableBufferMac.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4D687760ED7AE3D006EA978 /* PurgeableBufferMac.cpp */; };
 		E4D687790ED7AE4F006EA978 /* PurgeableBuffer.h in Headers */ = {isa = PBXBuildFile; fileRef = E4D687780ED7AE4F006EA978 /* PurgeableBuffer.h */; };
 		E4DEAA1717A93DC3000E0430 /* StyleResolveTree.cpp in Sources */ = {isa = PBXBuildFile; fileRef = E4DEAA1517A93DC3000E0430 /* StyleResolveTree.cpp */; };
@@ -12372,6 +12373,7 @@
 		E4D58EB317B4DBDC00CBDCA8 /* StyleResolveForDocument.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = StyleResolveForDocument.h; sourceTree = "<group>"; };
 		E4D58EB617B4ED8900CBDCA8 /* StyleFontSizeFunctions.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = StyleFontSizeFunctions.cpp; sourceTree = "<group>"; };
 		E4D58EB717B4ED8900CBDCA8 /* StyleFontSizeFunctions.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = StyleFontSizeFunctions.h; sourceTree = "<group>"; };
+		E4D58EBA17B8F12800CBDCA8 /* ElementTraversal.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ElementTraversal.h; sourceTree = "<group>"; };
 		E4D687760ED7AE3D006EA978 /* PurgeableBufferMac.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = PurgeableBufferMac.cpp; sourceTree = "<group>"; };
 		E4D687780ED7AE4F006EA978 /* PurgeableBuffer.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = PurgeableBuffer.h; sourceTree = "<group>"; };
 		E4DEAA1517A93DC3000E0430 /* StyleResolveTree.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = StyleResolveTree.cpp; sourceTree = "<group>"; };
@@ -20308,6 +20310,7 @@
 				637B7ADE0E8767B800E32194 /* ElementRareData.h */,
 				57CF4C8414F7597A00ECFF14 /* ElementShadow.cpp */,
 				57CF4C8514F7597A00ECFF14 /* ElementShadow.h */,
+				E4D58EBA17B8F12800CBDCA8 /* ElementTraversal.h */,
 				A8EA7EBA0A1945D000A8EF5F /* Entity.h */,
 				93EEC1EB09C2877700C515D1 /* Entity.idl */,
 				A8EA7EB90A1945D000A8EF5F /* EntityReference.cpp */,
@@ -21907,6 +21910,7 @@
 				656D373C0ADBA5DE00A4554D /* FrameLoader.h in Headers */,
 				656D373E0ADBA5DE00A4554D /* FrameLoaderClient.h in Headers */,
 				D000EBA311BDAFD400C47726 /* FrameLoaderStateMachine.h in Headers */,
+				E4D58EBB17B8F12800CBDCA8 /* ElementTraversal.h in Headers */,
 				93B77A380ADD792500EA4B81 /* FrameLoaderTypes.h in Headers */,
 				658436860AE01B7400E53753 /* FrameLoadRequest.h in Headers */,
 				628D214E12131EF40055DCFC /* FrameNetworkingContext.h in Headers */,

Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -37,6 +37,7 @@
 #include "AccessibilityTable.h"
 #include "CachedImage.h"
 #include "Chrome.h"
+#include "ElementTraversal.h"
 #include "EventNames.h"
 #include "FloatRect.h"
 #include "Frame.h"
@@ -62,7 +63,6 @@
 #include "LocalizedStrings.h"
 #include "MathMLNames.h"
 #include "NodeList.h"
-#include "NodeTraversal.h"
 #include "Page.h"
 #include "ProgressTracker.h"
 #include "RenderButton.h"

Source/WebCore/css/StyleInvalidationAnalysis.cpp
@@ -28,7 +28,7 @@
 
 #include "CSSSelectorList.h"
 #include "Document.h"
-#include "NodeTraversal.h"
+#include "ElementTraversal.h"
 #include "StyleRuleImport.h"
 #include "StyleSheetContents.h"
 #include "StyledElement.h"

Source/WebCore/dom/Document.cpp
@@ -60,6 +60,7 @@
 #include "Editor.h"
 #include "Element.h"
 #include "ElementShadow.h"
+#include "ElementTraversal.h"
 #include "EntityReference.h"
 #include "Event.h"
 #include "EventFactory.h"
@@ -113,7 +114,6 @@
 #include "NodeFilter.h"
 #include "NodeIterator.h"
 #include "NodeRareData.h"
-#include "NodeTraversal.h"
 #include "NodeWithIndex.h"
 #include "Page.h"
 #include "PageConsole.h"

Source/WebCore/dom/DocumentOrderedMap.cpp
@@ -32,11 +32,11 @@
 #include "DocumentOrderedMap.h"
 
 #include "Element.h"
+#include "ElementTraversal.h"
 #include "HTMLLabelElement.h"
 #include "HTMLMapElement.h"
 #include "HTMLNameCollection.h"
 #include "HTMLNames.h"
-#include "NodeTraversal.h"
 #include "TreeScope.h"
 
 namespace WebCore {

commit 9bab5fd15035cd5ed2640338daa8fab64ffd873c

REGRESSION(r153939) Fix typo in Qt build-files
Reviewed by Antti Koivisto.

Fix simple typo in Target.pri introduced in r153939.

* Target.pri:

Identifier: 138141@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@154482 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Target.pri
@@ -1612,7 +1612,7 @@ HEADERS += \
     dom/DatasetDOMStringMap.h \
     dom/Element.h \
     dom/ElementData.h \
-    dom/ElementTravesal.h \
+    dom/ElementTraversal.h \
     dom/Entity.h \
     dom/EntityReference.h \
     dom/Event.h \