night-codes/WebKit

commit c0caf070fffd4592be875718b2f667f45ba90bd3

FTL should support PutById https://bugs.webkit.org/show_bug.cgi?id=123784
Reviewed by Geoffrey Garen.

* ftl/FTLAbbreviations.h:
(JSC::FTL::buildCall):
* ftl/FTLCapabilities.cpp:
(JSC::FTL::canCompile):
* ftl/FTLCompile.cpp:
(JSC::FTL::generateICFastPath):
(JSC::FTL::fixFunctionBasedOnStackMaps):
* ftl/FTLInlineCacheDescriptor.h:
(JSC::FTL::InlineCacheDescriptor::InlineCacheDescriptor):
(JSC::FTL::GetByIdDescriptor::GetByIdDescriptor):
(JSC::FTL::PutByIdDescriptor::PutByIdDescriptor):
(JSC::FTL::PutByIdDescriptor::ecmaMode):
(JSC::FTL::PutByIdDescriptor::putKind):
* ftl/FTLIntrinsicRepository.h:
* ftl/FTLLowerDFGToLLVM.cpp:
(JSC::FTL::LowerDFGToLLVM::compileNode):
(JSC::FTL::LowerDFGToLLVM::compilePutById):
* ftl/FTLOutput.h:
(JSC::FTL::Output::call):
* ftl/FTLSlowPathCall.cpp:
(JSC::FTL::callOperation):
* ftl/FTLSlowPathCall.h:
* ftl/FTLState.h:
* jit/CCallHelpers.h:
(JSC::CCallHelpers::setupArguments):
* runtime/Executable.h:
(JSC::ScriptExecutable::ecmaMode):



Identifier: 142008@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@158681 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/jit/CCallHelpers.h
@@ -913,6 +913,13 @@ public:
         move(arg4, GPRInfo::argumentGPR3);
     }
     
+    ALWAYS_INLINE void setupArguments(GPRReg arg1, TrustedImmPtr arg2, GPRReg arg3, GPRReg arg4, TrustedImmPtr arg5)
+    {
+        setupThreeStubArgsGPR<GPRInfo::argumentGPR0, GPRInfo::argumentGPR2, GPRInfo::argumentGPR3>(arg1, arg3, arg4);
+        move(arg2, GPRInfo::argumentGPR1);
+        move(arg5, GPRInfo::argumentGPR4);
+    }
+    
     ALWAYS_INLINE void setupArguments(TrustedImmPtr arg1)
     {
         move(arg1, GPRInfo::argumentGPR0);

Source/JavaScriptCore/ftl/FTLAbbreviations.h
@@ -259,6 +259,16 @@ static inline LValue buildCall(LBuilder builder, LValue function, LValue arg1, L
     LValue args[] = { arg1, arg2, arg3, arg4, arg5, arg6 };
     return buildCall(builder, function, args, 6);
 }
+static inline LValue buildCall(LBuilder builder, LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4, LValue arg5, LValue arg6, LValue arg7)
+{
+    LValue args[] = { arg1, arg2, arg3, arg4, arg5, arg6, arg7 };
+    return buildCall(builder, function, args, 7);
+}
+static inline LValue buildCall(LBuilder builder, LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4, LValue arg5, LValue arg6, LValue arg7, LValue arg8)
+{
+    LValue args[] = { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 };
+    return buildCall(builder, function, args, 8);
+}
 enum TailCallMode { IsNotTailCall, IsTailCall };
 static inline void setTailCall(LValue call, TailCallMode mode) { llvm->SetTailCall(call, mode == IsTailCall); }
 static inline LValue buildExtractValue(LBuilder builder, LValue aggVal, unsigned index) { return llvm->BuildExtractValue(builder, aggVal, index, ""); }

Source/JavaScriptCore/ftl/FTLCapabilities.cpp
@@ -99,6 +99,7 @@ inline CapabilityLevel canCompile(Node* node)
         // These are OK.
         break;
     case GetById:
+    case PutById:
         if (node->child1().useKind() == CellUse)
             break;
         return CannotCompile;

Source/JavaScriptCore/ftl/FTLCompile.cpp
@@ -106,6 +106,42 @@ static void dumpDataSection(RefCountedArray<LSectionWord> section, const char* p
     }
 }
 
+template<typename DescriptorType>
+void generateICFastPath(
+    State& state, CodeBlock* codeBlock, GeneratedFunction generatedFunction,
+    StackMaps::RecordMap& recordMap, DescriptorType& ic, size_t sizeOfIC)
+{
+    VM& vm = state.graph.m_vm;
+
+    StackMaps::RecordMap::iterator iter = recordMap.find(ic.stackmapID());
+    if (iter == recordMap.end()) {
+        // It was optimized out.
+        return;
+    }
+    
+    StackMaps::Record& record = iter->value;
+
+    CCallHelpers fastPathJIT(&vm, codeBlock);
+    ic.m_generator.generateFastPath(fastPathJIT);
+
+    char* startOfIC =
+        bitwise_cast<char*>(generatedFunction) + record.instructionOffset;
+            
+    LinkBuffer linkBuffer(vm, &fastPathJIT, startOfIC, sizeOfIC);
+    // Note: we could handle the !isValid() case. We just don't appear to have a
+    // reason to do so, yet.
+    RELEASE_ASSERT(linkBuffer.isValid());
+    
+    state.finalizer->sideCodeLinkBuffer->link(
+        ic.m_slowPathDone, CodeLocationLabel(startOfIC + sizeOfIC));
+            
+    linkBuffer.link(
+        ic.m_generator.slowPathJump(),
+        state.finalizer->sideCodeLinkBuffer->locationOf(ic.m_generator.slowPathBegin()));
+            
+    ic.m_generator.finalize(linkBuffer, *state.finalizer->sideCodeLinkBuffer);
+}
+
 static void fixFunctionBasedOnStackMaps(
     State& state, CodeBlock* codeBlock, JITCode* jitCode, GeneratedFunction generatedFunction,
     StackMaps::RecordMap& recordMap)
@@ -138,7 +174,7 @@ static void fixFunctionBasedOnStackMaps(
         state.finalizer->exitThunksLinkBuffer = linkBuffer.release();
     }
 
-    if (!state.getByIds.isEmpty()) {
+    if (!state.getByIds.isEmpty() || !state.putByIds.isEmpty()) {
         CCallHelpers slowPathJIT(&vm, codeBlock);
         
         for (unsigned i = state.getByIds.size(); i--;) {
@@ -177,40 +213,55 @@ static void fixFunctionBasedOnStackMaps(
             getById.m_generator = gen;
         }
         
-        state.finalizer->sideCodeLinkBuffer = adoptPtr(
-            new LinkBuffer(vm, &slowPathJIT, codeBlock, JITCompilationMustSucceed));
-        
-        for (unsigned i = state.getByIds.size(); i--;) {
-            GetByIdDescriptor& getById = state.getByIds[i];
+        for (unsigned i = state.putByIds.size(); i--;) {
+            PutByIdDescriptor& putById = state.putByIds[i];
             
-            StackMaps::RecordMap::iterator iter = recordMap.find(getById.stackmapID());
+            StackMaps::RecordMap::iterator iter = recordMap.find(putById.stackmapID());
             if (iter == recordMap.end()) {
                 // It was optimized out.
                 continue;
             }
             
             StackMaps::Record& record = iter->value;
+            
+            UNUSED_PARAM(record); // FIXME: use AnyRegs.
 
-            CCallHelpers fastPathJIT(&vm, codeBlock);
-            getById.m_generator.generateFastPath(fastPathJIT);
-
-            char* startOfIC =
-                bitwise_cast<char*>(generatedFunction) + record.instructionOffset;
-            size_t sizeOfIC = sizeOfGetById();
+            // FIXME: LLVM should tell us which registers are live.
+            RegisterSet usedRegisters = RegisterSet::allRegisters();
             
-            LinkBuffer linkBuffer(vm, &fastPathJIT, startOfIC, sizeOfIC);
-            // Note: we could handle the !isValid() case. We just don't appear to have a
-            // reason to do so, yet.
-            RELEASE_ASSERT(linkBuffer.isValid());
+            GPRReg callFrameRegister = GPRInfo::argumentGPR0;
+            GPRReg base = GPRInfo::argumentGPR1;
+            GPRReg value = GPRInfo::argumentGPR2;
             
-            state.finalizer->sideCodeLinkBuffer->link(
-                getById.m_slowPathDone, CodeLocationLabel(startOfIC + sizeOfIC));
+            JITPutByIdGenerator gen(
+                codeBlock, putById.codeOrigin(), usedRegisters, JSValueRegs(base),
+                JSValueRegs(value), GPRInfo::argumentGPR3, false, putById.ecmaMode(),
+                putById.putKind());
             
-            linkBuffer.link(
-                getById.m_generator.slowPathJump(),
-                state.finalizer->sideCodeLinkBuffer->locationOf(getById.m_generator.slowPathBegin()));
+            MacroAssembler::Label begin = slowPathJIT.label();
             
-            getById.m_generator.finalize(linkBuffer, *state.finalizer->sideCodeLinkBuffer);
+            MacroAssembler::Call call = callOperation(
+                state, usedRegisters, slowPathJIT, gen.slowPathFunction(), callFrameRegister,
+                gen.stubInfo(), value, base, putById.uid());
+            
+            gen.reportSlowPathCall(begin, call);
+            
+            putById.m_slowPathDone = slowPathJIT.jump();
+            putById.m_generator = gen;
+        }
+        
+        state.finalizer->sideCodeLinkBuffer = adoptPtr(
+            new LinkBuffer(vm, &slowPathJIT, codeBlock, JITCompilationMustSucceed));
+        
+        for (unsigned i = state.getByIds.size(); i--;) {
+            generateICFastPath(
+                state, codeBlock, generatedFunction, recordMap, state.getByIds[i],
+                sizeOfGetById());
+        }
+        for (unsigned i = state.putByIds.size(); i--;) {
+            generateICFastPath(
+                state, codeBlock, generatedFunction, recordMap, state.putByIds[i],
+                sizeOfPutById());
         }
     }
     

Source/JavaScriptCore/ftl/FTLInlineCacheDescriptor.h
@@ -39,14 +39,7 @@ class InlineCacheDescriptor {
 public:
     InlineCacheDescriptor() { }
     
-    MacroAssembler::Jump m_slowPathDone;
-};
-
-class GetByIdDescriptor : public InlineCacheDescriptor {
-public:
-    GetByIdDescriptor() { }
-    
-    GetByIdDescriptor(unsigned stackmapID, CodeOrigin codeOrigin, StringImpl* uid)
+    InlineCacheDescriptor(unsigned stackmapID, CodeOrigin codeOrigin, StringImpl* uid)
         : m_stackmapID(stackmapID)
         , m_codeOrigin(codeOrigin)
         , m_uid(uid)
@@ -61,9 +54,44 @@ private:
     unsigned m_stackmapID;
     CodeOrigin m_codeOrigin;
     StringImpl* m_uid;
-
+    
 public:
+    MacroAssembler::Jump m_slowPathDone;
+};
+
+class GetByIdDescriptor : public InlineCacheDescriptor {
+public:
+    GetByIdDescriptor() { }
+    
+    GetByIdDescriptor(unsigned stackmapID, CodeOrigin codeOrigin, StringImpl* uid)
+        : InlineCacheDescriptor(stackmapID, codeOrigin, uid)
+    {
+    }
+    
     JITGetByIdGenerator m_generator;
+};
+
+class PutByIdDescriptor : public InlineCacheDescriptor {
+public:
+    PutByIdDescriptor() { }
+    
+    PutByIdDescriptor(
+        unsigned stackmapID, CodeOrigin codeOrigin, StringImpl* uid,
+        ECMAMode ecmaMode, PutKind putKind)
+        : InlineCacheDescriptor(stackmapID, codeOrigin, uid)
+        , m_ecmaMode(ecmaMode)
+        , m_putKind(putKind)
+    {
+    }
+    
+    JITPutByIdGenerator m_generator;
+    
+    ECMAMode ecmaMode() const { return m_ecmaMode; }
+    PutKind putKind() const { return m_putKind; }
+
+private:
+    ECMAMode m_ecmaMode;
+    PutKind m_putKind;
 };
 
 } } // namespace JSC::FTL

Source/JavaScriptCore/ftl/FTLIntrinsicRepository.h
@@ -45,6 +45,7 @@ namespace JSC { namespace FTL {
     macro(subWithOverflow32, "llvm.ssub.with.overflow.i32", functionType(structType(m_context, int32, boolean), int32, int32)) \
     macro(subWithOverflow64, "llvm.ssub.with.overflow.i64", functionType(structType(m_context, int64, boolean), int64, int64)) \
     macro(patchpointInt64, "llvm.experimental.patchpoint.i64", functionType(int64, int32, int32, ref8, int32, Variadic)) \
+    macro(patchpointVoid, "llvm.experimental.patchpoint.void", functionType(voidType, int32, int32, ref8, int32, Variadic)) \
     macro(stackmap, "llvm.experimental.stackmap", functionType(voidType, int32, int32, Variadic)) \
     macro(trap, "llvm.trap", functionType(voidType)) \
     macro(osrExit, "webkit_osr_exit", functionType(voidType, boolean, int32, Variadic))

Source/JavaScriptCore/ftl/FTLLowerDFGToLLVM.cpp
@@ -343,6 +343,9 @@ private:
         case GetById:
             compileGetById();
             break;
+        case PutById:
+            compilePutById();
+            break;
         case GetButterfly:
             compileGetButterfly();
             break;
@@ -1257,6 +1260,28 @@ private:
             constNull(m_out.ref8), m_out.constInt32(2), m_callFrame, base));
         
         m_ftlState.getByIds.append(GetByIdDescriptor(stackmapID, m_node->codeOrigin, uid));
+    }
+    
+    void compilePutById()
+    {
+        // See above; CellUse is easier so we do only that for now.
+        ASSERT(m_node->child1().useKind() == CellUse);
+
+        LValue base = lowCell(m_node->child1());
+        LValue value = lowJSValue(m_node->child2());
+        StringImpl* uid = m_graph.identifiers()[m_node->identifierNumber()];
+
+        // Arguments: id, bytes, target, numArgs, args...
+        unsigned stackmapID = m_stackmapIDs++;
+        m_out.call(
+            m_out.patchpointVoidIntrinsic(),
+            m_out.constInt32(stackmapID), m_out.constInt32(sizeOfPutById()),
+            constNull(m_out.ref8), m_out.constInt32(3), m_callFrame, base, value);
+        
+        m_ftlState.putByIds.append(PutByIdDescriptor(
+            stackmapID, m_node->codeOrigin, uid,
+            m_graph.executableFor(m_node->codeOrigin)->ecmaMode(),
+            m_node->op() == PutByIdDirect ? Direct : NotDirect));
     }
     
     void compileGetButterfly()

Source/JavaScriptCore/ftl/FTLOutput.h
@@ -350,6 +350,8 @@ public:
     LValue call(LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4) { return buildCall(m_builder, function, arg1, arg2, arg3, arg4); }
     LValue call(LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4, LValue arg5) { return buildCall(m_builder, function, arg1, arg2, arg3, arg4, arg5); }
     LValue call(LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4, LValue arg5, LValue arg6) { return buildCall(m_builder, function, arg1, arg2, arg3, arg4, arg5, arg6); }
+    LValue call(LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4, LValue arg5, LValue arg6, LValue arg7) { return buildCall(m_builder, function, arg1, arg2, arg3, arg4, arg5, arg6, arg7); }
+    LValue call(LValue function, LValue arg1, LValue arg2, LValue arg3, LValue arg4, LValue arg5, LValue arg6, LValue arg7, LValue arg8) { return buildCall(m_builder, function, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8); }
     
     template<typename FunctionType>
     LValue operation(FunctionType function)

commit 2925ccb58839a54f14a2b4e78b0ba22d44a015bf

Fix build for architectures with 4 argument registers (broken since r158681). https://bugs.webkit.org/show_bug.cgi?id=123826
Patch by Julien Brianceau <jbriance@cisco.com> on 2013-11-05
Reviewed by Michael Saboff.

* jit/CCallHelpers.h:
(JSC::CCallHelpers::setupArgumentsWithExecState):
(JSC::CCallHelpers::setupArguments):

Identifier: 142019@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@158692 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/jit/CCallHelpers.h
@@ -913,13 +913,6 @@ public:
         move(arg4, GPRInfo::argumentGPR3);
     }
     
-    ALWAYS_INLINE void setupArguments(GPRReg arg1, TrustedImmPtr arg2, GPRReg arg3, GPRReg arg4, TrustedImmPtr arg5)
-    {
-        setupThreeStubArgsGPR<GPRInfo::argumentGPR0, GPRInfo::argumentGPR2, GPRInfo::argumentGPR3>(arg1, arg3, arg4);
-        move(arg2, GPRInfo::argumentGPR1);
-        move(arg5, GPRInfo::argumentGPR4);
-    }
-    
     ALWAYS_INLINE void setupArguments(TrustedImmPtr arg1)
     {
         move(arg1, GPRInfo::argumentGPR0);
@@ -1463,6 +1456,13 @@ public:
         move(arg1, GPRInfo::argumentGPR1);
         move(arg4, GPRInfo::argumentGPR4);
         move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
+    }
+
+    ALWAYS_INLINE void setupArguments(GPRReg arg1, TrustedImmPtr arg2, GPRReg arg3, GPRReg arg4, TrustedImmPtr arg5)
+    {
+        setupThreeStubArgsGPR<GPRInfo::argumentGPR0, GPRInfo::argumentGPR2, GPRInfo::argumentGPR3>(arg1, arg3, arg4);
+        move(arg2, GPRInfo::argumentGPR1);
+        move(arg5, GPRInfo::argumentGPR4);
     }
 #endif
 