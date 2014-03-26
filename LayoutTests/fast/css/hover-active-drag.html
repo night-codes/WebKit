night-codes/WebKit

commit d2cfe7a786429645d7795a577f49cebbcbf3a915

:active style is not cleared when its display property is set to none before mouse released. https://bugs.webkit.org/show_bug.cgi?id=129465
Patch by Sanghyup Lee <sh53.lee@samsung.com> on 2014-03-03
Reviewed by Antonio Gomes.

Source/WebCore:

We currently clearing the :active style when element has a renderer.
This patch makes elements clear its active style regardless of renderer.

* dom/Document.cpp:
(WebCore::Document::updateHoverActiveState):

LayoutTests:

* fast/css/active-display-none-expected.txt: Added.
* fast/css/active-display-none.html: Added.

Identifier: 147703@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@165037 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -5835,12 +5835,9 @@ void Document::updateHoverActiveState(const HitTestRequest& request, Element* in
     Element* oldActiveElement = m_activeElement.get();
     if (oldActiveElement && !request.active()) {
         // We are clearing the :active chain because the mouse has been released.
-        for (RenderElement* curr = oldActiveElement->renderer(); curr; curr = curr->parent()) {
-            Element* element = curr->element();
-            if (!element)
-                continue;
-            element->setActive(false);
-            m_userActionElements.setInActiveChain(element, false);
+        for (Element* curr = oldActiveElement; curr; curr = curr->parentElement()) {
+            curr->setActive(false);
+            m_userActionElements.setInActiveChain(curr, false);
         }
         m_activeElement.clear();
     } else {

LayoutTests/fast/css/active-display-none-expected.txt
@@ -1,0 +1,13 @@
+:active style should be cleared even if display property is set to none before mouse released.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+PASS background is "rgb(255, 0, 0)"
+PASS background is "rgb(255, 0, 0)"
+PASS background is "rgb(0, 0, 0)"
+PASS background is "rgb(0, 0, 0)"
+
+
+
+
+

LayoutTests/fast/css/active-display-none.html
@@ -1,0 +1,53 @@
+<!DOCTYPE html>
+<style>
+  div { background: rgb(0, 0, 0); }
+  div:active { background: rgb(255, 0, 0); }
+  #parent {
+    width: 200px;
+    height: 200px;
+    border: 2px solid rgb(0, 0, 255);
+  }
+  #child {
+    width: 100px;
+    height: 100px;
+    border: 2px solid rgb(0, 0, 255);
+  }
+</style>
+
+<body>
+  <div id="parent">
+    <div id="child"></div>
+  </div>
+  <pre id="description"></div>
+  <pre id="console"></pre>
+</body>
+
+<script src="../../resources/js-test-pre.js"></script>
+<script>
+  function shouldHaveBackground(element, bg) {
+    background = getComputedStyle(element, null).getPropertyValue("background-color")
+    shouldBeEqualToString('background', bg)
+  }
+
+  if (window.testRunner) {
+    description(":active style should be cleared even if display property is set to none before mouse released.")
+    var child = document.getElementById('child')
+    var parent = document.getElementById('parent')
+    testRunner.dumpAsText();
+
+    // Move and down into the child box.
+    eventSender.mouseMoveTo(50, 50)
+    eventSender.mouseDown()
+    shouldHaveBackground(child, 'rgb(255, 0, 0)')
+    shouldHaveBackground(parent, 'rgb(255, 0, 0)')
+
+    // With the mouse still down, set parent box to display:none.
+    parent.style.display = "none";
+
+    // Mouse up to clear active style.
+    eventSender.mouseUp()
+    parent.style.display = "block";
+    shouldHaveBackground(child, 'rgb(0, 0, 0)')
+    shouldHaveBackground(parent, 'rgb(0, 0, 0)')
+  }
+</script>

commit 3b04fab5e022b482497d71082d2b5a6deba9a845

When the mouse is upped after dragged out of shadowDOM, it should lose :active. https://bugs.webkit.org/show_bug.cgi?id=130660
Patch by Sanghyup Lee <sh53.lee@samsung.com> on 2014-03-25
Reviewed by Darin Adler.

Source/WebCore:

This caused a regression after r165037.
When we have to clear :active style of shadow DOM, we should clear host's style.
This patch replaces parentElement() by parentOrShadowHostElement().

* dom/Document.cpp:
(WebCore::Document::updateHoverActiveState):

LayoutTests:

* fast/css/hover-active-drag-expected.txt: Added a test for textarea.
* fast/css/hover-active-drag.html: Updated.

Identifier: 148799@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@166277 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -5841,7 +5841,7 @@ void Document::updateHoverActiveState(const HitTestRequest& request, Element* in
     Element* oldActiveElement = m_activeElement.get();
     if (oldActiveElement && !request.active()) {
         // We are clearing the :active chain because the mouse has been released.
-        for (Element* curr = oldActiveElement; curr; curr = curr->parentElement()) {
+        for (Element* curr = oldActiveElement; curr; curr = curr->parentOrShadowHostElement()) {
             curr->setActive(false);
             m_userActionElements.setInActiveChain(curr, false);
         }

LayoutTests/fast/css/hover-active-drag-expected.txt
@@ -1,3 +1,4 @@
+
 Dragging out of an element should cause it to lose :hover
 
 On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
@@ -11,6 +12,10 @@ PASS background is "rgb(0, 0, 0)"
 PASS background is "rgb(0, 255, 0)"
 PASS background is "rgb(0, 0, 0)"
 PASS background is "rgb(0, 0, 0)"
+PASS background is "rgb(0, 0, 0)"
+PASS background is "rgb(255, 0, 0)"
+PASS background is "rgb(255, 255, 0)"
+PASS background is "rgb(0, 255, 0)"
 PASS background is "rgb(0, 0, 0)"
 
 

LayoutTests/fast/css/hover-active-drag.html
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <style>
-  div { background: rgb(0, 0, 0); }
-  div:hover { background: rgb(255, 0, 0); }
-  div:hover:active { background: rgb(255, 255, 0); }
-  div:active { background: rgb(0, 255, 0); }
-  div {
+  div, textarea { background: rgb(0, 0, 0); }
+  div:hover, textarea:hover { background: rgb(255, 0, 0); }
+  div:hover:active, textarea:hover:active { background: rgb(255, 255, 0); }
+  div:active, textarea:active { background: rgb(0, 255, 0); }
+  div, textarea {
     width: 100px;
     height: 100px;
     border: 2px solid rgb(0, 0, 255);
@@ -14,6 +14,7 @@
 <body>
   <div id="box"></div>
   <div id="box2"></div>
+  <textarea id="textarea"></textarea>
   <pre id="description"></div>
   <pre id="console"></pre>
 </body>
@@ -59,5 +60,18 @@
     eventSender.mouseUp()
     shouldHaveBackground(box, 'rgb(0, 0, 0)')
     shouldHaveBackground(box2, 'rgb(0, 0, 0)')
+
+    // Move into the textarea.
+    eventSender.mouseMoveTo(50, 250)
+    shouldHaveBackground(textarea, 'rgb(255, 0, 0)')
+
+    eventSender.mouseDown()
+    shouldHaveBackground(textarea, 'rgb(255, 255, 0)')
+
+    eventSender.mouseMoveTo(400, 250)
+    shouldHaveBackground(textarea, 'rgb(0, 255, 0)')
+
+    eventSender.mouseUp()
+    shouldHaveBackground(textarea, 'rgb(0, 0, 0)')
   }
 </script>