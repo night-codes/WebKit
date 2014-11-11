night-codes/WebKit

commit 56fe7c6e4c9b456a7dbda88e7dee847a8996bfea

Use std::unique_ptr<> and std::make_unique<> in WebCore https://bugs.webkit.org/show_bug.cgi?id=138560
Reviewed by Chris Dumez.

Clean up unused OwnPtr.h and PassOwnPtr.h and use std::unique_ptr.

No new tests, no behavior changes.

* Modules/encryptedmedia/CDMPrivateMediaPlayer.h:
* dom/ContainerNode.h:
* dom/ElementIteratorAssertions.h:
(WebCore::ElementIteratorAssertions::ElementIteratorAssertions):
* dom/ElementRareData.h:
* page/DragController.h:
* rendering/RenderView.h:


Identifier: 156422@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175844 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/encryptedmedia/CDMPrivateMediaPlayer.h
@@ -27,7 +27,6 @@
 #define CDMPrivateMediaPlayer_h
 
 #include "CDMPrivate.h"
-#include <wtf/PassOwnPtr.h>
 #include <wtf/RetainPtr.h>
 
 #if ENABLE(ENCRYPTED_MEDIA_V2)

Source/WebCore/dom/ContainerNode.h
@@ -27,7 +27,6 @@
 #include "ExceptionCodePlaceholder.h"
 #include "Node.h"
 
-#include <wtf/OwnPtr.h>
 #include <wtf/Vector.h>
 
 namespace WebCore {

Source/WebCore/dom/ElementIteratorAssertions.h
@@ -41,7 +41,7 @@ public:
 private:
     const Document* m_document;
     uint64_t m_initialDOMTreeVersion;
-    OwnPtr<NoEventDispatchAssertion> m_noEventDispatchAssertion;
+    std::unique_ptr<NoEventDispatchAssertion> m_noEventDispatchAssertion;
 };
 
 inline ElementIteratorAssertions::ElementIteratorAssertions()
@@ -53,7 +53,7 @@ inline ElementIteratorAssertions::ElementIteratorAssertions()
 inline ElementIteratorAssertions::ElementIteratorAssertions(const Element* first)
     : m_document(first ? &first->document() : nullptr)
     , m_initialDOMTreeVersion(m_document ? m_document->domTreeVersion() : 0)
-    , m_noEventDispatchAssertion(m_document ? adoptPtr(new NoEventDispatchAssertion) : nullptr)
+    , m_noEventDispatchAssertion(m_document ? std::make_unique<NoEventDispatchAssertion>() : nullptr)
 {
 }
 

Source/WebCore/dom/ElementRareData.h
@@ -30,7 +30,6 @@
 #include "RenderElement.h"
 #include "ShadowRoot.h"
 #include "StyleInheritedData.h"
-#include <wtf/OwnPtr.h>
 
 namespace WebCore {
 

Source/WebCore/page/DragController.h
@@ -55,8 +55,6 @@ namespace WebCore {
         DragController(Page&, DragClient&);
         ~DragController();
 
-        static PassOwnPtr<DragController> create(Page*, DragClient*);
-
         DragClient& client() const { return m_client; }
 
         WEBCORE_EXPORT DragOperation dragEntered(DragData&);

Source/WebCore/rendering/RenderView.h
@@ -30,7 +30,6 @@
 #include "SelectionSubtreeRoot.h"
 #include <memory>
 #include <wtf/HashSet.h>
-#include <wtf/OwnPtr.h>
 
 #if ENABLE(SERVICE_CONTROLS)
 #include "SelectionRectGatherer.h"

commit 1384466e2a3843e1003d26f7363c2ccd2e0c9acd

Unreviewed, rolling out r175844.
Win debug build has broken since r175844.

Reverted changeset:

"Use std::unique_ptr<> and std::make_unique<> in WebCore"
https://bugs.webkit.org/show_bug.cgi?id=138560
http://trac.webkit.org/changeset/175844

Identifier: 156441@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@175939 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/encryptedmedia/CDMPrivateMediaPlayer.h
@@ -27,6 +27,7 @@
 #define CDMPrivateMediaPlayer_h
 
 #include "CDMPrivate.h"
+#include <wtf/PassOwnPtr.h>
 #include <wtf/RetainPtr.h>
 
 #if ENABLE(ENCRYPTED_MEDIA_V2)

Source/WebCore/dom/ContainerNode.h
@@ -27,6 +27,7 @@
 #include "ExceptionCodePlaceholder.h"
 #include "Node.h"
 
+#include <wtf/OwnPtr.h>
 #include <wtf/Vector.h>
 
 namespace WebCore {

Source/WebCore/dom/ElementIteratorAssertions.h
@@ -41,7 +41,7 @@ public:
 private:
     const Document* m_document;
     uint64_t m_initialDOMTreeVersion;
-    std::unique_ptr<NoEventDispatchAssertion> m_noEventDispatchAssertion;
+    OwnPtr<NoEventDispatchAssertion> m_noEventDispatchAssertion;
 };
 
 inline ElementIteratorAssertions::ElementIteratorAssertions()
@@ -53,7 +53,7 @@ inline ElementIteratorAssertions::ElementIteratorAssertions()
 inline ElementIteratorAssertions::ElementIteratorAssertions(const Element* first)
     : m_document(first ? &first->document() : nullptr)
     , m_initialDOMTreeVersion(m_document ? m_document->domTreeVersion() : 0)
-    , m_noEventDispatchAssertion(m_document ? std::make_unique<NoEventDispatchAssertion>() : nullptr)
+    , m_noEventDispatchAssertion(m_document ? adoptPtr(new NoEventDispatchAssertion) : nullptr)
 {
 }
 

Source/WebCore/dom/ElementRareData.h
@@ -30,6 +30,7 @@
 #include "RenderElement.h"
 #include "ShadowRoot.h"
 #include "StyleInheritedData.h"
+#include <wtf/OwnPtr.h>
 
 namespace WebCore {
 

Source/WebCore/page/DragController.h
@@ -55,6 +55,8 @@ namespace WebCore {
         DragController(Page&, DragClient&);
         ~DragController();
 
+        static PassOwnPtr<DragController> create(Page*, DragClient*);
+
         DragClient& client() const { return m_client; }
 
         WEBCORE_EXPORT DragOperation dragEntered(DragData&);

Source/WebCore/rendering/RenderView.h
@@ -30,6 +30,7 @@
 #include "SelectionSubtreeRoot.h"
 #include <memory>
 #include <wtf/HashSet.h>
+#include <wtf/OwnPtr.h>
 
 #if ENABLE(SERVICE_CONTROLS)
 #include "SelectionRectGatherer.h"