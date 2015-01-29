night-codes/WebKit

commit 4ffffead024c9d7ea38fec80110e0ba703d1135e

Fix Border-radius clipping issue on a composited descendants https://bugs.webkit.org/show_bug.cgi?id=138551
Patch by Byungseon Shin <sun.shin@lge.com> on 2015-01-07
Reviewed by Simon Fraser.

Fix clipping compositing descendants of an accelerated layer having
border radius and clip overflow issue by using layer corner-radius
or a CAShapeLayer mask on Mac, and setting up a separate mask layer
on the childContainmentLayer on other platforms.

This patch is extracted from following Blink revisions:
<http://src.chromium.org/viewvc/blink?view=revision&revision=162463>
<http://src.chromium.org/viewvc/blink?view=revision&revision=158258>
by <rosca@adobe.com>

Clip accelerated descendants of an accelerated layer having border radius and clip overflow.
Large canvas does not honor containing div's border radius.

and <http://src.chromium.org/viewvc/blink?view=revision&revision=160578>
by <junov@chromium.org>

Fix for CSS clip-path with accelerated 2D canvas.

* WebCore.exp.in:
* platform/graphics/GraphicsLayer.cpp:
(WebCore::GraphicsLayer::dumpProperties):
* platform/graphics/GraphicsLayer.h:
(WebCore::GraphicsLayer::applyClippingBorder):
(WebCore::GraphicsLayer::clearClippingBorder):
(WebCore::GraphicsLayer::needsClippingMaskLayer):
* platform/graphics/GraphicsLayerClient.h:
* platform/graphics/ca/GraphicsLayerCA.cpp:
(WebCore::GraphicsLayerCA::applyClippingBorder):
(WebCore::GraphicsLayerCA::clearClippingBorder):
* platform/graphics/ca/GraphicsLayerCA.h:
* rendering/PaintPhase.h:
* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::paintObject):
* rendering/RenderBox.cpp:
(WebCore::RenderBox::paintClippingMask):
* rendering/RenderBox.h:
* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::paintLayerContents):
(WebCore::RenderLayer::paintChildClippingMaskForFragments):
* rendering/RenderLayer.h:
* rendering/RenderLayerBacking.cpp:
(WebCore::RenderLayerBacking::~RenderLayerBacking):
(WebCore::RenderLayerBacking::destroyGraphicsLayers):

Source/WebCore/WebCore.exp.in
@@ -589,6 +589,8 @@ __ZN7WebCore15GraphicsLayerCA18setContentsVisibleEb
 __ZN7WebCore15GraphicsLayerCA18setOpacityInternalEf
 __ZN7WebCore15GraphicsLayerCA18setReplicatedLayerEPNS_13GraphicsLayerE
 __ZN7WebCore15GraphicsLayerCA18setShowDebugBorderEb
+__ZN7WebCore15GraphicsLayerCA19applyClippingBorderERKNS_16FloatRoundedRectE
+__ZN7WebCore15GraphicsLayerCA19clearClippingBorderEv
 __ZN7WebCore15GraphicsLayerCA19setCustomAppearanceENS_13GraphicsLayer16CustomAppearanceE
 __ZN7WebCore15GraphicsLayerCA20setChildrenTransformERKNS_20TransformationMatrixE
 __ZN7WebCore15GraphicsLayerCA20setReplicatedByLayerEPNS_13GraphicsLayerE

Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -368,6 +368,9 @@ public:
     FloatRoundedRect contentsClippingRect() const { return m_contentsClippingRect; }
     virtual void setContentsClippingRect(const FloatRoundedRect& roundedRect) { m_contentsClippingRect = roundedRect; }
 
+    virtual bool applyClippingBorder(const FloatRoundedRect&) { return false; }
+    virtual void clearClippingBorder() { return; }
+
     // Transitions are identified by a special animation name that cannot clash with a keyframe identifier.
     static String animationNameForTransition(AnimatedPropertyID);
     
@@ -497,6 +500,8 @@ public:
     virtual bool isGraphicsLayerCA() const { return false; }
     virtual bool isGraphicsLayerCARemote() const { return false; }
     virtual bool isGraphicsLayerTextureMapper() const { return false; }
+
+    virtual bool needsClippingMaskLayer() { return true; };
 
 protected:
     // Should be called from derived class destructors. Should call willBeDestroyed() on super.

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -2027,6 +2027,38 @@ void GraphicsLayerCA::updateContentsColorLayer()
     }
 }
 
+bool GraphicsLayerCA::applyClippingBorder(const FloatRoundedRect& roundedRect)
+{
+    if (roundedRect.radii().isUniformCornerRadius()) {
+        m_layer->setMask(nullptr);
+        m_layer->setMasksToBounds(true);
+        m_layer->setCornerRadius(roundedRect.radii().topLeft().width());
+    } else {
+        if (!m_shapeMaskLayer) {
+            m_shapeMaskLayer = createPlatformCALayer(PlatformCALayer::LayerTypeShapeLayer, this);
+            m_shapeMaskLayer->setAnchorPoint(FloatPoint3D());
+        }
+
+        m_shapeMaskLayer->setPosition(FloatPoint());
+        m_shapeMaskLayer->setBounds(m_layer->bounds());
+    
+        m_layer->setCornerRadius(0);
+        m_layer->setMask(m_shapeMaskLayer.get());
+
+        FloatRoundedRect offsetClippingRoundedRect(m_layer->bounds(), roundedRect.radii());
+        m_shapeMaskLayer->setShapeRoundedRect(offsetClippingRoundedRect);
+    }
+
+    return true;
+}
+
+void GraphicsLayerCA::clearClippingBorder()
+{
+    m_layer->setCornerRadius(0);
+    m_layer->setMasksToBounds(false);
+    m_layer->setMask(nullptr);
+}
+
 // The clipping strategy depends on whether the rounded rect has equal corner radii.
 void GraphicsLayerCA::updateClippingStrategy(PlatformCALayer& clippingLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, const FloatRoundedRect& roundedRect)
 {

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -173,6 +173,9 @@ protected:
 private:
     virtual bool isGraphicsLayerCA() const override { return true; }
 
+    virtual bool applyClippingBorder(const FloatRoundedRect&) override;
+    virtual void clearClippingBorder() override;
+
     WEBCORE_EXPORT virtual void willBeDestroyed() override;
 
     // PlatformCALayerClient overrides

commit ca90ddc14bf5e4c6565270c31c89505ab85f4b9b

Border-radius clipping on a stacking context causes descendants to not render https://bugs.webkit.org/show_bug.cgi?id=140536
Reviewed by Zalan Bujtas.

Source/WebCore:

Fix one of the issues introduced in r178029. The changes in GraphicsLayerCA
failed to adhere to the "set a bit and flush later" pattern that this class uses,
instead poking the platform layers directly. This caused an issue where the bounds
of the clipping layer would later be set to 0x0, causing content to disappear.

Fix by changing the "applyClippingBorder" function to "setMasksToBoundsRect"
and have it update via a new MasksToBoundsRectChanged dirty bit.

In order to avoid clobbering the mask layer used to clip contents, we need another
shape mask layer, so rename m_shapeMaskLayer to m_contentsShapeMaskLayer, then
introduce a new m_shapeMaskLayer which is used for masks-to-bounds clipping. Update
the reflection cloning code to correctly clone this layer.

Test: compositing/clipping/border-radius-stacking-context-clip.html

* WebCore.exp.in:
* platform/graphics/GraphicsLayer.h:
(WebCore::GraphicsLayer::maskToBoundsRect):
(WebCore::GraphicsLayer::setMasksToBoundsRect):
(WebCore::GraphicsLayer::applyClippingBorder): Deleted.
(WebCore::GraphicsLayer::clearClippingBorder): Deleted.
* platform/graphics/ca/GraphicsLayerCA.cpp:
(WebCore::GraphicsLayerCA::willBeDestroyed):
(WebCore::GraphicsLayerCA::setMasksToBoundsRect):
(WebCore::GraphicsLayerCA::setContentsToSolidColor):
(WebCore::GraphicsLayerCA::commitLayerChangesBeforeSublayers):
(WebCore::GraphicsLayerCA::updateContentsImage):
(WebCore::GraphicsLayerCA::updateContentsRects):
(WebCore::GraphicsLayerCA::updateMasksToBoundsRect):
(WebCore::GraphicsLayerCA::dumpAdditionalProperties):
(WebCore::GraphicsLayerCA::applyClippingBorder): Deleted.
(WebCore::GraphicsLayerCA::clearClippingBorder): Deleted.
* platform/graphics/ca/GraphicsLayerCA.h:
* rendering/RenderLayerBacking.cpp:
(WebCore::RenderLayerBacking::updateChildClippingStrategy): Add a FIXME since

Source/WebCore/WebCore.exp.in
@@ -598,10 +598,9 @@ __ZN7WebCore15GraphicsLayerCA18setContentsVisibleEb
 __ZN7WebCore15GraphicsLayerCA18setOpacityInternalEf
 __ZN7WebCore15GraphicsLayerCA18setReplicatedLayerEPNS_13GraphicsLayerE
 __ZN7WebCore15GraphicsLayerCA18setShowDebugBorderEb
-__ZN7WebCore15GraphicsLayerCA19applyClippingBorderERKNS_16FloatRoundedRectE
-__ZN7WebCore15GraphicsLayerCA19clearClippingBorderEv
 __ZN7WebCore15GraphicsLayerCA19setCustomAppearanceENS_13GraphicsLayer16CustomAppearanceE
 __ZN7WebCore15GraphicsLayerCA20setChildrenTransformERKNS_20TransformationMatrixE
+__ZN7WebCore15GraphicsLayerCA20setMasksToBoundsRectERKNS_16FloatRoundedRectE
 __ZN7WebCore15GraphicsLayerCA20setReplicatedByLayerEPNS_13GraphicsLayerE
 __ZN7WebCore15GraphicsLayerCA21flushCompositingStateERKNS_9FloatRectE
 __ZN7WebCore15GraphicsLayerCA21setAcceleratesDrawingEb

Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -365,11 +365,14 @@ public:
     FloatRect contentsRect() const { return m_contentsRect; }
     virtual void setContentsRect(const FloatRect& r) { m_contentsRect = r; }
 
+    // Set a rounded rect that will be used to clip the layer contents.
     FloatRoundedRect contentsClippingRect() const { return m_contentsClippingRect; }
     virtual void setContentsClippingRect(const FloatRoundedRect& roundedRect) { m_contentsClippingRect = roundedRect; }
 
-    virtual bool applyClippingBorder(const FloatRoundedRect&) { return false; }
-    virtual void clearClippingBorder() { return; }
+    // Set a rounded rect that is used to clip this layer and its descendants (implies setting masksToBounds).
+    // Returns false if the platform can't support this rounded clip, and we should fall back to painting a mask.
+    FloatRoundedRect maskToBoundsRect() const { return m_masksToBoundsRect; };
+    virtual bool setMasksToBoundsRect(const FloatRoundedRect& roundedRect) { m_masksToBoundsRect = roundedRect; return false; }
 
     // Transitions are identified by a special animation name that cannot clash with a keyframe identifier.
     static String animationNameForTransition(AnimatedPropertyID);
@@ -592,6 +595,7 @@ protected:
 
     FloatRect m_contentsRect;
     FloatRoundedRect m_contentsClippingRect;
+    FloatRoundedRect m_masksToBoundsRect;
     FloatPoint m_contentsTilePhase;
     FloatSize m_contentsTileSize;
 

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -368,7 +368,13 @@ void GraphicsLayerCA::willBeDestroyed()
 
     if (m_contentsClippingLayer)
         m_contentsClippingLayer->setOwner(nullptr);
-        
+
+    if (m_contentsShapeMaskLayer)
+        m_contentsShapeMaskLayer->setOwner(nullptr);
+
+    if (m_shapeMaskLayer)
+        m_shapeMaskLayer->setOwner(nullptr);
+    
     if (m_structuralLayer)
         m_structuralLayer->setOwner(nullptr);
 
@@ -782,6 +788,16 @@ void GraphicsLayerCA::setContentsClippingRect(const FloatRoundedRect& rect)
     noteLayerPropertyChanged(ContentsRectsChanged);
 }
 
+bool GraphicsLayerCA::setMasksToBoundsRect(const FloatRoundedRect& roundedRect)
+{
+    if (roundedRect == m_masksToBoundsRect)
+        return true;
+
+    GraphicsLayer::setMasksToBoundsRect(roundedRect);
+    noteLayerPropertyChanged(MasksToBoundsRectChanged);
+    return true;
+}
+
 bool GraphicsLayerCA::shouldRepaintOnSizeChange() const
 {
     return drawsContent() && !tiledBacking();
@@ -871,7 +887,7 @@ void GraphicsLayerCA::setContentsToSolidColor(const Color& color)
             m_contentsLayerPurpose = ContentsLayerForBackgroundColor;
             m_contentsLayer = createPlatformCALayer(PlatformCALayer::LayerTypeLayer, this);
 #ifndef NDEBUG
-            m_contentsLayer->setName("Background Color Layer");
+            m_contentsLayer->setName(String::format("Background Color Layer %llu", m_contentsLayer->layerID()));
 #endif
             contentsLayerChanged = true;
         }
@@ -1365,6 +1381,9 @@ void GraphicsLayerCA::commitLayerChangesBeforeSublayers(CommitState& commitState
     
     if (m_uncommittedChanges & ContentsRectsChanged) // Needs to happen before ChildrenChanged
         updateContentsRects();
+
+    if (m_uncommittedChanges & MasksToBoundsRectChanged) // Needs to happen before ChildrenChanged
+        updateMasksToBoundsRect();
 
     if (m_uncommittedChanges & MaskLayerChanged) {
         updateMaskLayer();
@@ -1968,7 +1987,7 @@ void GraphicsLayerCA::updateContentsImage()
         if (!m_contentsLayer.get()) {
             m_contentsLayer = createPlatformCALayer(PlatformCALayer::LayerTypeLayer, this);
 #ifndef NDEBUG
-            m_contentsLayer->setName("Image Layer");
+            m_contentsLayer->setName(String::format("Image Layer %llu", m_contentsLayer->layerID()));
 #endif
             setupContentsLayer(m_contentsLayer.get());
             // m_contentsLayer will be parented by updateSublayerList
@@ -2026,38 +2045,6 @@ void GraphicsLayerCA::updateContentsColorLayer()
     }
 }
 
-bool GraphicsLayerCA::applyClippingBorder(const FloatRoundedRect& roundedRect)
-{
-    if (roundedRect.radii().isUniformCornerRadius()) {
-        m_layer->setMask(nullptr);
-        m_layer->setMasksToBounds(true);
-        m_layer->setCornerRadius(roundedRect.radii().topLeft().width());
-    } else {
-        if (!m_shapeMaskLayer) {
-            m_shapeMaskLayer = createPlatformCALayer(PlatformCALayer::LayerTypeShapeLayer, this);
-            m_shapeMaskLayer->setAnchorPoint(FloatPoint3D());
-        }
-
-        m_shapeMaskLayer->setPosition(FloatPoint());
-        m_shapeMaskLayer->setBounds(m_layer->bounds());
-    
-        m_layer->setCornerRadius(0);
-        m_layer->setMask(m_shapeMaskLayer.get());
-
-        FloatRoundedRect offsetClippingRoundedRect(m_layer->bounds(), roundedRect.radii());
-        m_shapeMaskLayer->setShapeRoundedRect(offsetClippingRoundedRect);
-    }
-
-    return true;
-}
-
-void GraphicsLayerCA::clearClippingBorder()
-{
-    m_layer->setCornerRadius(0);
-    m_layer->setMasksToBounds(false);
-    m_layer->setMask(nullptr);
-}
-
 // The clipping strategy depends on whether the rounded rect has equal corner radii.
 void GraphicsLayerCA::updateClippingStrategy(PlatformCALayer& clippingLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, const FloatRoundedRect& roundedRect)
 {
@@ -2105,7 +2092,7 @@ void GraphicsLayerCA::updateContentsRects()
             m_contentsClippingLayer = createPlatformCALayer(PlatformCALayer::LayerTypeLayer, this);
             m_contentsClippingLayer->setAnchorPoint(FloatPoint());
 #ifndef NDEBUG
-            m_contentsClippingLayer->setName("Contents Clipping");
+            m_contentsClippingLayer->setName(String::format("Contents Clipping Layer %llu", m_contentsClippingLayer->layerID()));
 #endif
             gainedOrLostClippingLayer = true;
         }
@@ -2113,7 +2100,7 @@ void GraphicsLayerCA::updateContentsRects()
         m_contentsClippingLayer->setPosition(clippingOrigin);
         m_contentsClippingLayer->setBounds(clippingBounds);
 
-        updateClippingStrategy(*m_contentsClippingLayer, m_shapeMaskLayer, m_contentsClippingRect);
+        updateClippingStrategy(*m_contentsClippingLayer, m_contentsShapeMaskLayer, m_contentsClippingRect);
 
         if (gainedOrLostClippingLayer) {
             m_contentsLayer->removeFromSuperlayer();
@@ -2132,9 +2119,9 @@ void GraphicsLayerCA::updateContentsRects()
             gainedOrLostClippingLayer = true;
         }
 
-        if (m_shapeMaskLayer) {
-            m_shapeMaskLayer->setOwner(nullptr);
-            m_shapeMaskLayer = nullptr;
+        if (m_contentsShapeMaskLayer) {
+            m_contentsShapeMaskLayer->setOwner(nullptr);
+            m_contentsShapeMaskLayer = nullptr;
         }
 
         contentOrigin = m_contentsRect.location();
@@ -2154,17 +2141,39 @@ void GraphicsLayerCA::updateContentsRects()
     }
 
     if (m_contentsClippingLayerClones) {
-        if (!m_shapeMaskLayerClones && m_shapeMaskLayer)
-            m_shapeMaskLayerClones = std::make_unique<LayerMap>();
+        if (!m_contentsShapeMaskLayerClones && m_contentsShapeMaskLayer)
+            m_contentsShapeMaskLayerClones = std::make_unique<LayerMap>();
 
         for (auto& clone : *m_contentsClippingLayerClones) {
+            CloneID cloneID = clone.key;
+            RefPtr<PlatformCALayer> shapeMaskLayerClone;
+            if (m_contentsShapeMaskLayerClones)
+                shapeMaskLayerClone = m_contentsShapeMaskLayerClones->get(cloneID);
+
+            bool hadShapeMask = shapeMaskLayerClone;
+            updateClippingStrategy(*clone.value, shapeMaskLayerClone, m_contentsClippingRect);
+
+            if (!shapeMaskLayerClone && m_contentsShapeMaskLayerClones)
+                m_contentsShapeMaskLayerClones->remove(cloneID);
+            else if (shapeMaskLayerClone && !hadShapeMask)
+                m_contentsShapeMaskLayerClones->add(cloneID, shapeMaskLayerClone);
+        }
+    }
+}
+
+void GraphicsLayerCA::updateMasksToBoundsRect()
+{
+    updateClippingStrategy(*m_layer, m_shapeMaskLayer, m_masksToBoundsRect);
+
+    if (m_layerClones) {
+        for (auto& clone : *m_layerClones) {
             CloneID cloneID = clone.key;
             RefPtr<PlatformCALayer> shapeMaskLayerClone;
             if (m_shapeMaskLayerClones)
                 shapeMaskLayerClone = m_shapeMaskLayerClones->get(cloneID);
 
             bool hadShapeMask = shapeMaskLayerClone;
-            updateClippingStrategy(*clone.value, shapeMaskLayerClone, m_contentsClippingRect);
+            updateClippingStrategy(*clone.value, shapeMaskLayerClone, m_masksToBoundsRect);
 
             if (!shapeMaskLayerClone && m_shapeMaskLayerClones)
                 m_shapeMaskLayerClones->remove(cloneID);
@@ -3082,6 +3091,7 @@ void GraphicsLayerCA::dumpAdditionalProperties(TextStream& textStream, int inden
         dumpInnerLayer(textStream, "contents clipping layer", m_contentsClippingLayer.get(), indent, behavior);
         dumpInnerLayer(textStream, "shape mask layer", m_shapeMaskLayer.get(), indent, behavior);
         dumpInnerLayer(textStream, "contents layer", m_contentsLayer.get(), indent, behavior);
+        dumpInnerLayer(textStream, "contents shape mask layer", m_contentsShapeMaskLayer.get(), indent, behavior);
         dumpInnerLayer(textStream, "backdrop layer", m_backdropLayer.get(), indent, behavior);
     }
 }
@@ -3239,7 +3249,7 @@ PassRefPtr<PlatformCALayer> GraphicsLayerCA::findOrMakeClone(CloneID cloneID, Pl
 }   
 
 void GraphicsLayerCA::ensureCloneLayers(CloneID cloneID, RefPtr<PlatformCALayer>& primaryLayer, RefPtr<PlatformCALayer>& structuralLayer,
-    RefPtr<PlatformCALayer>& contentsLayer, RefPtr<PlatformCALayer>& contentsClippingLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, CloneLevel cloneLevel)
+    RefPtr<PlatformCALayer>& contentsLayer, RefPtr<PlatformCALayer>& contentsClippingLayer, RefPtr<PlatformCALayer>& contentsShapeMaskLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, CloneLevel cloneLevel)
 {
     structuralLayer = nullptr;
     contentsLayer = nullptr;
@@ -3256,6 +3266,9 @@ void GraphicsLayerCA::ensureCloneLayers(CloneID cloneID, RefPtr<PlatformCALayer>
     if (!m_contentsClippingLayerClones && m_contentsClippingLayer)
         m_contentsClippingLayerClones = std::make_unique<LayerMap>();
 
+    if (!m_contentsShapeMaskLayerClones && m_contentsShapeMaskLayer)
+        m_contentsShapeMaskLayerClones = std::make_unique<LayerMap>();
+
     if (!m_shapeMaskLayerClones && m_shapeMaskLayer)
         m_shapeMaskLayerClones = std::make_unique<LayerMap>();
 
@@ -3263,6 +3276,7 @@ void GraphicsLayerCA::ensureCloneLayers(CloneID cloneID, RefPtr<PlatformCALayer>
     structuralLayer = findOrMakeClone(cloneID, m_structuralLayer.get(), m_structuralLayerClones.get(), cloneLevel);
     contentsLayer = findOrMakeClone(cloneID, m_contentsLayer.get(), m_contentsLayerClones.get(), cloneLevel);
     contentsClippingLayer = findOrMakeClone(cloneID, m_contentsClippingLayer.get(), m_contentsClippingLayerClones.get(), cloneLevel);
+    contentsShapeMaskLayer = findOrMakeClone(cloneID, m_contentsShapeMaskLayer.get(), m_contentsShapeMaskLayerClones.get(), cloneLevel);
     shapeMaskLayer = findOrMakeClone(cloneID, m_shapeMaskLayer.get(), m_shapeMaskLayerClones.get(), cloneLevel);
 }
 
@@ -3272,6 +3286,8 @@ void GraphicsLayerCA::removeCloneLayers()
     m_structuralLayerClones = nullptr;
     m_contentsLayerClones = nullptr;
     m_contentsClippingLayerClones = nullptr;
+    m_contentsShapeMaskLayerClones = nullptr;
+    m_shapeMaskLayerClones = nullptr;
 }
 
 FloatPoint GraphicsLayerCA::positionForCloneRootLayer() const
@@ -3303,8 +3319,9 @@ PassRefPtr<PlatformCALayer> GraphicsLayerCA::fetchCloneLayers(GraphicsLayer* rep
     RefPtr<PlatformCALayer> structuralLayer;
     RefPtr<PlatformCALayer> contentsLayer;
     RefPtr<PlatformCALayer> contentsClippingLayer;
+    RefPtr<PlatformCALayer> contentsShapeMaskLayer;
     RefPtr<PlatformCALayer> shapeMaskLayer;
-    ensureCloneLayers(replicaState.cloneID(), primaryLayer, structuralLayer, contentsLayer, contentsClippingLayer, shapeMaskLayer, cloneLevel);
+    ensureCloneLayers(replicaState.cloneID(), primaryLayer, structuralLayer, contentsLayer, contentsClippingLayer, contentsShapeMaskLayer, shapeMaskLayer, cloneLevel);
 
     if (m_maskLayer) {
         RefPtr<PlatformCALayer> maskClone = downcast<GraphicsLayerCA>(*m_maskLayer).fetchCloneLayers(replicaRoot, replicaState, IntermediateCloneLevel);
@@ -3343,8 +3360,11 @@ PassRefPtr<PlatformCALayer> GraphicsLayerCA::fetchCloneLayers(GraphicsLayer* rep
         contentsClippingLayer->appendSublayer(*contentsLayer);
     }
 
+    if (contentsShapeMaskLayer)
+        contentsClippingLayer->setMask(contentsShapeMaskLayer.get());
+
     if (shapeMaskLayer)
-        contentsClippingLayer->setMask(shapeMaskLayer.get());
+        primaryLayer->setMask(shapeMaskLayer.get());
     
     if (replicaLayer || structuralLayer || contentsLayer || contentsClippingLayer || childLayers.size() > 0) {
         if (structuralLayer) {

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -118,7 +118,8 @@ public:
     
     WEBCORE_EXPORT virtual void setContentsRect(const FloatRect&) override;
     WEBCORE_EXPORT virtual void setContentsClippingRect(const FloatRoundedRect&) override;
-    
+    WEBCORE_EXPORT virtual bool setMasksToBoundsRect(const FloatRoundedRect&);
+
     WEBCORE_EXPORT virtual void suspendAnimations(double time) override;
     WEBCORE_EXPORT virtual void resumeAnimations() override;
 
@@ -172,9 +173,6 @@ protected:
 
 private:
     virtual bool isGraphicsLayerCA() const override { return true; }
-
-    virtual bool applyClippingBorder(const FloatRoundedRect&) override;
-    virtual void clearClippingBorder() override;
 
     WEBCORE_EXPORT virtual void willBeDestroyed() override;
 
@@ -350,7 +348,7 @@ private:
     PassRefPtr<PlatformCALayer> findOrMakeClone(CloneID, PlatformCALayer *, LayerMap*, CloneLevel);
 
     void ensureCloneLayers(CloneID, RefPtr<PlatformCALayer>& primaryLayer, RefPtr<PlatformCALayer>& structuralLayer,
-        RefPtr<PlatformCALayer>& contentsLayer, RefPtr<PlatformCALayer>& contentsClippingLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, CloneLevel);
+        RefPtr<PlatformCALayer>& contentsLayer, RefPtr<PlatformCALayer>& contentsClippingLayer, RefPtr<PlatformCALayer>& contentsShapeMaskLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, CloneLevel);
 
     bool hasCloneLayers() const { return !!m_layerClones; }
     void removeCloneLayers();
@@ -374,6 +372,7 @@ private:
     void updateContentsPlatformLayer();
     void updateContentsColorLayer();
     void updateContentsRects();
+    void updateMasksToBoundsRect();
     void updateMaskLayer();
     void updateReplicatedLayers();
 
@@ -427,21 +426,22 @@ private:
         ContentsPlatformLayerChanged =  1LLU << 16,
         ContentsColorLayerChanged =     1LLU << 17,
         ContentsRectsChanged =          1LLU << 18,
-        MaskLayerChanged =              1LLU << 19,
-        ReplicatedLayerChanged =        1LLU << 20,
-        ContentsNeedsDisplay =          1LLU << 21,
-        AcceleratesDrawingChanged =     1LLU << 22,
-        ContentsScaleChanged =          1LLU << 23,
-        ContentsVisibilityChanged =     1LLU << 24,
-        VisibleRectChanged =            1LLU << 25,
-        FiltersChanged =                1LLU << 26,
-        BackdropFiltersChanged =        1LLU << 27,
-        TilingAreaChanged =             1LLU << 28,
-        TilesAdded =                    1LLU << 29,
-        DebugIndicatorsChanged =        1LLU << 30,
-        CustomAppearanceChanged =       1LLU << 31,
-        CustomBehaviorChanged =         1LLU << 32,
-        BlendModeChanged =              1LLU << 33,
+        MasksToBoundsRectChanged =      1LLU << 19,
+        MaskLayerChanged =              1LLU << 20,
+        ReplicatedLayerChanged =        1LLU << 21,
+        ContentsNeedsDisplay =          1LLU << 22,
+        AcceleratesDrawingChanged =     1LLU << 23,
+        ContentsScaleChanged =          1LLU << 24,
+        ContentsVisibilityChanged =     1LLU << 25,
+        VisibleRectChanged =            1LLU << 26,
+        FiltersChanged =                1LLU << 27,
+        BackdropFiltersChanged =        1LLU << 28,
+        TilingAreaChanged =             1LLU << 29,
+        TilesAdded =                    1LLU << 30,
+        DebugIndicatorsChanged =        1LLU << 31,
+        CustomAppearanceChanged =       1LLU << 32,
+        CustomBehaviorChanged =         1LLU << 33,
+        BlendModeChanged =              1LLU << 34,
     };
     typedef uint64_t LayerChangeFlags;
     enum ScheduleFlushOrNot { ScheduleFlush, DontScheduleFlush };
@@ -458,6 +458,7 @@ private:
     RefPtr<PlatformCALayer> m_contentsClippingLayer; // A layer used to clip inner content
     RefPtr<PlatformCALayer> m_shapeMaskLayer; // Used to clip with non-trivial corner radii.
     RefPtr<PlatformCALayer> m_contentsLayer; // A layer used for inner content, like image and video
+    RefPtr<PlatformCALayer> m_contentsShapeMaskLayer; // Used to clip the content layer with non-trivial corner radii.
     RefPtr<PlatformCALayer> m_backdropLayer; // The layer used for backdrop rendering, if necessary.
 
     // References to clones of our layers, for replicated layers.
@@ -465,6 +466,7 @@ private:
     std::unique_ptr<LayerMap> m_structuralLayerClones;
     std::unique_ptr<LayerMap> m_contentsLayerClones;
     std::unique_ptr<LayerMap> m_contentsClippingLayerClones;
+    std::unique_ptr<LayerMap> m_contentsShapeMaskLayerClones;
     std::unique_ptr<LayerMap> m_shapeMaskLayerClones;
 
 #ifdef VISIBLE_TILE_WASH

Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -1449,10 +1449,11 @@ void RenderLayerBacking::updateChildClippingStrategy(bool needsDescendantsClippi
 {
     if (hasClippingLayer() && needsDescendantsClippingLayer) {
         if (is<RenderBox>(renderer()) && (renderer().style().clipPath() || renderer().style().hasBorderRadius())) {
+            // FIXME: we shouldn't get geometry here as layout may not have been udpated.
             LayoutRect boxRect(LayoutPoint(), downcast<RenderBox>(renderer()).size());
             FloatRoundedRect contentsClippingRect = renderer().style().getRoundedInnerBorderFor(boxRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor());
             contentsClippingRect.move(contentOffsetInCompostingLayer());
-            if (clippingLayer()->applyClippingBorder(contentsClippingRect)) {
+            if (clippingLayer()->setMasksToBoundsRect(contentsClippingRect)) {
                 if (m_childClippingMaskLayer) 
                     m_childClippingMaskLayer = nullptr;
                 return;
@@ -1472,7 +1473,7 @@ void RenderLayerBacking::updateChildClippingStrategy(bool needsDescendantsClippi
                 clippingLayer()->setMaskLayer(nullptr);
         } else 
             if (hasClippingLayer())
-                clippingLayer()->clearClippingBorder();
+                clippingLayer()->setMasksToBoundsRect(FloatRoundedRect(FloatRect(FloatPoint(), clippingLayer()->size())));
     }
 }
 

LayoutTests/compositing/clipping/border-radius-stacking-context-clip-expected.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <style>
+        .clipper {
+            position: absolute;
+            width: 300px;
+            height: 200px;
+            clip: rect(50px, 250px, 150px, 50px);
+            -webkit-box-reflect: below 10px;
+        }
+
+        .container {
+            width: 400px;
+            height: 300px;
+            border: 2px solid black;
+            overflow: hidden;
+            position: relative;
+            z-index: 0;
+        }
+        
+        .child {
+            height: 100%;
+            width: 100%;
+            background-color: blue;
+        }
+        
+        .composited {
+            -webkit-transform: translateZ(0);
+        }
+    </style>
+</head>
+<body>
+    <div class="clipper">
+        <div class="container">
+            <div class="composited child">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+

LayoutTests/compositing/clipping/border-radius-stacking-context-clip.html
@@ -1,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <style>
+        .clipper {
+            position: absolute;
+            width: 300px;
+            height: 200px;
+            clip: rect(50px, 250px, 150px, 50px);
+            -webkit-box-reflect: below 10px;
+        }
+
+        .container {
+            width: 400px;
+            height: 300px;
+            border: 2px solid black;
+            border-radius: 30%;
+            overflow: hidden;
+            position: relative;
+            z-index: 0;
+        }
+        
+        .child {
+            height: 100%;
+            width: 100%;
+            background-color: blue;
+        }
+        
+        .composited {
+            -webkit-transform: translateZ(0);
+        }
+    </style>
+</head>
+<body>
+    <div class="clipper">
+        <div class="container">
+            <div class="composited child">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+