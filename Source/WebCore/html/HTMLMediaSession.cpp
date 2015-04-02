night-codes/WebKit

commit 8e6d964e92caa35f07a79f43d5447ef1e4a92a50

[Mac] Do not include route button if element does not support target playback https://bugs.webkit.org/show_bug.cgi?id=143251
Reviewed by Jer Noble.

* Modules/mediacontrols/mediaControlsApple.js:
(Controller.prototype.currentPlaybackTargetIsWireless): Return false if target playback is disabled.
(Controller.prototype.updateWirelessTargetAvailable): Do not show the picker if target
    playback is disabled.
(Controller.prototype.handleWirelessPlaybackChange): Call updateWirelessTargetAvailable.

* dom/Document.cpp:
(WebCore::Document::showPlaybackTargetPicker): Update for Page::showPlaybackTargetPicker change.
(WebCore::Document::didChoosePlaybackTarget): Notify clients in vector-order, lower level code
    now tracks which one requested the picker.

* html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::loadResource): Don't call applyMediaPlayerRestrictions, it is done
    in mediaPlayerEngineFailedToLoad.
(WebCore::HTMLMediaElement::setReadyState): Enqueue a target availability event if there are
    listeners readyState reaches HAVE_METADATA so controls are setup correctly.
(WebCore::HTMLMediaElement::mediaPlayerEngineFailedToLoad): Call applyMediaPlayerRestrictions.
(WebCore::HTMLMediaElement::clearMediaPlayer): Enqueue a target availability event in so controls
    have a chance to hide the picker.
(WebCore::HTMLMediaElement::webkitCurrentPlaybackTargetIsSupported): New, passthrough to the
    media engine.
(WebCore::HTMLMediaElement::canPlayToWirelessPlaybackTarget): Ditto.
(WebCore::HTMLMediaElement::startPlayingToPlaybackTarget): Ditto.
(WebCore::HTMLMediaElement::stopPlayingToPlaybackTarget): Ditto.
* html/HTMLMediaElement.h:
* html/HTMLMediaElement.idl:

* html/HTMLMediaSession.cpp:
(WebCore::HTMLMediaSession::HTMLMediaSession): Initialize m_playbackTarget.
(WebCore::HTMLMediaSession::currentPlaybackTargetIsSupported): New.
(WebCore::HTMLMediaSession::showPlaybackTargetPicker): Pull logic from showingPlaybackTargetPickerPermitted

Source/WebCore/html/HTMLMediaSession.cpp
@@ -76,6 +76,7 @@ HTMLMediaSession::HTMLMediaSession(MediaSessionClient& client)
     , m_restrictions(NoRestrictions)
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
     , m_targetAvailabilityChangedTimer(*this, &HTMLMediaSession::targetAvailabilityChangedTimerFired)
+    , m_playbackTarget(std::make_unique<MediaPlaybackTarget>())
 #endif
 {
 }
@@ -163,21 +164,6 @@ bool HTMLMediaSession::pageAllowsPlaybackAfterResuming(const HTMLMediaElement& e
 }
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
-bool HTMLMediaSession::showingPlaybackTargetPickerPermitted(const HTMLMediaElement& element) const
-{
-    if (m_restrictions & RequireUserGestureToShowPlaybackTargetPicker && !ScriptController::processingUserGesture()) {
-        LOG(Media, "HTMLMediaSession::showingPlaybackTargetPickerPermitted - returning FALSE because of permissions");
-        return false;
-    }
-
-    if (!element.document().page()) {
-        LOG(Media, "HTMLMediaSession::showingPlaybackTargetPickerPermitted - returning FALSE because page is NULL");
-        return false;
-    }
-
-    return !wirelessVideoPlaybackDisabled(element);
-}
-
 bool HTMLMediaSession::currentPlaybackTargetIsWireless(const HTMLMediaElement& element) const
 {
     MediaPlayer* player = element.player();
@@ -192,14 +178,35 @@ bool HTMLMediaSession::currentPlaybackTargetIsWireless(const HTMLMediaElement& e
     return isWireless;
 }
 
+bool HTMLMediaSession::currentPlaybackTargetIsSupported(const HTMLMediaElement& element) const
+{
+    MediaPlayer* player = element.player();
+    if (!player) {
+        LOG(Media, "HTMLMediaSession::currentPlaybackTargetIsSupported - returning FALSE because player is NULL");
+        return false;
+    }
+
+    bool isSupported = player->isCurrentPlaybackTargetSupported();
+    LOG(Media, "HTMLMediaSession::currentPlaybackTargetIsSupported - returning %s", isSupported ? "TRUE" : "FALSE");
+    
+    return isSupported;
+}
+
 void HTMLMediaSession::showPlaybackTargetPicker(const HTMLMediaElement& element)
 {
     LOG(Media, "HTMLMediaSession::showPlaybackTargetPicker");
 
-    if (!showingPlaybackTargetPickerPermitted(element))
+    if (m_restrictions & RequireUserGestureToShowPlaybackTargetPicker && !ScriptController::processingUserGesture()) {
+        LOG(Media, "HTMLMediaSession::showPlaybackTargetPicker - returning early because of permissions");
         return;
+    }
 
-    m_haveRequestedPlaybackTargetPicker = true;
+    if (!element.document().page()) {
+        LOG(Media, "HTMLMediaSession::showingPlaybackTargetPickerPermitted - returning early because page is NULL");
+        return;
+    }
+
+    MediaSessionManager::sharedManager().setCurrentSession(*this);
     element.document().showPlaybackTargetPicker(element);
 }
 
@@ -238,7 +245,7 @@ bool HTMLMediaSession::wirelessVideoPlaybackDisabled(const HTMLMediaElement& ele
 
     MediaPlayer* player = element.player();
     if (!player)
-        return false;
+        return true;
 
     bool disabled = player->wirelessVideoPlaybackDisabled();
     LOG(Media, "HTMLMediaSession::wirelessVideoPlaybackDisabled - returning %s because media engine says so", disabled ? "TRUE" : "FALSE");
@@ -277,8 +284,12 @@ void HTMLMediaSession::setHasPlaybackTargetAvailabilityListeners(const HTMLMedia
 
 void HTMLMediaSession::didChoosePlaybackTarget(const MediaPlaybackTarget& device)
 {
-    m_haveRequestedPlaybackTargetPicker = false;
-    client().setWirelessPlaybackTarget(device);
+    m_playbackTarget->setDevicePickerContext(device.devicePickerContext());
+    client().setWirelessPlaybackTarget(*m_playbackTarget.get());
+    if (device.hasActiveRoute() && MediaSessionManager::sharedManager().currentSession() == this)
+        startPlayingToPlaybackTarget();
+    else
+        stopPlayingToPlaybackTarget();
 }
 
 void HTMLMediaSession::targetAvailabilityChangedTimerFired()
@@ -290,6 +301,8 @@ void HTMLMediaSession::externalOutputDeviceAvailableDidChange(bool hasTargets) c
 {
     if (m_hasPlaybackTargets == hasTargets)
         return;
+
+    LOG(Media, "HTMLMediaSession::externalOutputDeviceAvailableDidChange - hasTargets %s", hasTargets ? "TRUE" : "FALSE");
 
     m_hasPlaybackTargets = hasTargets;
     if (!m_targetAvailabilityChangedTimer.isActive())

Source/WebCore/Modules/mediacontrols/mediaControlsApple.js
@@ -1695,7 +1695,13 @@ Controller.prototype = {
     },
 
     currentPlaybackTargetIsWireless: function() {
-        return Controller.gSimulateWirelessPlaybackTarget || (('webkitCurrentPlaybackTargetIsWireless' in this.video) && this.video.webkitCurrentPlaybackTargetIsWireless);
+        if (Controller.gSimulateWirelessPlaybackTarget)
+            return true;
+
+        if (!this.video.webkitCurrentPlaybackTargetIsWireless || this.video.webkitWirelessVideoPlaybackDisabled)
+            return false;
+
+        return true;
     },
 
     updateShouldListenForPlaybackTargetAvailabilityEvent: function() {
@@ -1740,7 +1746,11 @@ Controller.prototype = {
     },
 
     updateWirelessTargetAvailable: function() {
-        if (Controller.gSimulateWirelessPlaybackTarget || this.hasWirelessPlaybackTargets)
+        var wirelessPlaybackTargetsAvailable = Controller.gSimulateWirelessPlaybackTarget || this.hasWirelessPlaybackTargets;
+        if (this.video.webkitWirelessVideoPlaybackDisabled)
+            wirelessPlaybackTargetsAvailable = false;
+
+        if (wirelessPlaybackTargetsAvailable)
             this.controls.wirelessTargetPicker.classList.remove(this.ClassNames.hidden);
         else
             this.controls.wirelessTargetPicker.classList.add(this.ClassNames.hidden);
@@ -1753,6 +1763,7 @@ Controller.prototype = {
     },
 
     handleWirelessPlaybackChange: function(event) {
+        this.updateWirelessTargetAvailable();
         this.updateWirelessPlaybackStatus();
         this.setNeedsTimelineMetricsUpdate();
     },

Source/WebCore/dom/Document.cpp
@@ -6550,7 +6550,7 @@ void Document::showPlaybackTargetPicker(const HTMLMediaElement& element)
     if (!page)
         return;
 
-    page->showPlaybackTargetPicker(this, view()->lastKnownMousePosition(), is<HTMLVideoElement>(element));
+    page->showPlaybackTargetPicker(view()->lastKnownMousePosition(), is<HTMLVideoElement>(element));
 }
 
 void Document::addPlaybackTargetPickerClient(MediaPlaybackTargetPickerClient& client)
@@ -6604,22 +6604,8 @@ void Document::playbackTargetAvailabilityDidChange(bool available)
 
 void Document::didChoosePlaybackTarget(const MediaPlaybackTarget& device)
 {
-    MediaPlaybackTargetPickerClient* clientThatRequestedPicker = nullptr;
-
-    for (auto* client : m_playbackTargetClients) {
-        if (client->requestedPlaybackTargetPicker()) {
-            clientThatRequestedPicker = client;
-            continue;
-        }
-
+    for (auto* client : m_playbackTargetClients)
         client->didChoosePlaybackTarget(device);
-    }
-
-    // Notify the client that requested the chooser last because if more than one
-    // is playing, only the last one to set the context will actually get to play
-    //  to the external device.
-    if (clientThatRequestedPicker)
-        clientThatRequestedPicker->didChoosePlaybackTarget(device);
 }
 
 #endif

Source/WebCore/html/HTMLMediaElement.cpp
@@ -1179,8 +1179,6 @@ void HTMLMediaElement::loadResource(const URL& initialURL, ContentType& contentT
     if (!m_player->load(url, contentType, keySystem))
         mediaLoadingFailed(MediaPlayer::FormatError);
 
-    m_mediaSession->applyMediaPlayerRestrictions(*this);
-
     // If there is no poster to display, allow the media engine to render video frames as soon as
     // they are available.
     updateDisplayState();
@@ -1987,6 +1985,11 @@ void HTMLMediaElement::setReadyState(MediaPlayer::ReadyState state)
         prepareMediaFragmentURI();
         scheduleEvent(eventNames().durationchangeEvent);
         scheduleEvent(eventNames().loadedmetadataEvent);
+#if ENABLE(WIRELESS_PLAYBACK_TARGET)
+        if (hasEventListeners(eventNames().webkitplaybacktargetavailabilitychangedEvent))
+            enqueuePlaybackTargetAvailabilityChangedEvent();
+#endif
+        
         if (hasMediaControls())
             mediaControls()->loadedMetadata();
         if (renderer())
@@ -4268,6 +4271,8 @@ void HTMLMediaElement::mediaPlayerEngineUpdated(MediaPlayer*)
 
     m_havePreparedToPlay = false;
 
+    m_mediaSession->applyMediaPlayerRestrictions(*this);
+
 #if PLATFORM(IOS)
     if (!m_player)
         return;
@@ -4654,8 +4659,13 @@ void HTMLMediaElement::clearMediaPlayer(int flags)
 #endif
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
-    if (hasEventListeners(eventNames().webkitplaybacktargetavailabilitychangedEvent))
+    if (hasEventListeners(eventNames().webkitplaybacktargetavailabilitychangedEvent)) {
         m_mediaSession->setHasPlaybackTargetAvailabilityListeners(*this, false);
+
+        // Send an availability event in case scripts want to hide the picker when the element
+        // doesn't support playback to a target.
+        enqueuePlaybackTargetAvailabilityChangedEvent();
+    }
 #endif
 
     m_player = nullptr;
@@ -4810,6 +4820,11 @@ bool HTMLMediaElement::webkitCurrentPlaybackTargetIsWireless() const
     return m_mediaSession->currentPlaybackTargetIsWireless(*this);
 }
 
+bool HTMLMediaElement::webkitCurrentPlaybackTargetIsSupported() const
+{
+    return m_mediaSession->currentPlaybackTargetIsSupported(*this);
+}
+
 void HTMLMediaElement::wirelessRoutesAvailableDidChange()
 {
     enqueuePlaybackTargetAvailabilityChangedEvent();
@@ -4868,6 +4883,27 @@ void HTMLMediaElement::setWirelessPlaybackTarget(const MediaPlaybackTarget& devi
     LOG(Media, "HTMLMediaElement::setWirelessPlaybackTarget(%p)", this);
     if (m_player)
         m_player->setWirelessPlaybackTarget(device);
+}
+
+bool HTMLMediaElement::canPlayToWirelessPlaybackTarget()
+{
+    bool canPlay = m_player && m_player->canPlayToWirelessPlaybackTarget();
+
+    LOG(Media, "HTMLMediaElement::canPlayToWirelessPlaybackTarget(%p) - returning %s", this, boolString(canPlay));
+
+    return canPlay;
+}
+
+void HTMLMediaElement::startPlayingToPlaybackTarget()
+{
+    if (m_player)
+        m_player->startPlayingToPlaybackTarget();
+}
+
+void HTMLMediaElement::stopPlayingToPlaybackTarget()
+{
+    if (m_player)
+        m_player->stopPlayingToPlaybackTarget();
 }
 #endif
 

Source/WebCore/html/HTMLMediaElement.h
@@ -354,12 +354,16 @@ public:
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
     void webkitShowPlaybackTargetPicker();
     bool webkitCurrentPlaybackTargetIsWireless() const;
+    bool webkitCurrentPlaybackTargetIsSupported() const;
 
     virtual bool addEventListener(const AtomicString& eventType, PassRefPtr<EventListener>, bool useCapture) override;
     virtual bool removeEventListener(const AtomicString& eventType, EventListener*, bool useCapture) override;
 
     virtual void wirelessRoutesAvailableDidChange() override;
+    virtual bool canPlayToWirelessPlaybackTarget() override;
     virtual void setWirelessPlaybackTarget(const MediaPlaybackTarget&) override;
+    virtual void startPlayingToPlaybackTarget() override;
+    virtual void stopPlayingToPlaybackTarget() override;
 #endif
 
     // EventTarget function.

Source/WebCore/html/HTMLMediaElement.idl
@@ -116,4 +116,5 @@
 
     [Conditional=WIRELESS_PLAYBACK_TARGET] void webkitShowPlaybackTargetPicker();
     [Conditional=WIRELESS_PLAYBACK_TARGET] readonly attribute boolean webkitCurrentPlaybackTargetIsWireless;
+    [Conditional=WIRELESS_PLAYBACK_TARGET] readonly attribute boolean webkitCurrentPlaybackTargetIsSupported;
 };

Source/WebCore/html/HTMLMediaSession.h
@@ -54,8 +54,8 @@ public:
     bool pageAllowsPlaybackAfterResuming(const HTMLMediaElement&) const;
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
-    bool showingPlaybackTargetPickerPermitted(const HTMLMediaElement&) const;
     bool currentPlaybackTargetIsWireless(const HTMLMediaElement&) const;
+    bool currentPlaybackTargetIsSupported(const HTMLMediaElement&) const;
     void showPlaybackTargetPicker(const HTMLMediaElement&);
     bool hasWirelessPlaybackTargets(const HTMLMediaElement&) const;
 
@@ -102,16 +102,15 @@ private:
     virtual void didChoosePlaybackTarget(const MediaPlaybackTarget&) override;
     virtual void externalOutputDeviceAvailableDidChange(bool) const override;
     virtual bool requiresPlaybackTargetRouteMonitoring() const override;
-    virtual bool requestedPlaybackTargetPicker() const override { return m_haveRequestedPlaybackTargetPicker; }
 #endif
 
     BehaviorRestrictions m_restrictions;
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
     mutable Timer m_targetAvailabilityChangedTimer;
+    std::unique_ptr<MediaPlaybackTarget> m_playbackTarget;
     bool m_hasPlaybackTargetAvailabilityListeners { false };
     mutable bool m_hasPlaybackTargets { false };
-    mutable bool m_haveRequestedPlaybackTargetPicker { false };
 #endif
 };
 

Source/WebCore/page/Page.cpp
@@ -212,6 +212,9 @@ Page::Page(PageConfiguration& pageConfiguration)
     , m_userContentController(WTF::move(pageConfiguration.userContentController))
     , m_visitedLinkStore(*WTF::move(pageConfiguration.visitedLinkStore))
     , m_sessionID(SessionID::defaultSessionID())
+#if ENABLE(WIRELESS_PLAYBACK_TARGET)
+    , m_playbackTarget(std::make_unique<MediaPlaybackTarget>())
+#endif
     , m_isClosing(false)
     , m_isPlayingAudio(false)
 {
@@ -1681,10 +1684,8 @@ void Page::setSessionID(SessionID sessionID)
 }
 
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
-void Page::showPlaybackTargetPicker(Document* document, const WebCore::IntPoint& location, bool isVideo)
+void Page::showPlaybackTargetPicker(const WebCore::IntPoint& location, bool isVideo)
 {
-    m_documentRequestingPlaybackTargetPicker = document;
-
 #if PLATFORM(IOS)
     // FIXME: refactor iOS implementation.
     UNUSED_PARAM(location);
@@ -1696,25 +1697,9 @@ void Page::showPlaybackTargetPicker(Document* document, const WebCore::IntPoint&
 
 void Page::didChoosePlaybackTarget(const MediaPlaybackTarget& target)
 {
-    Document* documentThatRequestedPicker = nullptr;
-
-    m_playbackTarget = std::make_unique<MediaPlaybackTarget>(target.devicePickerContext());
-    for (Frame* frame = &mainFrame(); frame; frame = frame->tree().traverseNext()) {
-        Document* document = frame->document();
-        if (frame->document() == m_documentRequestingPlaybackTargetPicker) {
-            documentThatRequestedPicker = document;
-            continue;
-        }
+    m_playbackTarget->setDevicePickerContext(target.devicePickerContext());
+    for (Frame* frame = &mainFrame(); frame; frame = frame->tree().traverseNext())
         frame->document()->didChoosePlaybackTarget(target);
-    }
-
-    // Notify the document that requested the chooser last because if more than one element
-    // is playing the last one to set the context will be the one that actually gets to
-    //  play to the external device.
-    if (documentThatRequestedPicker)
-        documentThatRequestedPicker->didChoosePlaybackTarget(target);
-
-    m_documentRequestingPlaybackTargetPicker = nullptr;
 }
 
 void Page::playbackTargetAvailabilityDidChange(bool available)
@@ -1733,10 +1718,6 @@ void Page::configurePlaybackTargetMonitoring()
             break;
         }
     }
-
-    if (m_requiresPlaybackTargetMonitoring == monitoringRequired)
-        return;
-    m_requiresPlaybackTargetMonitoring = monitoringRequired;
 
     if (monitoringRequired)
         chrome().client().startingMonitoringPlaybackTargets();

commit 15c3553dcb748197ded89e39f84f4e7d7dc5c3f0

[iOS] Fix AirPlay problem caused by r182240 https://bugs.webkit.org/show_bug.cgi?id=143337
Reviewed by Jer Noble.

* html/HTMLMediaSession.cpp:
(WebCore::HTMLMediaSession::wirelessVideoPlaybackDisabled): Return false if the legacy attribute
    "x-webkit-airplay" has the value "allow".
* html/HTMLVideoElement.cpp:
(WebCore::HTMLVideoElement::parseAttribute): Don't call webkitWirelessVideoPlaybackDisabled when
    processing "x-webkit-airplay" and "x-webkit-wirelessvideoplaybackdisabled", just consider
    the attribute/value.


Identifier: 161368@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@182274 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLMediaSession.cpp
@@ -241,6 +241,10 @@ bool HTMLMediaSession::wirelessVideoPlaybackDisabled(const HTMLMediaElement& ele
         LOG(Media, "HTMLMediaSession::wirelessVideoPlaybackDisabled - returning TRUE because of legacy attribute");
         return true;
     }
+    if (equalIgnoringCase(legacyAirplayAttributeValue, "allow")) {
+        LOG(Media, "HTMLMediaSession::wirelessVideoPlaybackDisabled - returning FALSE because of legacy attribute");
+        return false;
+    }
 #endif
 
     MediaPlayer* player = element.player();

Source/WebCore/html/HTMLVideoElement.cpp
@@ -122,14 +122,18 @@ void HTMLVideoElement::parseAttribute(const QualifiedName& name, const AtomicStr
     }
 #if ENABLE(WIRELESS_PLAYBACK_TARGET)
     else if (name == webkitwirelessvideoplaybackdisabledAttr)
-        mediaSession().setWirelessVideoPlaybackDisabled(*this, webkitWirelessVideoPlaybackDisabled());
+        mediaSession().setWirelessVideoPlaybackDisabled(*this, true);
 #endif
     else {
         HTMLMediaElement::parseAttribute(name, value);    
 
 #if PLATFORM(IOS) && ENABLE(WIRELESS_PLAYBACK_TARGET)
-        if (name == webkitairplayAttr)
-            mediaSession().setWirelessVideoPlaybackDisabled(*this, webkitWirelessVideoPlaybackDisabled());
+        if (name == webkitairplayAttr) {
+            bool disabled = false;
+            if (equalIgnoringCase(fastGetAttribute(HTMLNames::webkitairplayAttr), "deny"))
+                disabled = true;
+            mediaSession().setWirelessVideoPlaybackDisabled(*this, disabled);
+        }
 #endif
     }
 