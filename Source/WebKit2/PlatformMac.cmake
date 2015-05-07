night-codes/WebKit

commit 865f1dadc5bd025fc705899b789328726d9d5ef7

Progress towards CMake on Mac. https://bugs.webkit.org/show_bug.cgi?id=143112
Patch by Alex Christensen <achristensen@webkit.org> on 2015-03-26
Reviewed by Chris Dumez.

.:

* Source/cmake/OptionsMac.cmake:

Source/WebCore:

* CMakeLists.txt:
* PlatformEfl.cmake:
* PlatformGTK.cmake:
* PlatformMac.cmake:
* platform/graphics/mac/IconMac.mm:

Source/WebKit:

* PlatformMac.cmake:

Source/WebKit/mac:

* WebCoreSupport/WebFrameLoaderClient.h:

Source/WebKit2:

* CMakeLists.txt:
* PlatformMac.cmake:
* UIProcess/Cocoa/WebPageProxyCocoa.mm:

Source/WTF:

* wtf/PlatformMac.cmake:

Identifier: 161139@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@182032 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/CMakeLists.txt
@@ -154,17 +154,7 @@ if (WTF_USE_3D_GRAPHICS)
     )
 endif ()
 
-set(WebKit2_SOURCES
-    DatabaseProcess/DatabaseToWebProcessConnection.cpp
-    DatabaseProcess/DatabaseProcess.cpp
-    DatabaseProcess/IndexedDB/DatabaseProcessIDBConnection.cpp
-    DatabaseProcess/IndexedDB/IDBSerialization.cpp
-    DatabaseProcess/IndexedDB/UniqueIDBDatabase.cpp
-    DatabaseProcess/IndexedDB/UniqueIDBDatabaseIdentifier.cpp
-    DatabaseProcess/IndexedDB/sqlite/SQLiteIDBCursor.cpp
-    DatabaseProcess/IndexedDB/sqlite/SQLiteIDBTransaction.cpp
-    DatabaseProcess/IndexedDB/sqlite/UniqueIDBDatabaseBackingStoreSQLite.cpp
-
+set(NetworkProcess_SOURCES
     NetworkProcess/NetworkConnectionToWebProcess.cpp
     NetworkProcess/NetworkProcess.cpp
     NetworkProcess/NetworkProcessPlatformStrategies.cpp
@@ -178,6 +168,18 @@ set(WebKit2_SOURCES
     NetworkProcess/cache/NetworkCacheEncoder.cpp
     NetworkProcess/cache/NetworkCacheDecoder.cpp
     NetworkProcess/cache/NetworkCacheKey.cpp
+)
+
+set(WebKit2_SOURCES
+    DatabaseProcess/DatabaseToWebProcessConnection.cpp
+    DatabaseProcess/DatabaseProcess.cpp
+    DatabaseProcess/IndexedDB/DatabaseProcessIDBConnection.cpp
+    DatabaseProcess/IndexedDB/IDBSerialization.cpp
+    DatabaseProcess/IndexedDB/UniqueIDBDatabase.cpp
+    DatabaseProcess/IndexedDB/UniqueIDBDatabaseIdentifier.cpp
+    DatabaseProcess/IndexedDB/sqlite/SQLiteIDBCursor.cpp
+    DatabaseProcess/IndexedDB/sqlite/SQLiteIDBTransaction.cpp
+    DatabaseProcess/IndexedDB/sqlite/UniqueIDBDatabaseBackingStoreSQLite.cpp
 
     Platform/Logging.cpp
     Platform/Module.cpp
@@ -319,9 +321,6 @@ set(WebKit2_SOURCES
 
     Shared/WebsiteData/WebsiteData.cpp
 
-    UIProcess/API/APINavigationData.cpp
-    UIProcess/API/APIProcessPoolConfiguration.cpp
-    UIProcess/API/APISession.cpp
     UIProcess/BackingStore.cpp
     UIProcess/DrawingAreaProxy.cpp
     UIProcess/FrameLoadState.cpp
@@ -389,6 +388,9 @@ set(WebKit2_SOURCES
 
     UIProcess/API/APIFrameInfo.cpp
     UIProcess/API/APINavigation.cpp
+    UIProcess/API/APINavigationData.cpp
+    UIProcess/API/APIProcessPoolConfiguration.cpp
+    UIProcess/API/APISession.cpp
     UIProcess/API/APISessionState.cpp
     UIProcess/API/APIWebsiteDataStore.cpp
     UIProcess/API/APIUserContentExtension.cpp
@@ -620,6 +622,8 @@ set(WebKit2_SOURCES
     WebProcess/WebPage/WebPageOverlay.cpp
     WebProcess/WebPage/VisitedLinkTableController.cpp
     WebProcess/WebPage/WebUndoStep.cpp
+
+    ${NetworkProcess_SOURCES}
 )
 
 set(WebKit2_MESSAGES_IN_FILES

Source/WebKit2/PlatformMac.cmake
@@ -13,6 +13,7 @@ list(APPEND WebKit2_INCLUDE_DIRECTORIES
     "${WEBKIT2_DIR}/Platform/mac"
     "${WEBKIT2_DIR}/Platform/IPC/mac"
     "${WEBKIT2_DIR}/Shared/API/Cocoa"
+    "${WEBKIT2_DIR}/Shared/API/c/cf"
     "${WEBKIT2_DIR}/Shared/cf"
     "${WEBKIT2_DIR}/Shared/mac"
     "${WEBKIT2_DIR}/Shared/Plugins/mac"
@@ -42,9 +43,12 @@ set(WebKit2_FORWARDING_HEADERS_FILES
 )
 
 set(WebKit2_FORWARDING_HEADERS_DIRECTORIES
-    UIProcess/API/C
     Shared/API/c
+    Shared/API/c/mac
+    UIProcess/API/C
     UIProcess/Cocoa
+    WebProcess/InjectedBundle/API/c
+    WebProcess/WebPage
 )
 
 WEBKIT_CREATE_FORWARDING_HEADERS(WebKit FILES ${WebKit2_FORWARDING_HEADERS_FILES} DIRECTORIES ${WebKit2_FORWARDING_HEADERS_DIRECTORIES})

Source/WTF/wtf/PlatformMac.cmake
@@ -1,4 +1,13 @@
 list(APPEND WTF_SOURCES
+    AutodrainedPoolMac.mm
+    RunLoopTimerCF.cpp
+    SchedulePairCF.cpp
+    SchedulePairMac.mm
+
+    cf/RunLoopCF.cpp
+
+    cocoa/WorkQueueCocoa.cpp
+
     mac/DeprecatedSymbolsUsedBySafari.mm
     mac/MainThreadMac.mm
 

Source/WebCore/CMakeLists.txt
@@ -1998,6 +1998,7 @@ set(WebCore_SOURCES
     platform/ContextMenu.cpp
     platform/ContextMenuItem.cpp
     platform/CrossThreadCopier.cpp
+    platform/Cursor.cpp
     platform/DateComponents.cpp
     platform/Decimal.cpp
     platform/DisplaySleepDisabler.cpp
@@ -3146,6 +3147,7 @@ endif ()
 set(WebCoreTestSupport_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/platform/mock"
     "${WEBCORE_DIR}/testing"
+    "${DERIVED_SOURCES_WEBCORE_DIR}"
 )
 
 set(WebCoreTestSupport_IDL_INCLUDES

Source/WebCore/PlatformMac.cmake
@@ -8,6 +8,7 @@ list(APPEND WebCore_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/loader/mac"
     "${WEBCORE_DIR}/page/cocoa"
     "${WEBCORE_DIR}/page/mac"
+    "${WEBCORE_DIR}/platform/audio/mac"
     "${WEBCORE_DIR}/platform/cf"
     "${WEBCORE_DIR}/platform/cocoa"
     "${WEBCORE_DIR}/platform/graphics/avfoundation"
@@ -18,6 +19,7 @@ list(APPEND WebCore_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/platform/graphics/cocoa"
     "${WEBCORE_DIR}/platform/graphics/cg"
     "${WEBCORE_DIR}/platform/graphics/opentype"
+    "${WEBCORE_DIR}/platform/graphics/opengl"
     "${WEBCORE_DIR}/platform/graphics/mac"
     "${WEBCORE_DIR}/platform/mac"
     "${WEBCORE_DIR}/platform/network/cocoa"
@@ -41,6 +43,14 @@ list(APPEND WebCore_SOURCES
     accessibility/mac/WebAccessibilityObjectWrapperBase.mm
     accessibility/mac/WebAccessibilityObjectWrapperMac.mm
 
+    crypto/CommonCryptoUtilities.cpp
+    crypto/CryptoAlgorithm.cpp
+    crypto/CryptoAlgorithmDescriptionBuilder.cpp
+    crypto/CryptoAlgorithmRegistry.cpp
+    crypto/CryptoKey.cpp
+    crypto/CryptoKeyPair.cpp
+    crypto/SubtleCrypto.cpp
+
     loader/archive/cf/LegacyWebArchive.cpp
     loader/archive/cf/LegacyWebArchiveMac.mm
 
@@ -56,16 +66,41 @@ list(APPEND WebCore_SOURCES
     page/mac/SettingsMac.mm
     page/mac/UserAgentMac.mm
 
+    platform/LocalizedStrings.cpp
+
+    platform/audio/mac/AudioBusMac.mm
+    platform/audio/mac/AudioDestinationMac.cpp
+    platform/audio/mac/AudioFileReaderMac.cpp
+    platform/audio/mac/AudioHardwareListenerMac.cpp
+    platform/audio/mac/AudioSessionMac.cpp
+    platform/audio/mac/CARingBuffer.cpp
+    platform/audio/mac/FFTFrameMac.cpp
+    platform/audio/mac/MediaSessionManagerMac.cpp
+
+    platform/cf/CFURLExtras.cpp
+    platform/cf/CoreMediaSoftLink.cpp
+    platform/cf/FileSystemCF.cpp
+    platform/cf/MediaAccessibilitySoftLink.cpp
+    platform/cf/RunLoopObserver.cpp
+    platform/cf/SharedBufferCF.cpp
+    platform/cf/SharedTimerCF.mm
+    platform/cf/URLCF.cpp
+
     platform/cocoa/DisplaySleepDisablerCocoa.cpp
     platform/cocoa/KeyEventCocoa.mm
     platform/cocoa/MemoryPressureHandlerCocoa.mm
     platform/cocoa/SystemVersion.mm
     platform/cocoa/TelephoneNumberDetectorCocoa.cpp
 
+    platform/graphics/avfoundation/AudioSourceProviderAVFObjC.mm
     platform/graphics/avfoundation/AVTrackPrivateAVFObjCImpl.mm
+    platform/graphics/avfoundation/CDMPrivateMediaSourceAVFObjC.mm
     platform/graphics/avfoundation/InbandMetadataTextTrackPrivateAVF.cpp
     platform/graphics/avfoundation/InbandTextTrackPrivateAVF.cpp
+    platform/graphics/avfoundation/MediaPlaybackTargetMac.mm
     platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
+    platform/graphics/avfoundation/MediaSelectionGroupAVFObjC.mm
+    platform/graphics/avfoundation/MediaTimeAVFoundation.cpp
 
     platform/graphics/avfoundation/objc/AudioTrackPrivateAVFObjC.mm
     platform/graphics/avfoundation/objc/AudioTrackPrivateMediaSourceAVFObjC.cpp
@@ -95,6 +130,59 @@ list(APPEND WebCore_SOURCES
     platform/graphics/ca/mac/PlatformCAFiltersMac.mm
     platform/graphics/ca/mac/PlatformCALayerMac.mm
     platform/graphics/ca/mac/WebTiledBackingLayer.mm
+
+    platform/graphics/cg/BitmapImageCG.cpp
+    platform/graphics/cg/ColorCG.cpp
+    platform/graphics/cg/FloatPointCG.cpp
+    platform/graphics/cg/FloatRectCG.cpp
+    platform/graphics/cg/FloatSizeCG.cpp
+    platform/graphics/cg/GradientCG.cpp
+    platform/graphics/cg/GraphicsContext3DCG.cpp
+    platform/graphics/cg/GraphicsContextCG.cpp
+    platform/graphics/cg/IOSurfacePool.cpp
+    platform/graphics/cg/ImageBufferCG.cpp
+    platform/graphics/cg/ImageBufferDataCG.cpp
+    platform/graphics/cg/ImageCG.cpp
+    platform/graphics/cg/ImageSourceCG.cpp
+    platform/graphics/cg/ImageSourceCGMac.mm
+    platform/graphics/cg/ImageSourceCGWin.cpp
+    platform/graphics/cg/IntPointCG.cpp
+    platform/graphics/cg/IntRectCG.cpp
+    platform/graphics/cg/IntSizeCG.cpp
+    platform/graphics/cg/PDFDocumentImage.cpp
+    platform/graphics/cg/PathCG.cpp
+    platform/graphics/cg/PatternCG.cpp
+    platform/graphics/cg/SubimageCacheWithTimer.cpp
+    platform/graphics/cg/TransformationMatrixCG.cpp
+
+    platform/graphics/mac/ColorMac.mm
+    platform/graphics/mac/ComplexTextController.cpp
+    platform/graphics/mac/ComplexTextControllerCoreText.mm
+    platform/graphics/mac/DisplayRefreshMonitorMac.cpp
+    platform/graphics/mac/FloatPointMac.mm
+    platform/graphics/mac/FloatRectMac.mm
+    platform/graphics/mac/FloatSizeMac.mm
+    platform/graphics/mac/FontCacheMac.mm
+    platform/graphics/mac/FontCustomPlatformData.cpp
+    platform/graphics/mac/GlyphPageMac.cpp
+    platform/graphics/mac/GraphicsContext3DMac.mm
+    platform/graphics/mac/GraphicsContextMac.mm
+    platform/graphics/mac/IconMac.mm
+    platform/graphics/mac/ImageMac.mm
+    platform/graphics/mac/IntPointMac.mm
+    platform/graphics/mac/IntRectMac.mm
+    platform/graphics/mac/IntSizeMac.mm
+    platform/graphics/mac/MediaTimeQTKit.mm
+    platform/graphics/mac/PDFDocumentImageMac.mm
+    platform/graphics/mac/SimpleFontDataCoreText.cpp
+    platform/graphics/mac/WebGLLayer.mm
+    platform/graphics/mac/WebLayer.mm
+
+    platform/graphics/opengl/Extensions3DOpenGL.cpp
+    platform/graphics/opengl/Extensions3DOpenGLCommon.cpp
+    platform/graphics/opengl/GraphicsContext3DOpenGL.cpp
+    platform/graphics/opengl/GraphicsContext3DOpenGLCommon.cpp
+    platform/graphics/opengl/TemporaryOpenGLSetting.cpp
 
     platform/graphics/opentype/OpenTypeMathData.cpp
 
@@ -191,6 +279,9 @@ list(APPEND WebCore_SOURCES
     platform/network/mac/WebCoreResourceHandleAsOperationQueueDelegate.mm
     platform/network/mac/WebCoreURLResponse.mm
 
+    platform/posix/FileSystemPOSIX.cpp
+    platform/posix/SharedBufferPOSIX.cpp
+
     platform/text/cf/HyphenationCF.cpp
 
     platform/text/mac/LocaleMac.mm
@@ -214,6 +305,7 @@ set(WebCore_FORWARDING_HEADERS_DIRECTORIES
     editing
     editing/cocoa
     editing/mac
+    fileapi
     history
     html
     html/forms
@@ -221,6 +313,7 @@ set(WebCore_FORWARDING_HEADERS_DIRECTORIES
     html/shadow
     inspector
     loader
+
     loader/appcache
     loader/archive
     loader/archive/cf
@@ -233,6 +326,7 @@ set(WebCore_FORWARDING_HEADERS_DIRECTORIES
     platform
     plugins
     rendering
+    replay
     storage
     style
     svg
@@ -242,6 +336,7 @@ set(WebCore_FORWARDING_HEADERS_DIRECTORIES
     page/scrolling
 
     platform/animation
+    platform/audio
     platform/graphics
     platform/mac
     platform/mock
@@ -250,6 +345,7 @@ set(WebCore_FORWARDING_HEADERS_DIRECTORIES
     platform/text
 
     platform/graphics/ca
+    platform/graphics/cg
     platform/graphics/filters
     platform/graphics/mac
     platform/graphics/transforms

Source/WebKit/PlatformMac.cmake
@@ -1,3 +1,36 @@
+find_library(ACCELERATE_LIBRARY accelerate)
+find_library(AUDIOUNIT_LIBRARY AudioUnit)
+find_library(CARBON_LIBRARY Carbon)
+find_library(COCOA_LIBRARY Cocoa)
+find_library(DISKARBITRATION_LIBRARY DiskArbitration)
+find_library(IOKIT_LIBRARY IOKit)
+find_library(OPENGL_LIBRARY OpenGL)
+find_library(QUARTZCORE_LIBRARY QuartzCore)
+find_library(SQLITE3_LIBRARY sqlite3)
+find_library(XML2_LIBRARY XML2)
+find_package(ZLIB REQUIRED)
+
+if ("${CURRENT_OSX_VERSION}" MATCHES "10.9")
+set(WEBKITSYSTEMINTERFACE_LIBRARY libWebKitSystemInterfaceMavericks.a)
+else ()
+set(WEBKITSYSTEMINTERFACE_LIBRARY libWebKitSystemInterfaceYosemite.a)
+endif ()
+link_directories(../../WebKitLibraries)
+
+list(APPEND WebKit_LIBRARIES
+    ${ACCELERATE_LIBRARY}
+    ${AUDIOUNIT_LIBRARY}
+    ${CARBON_LIBRARY}
+    ${COCOA_LIBRARY}
+    ${DISKARBITRATION_LIBRARY}
+    ${IOKIT_LIBRARY}
+    ${OPENGL_LIBRARY}
+    ${QUARTZCORE_LIBRARY}
+    ${SQLITE3_LIBRARY}
+    ${WEBKITSYSTEMINTERFACE_LIBRARY}
+    ${XML2_LIBRARY}
+)
+
 list(APPEND WebKit_INCLUDE_DIRECTORIES
     "${JAVASCRIPTCORE_DIR}/dfg"
     "${WEBCORE_DIR}/accessibility/mac"

Source/WebCore/PlatformEfl.cmake
@@ -66,7 +66,6 @@ list(APPEND WebCore_SOURCES
 
     platform/ContextMenuItemNone.cpp
     platform/ContextMenuNone.cpp
-    platform/Cursor.cpp
 
     platform/audio/efl/AudioBusEfl.cpp
 

Source/WebCore/PlatformGTK.cmake
@@ -49,7 +49,6 @@ list(APPEND WebCore_SOURCES
     loader/soup/CachedRawResourceSoup.cpp
     loader/soup/SubresourceLoaderSoup.cpp
 
-    platform/Cursor.cpp
     platform/PlatformStrategies.cpp
 
     platform/audio/gtk/AudioBusGtk.cpp

commit 092a22aa3086a82610315f71deb95d83c942275d

REGRESSION(r182032): [GTK] Production build is broken after r182032 https://bugs.webkit.org/show_bug.cgi?id=144736
Reviewed by Martin Robinson.

The problem is that r182032 moved the NetworkProcess sources from
WebKit2 sources to NetworkProcess sources. That's not a problem
for developer builds where all internal symbols are exported, but
doesn't work for production builds because all those internal
symbols are private.

* CMakeLists.txt: Use a different name for common network process
sources variable.
* PlatformMac.cmake: Append common network process sources to the
network process source list for Mac.

Identifier: 162762@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183930 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/CMakeLists.txt
@@ -154,7 +154,7 @@ if (ENABLE_GRAPHICS_CONTEXT_3D)
     )
 endif ()
 
-set(NetworkProcess_SOURCES
+set(NetworkProcess_COMMON_SOURCES
     NetworkProcess/NetworkConnectionToWebProcess.cpp
     NetworkProcess/NetworkProcess.cpp
     NetworkProcess/NetworkProcessPlatformStrategies.cpp
@@ -625,7 +625,7 @@ set(WebKit2_SOURCES
     WebProcess/WebPage/VisitedLinkTableController.cpp
     WebProcess/WebPage/WebUndoStep.cpp
 
-    ${NetworkProcess_SOURCES}
+    ${NetworkProcess_COMMON_SOURCES}
 )
 
 set(WebKit2_MESSAGES_IN_FILES

Source/WebKit2/PlatformMac.cmake
@@ -32,6 +32,10 @@ set(WebProcess_SOURCES
      WebProcess/mac/SecItemShimLibrary.mm
 )
 
+list(APPEND NetworkProcess_SOURCES
+     ${NetworkProcess_COMMON_SOURCES}
+)
+
 add_definitions("-include WebKit2Prefix.h")
 
 set(WebKit2_FORWARDING_HEADERS_FILES