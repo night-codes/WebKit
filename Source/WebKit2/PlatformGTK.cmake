night-codes/WebKit

commit 0f33ec2088c1d2eb91977544283220a532b646cf

[GTK] All spell checking layout tests fail https://bugs.webkit.org/show_bug.cgi?id=144648
Reviewed by Carlos Garcia Campos.

Source/WebKit2:

Simplify and complete the GTK+ spell-checking implementation. Instead of using
the EFL/GTK+ only WKTextChecker interface, directly implement Enchant spell-checking
in TextCheckerGtk. The per-context spell checking API is an illusion, since all
WKTextChecker implementations will adjust the same singleton. This can be a first step
toward removing it entirely.

Also complete the implementation of the TextChecker interface by duplicating the EFL code,
which reuses other methods in the same class. Eventually this duplication will collapse,
once GTK+ and EFL can share an Enchant implementation.

* PlatformGTK.cmake:
* UIProcess/API/gtk/WebKitPrivate.h:
* UIProcess/API/gtk/WebKitTextChecker.cpp: Removed.
* UIProcess/API/gtk/WebKitTextChecker.h: Removed.
* UIProcess/API/gtk/WebKitWebContext.cpp: Remove WebTextChecker usage and
replace it with calls directly to the TextChecker singleton via static methods.
* UIProcess/TextChecker.h:
* UIProcess/gtk/TextCheckerGtk.cpp: Move the WebTextChecker enchant implementation
here and reuse the EFL version of WebKit::TextChecker::requestCheckingOfString.
Also initialize languages properly when building for developer builds. This
allows WebKitTestRunner to work even when it doesn't explicitly set the languages.

Source/WTF:

* wtf/Platform.h: Activate UNIFIED_TEXT_CHECKING for GTK+.

LayoutTests:

* platform/gtk/TestExpectations: Unskip and reclassify previously failing tests.
* platform/gtk/editing/spelling/spellcheck-paste-expected.txt: Removed.

Identifier: 162768@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@183936 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/PlatformGTK.cmake
@@ -214,8 +214,6 @@ list(APPEND WebKit2_SOURCES
     UIProcess/API/gtk/WebKitSettings.cpp
     UIProcess/API/gtk/WebKitSettings.h
     UIProcess/API/gtk/WebKitSettingsPrivate.h
-    UIProcess/API/gtk/WebKitTextChecker.cpp
-    UIProcess/API/gtk/WebKitTextChecker.h
     UIProcess/API/gtk/WebKitUIClient.cpp
     UIProcess/API/gtk/WebKitUIClient.h
     UIProcess/API/gtk/WebKitURIRequest.cpp

Source/WebKit2/UIProcess/gtk/TextCheckerGtk.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2010 Apple Inc. All rights reserved.
  * Portions Copyright (c) 2010 Motorola Mobility, Inc.  All rights reserved.
+ * Copyright (C) 2011-2013 Samsung Electronics
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,120 +28,227 @@
 #include "config.h"
 #include "TextChecker.h"
 
+#include "TextBreakIterator.h"
 #include "TextCheckerState.h"
+#include "WebProcessPool.h"
 #include "WebTextChecker.h"
 #include <WebCore/NotImplemented.h>
+#include <WebCore/TextCheckerEnchant.h>
+#include <wtf/NeverDestroyed.h>
 
 using namespace WebCore;
- 
+
 namespace WebKit {
 
-static TextCheckerState textCheckerState;
-
-const TextCheckerState& TextChecker::state()
+static WebCore::TextCheckerEnchant& enchantTextChecker()
 {
-    static bool didInitializeState = false;
-    if (didInitializeState)
-        return textCheckerState;
+    static NeverDestroyed<WebCore::TextCheckerEnchant> checker;
 
-    WebTextCheckerClient& client = WebTextChecker::singleton()->client();
-    textCheckerState.isContinuousSpellCheckingEnabled = client.continuousSpellCheckingEnabled();
-    textCheckerState.isGrammarCheckingEnabled =  client.grammarCheckingEnabled();
+#if defined(DEVELOPMENT_BUILD)
+    // This is a bit of a hack, but ensures that for testing purposes,
+    // spell checking is properly initialized in WebKitTestRunner while
+    // running layout tests. We should consider replacing this with some
+    // more accessible C API at some point.
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [] {
+        checker.get().updateSpellCheckingLanguages(Vector<String> { "en_US" });
+    });
+#endif
 
-    didInitializeState = true;
+    return checker;
+}
+
+TextCheckerState& checkerState()
+{
+    static TextCheckerState textCheckerState;
+    static std::once_flag onceFlag;
+    std::call_once(onceFlag, [] {
+        textCheckerState.isContinuousSpellCheckingEnabled = false;;
+        textCheckerState.isGrammarCheckingEnabled = false;;
+    });
 
     return textCheckerState;
 }
-  
+
+const TextCheckerState& TextChecker::state()
+{
+    return checkerState();
+}
+
+static void updateStateForAllContexts()
+{
+    const Vector<WebProcessPool*>& contexts = WebProcessPool::allProcessPools();
+    for (size_t i = 0; i < contexts.size(); ++i)
+        contexts[i]->textCheckerStateChanged();
+}
+
 bool TextChecker::isContinuousSpellCheckingAllowed()
 {
-    return WebTextChecker::singleton()->client().continuousSpellCheckingAllowed();
+    return true;
 }
 
 void TextChecker::setContinuousSpellCheckingEnabled(bool isContinuousSpellCheckingEnabled)
 {
-    if (state().isContinuousSpellCheckingEnabled == isContinuousSpellCheckingEnabled)
+    if (checkerState().isContinuousSpellCheckingEnabled == isContinuousSpellCheckingEnabled)
         return;
-    textCheckerState.isContinuousSpellCheckingEnabled = isContinuousSpellCheckingEnabled;
-    WebTextChecker::singleton()->client().setContinuousSpellCheckingEnabled(isContinuousSpellCheckingEnabled);
+    checkerState().isContinuousSpellCheckingEnabled = isContinuousSpellCheckingEnabled;
+    updateStateForAllContexts();
 }
 
 void TextChecker::setGrammarCheckingEnabled(bool isGrammarCheckingEnabled)
 {
-    if (state().isGrammarCheckingEnabled == isGrammarCheckingEnabled)
+    if (checkerState().isGrammarCheckingEnabled == isGrammarCheckingEnabled)
         return;
-    textCheckerState.isGrammarCheckingEnabled = isGrammarCheckingEnabled;
-    WebTextChecker::singleton()->client().setGrammarCheckingEnabled(isGrammarCheckingEnabled);
+    checkerState().isGrammarCheckingEnabled = isGrammarCheckingEnabled;
+    updateStateForAllContexts();
 }
 
 void TextChecker::continuousSpellCheckingEnabledStateChanged(bool enabled)
 {
-    textCheckerState.isContinuousSpellCheckingEnabled = enabled;
+    checkerState().isContinuousSpellCheckingEnabled = enabled;
 }
 
 void TextChecker::grammarCheckingEnabledStateChanged(bool enabled)
 {
-    textCheckerState.isGrammarCheckingEnabled = enabled;
+    checkerState().isGrammarCheckingEnabled = enabled;
 }
 
-int64_t TextChecker::uniqueSpellDocumentTag(WebPageProxy* page)
+int64_t TextChecker::uniqueSpellDocumentTag(WebPageProxy*)
 {
-    return WebTextChecker::singleton()->client().uniqueSpellDocumentTag(page);
+    return 0;
 }
 
-void TextChecker::closeSpellDocumentWithTag(int64_t tag)
+void TextChecker::closeSpellDocumentWithTag(int64_t /* tag */)
 {
-    WebTextChecker::singleton()->client().closeSpellDocumentWithTag(tag);
 }
 
-void TextChecker::checkSpellingOfString(int64_t spellDocumentTag, StringView text, int32_t& misspellingLocation, int32_t& misspellingLength)
+void TextChecker::checkSpellingOfString(int64_t /* spellDocumentTag */, StringView text, int32_t& misspellingLocation, int32_t& misspellingLength)
 {
-    WebTextChecker::singleton()->client().checkSpellingOfString(spellDocumentTag, text.toStringWithoutCopying(), misspellingLocation, misspellingLength);
+    misspellingLocation = -1;
+    misspellingLength = 0;
+    enchantTextChecker().checkSpellingOfString(text.toStringWithoutCopying(), misspellingLocation, misspellingLength);
 }
 
-void TextChecker::checkGrammarOfString(int64_t spellDocumentTag, StringView text, Vector<WebCore::GrammarDetail>& grammarDetails, int32_t& badGrammarLocation, int32_t& badGrammarLength)
+void TextChecker::checkGrammarOfString(int64_t /* spellDocumentTag */, StringView /* text */, Vector<WebCore::GrammarDetail>& /* grammarDetails */, int32_t& /* badGrammarLocation */, int32_t& /* badGrammarLength */)
 {
-    WebTextChecker::singleton()->client().checkGrammarOfString(spellDocumentTag, text.toStringWithoutCopying(), grammarDetails, badGrammarLocation, badGrammarLength);
 }
 
 bool TextChecker::spellingUIIsShowing()
 {
-    return WebTextChecker::singleton()->client().spellingUIIsShowing();
+    return false;
 }
 
 void TextChecker::toggleSpellingUIIsShowing()
 {
-    WebTextChecker::singleton()->client().toggleSpellingUIIsShowing();
 }
 
-void TextChecker::updateSpellingUIWithMisspelledWord(int64_t spellDocumentTag, const String& misspelledWord)
+void TextChecker::updateSpellingUIWithMisspelledWord(int64_t /* spellDocumentTag */, const String& /* misspelledWord */)
 {
-    WebTextChecker::singleton()->client().updateSpellingUIWithMisspelledWord(spellDocumentTag, misspelledWord);
 }
 
-void TextChecker::updateSpellingUIWithGrammarString(int64_t spellDocumentTag, const String& badGrammarPhrase, const GrammarDetail& grammarDetail)
+void TextChecker::updateSpellingUIWithGrammarString(int64_t /* spellDocumentTag */, const String& /* badGrammarPhrase */, const GrammarDetail& /* grammarDetail */)
 {
-    WebTextChecker::singleton()->client().updateSpellingUIWithGrammarString(spellDocumentTag, badGrammarPhrase, grammarDetail);
 }
 
-void TextChecker::getGuessesForWord(int64_t spellDocumentTag, const String& word, const String& /* context */, Vector<String>& guesses)
+void TextChecker::getGuessesForWord(int64_t /* spellDocumentTag */, const String& word, const String& /* context */, Vector<String>& guesses)
 {
-    WebTextChecker::singleton()->client().guessesForWord(spellDocumentTag, word, guesses);
+    guesses = enchantTextChecker().getGuessesForWord(word);
 }
 
-void TextChecker::learnWord(int64_t spellDocumentTag, const String& word)
+void TextChecker::learnWord(int64_t /* spellDocumentTag */, const String& word)
 {
-    WebTextChecker::singleton()->client().learnWord(spellDocumentTag, word);
+    enchantTextChecker().learnWord(word);
 }
 
 void TextChecker::ignoreWord(int64_t spellDocumentTag, const String& word)
 {
-    WebTextChecker::singleton()->client().ignoreWord(spellDocumentTag, word);
+    enchantTextChecker().ignoreWord(word);
 }
 
-void TextChecker::requestCheckingOfString(PassRefPtr<TextCheckerCompletion>)
+void TextChecker::requestCheckingOfString(PassRefPtr<TextCheckerCompletion> completion)
 {
-    notImplemented();
+    if (!completion)
+        return;
+
+    TextCheckingRequestData request = completion->textCheckingRequestData();
+    ASSERT(request.sequence() != unrequestedTextCheckingSequence);
+    ASSERT(request.mask() != TextCheckingTypeNone);
+
+    completion->didFinishCheckingText(checkTextOfParagraph(completion->spellDocumentTag(), request.text(), request.mask()));
+}
+
+#if USE(UNIFIED_TEXT_CHECKING)
+static unsigned nextWordOffset(StringView text, unsigned currentOffset)
+{
+    // FIXME: avoid creating textIterator object here, it could be passed as a parameter.
+    //        isTextBreak() leaves the iterator pointing to the first boundary position at
+    //        or after "offset" (ubrk_isBoundary side effect).
+    //        For many word separators, the method doesn't properly determine the boundaries
+    //        without resetting the iterator.
+    TextBreakIterator* textIterator = wordBreakIterator(text);
+    if (!textIterator)
+        return currentOffset;
+
+    unsigned wordOffset = currentOffset;
+    while (wordOffset < text.length() && isTextBreak(textIterator, wordOffset))
+        ++wordOffset;
+
+    // Do not treat the word's boundary as a separator.
+    if (!currentOffset && wordOffset == 1)
+        return currentOffset;
+
+    // Omit multiple separators.
+    if ((wordOffset - currentOffset) > 1)
+        --wordOffset;
+
+    return wordOffset;
+}
+
+Vector<TextCheckingResult> TextChecker::checkTextOfParagraph(int64_t spellDocumentTag, StringView text, uint64_t checkingTypes)
+{
+    if (!(checkingTypes & TextCheckingTypeSpelling))
+        return Vector<TextCheckingResult>();
+
+    TextBreakIterator* textIterator = wordBreakIterator(text);
+    if (!textIterator)
+        return Vector<TextCheckingResult>();
+
+    // Omit the word separators at the beginning/end of the text to don't unnecessarily
+    // involve the client to check spelling for them.
+    unsigned offset = nextWordOffset(text, 0);
+    unsigned lengthStrip = text.length();
+    while (lengthStrip > 0 && isTextBreak(textIterator, lengthStrip - 1))
+        --lengthStrip;
+
+    Vector<TextCheckingResult> paragraphCheckingResult;
+    while (offset < lengthStrip) {
+        int32_t misspellingLocation = -1;
+        int32_t misspellingLength = 0;
+        checkSpellingOfString(spellDocumentTag, text.substring(offset, lengthStrip - offset), misspellingLocation, misspellingLength);
+        if (!misspellingLength)
+            break;
+
+        TextCheckingResult misspellingResult;
+        misspellingResult.type = TextCheckingTypeSpelling;
+        misspellingResult.location = offset + misspellingLocation;
+        misspellingResult.length = misspellingLength;
+        paragraphCheckingResult.append(misspellingResult);
+        offset += misspellingLocation + misspellingLength;
+        // Generally, we end up checking at the word separator, move to the adjacent word.
+        offset = nextWordOffset(text.substring(0, lengthStrip), offset);
+    }
+    return paragraphCheckingResult;
+}
+#endif
+
+void TextChecker::setSpellCheckingLanguages(const Vector<String>& languages)
+{
+    enchantTextChecker().updateSpellCheckingLanguages(languages);
+}
+
+Vector<String> TextChecker::loadedSpellCheckingLanguages()
+{
+    return enchantTextChecker().loadedSpellCheckingLanguages();
 }
 
 } // namespace WebKit

LayoutTests/platform/gtk/TestExpectations
@@ -627,11 +627,6 @@ Bug(GTK) editing/spelling/inline-spelling-markers.html [ Skip ]
 Bug(GTK) editing/spelling/inline-spelling-markers-hidpi.html [ Skip ]
 Bug(GTK) editing/spelling/markers.html [ Skip ]
 
-# Missing asynchronous spellchecking implementation.
-Bug(GTK) editing/spelling/design-mode-spellcheck-off.html [ Skip ]
-Bug(GTK) editing/spelling/spelling-insert-html.html [ Skip ]
-Bug(GTK) editing/spelling/spelling-hasspellingmarker.html [ Skip ]
-
 # Requires ENABLE(MEDIA_CAPTURE)
 webkit.org/b/85958 fast/forms/file/file-input-capture.html [ Failure ]
 
@@ -1283,11 +1278,6 @@ webkit.org/b/122571 inspector-protocol/page/frameScheduledNavigation.html [ Time
 webkit.org/b/122571 inspector-protocol/page/setEmulatedMedia.html [ Timeout Pass ]
 webkit.org/b/122571 inspector-protocol/css/getSupportedCSSProperties.html [ Timeout Pass ]
 
-webkit.org/b/130411 editing/spelling/spellcheck-paste.html [ Timeout ]
-webkit.org/b/130411 editing/spelling/spelling-changed-text.html [ Timeout ]
-webkit.org/b/130411 editing/spelling/spellcheck-paste-disabled.html [ Timeout ]
-webkit.org/b/130411 editing/spelling/spellcheck-paste-continuous-disabled.html [ Timeout ]
-
 Bug(GTK) plugins/reloadplugins-and-pages.html [ Timeout ]
 
 Bug(GTK) fast/forms/select/menulist-popup-crash.html [ Timeout ]
@@ -1479,26 +1469,6 @@ webkit.org/b/93980 http/tests/appcache/load-from-appcache-defer-resume-crash.htm
 webkit.org/b/42194 fast/events/5056619.html [ Failure ]
 webkit.org/b/42194 http/tests/security/drag-drop-same-unique-origin.html [ Failure ]
 
-# Missing TextChecker::requestCheckingOfString() implementation for Gtk.
-webkit.org/b/73003 editing/spelling/delete-into-misspelled-word.html [ Skip ]
-webkit.org/b/73003 editing/spelling/editing-multiple-words-with-markers.html [ Skip ]
-webkit.org/b/73003 editing/spelling/editing-word-with-marker-2.html [ Skip ]
-webkit.org/b/73003 editing/spelling/move-cursor-around-misspelled-word.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spellcheck-async-mutation.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spellcheck-async-remove-frame.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spellcheck-async.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spellcheck-queue.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spellcheck-sequencenum.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spelling-backward.html [ Skip ]
-webkit.org/b/73003 editing/spelling/spelling-marker-description.html [ Skip ]
-
-# Spellcheck tests don't seem to work.
-Bug(GTK) editing/spelling/spelling-backspace-between-lines.html [ Skip ]
-Bug(GTK) editing/spelling/spellcheck-attribute.html [ Skip ]
-Bug(GTK) editing/spelling/spelling-attribute-at-child.html [ Skip ]
-Bug(GTK) editing/spelling/spelling-attribute-change.html [ Skip ]
-Bug(GTK) editing/spelling/spelling-linebreak.html [ Skip ]
-
 # Context menu problems.
 webkit.org/b/120401 fast/events [ Skip ]
 webkit.org/b/120401 editing/selection [ Skip ]
@@ -1580,9 +1550,6 @@ Bug(GTK) plugins/resize-from-plugin.html [ Failure ]
 # Selection doesn't work properly.
 Bug(GTK) editing/selection/context-menu-text-selection.html [ Failure Pass ]
 Bug(GTK) editing/selection/context-menu-text-selection-lookup.html [ Failure Pass ]
-
-# An oncontextmenu attribute is not called.
-Bug(GTK) editing/spelling/spellcheck-input-search-crash.html [ Failure ]
 
 Bug(GTK) http/tests/misc/iframe-beforeunload-dialog-matching-ancestor-securityorigin.html [ Failure ]
 
@@ -1718,9 +1685,6 @@ Bug(GTK) fast/events/platform-wheelevent-paging-y-in-scrolling-page.html [ Failu
 # Need to add functionality to DumpRenderTree to handle scrollbar policy changes
 Bug(GTK) fast/overflow/scrollbar-restored-and-then-locked.html [ Failure ]
 
-# DRT doesn't dispatch an event for the second call eventSender.contextClick().
-webkit.org/b/39102 editing/spelling/context-menu-suggestions.html [ Failure ]
-
 # Some input type=range tests fail because of the size
 # of the slider thumb in GTK+ themes.
 Bug(GTK) fast/forms/range/slider-mouse-events.html [ Failure ]
@@ -1799,8 +1763,6 @@ webkit.org/b/105693 http/tests/multipart/multipart-wait-before-boundary.html [ S
 
 # Does not render the rectangle as it should, even though the multiple parts seem to work
 webkit.org/b/54251 http/tests/multipart/invalid-image-data.html [ Failure ]
-
-webkit.org/b/72248 editing/spelling/spelling-unified-emulation.html [ Failure ]
 
 webkit.org/b/62204 svg/custom/relative-sized-use-without-attributes-on-symbol.xhtml [ Failure ]
 webkit.org/b/62204 svg/custom/use-on-symbol-inside-pattern.svg [ Failure ]
@@ -1908,14 +1870,6 @@ webkit.org/b/103927 http/tests/w3c/webperf/submission/Google/resource-timing/htm
 webkit.org/b/103927 http/tests/w3c/webperf/submission/Google/resource-timing/html/test_resource_frame_initiator_type.html [ Failure ]
 webkit.org/b/103927 http/tests/w3c/webperf/submission/Google/resource-timing/html/test_resource_initiator_types.html [ Failure ]
 webkit.org/b/103927 http/tests/w3c/webperf/submission/Google/resource-timing/html/test_resource_redirects.html [ Failure ]
-
-# Spellchecker behavior tests.
-webkit.org/b/108370 editing/spelling/context-menu-suggestions-multiword-selection.html [ Skip ]
-webkit.org/b/108370 editing/spelling/context-menu-suggestions-subword-selection.html [ Skip ]
-webkit.org/b/108370 editing/spelling/spelling-double-clicked-word.html [ Skip ]
-webkit.org/b/108370 editing/spelling/spelling-exactly-selected-word.html [ Skip ]
-webkit.org/b/108370 editing/spelling/spelling-with-punctuation-selection.html [ Skip ]
-webkit.org/b/108370 editing/spelling/spelling-with-whitespace-selection.html [ Skip ]
 
 webkit.org/b/107118 fast/canvas/canvas-quadratic-same-endpoint.html [ Failure ]
 
@@ -2370,6 +2324,14 @@ webkit.org/b/144574 svg/masking/mask-negative-scale.svg [ ImageOnlyFailure ]
 webkit.org/b/144575 http/tests/loading/promote-img-preload-priority.html [ Failure ]
 
 webkit.org/b/144673 fast/forms/button-line-height.html [ ImageOnlyFailure ]
+
+webkit.org/b/144690 editing/spelling/context-menu-suggestions-multiword-selection.html [ Timeout ]
+webkit.org/b/144690 editing/spelling/context-menu-suggestions-subword-selection.html [ Timeout ]
+webkit.org/b/144690 editing/spelling/spelling-exactly-selected-word.html [ Timeout ]
+webkit.org/b/144690 editing/spelling/spelling-with-punctuation-selection.html [ Timeout ]
+webkit.org/b/144690 editing/spelling/context-menu-suggestions.html [ Failure ]
+webkit.org/b/144690 editing/spelling/spelling-double-clicked-word.html [ Failure ]
+webkit.org/b/144690 editing/spelling/delete-into-misspelled-word.html [ Timeout Pass ]
 
 #////////////////////////////////////////////////////////////////////////////////////////
 # End of Tests failing

LayoutTests/platform/gtk/editing/spelling/spellcheck-paste-expected.txt
@@ -1,18 +1,0 @@
-For Bug 40092: Spell checking for pasted text.
-
-On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
-
-
-FAIL INPUT should have a marker on 'zz apple'
-FAIL INPUT should have a marker on 'z<b>z appl</b>e'
-FAIL INPUT should have a marker on 'zz zz zz'
-FAIL TEXTAREA should have a marker on 'zz apple'
-FAIL TEXTAREA should have a marker on 'z<b>z appl</b>e'
-FAIL TEXTAREA should have a marker on 'zz zz zz'
-FAIL DIV should have a marker on 'zz apple'
-FAIL DIV should have a marker on 'z<b>z appl</b>e'
-FAIL DIV should have a marker on 'zz zz zz'
-PASS successfullyParsed is true
-
-TEST COMPLETE
-

Source/WTF/wtf/Platform.h
@@ -1052,7 +1052,7 @@
 
 #define USE_GRAMMAR_CHECKING 1
 
-#if PLATFORM(COCOA) || PLATFORM(EFL)
+#if PLATFORM(COCOA) || PLATFORM(EFL) || PLATFORM(GTK)
 #define USE_UNIFIED_TEXT_CHECKING 1
 #endif
 #if PLATFORM(MAC)

Source/WebKit2/UIProcess/API/gtk/WebKitPrivate.h
@@ -41,7 +41,6 @@
 #include <WebKit/WKSerializedScriptValue.h>
 #include <WebKit/WKSoupCustomProtocolRequestManager.h>
 #include <WebKit/WKString.h>
-#include <WebKit/WKTextChecker.h>
 #include <WebKit/WKUserMediaPermissionRequest.h>
 #include <WebKit/WebKit2_C.h>
 #include <glib.h>

commit 0129a602b9c820971a549cc494a23b266d545d8d

[GTK] REGRESSION(r183936): Test /webkit2/WebKitWebContext/spell-checker fails since r183936 https://bugs.webkit.org/show_bug.cgi?id=144828
Reviewed by Carlos Garcia Campos.

Source/WebKit2:

Instead of setting spell checking languages unconditionally when building int
developer mode, add some C API that allows setting them in WebKitTestRunner.

* PlatformGTK.cmake: Add the new C API file to the source list.
* UIProcess/API/C/gtk/WKTextCheckerGtk.cpp:
(WKTextCheckerSetSpellCheckingLanguages): Added this function which allows settings
the spell checking languages from the C API.
* UIProcess/API/C/gtk/WKTextCheckerGtk.h: Added.
* UIProcess/gtk/TextCheckerGtk.cpp:
(WebKit::enchantTextChecker): Remove the hack which sets the spell checking languages automatically
for development builds.

Tools:

* WebKitTestRunner/gtk/main.cpp:
(main): Use the new WKTextChecker API to set the spell checking languages
when the process is starting up.

Identifier: 164594@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@186175 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/PlatformGTK.cmake
@@ -91,6 +91,7 @@ list(APPEND WebKit2_SOURCES
 
     UIProcess/API/C/gtk/WKFullScreenClientGtk.cpp
     UIProcess/API/C/gtk/WKInspectorClientGtk.cpp
+    UIProcess/API/C/gtk/WKTextCheckerGtk.cpp
     UIProcess/API/C/gtk/WKView.cpp
 
     UIProcess/API/C/soup/WKCookieManagerSoup.cpp

Source/WebKit2/UIProcess/API/C/gtk/WKTextCheckerGtk.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Igalia S.L.
+ * Copyright (C) 2015 Igalia S.L.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -24,23 +24,16 @@
  */
 
 #include "config.h"
+#include "WKTextCheckerGtk.h"
 
-#include "TestController.h"
-#include <WebKit/WKTextCheckerGtk.h>
-#include <gtk/gtk.h>
-#include <wtf/glib/GRefPtr.h>
+#include "TextChecker.h"
 
-int main(int argc, char** argv)
+void WKTextCheckerSetSpellCheckingLanguages(const char* const* languages)
 {
-    gtk_init(&argc, &argv);
-
-    GRefPtr<GPtrArray> languages = adoptGRef(g_ptr_array_new());
-    g_ptr_array_add(languages.get(), const_cast<gpointer>(static_cast<const void*>("en_US")));
-    g_ptr_array_add(languages.get(), nullptr);
-    WKTextCheckerSetSpellCheckingLanguages(reinterpret_cast<const char* const*>(languages->pdata));
-
-    // Prefer the not installed web and plugin processes.
-    WTR::TestController controller(argc, const_cast<const char**>(argv));
-
-    return 0;
+#if ENABLE(SPELLCHECK)
+    Vector<String> spellCheckingLanguages;
+    for (size_t i = 0; languages[i]; ++i)
+        spellCheckingLanguages.append(String::fromUTF8(languages[i]));
+    WebKit::TextChecker::setSpellCheckingLanguages(spellCheckingLanguages);
+#endif
 }

Source/WebKit2/UIProcess/API/C/gtk/WKTextCheckerGtk.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Igalia S.L.
+ * Copyright (C) 2015 Igalia S.L.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,24 +23,19 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
+#ifndef WKTextCheckerGtk_h
+#define WKTextCheckerGtk_h
 
-#include "TestController.h"
-#include <WebKit/WKTextCheckerGtk.h>
-#include <gtk/gtk.h>
-#include <wtf/glib/GRefPtr.h>
+#include <WebKit/WKBase.h>
 
-int main(int argc, char** argv)
-{
-    gtk_init(&argc, &argv);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-    GRefPtr<GPtrArray> languages = adoptGRef(g_ptr_array_new());
-    g_ptr_array_add(languages.get(), const_cast<gpointer>(static_cast<const void*>("en_US")));
-    g_ptr_array_add(languages.get(), nullptr);
-    WKTextCheckerSetSpellCheckingLanguages(reinterpret_cast<const char* const*>(languages->pdata));
+WK_EXPORT void WKTextCheckerSetSpellCheckingLanguages(const char* const* languages);
 
-    // Prefer the not installed web and plugin processes.
-    WTR::TestController controller(argc, const_cast<const char**>(argv));
-
-    return 0;
+#ifdef __cplusplus
 }
+#endif
+
+#endif /* WKTextCheckerGtk_h */

Source/WebKit2/UIProcess/gtk/TextCheckerGtk.cpp
@@ -43,18 +43,6 @@ namespace WebKit {
 static WebCore::TextCheckerEnchant& enchantTextChecker()
 {
     static NeverDestroyed<WebCore::TextCheckerEnchant> checker;
-
-#if ENABLE(DEVELOPER_MODE)
-    // This is a bit of a hack, but ensures that for testing purposes,
-    // spell checking is properly initialized in WebKitTestRunner while
-    // running layout tests. We should consider replacing this with some
-    // more accessible C API at some point.
-    static std::once_flag onceFlag;
-    std::call_once(onceFlag, [] {
-        checker.get().updateSpellCheckingLanguages(Vector<String> { "en_US" });
-    });
-#endif
-
     return checker;
 }
 