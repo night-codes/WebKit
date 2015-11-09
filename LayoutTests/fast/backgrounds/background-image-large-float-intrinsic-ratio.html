night-codes/WebKit

commit d5ecd429fd5a38e363891cb1170592853489e48d

REGRESSION(r184895): border-image should always slice the SVG image to nine pieces when drawing it in the container element https://bugs.webkit.org/show_bug.cgi?id=149901 <rdar://problem/21995596>
Patch by Said Abou-Hallawa <sabouhallawa@apple.com> on 2015-10-12
Reviewed by Darin Adler.

Source/WebCore:

The nine-pieces algorithm should be applied to the border-image regardless
whether the image has an intrinsic size or not. It is not guaranteed to have
a meaningful border-image in all the cases of non-intrinsic size images. But
it should work as expected in most of the cases.

* rendering/RenderBoxModelObject.cpp:
* rendering/RenderBoxModelObject.h:
(WebCore::RenderBoxModelObject::calculateImageIntrinsicDimensions):
Revert the changes which were added to return whether the image has
intrinsic size or not.

(WebCore::RenderBoxModelObject::calculateFillTileSize):
(WebCore::RenderBoxModelObject::paintNinePieceImage):
Size of the image is now the return value of calculateImageIntrinsicDimensions().

* rendering/RenderListMarker.cpp:
(WebCore::RenderListMarker::updateContent):
* rendering/shapes/ShapeOutsideInfo.cpp:
(WebCore::ShapeOutsideInfo::createShapeForImage):
Size of the image is now the return value of calculateImageIntrinsicDimensions().

* rendering/style/NinePieceImage.cpp:
* rendering/style/NinePieceImage.h:
(WebCore::NinePieceImage::paint):
Delete the logic for the non-intrinsic case. Both intrinsic and non-intrinsic
cases will be treated the same.

(WebCore::NinePieceImage::computeNineRects):
(WebCore::NinePieceImage::computeSideTileScale):
(WebCore::NinePieceImage::computeMiddleTileScale):
(WebCore::NinePieceImage::computeTileScales):
(WebCore::NinePieceImage::computeIntrinsicRects): Deleted.
(WebCore::NinePieceImage::computeIntrinsicSideTileScale): Deleted.
(WebCore::NinePieceImage::computeIntrinsicMiddleTileScale): Deleted.
(WebCore::NinePieceImage::computeIntrinsicTileScales): Deleted.
Remove *Intrinsic* from the name of the functions.

Source/WebCore/rendering/RenderBoxModelObject.cpp
@@ -897,13 +897,11 @@ static inline LayoutSize resolveAgainstIntrinsicRatio(const LayoutSize& size, co
     return LayoutSize(size.width(), solutionHeight);
 }
 
-bool RenderBoxModelObject::calculateImageIntrinsicDimensions(StyleImage* image, const LayoutSize& positioningAreaSize, ScaleByEffectiveZoomOrNot shouldScaleOrNot, LayoutSize& imageSize) const
+LayoutSize RenderBoxModelObject::calculateImageIntrinsicDimensions(StyleImage* image, const LayoutSize& positioningAreaSize, ScaleByEffectiveZoomOrNot shouldScaleOrNot) const
 {
     // A generated image without a fixed size, will always return the container size as intrinsic size.
-    if (image->isGeneratedImage() && image->usesImageContainerSize()) {
-        imageSize = LayoutSize(positioningAreaSize.width(), positioningAreaSize.height());
-        return true;
-    }
+    if (image->isGeneratedImage() && image->usesImageContainerSize())
+        return LayoutSize(positioningAreaSize.width(), positioningAreaSize.height());
 
     Length intrinsicWidth;
     Length intrinsicHeight;
@@ -913,36 +911,29 @@ bool RenderBoxModelObject::calculateImageIntrinsicDimensions(StyleImage* image, 
     ASSERT(!intrinsicWidth.isPercentOrCalculated());
     ASSERT(!intrinsicHeight.isPercentOrCalculated());
 
-    imageSize = LayoutSize(intrinsicWidth.value(), intrinsicHeight.value());
-    LayoutSize minimumSize(imageSize.width() > 0 ? 1 : 0, imageSize.height() > 0 ? 1 : 0);
-    if (shouldScaleOrNot == ScaleByEffectiveZoom)
-        imageSize.scale(style().effectiveZoom());
-    imageSize.clampToMinimumSize(minimumSize);
+    LayoutSize resolvedSize(intrinsicWidth.value(), intrinsicHeight.value());
+    LayoutSize minimumSize(resolvedSize.width() > 0 ? 1 : 0, resolvedSize.height() > 0 ? 1 : 0);
 
-    if (!imageSize.isEmpty())
-        return true;
+    if (shouldScaleOrNot == ScaleByEffectiveZoom)
+        resolvedSize.scale(style().effectiveZoom());
+    resolvedSize.clampToMinimumSize(minimumSize);
 
     // If the image has one of either an intrinsic width or an intrinsic height:
     // * and an intrinsic aspect ratio, then the missing dimension is calculated from the given dimension and the ratio.
     // * and no intrinsic aspect ratio, then the missing dimension is assumed to be the size of the rectangle that
     //   establishes the coordinate system for the 'background-position' property.
-    if (imageSize.width() > 0 || imageSize.height() > 0) {
-        imageSize = resolveAgainstIntrinsicWidthOrHeightAndRatio(positioningAreaSize, intrinsicRatio, imageSize.width(), imageSize.height());
-        return true;
-    }
+    if (resolvedSize.width() > 0 || resolvedSize.height() > 0)
+        return resolveAgainstIntrinsicWidthOrHeightAndRatio(positioningAreaSize, intrinsicRatio, resolvedSize.width(), resolvedSize.height());
 
     // If the image has no intrinsic dimensions and has an intrinsic ratio the dimensions must be assumed to be the
     // largest dimensions at that ratio such that neither dimension exceeds the dimensions of the rectangle that
     // establishes the coordinate system for the 'background-position' property.
-    if (!intrinsicRatio.isEmpty()) {
-        imageSize = resolveAgainstIntrinsicRatio(positioningAreaSize, intrinsicRatio);
-        return false;
-    }
+    if (!intrinsicRatio.isEmpty())
+        return resolveAgainstIntrinsicRatio(positioningAreaSize, intrinsicRatio);
 
     // If the image has no intrinsic ratio either, then the dimensions must be assumed to be the rectangle that
     // establishes the coordinate system for the 'background-position' property.
-    imageSize = positioningAreaSize;
-    return false;
+    return positioningAreaSize;
 }
 
 LayoutSize RenderBoxModelObject::calculateFillTileSize(const FillLayer& fillLayer, const LayoutSize& positioningAreaSize) const
@@ -952,7 +943,7 @@ LayoutSize RenderBoxModelObject::calculateFillTileSize(const FillLayer& fillLaye
 
     LayoutSize imageIntrinsicSize;
     if (image) {
-        calculateImageIntrinsicDimensions(image, positioningAreaSize, ScaleByEffectiveZoom, imageIntrinsicSize);
+        imageIntrinsicSize = calculateImageIntrinsicDimensions(image, positioningAreaSize, ScaleByEffectiveZoom);
         imageIntrinsicSize.scale(1 / image->imageScaleFactor(), 1 / image->imageScaleFactor());
     } else
         imageIntrinsicSize = positioningAreaSize;
@@ -1263,13 +1254,12 @@ bool RenderBoxModelObject::paintNinePieceImage(GraphicsContext& graphicsContext,
     rectWithOutsets.expand(style.imageOutsets(ninePieceImage));
     LayoutRect destination = LayoutRect(snapRectToDevicePixels(rectWithOutsets, deviceScaleFactor));
 
-    LayoutSize source;
-    bool intrinsicSource = calculateImageIntrinsicDimensions(styleImage, destination.size(), DoNotScaleByEffectiveZoom, source);
+    LayoutSize source = calculateImageIntrinsicDimensions(styleImage, destination.size(), DoNotScaleByEffectiveZoom);
 
     // If both values are ‘auto’ then the intrinsic width and/or height of the image should be used, if any.
     styleImage->setContainerSizeForRenderer(this, source, style.effectiveZoom());
 
-    ninePieceImage.paint(graphicsContext, this, style, destination, source, intrinsicSource, deviceScaleFactor, op);
+    ninePieceImage.paint(graphicsContext, this, style, destination, source, deviceScaleFactor, op);
     return true;
 }
 

LayoutTests/fast/borders/border-image-fill-no-intrinsic-size-expected.html
@@ -4,12 +4,18 @@
     .box {
       height: 100px;
       width: 100px;
-      margin: 10px;
       display: inline-block;
       background-color: red;
     }
-    .border {
+    .border-20 {
       border: 20px solid;
+    }
+    .border-40 {
+      border: solid;
+      border-top: 0px;
+      border-right: 40px;
+      border-bottom: 40px;
+      border-left: 0px;      
     }
     .no-border {
       border: 0px none;
@@ -20,6 +26,9 @@
     .border-image-100-intrinsic-slice {
        border-image: url('resources/svg-border-100x100-intrinsic.svg') 20 fill;
     }
+    .border-image-140-intrinsic {
+       border-image: url('resources/svg-border-140x140-intrinsic.svg') 0 fill;
+    }
     .border-image-140-intrinsic-slice {
        border-image: url('resources/svg-border-140x140-intrinsic.svg') 20 fill;
     }
@@ -27,12 +36,22 @@
 </head>
 <body>
   <div class="box no-border border-image-100-intrinsic"></div>
-  <div class="box border    border-image-100-intrinsic"></div>
+  <div class="box border-20 border-image-100-intrinsic"></div>
   <div class="box no-border border-image-100-intrinsic-slice"></div>
-  <div class="box border    border-image-100-intrinsic-slice"></div>
+  <div class="box border-20 border-image-100-intrinsic-slice"></div>
   <br>
   <div class="box no-border border-image-100-intrinsic"></div>
-  <div class="box border    border-image-100-intrinsic"></div>
+  <div class="box border-20 border-image-100-intrinsic"></div>
+  <div class="box no-border border-image-100-intrinsic-slice"></div>
+  <div class="box border-20 border-image-140-intrinsic-slice"></div>
+  <br>
   <div class="box no-border border-image-100-intrinsic"></div>
-  <div class="box border    border-image-140-intrinsic-slice"></div>
+  <div class="box border-20 border-image-100-intrinsic"></div>
+  <div class="box no-border border-image-100-intrinsic-slice"></div>
+  <div class="box border-20 border-image-140-intrinsic-slice"></div>
+  <br>
+  <div class="box no-border border-image-100-intrinsic"></div>
+  <div class="box border-20"></div>
+  <div class="box no-border border-image-100-intrinsic-slice"></div>
+  <div class="box border-40 border-image-100-intrinsic"></div>
 </body>

LayoutTests/fast/borders/border-image-fill-no-intrinsic-size.html
@@ -4,11 +4,10 @@
     .box {
       height: 100px;
       width: 100px;
-      margin: 10px;
       display: inline-block;
       background-color: red;
     }
-    .border {
+    .border-20 {
       border: 20px solid;
     }
     .no-border {
@@ -20,6 +19,18 @@
     .border-image-100-intrinsic-slice {
        border-image: url('resources/svg-border-100x100-intrinsic.svg') 20 fill;
     }
+    .border-image-100-viewbox {
+       border-image: url('resources/svg-border-100x100-viewbox.svg') 0 fill;
+    }
+    .border-image-100-viewbox-slice {
+       border-image: url('resources/svg-border-100x100-viewbox.svg') 20 fill;
+    }
+    .border-image-100-relative {
+       border-image: url('resources/svg-border-100x100-relative.svg') 0 fill;
+    }
+    .border-image-100-relative-slice {
+       border-image: url('resources/svg-border-100x100-relative.svg') 20 fill;
+    }
     .border-image-no-intrinsic {
        border-image: url('resources/svg-border-no-intrinsic.svg') 0 fill;
     }
@@ -30,12 +41,22 @@
 </head>
 <body>
   <div class="box no-border border-image-100-intrinsic"></div>
-  <div class="box border    border-image-100-intrinsic"></div>
+  <div class="box border-20 border-image-100-intrinsic"></div>
   <div class="box no-border border-image-100-intrinsic-slice"></div>
-  <div class="box border    border-image-100-intrinsic-slice"></div>
+  <div class="box border-20 border-image-100-intrinsic-slice"></div>
+  <br>
+  <div class="box no-border border-image-100-viewbox"></div>
+  <div class="box border-20 border-image-100-viewbox"></div>
+  <div class="box no-border border-image-100-viewbox-slice"></div>
+  <div class="box border-20 border-image-100-viewbox-slice"></div>
+  <br>
+  <div class="box no-border border-image-100-relative"></div>
+  <div class="box border-20 border-image-100-relative"></div>
+  <div class="box no-border border-image-100-relative-slice"></div>
+  <div class="box border-20 border-image-100-relative-slice"></div>
   <br>
   <div class="box no-border border-image-no-intrinsic"></div>
-  <div class="box border    border-image-no-intrinsic"></div>
+  <div class="box border-20"></div>
   <div class="box no-border border-image-no-intrinsic-slice"></div>
-  <div class="box border    border-image-no-intrinsic-slice"></div>
+  <div class="box border-20 border-image-no-intrinsic-slice"></div>
 </body>

LayoutTests/fast/borders/resources/svg-border-100x100-relative.svg
@@ -1,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <rect width="100%" height="100%" fill="lime"/>
+  <rect x="10%" y="10%" width="80%" height="80%" fill="none" stroke="black" stroke-width="4%"/>
+</svg>

LayoutTests/fast/borders/resources/svg-border-100x100-viewbox.svg
@@ -1,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 100 100">
+  <rect width="100" height="100" fill="lime"/>
+  <rect x="10" y="10" width="80" height="80" fill="none" stroke="black" stroke-width="4px"/>
+</svg>

LayoutTests/fast/borders/resources/svg-border-140x140-intrinsic.svg
@@ -1,52 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="140" height="140">
-  <defs>
-    <g id="border-image">
-      <rect width="100" height="100" fill="lime"/>
-      <rect x="10" y="10" width="80" height="80" fill="none" stroke="black" stroke-width="4px"/>
-    </g>
-    <clipPath id="top-left">
-      <rect x="0" y="0" width="20" height="20"/>
-    </clipPath>
-    <clipPath id="left">
-      <rect x="0" y="0" width="20" height="100"/>
-    </clipPath>
-    <clipPath id="bottom-left">
-      <rect x="0" y="0" width="20" height="20"/>
-    </clipPath>
-    <clipPath id="top-right">
-      <rect x="0" y="0" width="20" height="20"/>
-    </clipPath>
-    <clipPath id="right">
-      <rect x="0" y="0" width="20" height="100"/>
-    </clipPath>
-    <clipPath id="bottom-right">
-      <rect x="0" y="0" width="20" height="20"/>
-    </clipPath>
-    <clipPath id="top">
-      <rect x="0" y="0" width="100" height="20"/>
-    </clipPath>
-    <clipPath id="bottom">
-      <rect x="0" y="0" width="100" height="20"/>
-    </clipPath>
-    <clipPath id="middle">
-      <rect x="0" y="0" width="100" height="100"/>
-    </clipPath>
-  </defs>
-  
-  <!-- draw left side -->
-  <use x="0" y="0" xlink:href="#border-image" clip-path="url(#top-left)"/>
-  <use x="0" y="20" xlink:href="#border-image" clip-path="url(#left)"/>
-  <use x="0" y="120" xlink:href="#border-image" clip-path="url(#bottom-left)"/>
-  
-  <!-- draw right side -->
-  <use x="120" y="0" xlink:href="#border-image" clip-path="url(#top-right)"/>
-  <use x="120" y="20" xlink:href="#border-image" clip-path="url(#right)"/>
-  <use x="120" y="120" xlink:href="#border-image" clip-path="url(#bottom-right)"/>
-  
-  <!-- draw top and bottom sides -->
-  <use x="20" y="0" xlink:href="#border-image" clip-path="url(#top)"/>
-  <use x="20" y="120" xlink:href="#border-image" clip-path="url(#bottom)"/>
-  
-  <!-- draw middle -->
-  <use x="20" y="20" xlink:href="#border-image" clip-path="url(#middle)"/>
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="140" height="140">
+  <rect width="100%" height="100%" fill="lime"/>
+  <rect x="10%" y="10%" width="80%" height="80%" fill="none" stroke="black" stroke-width="4%"/>
 </svg>

Source/WebCore/rendering/RenderBoxModelObject.h
@@ -293,7 +293,7 @@ public:
     void moveChildrenTo(RenderBoxModelObject* toBoxModelObject, RenderObject* startChild, RenderObject* endChild, RenderObject* beforeChild, bool fullRemoveInsert = false);
 
     enum ScaleByEffectiveZoomOrNot { ScaleByEffectiveZoom, DoNotScaleByEffectiveZoom };
-    bool calculateImageIntrinsicDimensions(StyleImage*, const LayoutSize& scaledPositioningAreaSize, ScaleByEffectiveZoomOrNot, LayoutSize& imageSize) const;
+    LayoutSize calculateImageIntrinsicDimensions(StyleImage*, const LayoutSize& scaledPositioningAreaSize, ScaleByEffectiveZoomOrNot) const;
 
 private:
     LayoutUnit computedCSSPadding(const Length&) const;

Source/WebCore/rendering/RenderListMarker.cpp
@@ -1426,8 +1426,7 @@ void RenderListMarker::updateContent()
         // until we support the CSS3 marker pseudoclass to allow control over the width and height of the marker box.
         LayoutUnit bulletWidth = style().fontMetrics().ascent() / LayoutUnit(2);
         LayoutSize defaultBulletSize(bulletWidth, bulletWidth);
-        LayoutSize imageSize;
-        calculateImageIntrinsicDimensions(m_image.get(), defaultBulletSize, DoNotScaleByEffectiveZoom, imageSize);
+        LayoutSize imageSize = calculateImageIntrinsicDimensions(m_image.get(), defaultBulletSize, DoNotScaleByEffectiveZoom);
         m_image->setContainerSizeForRenderer(this, imageSize, style().effectiveZoom());
         return;
     }

commit 1901a2d4ecf6328d6d841dbbd915eadeb4117b6b

REGRESSION (r190883): Error calculating the tile size for an SVG with no intrinsic size but with large floating intrinsic ratio https://bugs.webkit.org/show_bug.cgi?id=150904
Patch by Said Abou-Hallawa <sabouhallawa@apple.com> on 2015-11-09
Reviewed by Darin Adler.
Source/WebCore:

This patch addresses two issues. The first one is a regression from r190883
which was rolling out r184895. There was a missing if-statement in
RenderBoxModelObject::calculateImageIntrinsicDimension(). We should return
it back. But this if-statement is an optimization; if we hit it we should
return the image resolvedSize. But we should also return the same result
if we call resolveAgainstIntrinsicWidthOrHeightAndRatio().

We had a bug in resolving the intrinsic size of an image using a large
intrinsic ratio. We need to do the calculation in LayoutUnits always.
Using float calculations and then casting the output to an integer results
in significant truncation if the intrinsic ratio is large.

Test: fast/backgrounds/background-image-large-float-intrinsic-ratio.html

* rendering/RenderBoxModelObject.cpp:
(WebCore::resolveWidthForRatio):
(WebCore::resolveHeightForRatio):
(WebCore::resolveAgainstIntrinsicWidthOrHeightAndRatio):
(WebCore::resolveAgainstIntrinsicRatio):
Resolve the image size using its intrinsic ratio in LayoutUnits.

(WebCore::RenderBoxModelObject::calculateImageIntrinsicDimensions):
Put back an if-statement which was missing from rolling out r184895

LayoutTests:

Make sure the image resolvedSize is calculated correctly when the intrinsic
ratio is a large non integer value.

* fast/backgrounds/background-image-large-float-intrinsic-ratio-expected.html: Added.
* fast/backgrounds/background-image-large-float-intrinsic-ratio.html: Added.

Identifier: 169238@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@192161 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderBoxModelObject.cpp
@@ -845,17 +845,17 @@ void RenderBoxModelObject::paintFillLayerExtended(const PaintInfo& paintInfo, co
     }
 }
 
-static inline int resolveWidthForRatio(LayoutUnit height, const FloatSize& intrinsicRatio)
+static inline LayoutUnit resolveWidthForRatio(LayoutUnit height, const LayoutSize& intrinsicRatio)
 {
     return height * intrinsicRatio.width() / intrinsicRatio.height();
 }
 
-static inline int resolveHeightForRatio(LayoutUnit width, const FloatSize& intrinsicRatio)
+static inline LayoutUnit resolveHeightForRatio(LayoutUnit width, const LayoutSize& intrinsicRatio)
 {
     return width * intrinsicRatio.height() / intrinsicRatio.width();
 }
 
-static inline LayoutSize resolveAgainstIntrinsicWidthOrHeightAndRatio(const LayoutSize& size, const FloatSize& intrinsicRatio, LayoutUnit useWidth, LayoutUnit useHeight)
+static inline LayoutSize resolveAgainstIntrinsicWidthOrHeightAndRatio(const LayoutSize& size, const LayoutSize& intrinsicRatio, LayoutUnit useWidth, LayoutUnit useHeight)
 {
     if (intrinsicRatio.isEmpty()) {
         if (useWidth)
@@ -868,7 +868,7 @@ static inline LayoutSize resolveAgainstIntrinsicWidthOrHeightAndRatio(const Layo
     return LayoutSize(resolveWidthForRatio(useHeight, intrinsicRatio), useHeight);
 }
 
-static inline LayoutSize resolveAgainstIntrinsicRatio(const LayoutSize& size, const FloatSize& intrinsicRatio)
+static inline LayoutSize resolveAgainstIntrinsicRatio(const LayoutSize& size, const LayoutSize& intrinsicRatio)
 {
     // Two possible solutions: (size.width(), solutionHeight) or (solutionWidth, size.height())
     // "... must be assumed to be the largest dimensions..." = easiest answer: the rect with the largest surface area.
@@ -915,18 +915,21 @@ LayoutSize RenderBoxModelObject::calculateImageIntrinsicDimensions(StyleImage* i
         resolvedSize.scale(style().effectiveZoom());
     resolvedSize.clampToMinimumSize(minimumSize);
 
+    if (!resolvedSize.isEmpty())
+        return resolvedSize;
+
     // If the image has one of either an intrinsic width or an intrinsic height:
     // * and an intrinsic aspect ratio, then the missing dimension is calculated from the given dimension and the ratio.
     // * and no intrinsic aspect ratio, then the missing dimension is assumed to be the size of the rectangle that
     //   establishes the coordinate system for the 'background-position' property.
     if (resolvedSize.width() > 0 || resolvedSize.height() > 0)
-        return resolveAgainstIntrinsicWidthOrHeightAndRatio(positioningAreaSize, intrinsicRatio, resolvedSize.width(), resolvedSize.height());
+        return resolveAgainstIntrinsicWidthOrHeightAndRatio(positioningAreaSize, LayoutSize(intrinsicRatio), resolvedSize.width(), resolvedSize.height());
 
     // If the image has no intrinsic dimensions and has an intrinsic ratio the dimensions must be assumed to be the
     // largest dimensions at that ratio such that neither dimension exceeds the dimensions of the rectangle that
     // establishes the coordinate system for the 'background-position' property.
     if (!intrinsicRatio.isEmpty())
-        return resolveAgainstIntrinsicRatio(positioningAreaSize, intrinsicRatio);
+        return resolveAgainstIntrinsicRatio(positioningAreaSize, LayoutSize(intrinsicRatio));
 
     // If the image has no intrinsic ratio either, then the dimensions must be assumed to be the rectangle that
     // establishes the coordinate system for the 'background-position' property.

LayoutTests/fast/backgrounds/background-image-large-float-intrinsic-ratio-expected.html
@@ -1,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        div {
+            height: 100px;
+            width: 100px;
+            display: inline-block;
+            border: 1px solid green;
+            margin: 10px;
+            background-color: green;
+        }
+    </style>
+</head>
+<body>
+    <div></div>
+    <div></div>
+</body>
+<html>

LayoutTests/fast/backgrounds/background-image-large-float-intrinsic-ratio.html
@@ -1,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        div {
+            background-position: -4000px 0;
+            background-size: auto 100px;
+            height: 100px;
+            width: 100px;
+            display: inline-block;
+            border: 1px solid green;
+            margin: 10px;
+        }
+        .intrinsic-size {
+            background-image: url("data:image/svg+xml;utf-8, \
+                <svg version='1.1' xmlns='http://www.w3.org/2000/svg' width='8000.8px' height='100px'> \
+                    <rect x='4000' y='0' width='101' height='100' fill='green'/> \
+                </svg>");
+        }
+        .non-intrinsic-size {
+            background-image: url("data:image/svg+xml;utf-8, \
+                <svg version='1.1' xmlns='http://www.w3.org/2000/svg' width='8000.8px' viewBox='0 0 8000.8 100'> \
+                    <rect x='4000' y='0' width='101' height='100' fill='green'/> \
+                </svg>");
+        }
+    </style>
+</head>
+<body>
+    <div class="intrinsic-size"></div>
+    <div class="non-intrinsic-size"></div>
+</body>
+<html>