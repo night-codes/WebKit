night-codes/WebKit

commit c757509b21f091419d5bd8788169ca05734d2e59

[EFL][GTK] Remove use of String::format() in versionForUAString() https://bugs.webkit.org/show_bug.cgi?id=151250
Reviewed by Darin Adler.

As String::format() will be deprecated due to the security problem, reimplement
versionForUAString() using a macro.

* platform/efl/UserAgentEfl.cpp:
(WebCore::versionForUAString):
* platform/gtk/UserAgentGtk.cpp:
(WebCore::platformVersionForUAString):
(WebCore::versionForUAString):


Identifier: 169457@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@192459 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/gtk/UserAgentGtk.cpp
@@ -111,10 +111,11 @@ static const String platformVersionForUAString()
 #endif
 }
 
-static const String versionForUAString()
+static const char* versionForUAString()
 {
-    static NeverDestroyed<const String> uaVersion(String::format("%i.%i", USER_AGENT_GTK_MAJOR_VERSION, USER_AGENT_GTK_MINOR_VERSION));
-    return uaVersion;
+#define MAKE_VERSION(major, minor) #major "." #minor
+    return MAKE_VERSION(USER_AGENT_GTK_MAJOR_VERSION, USER_AGENT_GTK_MINOR_VERSION);
+#undef MAKE_VERSION
 }
 
 static String buildUserAgentString(const UserAgentQuirks& quirks)

Source/WebCore/platform/efl/UserAgentEfl.cpp
@@ -55,15 +55,16 @@ static String platformVersionForUAString()
     return version;
 }
 
-static const String& versionForUAString()
+static const char* versionForUAString()
 {
-    static NeverDestroyed<String> version(String::format("%i.%i", WEBKIT_MAJOR_VERSION, WEBKIT_MINOR_VERSION));
-    return version;
+#define MAKE_VERSION(major, minor) #major "." #minor
+    return MAKE_VERSION(WEBKIT_MAJOR_VERSION, WEBKIT_MINOR_VERSION);
+#undef MAKE_VERSION
 }
 
 String standardUserAgent(const String& applicationName, const String& applicationVersion)
 {
-    const String& version = versionForUAString();
+    const String& version = ASCIILiteral(versionForUAString());
     static NeverDestroyed<String> standardUserAgentString = makeString("Mozilla/5.0 (", platformForUAString(), "; ", platformVersionForUAString(),
         ") AppleWebKit/", version, " (KHTML, like Gecko) Version/8.0 Safari/601.2.7");
 
@@ -72,7 +73,7 @@ String standardUserAgent(const String& applicationName, const String& applicatio
 
     String finalApplicationVersion = applicationVersion;
     if (finalApplicationVersion.isEmpty())
-        finalApplicationVersion = versionForUAString();
+        finalApplicationVersion = ASCIILiteral(versionForUAString());
 
     return standardUserAgentString + ' ' + applicationName + '/' + finalApplicationVersion;
 }

commit 1fbe2fb243409f9b89e67ec80304b325e55684c0

REGRESSION(r192459): [GTK] User agent string is broken after r192459 https://bugs.webkit.org/show_bug.cgi?id=151347
Reviewed by Žan Doberšek.

.:

Pass UA version numbers as strings to the build.

* Source/cmake/OptionsGTK.cmake:

Source/WebCore:

Remove the incorrect macro and simply use the given values
USER_AGENT_GTK_MAJOR_VERSION and USER_AGENT_GTK_MINOR_VERSION that
are now strings.

* platform/gtk/UserAgentGtk.cpp:
(WebCore::versionForUAString):
(WebCore::buildUserAgentString):

Identifier: 169537@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@192566 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/gtk/UserAgentGtk.cpp
@@ -113,9 +113,7 @@ static const String platformVersionForUAString()
 
 static const char* versionForUAString()
 {
-#define MAKE_VERSION(major, minor) #major "." #minor
-    return MAKE_VERSION(USER_AGENT_GTK_MAJOR_VERSION, USER_AGENT_GTK_MINOR_VERSION);
-#undef MAKE_VERSION
+    return USER_AGENT_GTK_MAJOR_VERSION "." USER_AGENT_GTK_MINOR_VERSION;
 }
 
 static String buildUserAgentString(const UserAgentQuirks& quirks)

Source/cmake/OptionsGTK.cmake
@@ -215,8 +215,8 @@ set(ENABLE_PLUGIN_PROCESS ${ENABLE_X11_TARGET})
 add_definitions(-DBUILDING_GTK__=1)
 add_definitions(-DGETTEXT_PACKAGE="WebKit2GTK-${WEBKITGTK_API_VERSION}")
 add_definitions(-DDATA_DIR="${CMAKE_INSTALL_DATADIR}")
-add_definitions(-DUSER_AGENT_GTK_MAJOR_VERSION=602)
-add_definitions(-DUSER_AGENT_GTK_MINOR_VERSION=1)
+add_definitions(-DUSER_AGENT_GTK_MAJOR_VERSION="602")
+add_definitions(-DUSER_AGENT_GTK_MINOR_VERSION="1")
 add_definitions(-DWEBKITGTK_API_VERSION_STRING="${WEBKITGTK_API_VERSION}")
 
 set(GTK_LIBRARIES ${GTK3_LIBRARIES})