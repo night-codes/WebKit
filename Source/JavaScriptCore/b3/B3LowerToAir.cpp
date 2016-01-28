night-codes/WebKit

commit abdbc90207471ebec9297f8362efec930aedf3b8

[JSC] When lowering B3 to Air, preferRightForResult() should prefer values from the same block https://bugs.webkit.org/show_bug.cgi?id=153477
Reviewed by Filip Pizlo.

In cases like this:

Block #0
    @1 = something
    @2 = Jump #1
Block #1
    @3 = something else
    @4 = Add(@3, @1)
    ...
    @42 = Branch(@x, #1, #2)

B3LowerToAir would pick @1 for the argument copied
for what goes into the UseDef side of Add.

This created a bunch of moves that could never be coalesced.
In Kraken's imaging-desaturate, there were enough Moves to slow
down the hot loop.

Ideally, we should not use UseCount for lowering. We should use
the real liveness for preferRightForResult(), and a loop-weighted
use-count for effective addresses. The problem is keeping the cost
low for those simple helpers.

In this patch, I went with a simple heuristic: prioritize the value
defined in the same block for UseDef.

There is one other way that would be cheap but a bit invasive:
-Get rid of UseDef.
-Make every ops, 3 operands.
-Tell the register allocator to attempt aliasing of the 2 uses
 with the def.
-If the allocator fails, just add a move as needed.

For now, the simple heuristic seems okay for the cases have.

* b3/B3LowerToAir.cpp:
(JSC::B3::Air::LowerToAir::preferRightForResult):


Identifier: 171601@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@195654 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/b3/B3LowerToAir.cpp
@@ -583,18 +583,27 @@ private:
         // - A child might be a candidate for coalescing with this value.
         //
         // Currently, we have machinery in place to recognize super obvious forms of the latter issue.
-
-        bool result = m_useCounts.numUsingInstructions(right) == 1;
         
         // We recognize when a child is a Phi that has this value as one of its children. We're very
         // conservative about this; for example we don't even consider transitive Phi children.
         bool leftIsPhiWithThis = m_phiChildren[left].transitivelyUses(m_value);
         bool rightIsPhiWithThis = m_phiChildren[right].transitivelyUses(m_value);
-        
-        if (leftIsPhiWithThis != rightIsPhiWithThis)
-            result = rightIsPhiWithThis;
 
-        return result;
+        if (leftIsPhiWithThis != rightIsPhiWithThis)
+            return rightIsPhiWithThis;
+
+        bool leftResult = m_useCounts.numUsingInstructions(left) == 1;
+        bool rightResult = m_useCounts.numUsingInstructions(right) == 1;
+        if (leftResult && rightResult) {
+            // If one operand is not in the block, it could be in a block dominating a loop
+            // containing m_value.
+            if (left->owner == m_value->owner)
+                return false;
+            if (right->owner == m_value->owner)
+                return true;
+        }
+
+        return rightResult;
     }
 
     template<Air::Opcode opcode32, Air::Opcode opcode64, Air::Opcode opcodeDouble, Air::Opcode opcodeFloat, Commutativity commutativity = NotCommutative>

commit 05bbe3f13b53a2e1aba437e8f3b7a3854a7550b3

LowerToAir::preferRightForResult() should resolve use count ties by selecting the child that is closest in an idom walk https://bugs.webkit.org/show_bug.cgi?id=153583
Reviewed by Benjamin Poulain.

This undoes the AsmBench/n-body regression in r195654 while preserving that revision's
Kraken progression.

* b3/B3LowerToAir.cpp:
(JSC::B3::Air::LowerToAir::LowerToAir):
(JSC::B3::Air::LowerToAir::preferRightForResult):



Identifier: 171667@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@195744 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/b3/B3LowerToAir.cpp
@@ -39,6 +39,7 @@
 #include "B3CCallValue.h"
 #include "B3CheckSpecial.h"
 #include "B3Commutativity.h"
+#include "B3Dominators.h"
 #include "B3IndexMap.h"
 #include "B3IndexSet.h"
 #include "B3MemoryValue.h"
@@ -75,6 +76,7 @@ public:
         , m_blockToBlock(procedure.size())
         , m_useCounts(procedure)
         , m_phiChildren(procedure)
+        , m_dominators(procedure.dominators())
         , m_procedure(procedure)
         , m_code(procedure.code())
     {
@@ -592,18 +594,22 @@ private:
         if (leftIsPhiWithThis != rightIsPhiWithThis)
             return rightIsPhiWithThis;
 
-        bool leftResult = m_useCounts.numUsingInstructions(left) == 1;
-        bool rightResult = m_useCounts.numUsingInstructions(right) == 1;
-        if (leftResult && rightResult) {
-            // If one operand is not in the block, it could be in a block dominating a loop
-            // containing m_value.
-            if (left->owner == m_value->owner)
-                return false;
-            if (right->owner == m_value->owner)
-                return true;
-        }
+        if (m_useCounts.numUsingInstructions(right) != 1)
+            return false;
+        
+        if (m_useCounts.numUsingInstructions(left) != 1)
+            return true;
 
-        return rightResult;
+        // The use count might be 1 if the variable is live around a loop. We can guarantee that we
+        // pick the the variable that is least likely to suffer this problem if we pick the one that
+        // is closest to us in an idom walk. By convention, we slightly bias this in favor of
+        // returning true.
+
+        // We cannot prefer right if right is further away in an idom walk.
+        if (m_dominators.strictlyDominates(right->owner, left->owner))
+            return false;
+
+        return true;
     }
 
     template<Air::Opcode opcode32, Air::Opcode opcode64, Air::Opcode opcodeDouble, Air::Opcode opcodeFloat, Commutativity commutativity = NotCommutative>
@@ -2297,6 +2303,7 @@ private:
     UseCounts m_useCounts;
     PhiChildren m_phiChildren;
     BlockWorklist m_fastWorklist;
+    Dominators& m_dominators;
 
     Vector<Vector<Inst, 4>> m_insts;
     Vector<Inst> m_prologue;