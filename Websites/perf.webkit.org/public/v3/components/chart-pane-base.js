night-codes/WebKit

commit 17d5e1fc11ea9dbfa2ae9d49af568aeef2f6380c

Associated commits don't immediately show up on an analysis task page https://bugs.webkit.org/show_bug.cgi?id=155692
Reviewed by Darin Adler.

The bug was caused by resolveCommits in AnalysisTask._constructAnalysisTasksFromRawData not being
able to find the matching commit log if the commit log had been created by the charts which don't
set the remote identifiers on each CommitLog objects.

Fixed the bug by modifying /api/measurement-set to include the commit ID, and making CommitLog
use the real database ID as its ID instead of a fake ID we create from repository and revision.

Also added a bunch of Mocha unit tests for AnalysisTask.fetchAll.

* public/api/measurement-set.php:
(MeasurementSetFetcher::execute_query): Fetch commit_id.
(MeasurementSetFetcher::format_run): Use pass-by-reference to avoid making a copy of the row.
(MeasurementSetFetcher::parse_revisions_array): Include commit_id as the first item in the result.
* public/v3/instrumentation.js:
* public/v3/models/analysis-task.js:
(AnalysisTask): Fixed a bug that _buildRequestCount and _finishedBuildRequestCount could be kept
as strings and hasPendingRequests() could return a wrong result because it would perform string
inequality instead of numerical inequality.
(AnalysisTask.prototype.updateSingleton): Ditto.
(AnalysisTask.prototype.dissociateCommit):
(AnalysisTask._constructAnalysisTasksFromRawData):
(AnalysisTask._constructAnalysisTasksFromRawData.resolveCommits): Use findById now that CommitLog
objects all use the same id as the database id.
* public/v3/models/commit-log.js:
(CommitLog):
(CommitLog.prototype.remoteId): Deleted since we no longer create a fake id for commit logs for
measurement sets.
(CommitLog.findByRemoteId): Deleted.
(CommitLog.ensureSingleton): Deleted.
(CommitLog.fetchBetweenRevisions):

* public/v3/models/data-model.js:
(DataModelObject.clearStaticMap): Added to aid unit testing.
(DataModelObject.ensureNamedStaticMap): Fixed a typo. Each map is a dictionary, not an array.

Websites/perf.webkit.org/public/v3/models/data-model.js
@@ -20,6 +20,8 @@ class DataModelObject {
 
     updateSingleton(object) { }
 
+    static clearStaticMap() { this[DataModelObject.StaticMapSymbol] = null; }
+
     static namedStaticMap(name)
     {
         var staticMap = this[DataModelObject.StaticMapSymbol];
@@ -32,7 +34,7 @@ class DataModelObject {
             this[DataModelObject.StaticMapSymbol] = {};
         var staticMap = this[DataModelObject.StaticMapSymbol];
         if (!staticMap[name])
-            staticMap[name] = [];
+            staticMap[name] = {};
         return staticMap[name];
     }
 

Websites/perf.webkit.org/public/v3/models/test.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class Test extends LabeledObject {
     constructor(id, object, isTopLevel)
@@ -42,3 +43,6 @@ class Test extends LabeledObject {
     addChildTest(test) { this._childTests.push(test); }
     addMetric(metric) { this._metrics.push(metric); }
 }
+
+if (typeof module != 'undefined')
+    module.exports.Test = Test;

Websites/perf.webkit.org/public/api/measurement-set.php
@@ -165,7 +165,7 @@ class MeasurementSetFetcher {
     function execute_query($config_id) {
         return $this->db->query('
             SELECT test_runs.*, builds.*,
-            array_agg((commit_repository, commit_revision, commit_time)) AS revisions,
+            array_agg((commit_id, commit_repository, commit_revision, commit_time)) AS revisions,
             max(commit_time) AS revision_time, max(commit_order) AS revision_order
                 FROM builds
                     LEFT OUTER JOIN build_commits ON commit_build = build_id
@@ -180,7 +180,7 @@ class MeasurementSetFetcher {
             'commitTime', 'build', 'buildTime', 'buildNumber', 'builder');
     }
 
-    private static function format_run($run, &$commit_time) {
+    private static function format_run(&$run, &$commit_time) {
         $commit_time = Database::to_js_time($run['revision_time']);
         $build_time = Database::to_js_time($run['build_time']);
         if (!$commit_time)
@@ -200,16 +200,19 @@ class MeasurementSetFetcher {
             intval($run['build_builder']));
     }
 
-    private static function parse_revisions_array($postgres_array) {
-        // e.g. {"(WebKit,131456,\"2012-10-16 14:53:00\")","(Chromium,162004,)"}
+    private static function parse_revisions_array(&$postgres_array) {
+        // e.g. {"(<commit-id>,<repository-id>,<revision>,\"2012-10-16 14:53:00\")","(<commit-id>,<repository-id>,<revision>,)"}
         $outer_array = json_decode('[' . trim($postgres_array, '{}') . ']');
         $revisions = array();
         foreach ($outer_array as $item) {
             $name_and_revision = explode(',', trim($item, '()'));
             if (!$name_and_revision[0])
                 continue;
-            $time = Database::to_js_time(trim($name_and_revision[2], '"'));
-            array_push($revisions, array(intval(trim($name_and_revision[0], '"')), trim($name_and_revision[1], '"'), $time));
+            $commit_id = intval(trim($name_and_revision[0], '"'));
+            $repository_id = intval(trim($name_and_revision[1], '"'));
+            $revision = trim($name_and_revision[2], '"');
+            $time = Database::to_js_time(trim($name_and_revision[3], '"'));
+            array_push($revisions, array($commit_id, $repository_id, $revision, $time));
         }
         return $revisions;
     }

Websites/perf.webkit.org/public/v3/instrumentation.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Instrumentation {
 
     static startMeasuringTime(domain, label)
@@ -53,3 +55,6 @@ class Instrumentation {
     }
 
 }
+
+if (typeof module != 'undefined')
+    module.exports.Instrumentation = Instrumentation;

Websites/perf.webkit.org/public/v3/models/analysis-task.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class AnalysisTask extends LabeledObject {
     constructor(id, object)
@@ -22,8 +23,8 @@ class AnalysisTask extends LabeledObject {
         this._bugs = object.bugs || [];
         this._causes = object.causes || [];
         this._fixes = object.fixes || [];
-        this._buildRequestCount = object.buildRequestCount;
-        this._finishedBuildRequestCount = object.finishedBuildRequestCount;
+        this._buildRequestCount = +object.buildRequestCount;
+        this._finishedBuildRequestCount = +object.finishedBuildRequestCount;
     }
 
     static findByPlatformAndMetric(platformId, metricId)
@@ -50,8 +51,8 @@ class AnalysisTask extends LabeledObject {
         this._bugs = object.bugs || [];
         this._causes = object.causes || [];
         this._fixes = object.fixes || [];
-        this._buildRequestCount = object.buildRequestCount;
-        this._finishedBuildRequestCount = object.finishedBuildRequestCount;
+        this._buildRequestCount = +object.buildRequestCount;
+        this._finishedBuildRequestCount = +object.finishedBuildRequestCount;
     }
 
     hasResults() { return this._finishedBuildRequestCount; }
@@ -138,7 +139,7 @@ class AnalysisTask extends LabeledObject {
         var id = this.id();
         return PrivilegedAPI.sendRequest('associate-commit', {
             task: id,
-            commit: commit.remoteId(),
+            commit: commit.id(),
         }).then(function (data) {
             return AnalysisTask.cachedFetch('../api/analysis-tasks', {id: id}, true)
                 .then(AnalysisTask._constructAnalysisTasksFromRawData.bind(AnalysisTask));
@@ -232,11 +233,11 @@ class AnalysisTask extends LabeledObject {
             rawData.repository = Repository.findById(rawData.repository);
             if (!rawData.repository)
                 continue;
-            CommitLog.ensureSingleton(rawData.repository, rawData);
+            CommitLog.ensureSingleton(rawData.id, rawData);
         }
 
         function resolveCommits(commits) {
-            return commits.map(function (id) { return CommitLog.findByRemoteId(id); }).filter(function (commit) { return !!commit; });
+            return commits.map(function (id) { return CommitLog.findById(id); }).filter(function (commit) { return !!commit; });
         }
 
         var results = [];
@@ -266,3 +267,6 @@ class AnalysisTask extends LabeledObject {
         });
     }
 }
+
+if (typeof module != 'undefined')
+    module.exports.AnalysisTask = AnalysisTask;

Websites/perf.webkit.org/public/v3/models/commit-log.js
@@ -5,25 +5,11 @@ class CommitLog extends DataModelObject {
     {
         super(id);
         this._repository = rawData.repository;
+        console.assert(this._repository instanceof Repository);
         this._rawData = rawData;
         this._remoteId = rawData.id;
         if (this._remoteId)
             this.ensureNamedStaticMap('remoteId')[this._remoteId] = this;
-    }
-
-    // FIXME: All this non-sense should go away once measurement-set start returning real commit id.
-    remoteId() { return this._remoteId; }
-    static findByRemoteId(id)
-    {
-        var remoteIdMap = super.namedStaticMap('remoteId');
-        return remoteIdMap ? remoteIdMap[id] : null;
-    }
-
-    static ensureSingleton(repository, rawData)
-    {
-        var id = repository.id() + '-' + rawData['revision'];
-        rawData.repository = repository;
-        return super.ensureSingleton(id, rawData);
     }
 
     updateSingleton(rawData)
@@ -99,7 +85,10 @@ class CommitLog extends DataModelObject {
 
         var self = this;
         return RemoteAPI.getJSONWithStatus(url).then(function (data) {
-            var commits = data['commits'].map(function (rawData) { return CommitLog.ensureSingleton(repository, rawData); });
+            var commits = data['commits'].map(function (rawData) {
+                rawData.repository = repository;
+                return CommitLog.ensureSingleton(rawData.id, rawData);
+            });
             self._cacheCommitLogs(repository, from, to, commits);
             return commits;
         });

Websites/perf.webkit.org/public/v3/models/metric.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class Metric extends LabeledObject {
     constructor(id, object)
@@ -86,3 +87,6 @@ class Metric extends LabeledObject {
         }
     };
 }
+
+if (typeof module != 'undefined')
+    module.exports.Metric = Metric;

Websites/perf.webkit.org/public/v3/models/platform.js
@@ -1,3 +1,4 @@
+'use strict';
 
 class Platform extends LabeledObject {
     constructor(id, object)
@@ -34,3 +35,6 @@ class Platform extends LabeledObject {
         return this._lastModifiedByMetric[metric.id()];
     }
 }
+
+if (typeof module != 'undefined')
+    module.exports.Platform = Platform;

commit 1f12ad2a94e08fdef0443b41923bd2fce9f9395e

v3 UI sometimes don't update the list of revisions on the commit log viewer https://bugs.webkit.org/show_bug.cgi?id=155729
Rubber-stamped by Chris Dumez.

Fixed multiple bugs that were affecting the list of blame range and commit logs for the range weren't
updated in some cases on v3 UI. Also, the commit log viewer state is now a part of the URL state so
opening and closing the commit log viewer will persist across page loads.

Also fixed a regression from r198479 that Test object can't be created for a top level test.

* public/v3/components/chart-pane-base.js:
(ChartPaneBase.prototype.configure):
(ChartPaneBase.prototype._mainSelectionDidChange): Fixed the bug that the list of blame range nor the
commit log viewer don't get updated when the selected range changes.
(ChartPaneBase.prototype._indicatorDidChange):
(ChartPaneBase.prototype._didFetchData):
(ChartPaneBase.prototype._updateStatus): Extracted from _indicatorDidChange and _didFetchData.
(ChartPaneBase.prototype._requestOpeningCommitViewer): Renamed from _openCommitViewer.

* public/v3/components/chart-status-view.js:
(ChartStatusView.prototype.updateStatusIfNeeded): Fixed the bug that the blame range doesn't get set
on the initial page load when the selection range is set but the chart data hadn't been fetched yet.

* public/v3/components/commit-log-viewer.js:
(CommitLogViewer.prototype.view): Fixed the bug that we don't clear out the old list of commits while
loading the next set of commits to show as it looked as if the list was never updated.
(CommitLogViewer.prototype.render): Fixed the bug that the view always show the last repository name
even if there were nothing being fetched or commits to show.

* public/v3/components/pane-selector.js:
(PaneSelector.prototype.focus): Removed superfluous call to console.log.

* public/v3/models/data-model.js:
(DataModelObject.listForStaticMap): Generalized the code for all to fix the bug in Test.
(DataModelObject.all):

* public/v3/models/test.js:

Websites/perf.webkit.org/public/v3/components/chart-pane-base.js
@@ -54,7 +54,7 @@ class ChartPaneBase extends ComponentBase {
         this._mainChart = new InteractiveTimeSeriesChart(result.sourceList, mainOptions);
         this.renderReplace(this.content().querySelector('.chart-pane-main'), this._mainChart);
 
-        this._mainChartStatus = new ChartPaneStatusView(result.metric, this._mainChart, this._openCommitViewer.bind(this));
+        this._mainChartStatus = new ChartPaneStatusView(result.metric, this._mainChart, this._requestOpeningCommitViewer.bind(this));
         this.renderReplace(this.content().querySelector('.chart-pane-details'), this._mainChartStatus);
 
         this.content().querySelector('.chart-pane').addEventListener('keyup', this._keyup.bind(this));
@@ -97,7 +97,7 @@ class ChartPaneBase extends ComponentBase {
 
     _mainSelectionDidChange(selection, didEndDrag)
     {
-        this.render();
+        this._updateStatus();
     }
 
     _mainSelectionDidZoom(selection)
@@ -109,13 +109,18 @@ class ChartPaneBase extends ComponentBase {
 
     _indicatorDidChange(indicatorID, isLocked)
     {
-        this._mainChartStatus.updateRevisionListWithNotification();
-        this.render();
+        this._updateStatus();
     }
 
     _didFetchData()
     {
-        this._mainChartStatus.updateRevisionListWithNotification();
+        this._updateStatus();
+    }
+
+    _updateStatus()
+    {
+        var range = this._mainChartStatus.updateRevisionList();
+        this._commitLogViewer.view(range.repository, range.from, range.to).then(this.render.bind(this));
         this.render();
     }
 
@@ -128,7 +133,7 @@ class ChartPaneBase extends ComponentBase {
 
     router() { return null; }
 
-    _openCommitViewer(repository, from, to)
+    _requestOpeningCommitViewer(repository, from, to)
     {
         this._mainChartStatus.setCurrentRepository(repository);
         this._commitLogViewer.view(repository, from, to).then(this.render.bind(this));

Websites/perf.webkit.org/public/v3/components/chart-status-view.js
@@ -53,9 +53,9 @@ class ChartStatusView extends ComponentBase {
                 var data = this._chart.sampledDataBetween('current', selection[0], selection[1]);
                 if (!data)
                     return false;
-                this._usedSelection = selection;
 
                 if (data && data.length > 1) {
+                    this._usedSelection = selection;
                     currentPoint = data[data.length - 1];
                     previousPoint = data[0];
                 }

Websites/perf.webkit.org/public/v3/components/commit-log-viewer.js
@@ -20,15 +20,17 @@ class CommitLogViewer extends ComponentBase {
             this._repository = null;
             return Promise.resolve(null);
         }
-        
+
+        this._repository = repository;
+
         if (!to) {
             this._fetchingPromise = null;
+            this.render();
             return Promise.resolve(null);
         }
 
         var promise = CommitLog.fetchBetweenRevisions(repository, from || to, to);
 
-        this._repository = repository;
         this._fetchingPromise = promise;
 
         var self = this;
@@ -42,6 +44,11 @@ class CommitLogViewer extends ComponentBase {
                 return;
             self._fetchingPromise = null;
             self._commits = commits;
+        }, function (error) {
+            if (self._fetchingPromise != promise)
+                return;
+            self._fetchingPromise = null;
+            self._commits = null;
         });
 
         return this._fetchingPromise;
@@ -49,8 +56,10 @@ class CommitLogViewer extends ComponentBase {
 
     render()
     {
-        if (this._repository)
-            this.content().querySelector('caption').textContent = this._repository.name();
+        var caption = '';
+        if (this._repository && (this._commits || this._fetchingPromise))
+            caption = this._repository.name();
+        this.content().querySelector('caption').textContent = caption;
 
         var element = ComponentBase.createElement;
         var link = ComponentBase.createLink;

Websites/perf.webkit.org/public/v3/components/pane-selector.js
@@ -23,7 +23,6 @@ class PaneSelector extends ComponentBase {
     focus()
     {
         var select = this.content().querySelector('select');
-        console.log(select);
         if (select) {
             if (select.selectedIndex < 0)
                 select.selectedIndex = 0;

Websites/perf.webkit.org/public/v3/models/data-model.js
@@ -47,16 +47,18 @@ class DataModelObject {
         return idMap ? idMap[id] : null;
     }
 
-    static all()
+    static listForStaticMap(name)
     {
         var list = [];
-        var idMap = this.namedStaticMap('id');
+        var idMap = this.namedStaticMap(name);
         if (idMap) {
             for (var id in idMap)
                 list.push(idMap[id]);
         }
         return list;
     }
+
+    static all() { return this.listForStaticMap('id'); }
 
     static cachedFetch(path, params, noCache)
     {

Websites/perf.webkit.org/public/v3/models/test.js
@@ -10,10 +10,10 @@ class Test extends LabeledObject {
         this._metrics = [];
 
         if (isTopLevel)
-            this.ensureNamedStaticMap('topLevelTests').push(this);
+            this.ensureNamedStaticMap('topLevelTests')[id] = this;
     }
 
-    static topLevelTests() { return this.sortByName(this.namedStaticMap('topLevelTests')); }
+    static topLevelTests() { return this.sortByName(this.listForStaticMap('topLevelTests')); }
 
     parentTest() { return this._parent; }
 

Websites/perf.webkit.org/public/v3/pages/chart-pane-status-view.js
@@ -16,7 +16,7 @@ class ChartPaneStatusView extends ChartStatusView {
         this._renderedRevisionList = null;
         this._renderedRepository = null;
 
-        this._usedRevisionRange = null;
+        this._usedRevisionRange = [null, null, null];
     }
 
     pointsRangeForAnalysis() { return this._pointsRangeForAnalysis; }
@@ -38,12 +38,12 @@ class ChartPaneStatusView extends ChartStatusView {
             var selected = info.repository == self._currentRepository;
             var action = function () {
                 if (self._currentRepository == info.repository)
-                    self._setRevisionRange(null, null, null);
+                    self._setRevisionRange(true, null, null, null);
                 else
-                    self._setRevisionRange(info.repository, info.from, info.to);
+                    self._setRevisionRange(true, info.repository, info.from, info.to);
             };
 
-            return element('tr', {class: selected ? 'selected' : '', onclick: action}, [
+            return element('tr', {class: selected ? 'selected' : ''}, [
                 element('td', info.repository.name()),
                 element('td', info.url ? link(info.label, info.label, info.url, true) : info.label),
                 element('td', {class: 'commit-viewer-opener'}, link('\u00BB', action)),
@@ -74,16 +74,17 @@ class ChartPaneStatusView extends ChartStatusView {
     setCurrentRepository(repository)
     {
         this._currentRepository = repository;
-        this._forceRender = true;
+        return this._updateRevisionListForNewCurrentRepository();
     }
 
-    _setRevisionRange(repository, from, to)
+    _setRevisionRange(shouldNotify, repository, from, to)
     {
-        if (this._usedRevisionRange && this._usedRevisionRange[0] == repository
+        if (this._usedRevisionRange[0] == repository
             && this._usedRevisionRange[1] == from && this._usedRevisionRange[2] == to)
             return;
         this._usedRevisionRange = [repository, from, to];
-        this._revisionCallback(repository, from, to);
+        if (shouldNotify)
+            this._revisionCallback(repository, from, to);
     }
 
     moveRepositoryWithNotification(forward)
@@ -99,26 +100,32 @@ class ChartPaneStatusView extends ChartStatusView {
         if (newIndex == index)
             return false;
 
-        var info = this._revisionList[newIndex];
-        this.setCurrentRepository(info.repository);
-        this.updateRevisionListWithNotification();
+        var item = this._revisionList[newIndex];
+        this.setCurrentRepository(item ? item.repository : null);
+
+        return true;
     }
 
-    updateRevisionListWithNotification()
+    updateRevisionList()
     {
         if (!this._currentRepository)
-            return;
+            return {repository: null, from: null, to: null};
+        return this._updateRevisionListForNewCurrentRepository();
+    }
 
+    _updateRevisionListForNewCurrentRepository()
+    {
         this.updateStatusIfNeeded();
 
         this._forceRender = true;
         for (var info of this._revisionList) {
             if (info.repository == this._currentRepository) {
-                this._setRevisionRange(info.repository, info.from, info.to);
-                return;
+                this._setRevisionRange(false, info.repository, info.from, info.to);
+                return {repository: info.repository, from: info.from, to: info.to};
             }
         }
-        this._setRevisionRange(this._currentRepository, null, null);
+        this._setRevisionRange(false, null, null, null);
+        return {repository: this._currentRepository, from: null, to: null};
     }
 
     computeChartStatusLabels(currentPoint, previousPoint)

Websites/perf.webkit.org/public/v3/pages/chart-pane.js
@@ -65,6 +65,21 @@ class ChartPane extends ChartPaneBase {
 
     router() { return this._chartsPage.router(); }
 
+    _requestOpeningCommitViewer(repository, from, to)
+    {
+        super._requestOpeningCommitViewer(repository, from, to);
+        this._chartsPage.setOpenRepository(repository);
+    }
+
+    setOpenRepository(repository)
+    {
+        if (repository != this._commitLogViewer.currentRepository()) {
+            var range = this._mainChartStatus.setCurrentRepository(repository);
+            this._commitLogViewer.view(repository, range.from, range.to).then(this.render.bind(this));
+            this.render();
+        }
+    }
+
     _indicatorDidChange(indicatorID, isLocked)
     {
         this._mainChartIndicatorWasLocked = isLocked;

Websites/perf.webkit.org/public/v3/pages/charts-page.js
@@ -7,6 +7,7 @@ class ChartsPage extends PageWithHeading {
         this._paneList = [];
         this._paneListChanged = false;
         this._mainDomain = null;
+        this._currentRepositoryId = null;
 
         toolbar.setAddPaneCallback(this.insertPaneAfter.bind(this));
     }
@@ -54,6 +55,10 @@ class ChartsPage extends PageWithHeading {
         if (serializedPaneList.length)
             state['paneList'] = serializedPaneList;
 
+        var repository = this._currentRepositoryId;
+        if (repository)
+            state['repository'] = this._currentRepositoryId;
+
         return state;
     }
 
@@ -72,9 +77,14 @@ class ChartsPage extends PageWithHeading {
 
         this._updateDomainsFromSerializedState(state);
 
+        this._currentRepositoryId = parseInt(state['repository']);
+        var currentRepository = Repository.findById(this._currentRepositoryId);
+
         console.assert(this._paneList.length == paneList.length);
-        for (var i = 0; i < this._paneList.length; i++)
+        for (var i = 0; i < this._paneList.length; i++) {
             this._paneList[i].updateFromSerializedState(state.paneList[i], isOpen);
+            this._paneList[i].setOpenRepository(currentRepository);
+        }
     }
 
     _updateDomainsFromSerializedState(state)
@@ -166,6 +176,14 @@ class ChartsPage extends PageWithHeading {
     {
         if (shouldUpdateState)
             this.scheduleUrlStateUpdate();
+    }
+
+    setOpenRepository(repository)
+    {
+        this._currentRepositoryId = repository ? repository.id() : null;
+        for (var pane of this._paneList)
+            pane.setOpenRepository(repository);
+        this.scheduleUrlStateUpdate();
     }
 
     _updateOverviewDomain()

Websites/perf.webkit.org/tests/api-measurement-set.js
@@ -304,7 +304,7 @@ describe("/api/measurement-set", function () {
                            sum: 65,
                            squareSum: 855,
                            markedOutlier: false,
-                           revisions: [[repositoryId, '144000', revisionTime]],
+                           revisions: [[1, repositoryId, '144000', revisionTime]],
                            commitTime: revisionTime,
                            buildTime: revisionBuildTime,
                            buildNumber: '124' });