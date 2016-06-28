night-codes/WebKit

commit d1e3d505aa19f3680e959432a72e2290c7ffa698

[iOS] Make DumpRenderTree and WebKitTestRunner in the simulator use render server snapshotting https://bugs.webkit.org/show_bug.cgi?id=159077
Reviewed by Tim Horton.

Source/WebKit2:

Fix WebPageProxy::forceRepaint() to correctly wait for the next commit from the web process,
which is necessary for UI-side compositing.

* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::forceRepaint):

Tools:

Re-enable render server snapshotting for iOS WTR via the change in InjectedBundlePage.cpp.

Fix WebPageProxy::forceRepaint() to correctly wait for the next commit from the web process,
which is necessary for UI-side compositing.

Add some null checks to fix issues when the WKWebView gets resized to be empty, which
seems to happen for some tests that call window.resizeTo().

* WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
(WTR::InjectedBundlePage::dump): Deleted.
* WebKitTestRunner/TestInvocation.cpp:
(WTR::TestInvocation::dumpResults):
* WebKitTestRunner/cg/TestInvocationCG.cpp:
(WTR::createCGContextFromImage):
(WTR::computeMD5HashStringForContext):
(WTR::TestInvocation::dumpPixelsAndCompareWithExpected):
* WebKitTestRunner/ios/PlatformWebViewIOS.mm:
(WTR::PlatformWebView::windowSnapshotImage):

LayoutTests:

fast/harness/snapshot-captures-compositing.html passes now.

* platform/ios-simulator-wk2/TestExpectations:

Identifier: 177283@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202532 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -2888,8 +2888,18 @@ void WebPageProxy::forceRepaint(PassRefPtr<VoidCallback> prpCallback)
         return;
     }
 
-    uint64_t callbackID = callback->callbackID();
-    m_callbacks.put(callback);
+    std::function<void (CallbackBase::Error)> didForceRepaintCallback = [this, callback](CallbackBase::Error error) {
+        callAfterNextPresentationUpdate([callback](CallbackBase::Error error) {
+            if (error != CallbackBase::Error::None) {
+                callback->invalidate(error);
+                return;
+            }
+
+            callback->performCallback();
+        });
+    };
+
+    uint64_t callbackID = m_callbacks.put(didForceRepaintCallback, m_process->throttler().backgroundActivityToken());
     m_drawingArea->waitForBackingStoreUpdateOnNextPaint();
     m_process->send(Messages::WebPage::ForceRepaint(callbackID), m_pageID); 
 }

LayoutTests/platform/ios-simulator-wk2/TestExpectations
@@ -1899,5 +1899,3 @@ fast/scrolling/ios/scroll-events-back-forward-after-pageshow.html [ Pass Failure
 webkit.org/b/157589 fast/text-autosizing/ios/text-autosizing-after-back.html [ Pass Timeout ]
 
 webkit.org/b/157990 [ Release ] mathml/wbr-in-mroot-crash.html [ Pass Timeout ]
-
-webkit.org/b/159077 fast/harness/snapshot-captures-compositing.html [ ImageOnlyFailure ]

Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -895,12 +895,7 @@ void InjectedBundlePage::dump()
         injectedBundle.dumpBackForwardListsForAllPages(stringBuilder);
 
     if (injectedBundle.shouldDumpPixels() && injectedBundle.testRunner()->shouldDumpPixels()) {
-#if PLATFORM(IOS)
-        // PlatformWebView::windowSnapshotImage() has timing problems, so use WebProcess snapshots for now.
-        bool shouldCreateSnapshot = true;
-#else
         bool shouldCreateSnapshot = injectedBundle.testRunner()->isPrinting();
-#endif
         if (shouldCreateSnapshot) {
             WKSnapshotOptions options = kWKSnapshotOptionsShareable;
             WKRect snapshotRect = WKBundleFrameGetVisibleContentBounds(WKBundlePageGetMainFrame(m_page));

Tools/WebKitTestRunner/TestInvocation.cpp
@@ -248,8 +248,8 @@ void TestInvocation::dumpResults()
                 m_webProcessIsUnresponsive = true;
                 return;
             }
+
             WKRetainPtr<WKImageRef> windowSnapshot = TestController::singleton().mainWebView()->windowSnapshotImage();
-            ASSERT(windowSnapshot);
             dumpPixelsAndCompareWithExpected(windowSnapshot.get(), m_repaintRects.get(), TestInvocation::SnapshotResultType::WebView);
         }
     }

Tools/WebKitTestRunner/cg/TestInvocationCG.cpp
@@ -57,15 +57,19 @@ static CGContextRef createCGContextFromImage(WKImageRef wkImage)
     // Creating this bitmap in the device color space should prevent any color conversion when the image of the web view is drawn into it.
     RetainPtr<CGColorSpaceRef> colorSpace = adoptCF(CGColorSpaceCreateDeviceRGB());
     CGContextRef context = CGBitmapContextCreate(0, pixelsWide, pixelsHigh, 8, rowBytes, colorSpace.get(), kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host);
+    if (!context)
+        return nullptr;
 
-    
     CGContextDrawImage(context, CGRectMake(0, 0, pixelsWide, pixelsHigh), image.get());
-
     return context;
 }
 
 void computeMD5HashStringForContext(CGContextRef bitmapContext, char hashString[33])
 {
+    if (!bitmapContext) {
+        WTFLogAlways("computeMD5HashStringForContext: context is null\n");
+        return;
+    }
     ASSERT(CGBitmapContextGetBitsPerPixel(bitmapContext) == 32); // ImageDiff assumes 32 bit RGBA, we must as well.
     size_t pixelsHigh = CGBitmapContextGetHeight(bitmapContext);
     size_t pixelsWide = CGBitmapContextGetWidth(bitmapContext);
@@ -144,7 +148,16 @@ static void paintRepaintRectOverlay(CGContextRef context, WKImageRef image, WKAr
 
 void TestInvocation::dumpPixelsAndCompareWithExpected(WKImageRef image, WKArrayRef repaintRects, SnapshotResultType snapshotType)
 {
+    if (!image) {
+        WTFLogAlways("dumpPixelsAndCompareWithExpected: image is null\n");
+        return;
+    }
+
     RetainPtr<CGContextRef> context = adoptCF(createCGContextFromImage(image));
+    if (!context) {
+        WTFLogAlways("dumpPixelsAndCompareWithExpected: context is null\n");
+        return;
+    }
 
     // A non-null repaintRects array means we're doing a repaint test.
     if (repaintRects)

Tools/WebKitTestRunner/ios/PlatformWebViewIOS.mm
@@ -212,6 +212,10 @@ WKRetainPtr<WKImageRef> PlatformWebView::windowSnapshotImage()
     CGSize viewSize = m_view.bounds.size;
     int bufferWidth = ceil(viewSize.width * deviceScaleFactor);
     int bufferHeight = ceil(viewSize.height * deviceScaleFactor);
+    if (!bufferWidth || !bufferHeight) {
+        WTFLogAlways("Being asked for snapshot of view with width %d height %d\n", bufferWidth, bufferHeight);
+        return nullptr;
+    }
 
     CARenderServerBufferRef buffer = CARenderServerCreateBuffer(bufferWidth, bufferHeight);
     if (!buffer) {

commit 26475674bbcfa319c54ab5ed84e98129c3fe0cb0

Fix TestWebKitAPI crash introduced in r202532.
WebPageProxy::forceRepaint() could trigger a call to the didForceRepaintCallback()
after the WebPageProxy had been invalidated, causing a null de-ref of m_drawingArea.
Fix by checking for error and isValid().

* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::forceRepaint):

Identifier: 177291@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@202540 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -2889,6 +2889,16 @@ void WebPageProxy::forceRepaint(PassRefPtr<VoidCallback> prpCallback)
     }
 
     std::function<void (CallbackBase::Error)> didForceRepaintCallback = [this, callback](CallbackBase::Error error) {
+        if (error != CallbackBase::Error::None) {
+            callback->invalidate(error);
+            return;
+        }
+
+        if (!isValid()) {
+            callback->invalidate(CallbackBase::Error::OwnerWasInvalidated);
+            return;
+        }
+    
         callAfterNextPresentationUpdate([callback](CallbackBase::Error error) {
             if (error != CallbackBase::Error::None) {
                 callback->invalidate(error);