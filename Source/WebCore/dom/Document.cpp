night-codes/WebKit

commit 8de8f05af9f8d9530b33dac1ff7c380f65fe91f6

com.apple.WebKit.WebContent.Development crashed in com.apple.WebCore: WebCore::FrameView::didDestroyRenderTree https://bugs.webkit.org/show_bug.cgi?id=167665 <rdar://problem/30241193>
Reviewed by Andreas Kling.

When destroying a PageCache entry, we would call Document::prepareFromDestruction()
which would destroy the render tree and then call FrameView::didDestroyRenderTree().
We would sometimes hit the ASSERT(m_widgetsInRenderTree.isEmpty()) assertion in
didDestroyRenderTree(). This indicates that there are still RenderWidgets associated
with the FrameView even though we claim we already destroyed the render tree.

The reason for this is that, in the PageCache context, the top document's frame is
reused on navigation and may not be associated with a new document that is currently
displayed, has a render tree (and possible RenderWidgets). The solution proposed is
to stop calling FrameView::didDestroyRenderTree() in Document::prepareFromDestruction()
if the frame's document is not |this|. This happens when the document is destroyed
while in PageCache. We do not want to mess with the frame / frameview in this case
since they are no longer associated to the present document anymore.

No new tests, already covered by existing tests which currently crash flakily.

* dom/Document.cpp:
(WebCore::Document::destroyRenderTree):


Identifier: 184694@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@211455 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -2213,7 +2213,8 @@ void Document::destroyRenderTree()
     ASSERT(frame()->view());
     ASSERT(page());
 
-    FrameView& frameView = *frame()->view();
+    FrameView* frameView = frame()->document() == this ? frame()->view() : nullptr;
+    ASSERT(frameView || pageCacheState() == InPageCache);
 
     // Prevent Widget tree changes from committing until the RenderView is dead and gone.
     WidgetHierarchyUpdatesSuspensionScope suspendWidgetHierarchyUpdates;
@@ -2225,7 +2226,8 @@ void Document::destroyRenderTree()
 
     documentWillBecomeInactive();
 
-    frameView.willDestroyRenderTree();
+    if (frameView)
+        frameView->willDestroyRenderTree();
 
 #if ENABLE(FULLSCREEN_API)
     if (m_fullScreenRenderer)
@@ -2252,7 +2254,8 @@ void Document::destroyRenderTree()
     m_textAutoSizedNodes.clear();
 #endif
 
-    frameView.didDestroyRenderTree();
+    if (frameView)
+        frameView->didDestroyRenderTree();
 }
 
 void Document::prepareForDestruction()

commit 20d27aa73666dae0240d6c517b0136cd128a07cb

Regression(r211455): ASSERTION FAILED: frameView || pageCacheState() == InPageCache in com.apple.WebCore: WebCore::Document::destroyRenderTree https://bugs.webkit.org/show_bug.cgi?id=168237 <rdar://problem/30494165>
Reviewed by Gavin Barraclough.

Drop bad assertion under document::destroyRenderTree() that was introduced in r211455.
The assertion seemed like a good idea but the issue is that CachedFrame::destroy()
reset's the document's pageCacheState before calling Document::prepareForDestruction().

No new tests, this fixes assertion hits on our bots.

* dom/Document.cpp:
(WebCore::Document::destroyRenderTree):

Identifier: 185274@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@212238 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -2206,7 +2206,6 @@ void Document::destroyRenderTree()
     ASSERT(page());
 
     FrameView* frameView = frame()->document() == this ? frame()->view() : nullptr;
-    ASSERT(frameView || pageCacheState() == InPageCache);
 
     // Prevent Widget tree changes from committing until the RenderView is dead and gone.
     WidgetHierarchyUpdatesSuspensionScope suspendWidgetHierarchyUpdates;