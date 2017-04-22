night-codes/WebKit

commit a4aeb23191e99fabbc04e1a172a3aceed3ca0890

[MediaCapture][iOS] AVAudioSession must be active and the correct category before IO AudioUnits start producing data. https://bugs.webkit.org/show_bug.cgi?id=171095
Reviewed by Youenn Fablet.

If an input audio unit is asked to start before the AVAudioSession is in a recording category and active,
CoreAudio will return an error stating that no input device is available.

The PlatformMediaSessionManager will automatically set the category of and activate the AVAudioSession when one
of its associated MediaStreams has a capturing RealtimeMediaSource and is currently producing data.  To solve
the chicken-or-egg problem of activating the AVAudioSession before the source produces data, move the state bit
of "producing data" directly into MediaStreams, and notify the PlatformMediaSessionManager that capturing is
occurring after flipping that bit, but before asking the constituent tracks to begin producing data.

In places (i.e. UserMediaRequest) where we previously told a stream's tracks to begin producing data, instead
allow the stream to handle that by telling it to produce data directly.

* Modules/mediastream/MediaStream.cpp:
(WebCore::MediaStream::startProducingData):
(WebCore::MediaStream::stopProducingData):
(WebCore::MediaStream::mediaState):
(WebCore::MediaStream::mediaType):
(WebCore::MediaStream::characteristics):
(WebCore::MediaStream::canProduceAudio):
* Modules/mediastream/MediaStream.h:
* Modules/mediastream/UserMediaRequest.cpp:
(WebCore::UserMediaRequest::allow):
* platform/mediastream/mac/CoreAudioCaptureSource.h:


Identifier: 188030@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215626 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/mediastream/MediaStream.cpp
@@ -294,11 +294,23 @@ void MediaStream::startProducingData()
         return;
     }
 
+    if (m_isProducingData)
+        return;
+    m_isProducingData = true;
+
+    m_mediaSession->canProduceAudioChanged();
+
     m_private->startProducingData();
 }
 
 void MediaStream::stopProducingData()
 {
+    if (!m_isProducingData)
+        return;
+    m_isProducingData = false;
+
+    m_mediaSession->canProduceAudioChanged();
+
     m_private->stopProducingData();
 }
 
@@ -324,7 +336,7 @@ MediaProducer::MediaStateFlags MediaStream::mediaState() const
     if (m_private->hasAudio()) {
         state |= HasAudioOrVideo;
         if (m_private->hasCaptureAudioSource()) {
-            if (m_private->isProducingData())
+            if (m_isProducingData)
                 state |= HasActiveAudioCaptureDevice;
             else if (m_private->muted())
                 state |= HasMutedAudioCaptureDevice;
@@ -334,7 +346,7 @@ MediaProducer::MediaStateFlags MediaStream::mediaState() const
     if (m_private->hasVideo()) {
         state |= HasAudioOrVideo;
         if (m_private->hasCaptureVideoSource()) {
-            if (m_private->isProducingData())
+            if (m_isProducingData)
                 state |= HasActiveVideoCaptureDevice;
             else if (m_private->muted())
                 state |= HasMutedVideoCaptureDevice;
@@ -433,7 +445,7 @@ PlatformMediaSession::MediaType MediaStream::mediaType() const
 {
     // We only need to override the type when capturing audio, HTMLMediaElement and/or WebAudio
     // will do the right thing when a stream is attached to a media element or an audio context.
-    if (m_private->hasAudio() && m_private->isProducingData() && m_private->hasCaptureAudioSource())
+    if (m_private->hasAudio() && m_isProducingData && m_private->hasCaptureAudioSource())
         return PlatformMediaSession::MediaStreamCapturingAudio;
 
     return PlatformMediaSession::None;
@@ -448,7 +460,7 @@ PlatformMediaSession::CharacteristicsFlags MediaStream::characteristics() const
 {
     PlatformMediaSession::CharacteristicsFlags state = PlatformMediaSession::HasNothing;
 
-    if (!m_private->isProducingData())
+    if (!m_isProducingData)
         return state;
 
     if (m_private->hasAudio())
@@ -483,7 +495,7 @@ String MediaStream::sourceApplicationIdentifier() const
 
 bool MediaStream::canProduceAudio() const
 {
-    return !muted() && active() && m_private->hasAudio() && m_private->isProducingData();
+    return !muted() && active() && m_private->hasAudio() && m_isProducingData;
 }
 
 } // namespace WebCore

Source/WebCore/Modules/mediastream/MediaStream.h
@@ -174,6 +174,7 @@ private:
 
     bool m_isActive { false };
     bool m_isMuted { true };
+    bool m_isProducingData { false };
     bool m_isWaitingUntilMediaCanStart { false };
 };
 

Source/WebCore/Modules/mediastream/UserMediaRequest.cpp
@@ -174,11 +174,7 @@ void UserMediaRequest::allow(const String& audioDeviceUID, const String& videoDe
             return;
         }
 
-        for (auto& track : stream->getAudioTracks())
-            track->source().startProducingData();
-
-        for (auto& track : stream->getVideoTracks())
-            track->source().startProducingData();
+        stream->startProducingData();
         
         m_promise.resolve(stream);
     };

Source/WebCore/platform/mediastream/mac/CoreAudioCaptureSource.h
@@ -71,6 +71,7 @@ private:
     CoreAudioCaptureSource(const String& deviceID);
     virtual ~CoreAudioCaptureSource();
 
+    bool isCaptureSource() const final { return true; }
     void startProducingData() final;
     void stopProducingData() final;
     bool isProducingData() const final { return m_ioUnitStarted; }

commit 45bff10a5ace8cc3648f42d5290553e0427d7df7

[MediaStream] Fix regression caused by r215626 https://bugs.webkit.org/show_bug.cgi?id=171168 <rdar://problem/31774787>
Reviewed by Antoine Quint.

No new tests, fixes fast/mediastream/MediaStream-page-muted.html

* Modules/mediastream/MediaStream.cpp:
(WebCore::MediaStream::mediaState): Test muted before m_isProducingData because if both are
true we want to report that the stream is muted.


Identifier: 188072@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@215668 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/Modules/mediastream/MediaStream.cpp
@@ -336,20 +336,20 @@ MediaProducer::MediaStateFlags MediaStream::mediaState() const
     if (m_private->hasAudio()) {
         state |= HasAudioOrVideo;
         if (m_private->hasCaptureAudioSource()) {
-            if (m_isProducingData)
-                state |= HasActiveAudioCaptureDevice;
-            else if (m_private->muted())
+            if (m_private->muted())
                 state |= HasMutedAudioCaptureDevice;
+            else if (m_isProducingData)
+                state |= HasActiveAudioCaptureDevice;
         }
     }
 
     if (m_private->hasVideo()) {
         state |= HasAudioOrVideo;
         if (m_private->hasCaptureVideoSource()) {
-            if (m_isProducingData)
-                state |= HasActiveVideoCaptureDevice;
-            else if (m_private->muted())
+            if (m_private->muted())
                 state |= HasMutedVideoCaptureDevice;
+            else if (m_isProducingData)
+                state |= HasActiveVideoCaptureDevice;
         }
     }
 

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -4198,9 +4198,6 @@ void WebPageProxy::setMediaVolume(float volume)
 
 void WebPageProxy::setMuted(WebCore::MediaProducer::MutedStateFlags state)
 {
-    if (m_mutedState == state)
-        return;
-
     m_mutedState = state;
 
     if (!isValid())