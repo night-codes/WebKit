night-codes/WebKit

commit 8cb155ea42362dac3aaecc82056898980cd7854b

Interacting with WKHTTPCookieStores before creating WKWebViews and WKProcessPools should affect cookies used https://bugs.webkit.org/show_bug.cgi?id=171987
Reviewed by Brady Eidson.

Source/WebCore:

Covered by new API tests.

* CMakeLists.txt:
* platform/Cookie.h:
(WebCore::Cookie::Cookie):
(WebCore::Cookie::isNull):
(WebCore::CookieHash::hash):
(WebCore::CookieHash::equal):
(WTF::HashTraits<WebCore::Cookie>::emptyValue):
(WTF::HashTraits<WebCore::Cookie>::constructDeletedValue):
(WTF::HashTraits<WebCore::Cookie>::isDeletedValue):
* platform/network/Cookie.cpp: Added.
(WebCore::Cookie::operator==):
(WebCore::Cookie::hash):
* platform/network/cocoa/CookieCocoa.mm:
(WebCore::Cookie::operator NSHTTPCookie *):
(WebCore::Cookie::operator==):
(WebCore::Cookie::hash):
* platform/network/cocoa/NetworkStorageSessionCocoa.mm:
(WebCore::NetworkStorageSession::setCookies):
Use NSHTTPCookie's hash and equality comparison to more closely match the NSHTTPCookie behavior.

Source/WebKit2:

When putting cookies in a WKHTTPCookieStore that hasn't been associated with a WKProcessPool yet, there is no
NetworkProcess into which to put the cookies.  In such a case, we should store these cookies in the WebsiteDataStore
until there is a NetworkProcess.

* NetworkProcess/NetworkConnectionToWebProcess.cpp:
(WebKit::NetworkConnectionToWebProcess::ensureLegacyPrivateBrowsingSession):
* NetworkProcess/NetworkProcess.cpp:
(WebKit::NetworkProcess::initializeNetworkProcess):
(WebKit::NetworkProcess::ensurePrivateBrowsingSession):
* NetworkProcess/NetworkProcess.h:
* NetworkProcess/NetworkProcess.messages.in:
* NetworkProcess/RemoteNetworkingContext.h:
* NetworkProcess/mac/RemoteNetworkingContext.mm:
(WebKit::RemoteNetworkingContext::ensurePrivateBrowsingSession):
(WebKit::RemoteNetworkingContext::ensureWebsiteDataStoreSession):
* Shared/WebsiteDataStoreParameters.cpp:
(WebKit::WebsiteDataStoreParameters::~WebsiteDataStoreParameters):
(WebKit::WebsiteDataStoreParameters::encode):

Source/WebKit2/UIProcess/WebProcessPool.cpp
@@ -578,7 +578,7 @@ void WebProcessPool::setAnyPageGroupMightHavePrivateBrowsingEnabled(bool private
 
     if (networkProcess()) {
         if (privateBrowsingEnabled)
-            networkProcess()->send(Messages::NetworkProcess::EnsurePrivateBrowsingSession(SessionID::legacyPrivateSessionID()), 0);
+            networkProcess()->send(Messages::NetworkProcess::EnsurePrivateBrowsingSession({SessionID::legacyPrivateSessionID(), { }, { }, { }}), 0);
         else
             networkProcess()->send(Messages::NetworkProcess::DestroySession(SessionID::legacyPrivateSessionID()), 0);
     }
@@ -945,7 +945,9 @@ void WebProcessPool::pageAddedToProcess(WebPageProxy& page)
 
     auto sessionID = page.sessionID();
     if (sessionID.isEphemeral()) {
-        sendToNetworkingProcess(Messages::NetworkProcess::EnsurePrivateBrowsingSession(sessionID));
+        // FIXME: Merge NetworkProcess::EnsurePrivateBrowsingSession and NetworkProcess::AddWebsiteDataStore into one message type.
+        // They do basically the same thing.
+        sendToNetworkingProcess(Messages::NetworkProcess::EnsurePrivateBrowsingSession(page.websiteDataStore().parameters()));
         page.process().send(Messages::WebProcess::EnsurePrivateBrowsingSession(sessionID), 0);
     } else if (sessionID != SessionID::defaultSessionID()) {
         sendToNetworkingProcess(Messages::NetworkProcess::AddWebsiteDataStore(page.websiteDataStore().parameters()));

Source/WebKit2/UIProcess/WebsiteData/WebsiteDataStore.cpp
@@ -1265,6 +1265,23 @@ DatabaseProcessCreationParameters WebsiteDataStore::databaseProcessParameters()
     return parameters;
 }
 
+Vector<WebCore::Cookie> WebsiteDataStore::pendingCookies() const
+{
+    Vector<WebCore::Cookie> cookies;
+    copyToVector(m_pendingCookies, cookies);
+    return cookies;
+}
+
+void WebsiteDataStore::addPendingCookie(const WebCore::Cookie& cookie)
+{
+    m_pendingCookies.add(cookie);
+}
+
+void WebsiteDataStore::removePendingCookie(const WebCore::Cookie& cookie)
+{
+    m_pendingCookies.remove(cookie);
+}
+
 #if !PLATFORM(COCOA)
 WebsiteDataStoreParameters WebsiteDataStore::parameters()
 {

Source/WebCore/CMakeLists.txt
@@ -2411,6 +2411,7 @@ set(WebCore_SOURCES
     platform/network/BlobRegistryImpl.cpp
     platform/network/BlobResourceHandle.cpp
     platform/network/CacheValidation.cpp
+    platform/network/Cookie.cpp
     platform/network/CredentialBase.cpp
     platform/network/CredentialStorage.cpp
     platform/network/DNSResolveQueue.cpp

Source/WebCore/platform/Cookie.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 Joseph Pecoraro. All rights reserved.
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,6 +38,10 @@ namespace WebCore {
 
 struct Cookie {
     Cookie() { }
+    
+    Cookie(WTF::HashTableDeletedValueType)
+        : name(WTF::HashTableDeletedValue)
+    { }
 
     Cookie(const String& name, const String& value, const String& domain, const String& path, double expires, bool httpOnly, bool secure, bool session, const String& comment, const URL& commentURL, const Vector<uint16_t> ports)
         : name(name)
@@ -56,36 +61,53 @@ struct Cookie {
     template<class Encoder> void encode(Encoder&) const;
     template<class Decoder> static bool decode(Decoder&, Cookie&);
 
+    WEBCORE_EXPORT bool operator==(const Cookie&) const;
+    WEBCORE_EXPORT unsigned hash() const;
+
 #ifdef __OBJC__
     WEBCORE_EXPORT Cookie(NSHTTPCookie *);
     WEBCORE_EXPORT operator NSHTTPCookie *() const;
 #endif
 
+    bool isNull() const
+    {
+        return name.isNull()
+        && value.isNull()
+        && domain.isNull()
+        && path.isNull()
+        && expires == 0
+        && !httpOnly
+        && !secure
+        && !session
+        && comment.isNull()
+        && commentURL.isNull();
+    }
+    
     String name;
     String value;
     String domain;
     String path;
     // Expiration date, expressed as milliseconds since the UNIX epoch.
-    double expires;
-    bool httpOnly;
-    bool secure;
-    bool session;
+    double expires { 0 };
+    bool httpOnly { false };
+    bool secure { false };
+    bool session { false };
     String comment;
     URL commentURL;
     Vector<uint16_t> ports;
-
 };
 
 struct CookieHash {
     static unsigned hash(const Cookie& key)
-    { 
-        return StringHash::hash(key.name) + StringHash::hash(key.domain) + StringHash::hash(key.path) + key.secure;
+    {
+        return key.hash();
     }
 
     static bool equal(const Cookie& a, const Cookie& b)
     {
-        return a.name == b.name && a.domain == b.domain && a.path == b.path && a.secure == b.secure;
+        return a == b;
     }
+    static const bool safeToCompareToEmptyOrDeleted = false;
 };
 
 template<class Encoder>
@@ -129,5 +151,10 @@ namespace WTF {
     template<typename T> struct DefaultHash;
     template<> struct DefaultHash<WebCore::Cookie> {
         typedef WebCore::CookieHash Hash;
+    };
+    template<> struct HashTraits<WebCore::Cookie> : GenericHashTraits<WebCore::Cookie> {
+        static WebCore::Cookie emptyValue() { return { }; }
+        static void constructDeletedValue(WebCore::Cookie& slot) { slot = WebCore::Cookie(WTF::HashTableDeletedValue); }
+        static bool isDeletedValue(const WebCore::Cookie& slot) { return slot.name.isHashTableDeletedValue(); }
     };
 }

Source/WebCore/platform/network/Cookie.cpp
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "Cookie.h"
+
+namespace WebCore {
+    
+#if !PLATFORM(COCOA)
+bool Cookie::operator==(const Cookie& other) const
+{
+    return name == other.name
+        && domain == other.domain
+        && path == other.path
+        && secure == other.secure;
+}
+    
+unsigned Cookie::hash() const
+{
+    return StringHash::hash(name) + StringHash::hash(domain) + StringHash::hash(path) + secure;
+}
+#endif
+    
+} // namespace WebCore
+

Source/WebCore/platform/network/cocoa/CookieCocoa.mm
@@ -64,6 +64,9 @@ Cookie::Cookie(NSHTTPCookie *cookie)
 
 Cookie::operator NSHTTPCookie *() const
 {
+    if (isNull())
+        return nil;
+
     // FIXME: existing APIs do not provide a way to set httpOnly without parsing headers from scratch.
 
     NSMutableDictionary *properties = [NSMutableDictionary dictionaryWithCapacity:11];
@@ -104,6 +107,26 @@ Cookie::operator NSHTTPCookie *() const
     [properties setObject:@"1" forKey:NSHTTPCookieVersion];
 
     return [NSHTTPCookie cookieWithProperties:properties];
+}
+    
+bool Cookie::operator==(const Cookie& other) const
+{
+    ASSERT(!name.isHashTableDeletedValue());
+    bool thisNull = isNull();
+    bool otherNull = other.isNull();
+    if (thisNull || otherNull)
+        return thisNull == otherNull;
+    
+    NSHTTPCookie *nsCookie(*this);
+    return [nsCookie isEqual:other];
+}
+    
+unsigned Cookie::hash() const
+{
+    ASSERT(!name.isHashTableDeletedValue());
+    ASSERT(!isNull());
+    NSHTTPCookie *nsCookie(*this);
+    return nsCookie.hash;
 }
 
 } // namespace WebCore

Source/WebCore/platform/network/cocoa/NetworkStorageSessionCocoa.mm
@@ -43,7 +43,7 @@ void NetworkStorageSession::setCookie(const Cookie& cookie)
 void NetworkStorageSession::setCookies(const Vector<Cookie>& cookies, const URL& url, const URL& mainDocumentURL)
 {
     RetainPtr<NSMutableArray> nsCookies = adoptNS([[NSMutableArray alloc] initWithCapacity:cookies.size()]);
-    for (auto cookie : cookies)
+    for (const auto& cookie : cookies)
         [nsCookies addObject:(NSHTTPCookie *)cookie];
 
     BEGIN_BLOCK_OBJC_EXCEPTIONS;

Source/WebKit2/NetworkProcess/NetworkConnectionToWebProcess.cpp
@@ -45,6 +45,7 @@
 #include "RemoteNetworkingContext.h"
 #include "SessionTracker.h"
 #include "WebCoreArgumentCoders.h"
+#include "WebsiteDataStoreParameters.h"
 #include <WebCore/NetworkStorageSession.h>
 #include <WebCore/PingHandle.h>
 #include <WebCore/PlatformCookieJar.h>
@@ -399,7 +400,7 @@ void NetworkConnectionToWebProcess::setCaptureExtraNetworkLoadMetricsEnabled(boo
 
 void NetworkConnectionToWebProcess::ensureLegacyPrivateBrowsingSession()
 {
-    NetworkProcess::singleton().ensurePrivateBrowsingSession(SessionID::legacyPrivateSessionID());
+    NetworkProcess::singleton().ensurePrivateBrowsingSession({SessionID::legacyPrivateSessionID(), { }, { }, { }});
 }
 
 } // namespace WebKit

commit 0d05efc7011db709b58004915475b7413fad7c32

REGRESSION(r216977): [GTK] Ephemeral sessions broken after r216977 https://bugs.webkit.org/show_bug.cgi?id=172345
Reviewed by Michael Catanzaro.

Since r216977, WebProcessPool sends the page session ID to the web process, but the WebsiteDataStoreParameters
session ID to the network process. WebsiteDataStoreParameters is only implemented for Cocoa, so we are sending 0
session ID to the network process, while using the right session ID everywhere else.

* UIProcess/WebProcessPool.cpp:
(WebKit::WebProcessPool::pageAddedToProcess): Add an ASSERT to ensure we are sending the same session ID to Web
and Network processes.
* UIProcess/WebsiteData/WebsiteDataStore.cpp:
(WebKit::WebsiteDataStore::parameters): Add minimal implementation for non-Cocoa ports to at least set the
session ID.

Identifier: 189263@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@217128 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/WebProcessPool.cpp
@@ -947,6 +947,7 @@ void WebProcessPool::pageAddedToProcess(WebPageProxy& page)
     if (sessionID.isEphemeral()) {
         // FIXME: Merge NetworkProcess::EnsurePrivateBrowsingSession and NetworkProcess::AddWebsiteDataStore into one message type.
         // They do basically the same thing.
+        ASSERT(page.websiteDataStore().parameters().sessionID == sessionID);
         sendToNetworkingProcess(Messages::NetworkProcess::EnsurePrivateBrowsingSession(page.websiteDataStore().parameters()));
         page.process().send(Messages::WebProcess::EnsurePrivateBrowsingSession(sessionID), 0);
     } else if (sessionID != SessionID::defaultSessionID()) {

Source/WebKit2/UIProcess/WebsiteData/WebsiteDataStore.cpp
@@ -1299,9 +1299,10 @@ void WebsiteDataStore::removePendingCookie(const WebCore::Cookie& cookie)
 #if !PLATFORM(COCOA)
 WebsiteDataStoreParameters WebsiteDataStore::parameters()
 {
-    // FIXME: Implement.
-
-    return { };
+    // FIXME: Implement cookies.
+    WebsiteDataStoreParameters parameters;
+    parameters.sessionID = m_sessionID;
+    return parameters;
 }
 #endif
 