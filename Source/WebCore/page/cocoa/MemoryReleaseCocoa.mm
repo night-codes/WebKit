night-codes/WebKit

commit 4a1dba8b2e5f015e4bf1eaddd1b330b81a75d668

Call _sqlite3_purgeEligiblePagerCacheMemory() on memory pressure only if sqlite is initialized https://bugs.webkit.org/show_bug.cgi?id=173264
Reviewed by Ryosuke Niwa.

Call _sqlite3_purgeEligiblePagerCacheMemory() on memory pressure only if sqlite is initialized.
Otherwise, not only is it no needed, it is also potentially unsafe.

* page/MemoryRelease.cpp:
(WebCore::registerSQLiteMemoryPressureHandler):
* page/MemoryRelease.h:
* page/cocoa/MemoryReleaseCocoa.mm:
(WebCore::platformReleaseMemory):
(WebCore::registerSQLiteMemoryPressureHandler):
* platform/sql/SQLiteDatabase.cpp:
(WebCore::initializeSQLiteIfNecessary):

Identifier: 190118@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218108 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/cocoa/MemoryReleaseCocoa.mm
@@ -40,6 +40,8 @@
 
 extern "C" void _sqlite3_purgeEligiblePagerCacheMemory(void);
 
+static bool isSQLiteMemoryPressureHandlerRegisted { false };
+
 namespace WebCore {
 
 void platformReleaseMemory(Critical)
@@ -48,7 +50,8 @@ void platformReleaseMemory(Critical)
     GSFontPurgeFontCache();
 #endif
 
-    _sqlite3_purgeEligiblePagerCacheMemory();
+    if (isSQLiteMemoryPressureHandlerRegisted)
+        _sqlite3_purgeEligiblePagerCacheMemory();
 
     for (auto& pool : LayerPool::allLayerPools())
         pool->drain();
@@ -98,6 +101,11 @@ void registerMemoryReleaseNotifyCallbacks()
             GCController::singleton().garbageCollectNow();
         });
     });
+}
+
+void registerSQLiteMemoryPressureHandler()
+{
+    isSQLiteMemoryPressureHandlerRegisted = true;
 }
 
 } // namespace WebCore

Source/WebCore/page/MemoryRelease.cpp
@@ -190,6 +190,7 @@ void logMemoryStatisticsAtTimeOfDeath()
 void platformReleaseMemory(Critical) { }
 void jettisonExpensiveObjectsOnTopLevelNavigation() { }
 void registerMemoryReleaseNotifyCallbacks() { }
+void registerSQLiteMemoryPressureHandler() { }
 #endif
 
 } // namespace WebCore

Source/WebCore/page/MemoryRelease.h
@@ -34,5 +34,6 @@ void platformReleaseMemory(Critical);
 void jettisonExpensiveObjectsOnTopLevelNavigation();
 WEBCORE_EXPORT void registerMemoryReleaseNotifyCallbacks();
 WEBCORE_EXPORT void logMemoryStatisticsAtTimeOfDeath();
+void registerSQLiteMemoryPressureHandler();
 
 } // namespace WebCore

Source/WebCore/platform/sql/SQLiteDatabase.cpp
@@ -29,6 +29,7 @@
 
 #include "DatabaseAuthorizer.h"
 #include "Logging.h"
+#include "MemoryRelease.h"
 #include "SQLiteFileSystem.h"
 #include "SQLiteStatement.h"
 #include <mutex>
@@ -66,6 +67,7 @@ static void initializeSQLiteIfNecessary()
 #endif
             CRASH();
         }
+        registerSQLiteMemoryPressureHandler();
     });
 }
 

commit 089e8263afdbc38d9381accd4d414492ceddbc34

Unreviewed, fix typo in variable introduced in r218108.
* page/cocoa/MemoryReleaseCocoa.mm:
(WebCore::platformReleaseMemory):
(WebCore::registerSQLiteMemoryPressureHandler):

Identifier: 190172@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@218178 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/cocoa/MemoryReleaseCocoa.mm
@@ -40,7 +40,7 @@
 
 extern "C" void _sqlite3_purgeEligiblePagerCacheMemory(void);
 
-static bool isSQLiteMemoryPressureHandlerRegisted { false };
+static bool isSQLiteMemoryPressureHandlerRegistered { false };
 
 namespace WebCore {
 
@@ -50,7 +50,7 @@ void platformReleaseMemory(Critical)
     GSFontPurgeFontCache();
 #endif
 
-    if (isSQLiteMemoryPressureHandlerRegisted)
+    if (isSQLiteMemoryPressureHandlerRegistered)
         _sqlite3_purgeEligiblePagerCacheMemory();
 
     for (auto& pool : LayerPool::allLayerPools())
@@ -105,7 +105,7 @@ void registerMemoryReleaseNotifyCallbacks()
 
 void registerSQLiteMemoryPressureHandler()
 {
-    isSQLiteMemoryPressureHandlerRegisted = true;
+    isSQLiteMemoryPressureHandlerRegistered = true;
 }
 
 } // namespace WebCore