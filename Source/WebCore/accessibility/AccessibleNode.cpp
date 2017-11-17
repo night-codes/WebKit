night-codes/WebKit

commit f405c550878f34555ff969e451a6e5df45f33a00

AX: AOM: Implement string type properties https://bugs.webkit.org/show_bug.cgi?id=179495
Reviewed by Chris Fleizach.

Source/WebCore:

Implemented the rest of the string type properties for Accessibility
Object Model.

Test: accessibility/mac/AOM-string-properties.html

* accessibility/AXObjectCache.cpp:
(WebCore::AXObjectCache::handleLiveRegionCreated):
* accessibility/AccessibilityNodeObject.cpp:
(WebCore::AccessibilityNodeObject::childrenChanged):
(WebCore::AccessibilityNodeObject::isPressed const):
(WebCore::AccessibilityNodeObject::isChecked const):
(WebCore::AccessibilityNodeObject::valueDescription const):
* accessibility/AccessibilityObject.cpp:
(WebCore::AccessibilityObject::isAccessibilityObjectSearchMatchAtIndex):
(WebCore::AccessibilityObject::supportsAutoComplete const):
(WebCore::AccessibilityObject::autoCompleteValue const):
(WebCore::AccessibilityObject::invalidStatus const):
(WebCore::AccessibilityObject::supportsCurrent const):
(WebCore::AccessibilityObject::currentState const):
(WebCore::AccessibilityObject::currentValue const):
(WebCore::AccessibilityObject::roleDescription const):
(WebCore::AccessibilityObject::keyShortcutsValue const):
(WebCore::AccessibilityObject::placeholderValue const):
(WebCore::AccessibilityObject::isInsideLiveRegion const):
(WebCore::AccessibilityObject::liveRegionAncestor const):
(WebCore::AccessibilityObject::supportsARIAAttributes const):
(WebCore::AccessibilityObject::supportsLiveRegion const):
(WebCore::AccessibilityObject::sortDirection const):
(WebCore::AccessibilityObject::supportsHasPopup const):
(WebCore::AccessibilityObject::hasPopupValue const):
(WebCore::AccessibilityObject::supportsPressed const):
(WebCore::AccessibilityObject::checkboxOrRadioValue const):
(WebCore::AccessibilityObject::pressedIsPresent const):
(WebCore::AccessibilityObject::buttonRoleType const):
(WebCore::AccessibilityObject::supportsARIAAutoComplete const): Deleted.
(WebCore::AccessibilityObject::ariaAutoCompleteValue const): Deleted.

Source/WebCore/accessibility/AccessibleNode.cpp
@@ -45,8 +45,22 @@ static ARIAAttributeMap& ariaAttributeMap()
             AXPropertyName name;
             QualifiedName ariaAttribute;
         } attributes[] = {
+            { AXPropertyName::Autocomplete, aria_autocompleteAttr },
+            { AXPropertyName::Checked, aria_checkedAttr },
+            { AXPropertyName::Current, aria_currentAttr },
+            { AXPropertyName::HasPopUp, aria_haspopupAttr },
+            { AXPropertyName::Invalid, aria_invalidAttr },
+            { AXPropertyName::KeyShortcuts, aria_keyshortcutsAttr },
             { AXPropertyName::Label, aria_labelAttr },
-            { AXPropertyName::Role, roleAttr }
+            { AXPropertyName::Live, aria_liveAttr },
+            { AXPropertyName::Orientation, aria_orientationAttr },
+            { AXPropertyName::Placeholder, aria_placeholderAttr },
+            { AXPropertyName::Pressed, aria_pressedAttr },
+            { AXPropertyName::Relevant, aria_relevantAttr },
+            { AXPropertyName::Role, roleAttr },
+            { AXPropertyName::RoleDescription, aria_roledescriptionAttr },
+            { AXPropertyName::Sort, aria_sortAttr },
+            { AXPropertyName::ValueText, aria_valuetextAttr }
         };
         ARIAAttributeMap map;
         for (auto& attribute : attributes)
@@ -59,8 +73,21 @@ static ARIAAttributeMap& ariaAttributeMap()
 static bool isPropertyValueString(AXPropertyName propertyName)
 {
     switch (propertyName) {
+    case AXPropertyName::Autocomplete:
+    case AXPropertyName::Checked:
+    case AXPropertyName::Current:
+    case AXPropertyName::HasPopUp:
+    case AXPropertyName::Invalid:
     case AXPropertyName::Label:
+    case AXPropertyName::Live:
+    case AXPropertyName::Orientation:
+    case AXPropertyName::Placeholder:
+    case AXPropertyName::Pressed:
+    case AXPropertyName::Relevant:
     case AXPropertyName::Role:
+    case AXPropertyName::RoleDescription:
+    case AXPropertyName::Sort:
+    case AXPropertyName::ValueText:
         return true;
     default:
         return false;
@@ -119,16 +146,87 @@ void AccessibleNode::setStringProperty(const String& value, AXPropertyName prope
     m_propertyMap.set(propertyName, value);
 }
 
-String AccessibleNode::role() const
+void AccessibleNode::notifyAttributeChanged(const WebCore::QualifiedName& name)
 {
-    return stringValueForProperty(m_ownerElement, AXPropertyName::Role);
+    if (AXObjectCache* cache = m_ownerElement.document().axObjectCache())
+        cache->handleAttributeChanged(name, &m_ownerElement);
 }
 
-void AccessibleNode::setRole(const String& role)
+String AccessibleNode::autocomplete() const
 {
-    setStringProperty(role, AXPropertyName::Role);
-    if (AXObjectCache* cache = m_ownerElement.document().axObjectCache())
-        cache->handleAttributeChanged(roleAttr, &m_ownerElement);
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Autocomplete);
+}
+
+void AccessibleNode::setAutocomplete(const String& autocomplete)
+{
+    setStringProperty(autocomplete, AXPropertyName::Autocomplete);
+    notifyAttributeChanged(aria_autocompleteAttr);
+}
+
+String AccessibleNode::checked() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Checked);
+}
+
+void AccessibleNode::setChecked(const String& checked)
+{
+    setStringProperty(checked, AXPropertyName::Checked);
+    notifyAttributeChanged(aria_checkedAttr);
+}
+
+String AccessibleNode::current() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Current);
+}
+
+void AccessibleNode::setCurrent(const String& current)
+{
+    setStringProperty(current, AXPropertyName::Current);
+    notifyAttributeChanged(aria_currentAttr);
+}
+
+String AccessibleNode::hasPopUp() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::HasPopUp);
+}
+
+void AccessibleNode::setHasPopUp(const String& hasPopUp)
+{
+    setStringProperty(hasPopUp, AXPropertyName::HasPopUp);
+    notifyAttributeChanged(aria_haspopupAttr);
+}
+
+String AccessibleNode::invalid() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Invalid);
+}
+
+void AccessibleNode::setInvalid(const String& invalid)
+{
+    setStringProperty(invalid, AXPropertyName::Invalid);
+    notifyAttributeChanged(aria_invalidAttr);
+}
+
+String AccessibleNode::keyShortcuts() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::KeyShortcuts);
+}
+
+void AccessibleNode::setKeyShortcuts(const String& keyShortcuts)
+{
+    setStringProperty(keyShortcuts, AXPropertyName::KeyShortcuts);
+    notifyAttributeChanged(aria_keyshortcutsAttr);
+}
+
+String AccessibleNode::live() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Live);
+}
+
+void AccessibleNode::setLive(const String& live)
+{
+    setStringProperty(live, AXPropertyName::Live);
+    notifyAttributeChanged(aria_liveAttr);
 }
 
 String AccessibleNode::label() const
@@ -139,8 +237,95 @@ String AccessibleNode::label() const
 void AccessibleNode::setLabel(const String& label)
 {
     setStringProperty(label, AXPropertyName::Label);
-    if (AXObjectCache* cache = m_ownerElement.document().axObjectCache())
-        cache->handleAttributeChanged(aria_labelAttr, &m_ownerElement);
+    notifyAttributeChanged(aria_labelAttr);
+}
+
+String AccessibleNode::orientation() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Orientation);
+}
+
+void AccessibleNode::setOrientation(const String& orientation)
+{
+    setStringProperty(orientation, AXPropertyName::Orientation);
+    notifyAttributeChanged(aria_orientationAttr);
+}
+
+String AccessibleNode::placeholder() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Placeholder);
+}
+
+void AccessibleNode::setPlaceholder(const String& placeholder)
+{
+    setStringProperty(placeholder, AXPropertyName::Placeholder);
+    notifyAttributeChanged(aria_placeholderAttr);
+}
+
+String AccessibleNode::pressed() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Pressed);
+}
+
+void AccessibleNode::setPressed(const String& pressed)
+{
+    setStringProperty(pressed, AXPropertyName::Pressed);
+    notifyAttributeChanged(aria_pressedAttr);
+}
+
+String AccessibleNode::relevant() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Relevant);
+}
+
+void AccessibleNode::setRelevant(const String& relevant)
+{
+    setStringProperty(relevant, AXPropertyName::Relevant);
+    notifyAttributeChanged(aria_relevantAttr);
+}
+
+String AccessibleNode::role() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Role);
+}
+
+void AccessibleNode::setRole(const String& role)
+{
+    setStringProperty(role, AXPropertyName::Role);
+    notifyAttributeChanged(roleAttr);
+}
+
+String AccessibleNode::roleDescription() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::RoleDescription);
+}
+
+void AccessibleNode::setRoleDescription(const String& roleDescription)
+{
+    setStringProperty(roleDescription, AXPropertyName::RoleDescription);
+    notifyAttributeChanged(aria_roledescriptionAttr);
+}
+
+String AccessibleNode::sort() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::Sort);
+}
+
+void AccessibleNode::setSort(const String& sort)
+{
+    setStringProperty(sort, AXPropertyName::Sort);
+    notifyAttributeChanged(aria_sortAttr);
+}
+
+String AccessibleNode::valueText() const
+{
+    return stringValueForProperty(m_ownerElement, AXPropertyName::ValueText);
+}
+
+void AccessibleNode::setValueText(const String& valueText)
+{
+    setStringProperty(valueText, AXPropertyName::ValueText);
+    notifyAttributeChanged(aria_valuetextAttr);
 }
 
 } // namespace WebCore

LayoutTests/accessibility/accessibility-object-model.html
@@ -111,7 +111,6 @@
             var button2 = document.getElementById("button2");
             button2.parentElement.removeChild(button2);
         })();
-        gc();
         shouldBe("aomRemovedButton.role", "'checkbox'");
     }
     

LayoutTests/accessibility/mac/AOM-string-properties-expected.txt
@@ -1,0 +1,75 @@
+
+This tests string type Accessibility Object Model properties.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+
+Autocomplete support
+PASS node.accessibleNode.autocomplete is "inline"
+PASS axNode.stringAttributeValue('AXAutocompleteValue') is "inline"
+
+Checked support
+PASS axNode.isChecked is false
+PASS node.accessibleNode.checked is "true"
+PASS axNode.isChecked is true
+
+Current support
+PASS axNode.stringAttributeValue('AXARIACurrent') is "false"
+PASS current.accessibleNode.current is "page"
+PASS axNode.stringAttributeValue('AXARIACurrent') is "page"
+
+HasPopUp support
+PASS axNode.stringAttributeValue('AXHasPopUpValue') is "false"
+PASS node.accessibleNode.hasPopUp is "dialog"
+PASS axNode.stringAttributeValue('AXHasPopUpValue') is "dialog"
+
+Invalid support
+PASS axNode.stringAttributeValue('AXInvalid') is "false"
+PASS node.accessibleNode.invalid is "grammar"
+PASS axNode.stringAttributeValue('AXInvalid') is "grammar"
+
+KeyShortcuts support
+PASS node.accessibleNode.keyShortcuts is "Ctrl+F"
+PASS axNode.stringAttributeValue('AXKeyShortcutsValue') is "Ctrl+F"
+
+Live support
+PASS axNode.stringAttributeValue('AXARIALive') is ""
+PASS node.accessibleNode.live is "polite"
+PASS axNode.stringAttributeValue('AXARIALive') is "polite"
+
+Orientation support
+PASS axNode.orientation is "AXOrientation: AXHorizontalOrientation"
+PASS node.accessibleNode.orientation is "vertical"
+PASS axNode.orientation is "AXOrientation: AXVerticalOrientation"
+
+Placeholder support
+PASS axNode.stringAttributeValue('AXPlaceholderValue') is ""
+PASS node.accessibleNode.placeholder is "test placeholder"
+PASS axNode.stringAttributeValue('AXPlaceholderValue') is "test placeholder"
+
+Pressed support
+PASS axNode.boolAttributeValue('AXARIAPressedIsPresent') is false
+PASS node.accessibleNode.pressed is "true"
+PASS axNode.boolAttributeValue('AXARIAPressedIsPresent') is true
+
+Relevant support
+PASS axNode.stringAttributeValue('AXARIARelevant') is "additions text"
+PASS node.accessibleNode.relevant is "additions"
+PASS axNode.stringAttributeValue('AXARIARelevant') is "additions"
+
+RoleDescription support
+PASS axNode.roleDescription is "AXRoleDescription: button"
+PASS node.accessibleNode.roleDescription is "test button"
+PASS axNode.roleDescription is "AXRoleDescription: test button"
+
+Sort support
+PASS axNode.stringAttributeValue('AXSortDirection') is "AXUnknownSortDirection"
+PASS node.accessibleNode.sort is "ascending"
+PASS axNode.stringAttributeValue('AXSortDirection') is "AXAscendingSortDirection"
+
+ValueText support
+PASS axNode.valueDescription is "AXValueDescription: "
+PASS node.accessibleNode.valueText is "Five"
+PASS axNode.valueDescription is "AXValueDescription: Five"
+

LayoutTests/accessibility/mac/AOM-string-properties.html
@@ -1,0 +1,190 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="../../resources/js-test-pre.js"></script>
+<script src="../../resources/accessibility-helper.js"></script>
+</head>
+<body>
+
+<div role="combobox" id="autocomplete"></div>
+<div role="checkbox" id="checked"></div>
+<div role="tab" id="current"></div>
+<div role="button" id="hasPopUp"></div>
+<div role="textbox" id="invalid"></div>
+<div role="button" id="keyShortcuts"></div>
+<div role="banner" id="live"></div>
+<div role="slider" id="orientation"></div>
+<input id="placeholder">
+<div role="switch" id="pressed"></div>
+<div role="banner" id="relevant"></div>
+<div role="button" id="roleDescription"></div>
+<div role="columnheader" id="sort"></div>
+<div role="slider" id="valueText"></div>
+
+<p id="description"></p>
+<div id="console"></div>
+
+<script>
+    description("This tests string type Accessibility Object Model properties.");
+    if (window.accessibilityController) {
+        var node;
+        var axNode;
+
+        testAutocomplete();
+        testChecked();
+        testCurrent();
+        testHasPopUp();
+        testInvalid();
+        testKeyShortcuts();
+        testLive();
+        testOrientation();
+        testPlaceholder();
+        testPressed();
+        testRelevant();
+        testRoleDescription();
+        testSort();
+        testValueText();
+    }
+
+    function testAutocomplete() {
+        debug("\nAutocomplete support");
+        node = document.getElementById("autocomplete");
+        axNode = accessibilityController.accessibleElementById("autocomplete");
+        node.accessibleNode.autocomplete = "inline";
+        shouldBeEqualToString("node.accessibleNode.autocomplete", "inline");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXAutocompleteValue')", "inline");
+    }
+
+    function testChecked() {
+        debug("\nChecked support");
+        node = document.getElementById("checked");
+        axNode = accessibilityController.accessibleElementById("checked");
+        shouldBeFalse("axNode.isChecked");
+        node.accessibleNode.checked = "true";
+        shouldBeEqualToString("node.accessibleNode.checked", "true");
+        shouldBeTrue("axNode.isChecked");
+    }
+
+    function testCurrent() {
+        debug("\nCurrent support");
+        node = document.getElementById("current");
+        axNode = accessibilityController.accessibleElementById("current");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXARIACurrent')", "false");
+        node.accessibleNode.current = "page";
+        shouldBeEqualToString("current.accessibleNode.current", "page");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXARIACurrent')", "page");
+    }
+
+    function testHasPopUp() {
+        debug("\nHasPopUp support");
+        node = document.getElementById("hasPopUp");
+        axNode = accessibilityController.accessibleElementById("hasPopUp");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXHasPopUpValue')", "false");
+        node.accessibleNode.hasPopUp = "dialog";
+        shouldBeEqualToString("node.accessibleNode.hasPopUp", "dialog");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXHasPopUpValue')", "dialog");
+    }
+
+    function testInvalid() {
+        debug("\nInvalid support");
+        node = document.getElementById("invalid");
+        axNode = accessibilityController.accessibleElementById("invalid");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXInvalid')", "false");
+        node.accessibleNode.invalid = "grammar";
+        shouldBeEqualToString("node.accessibleNode.invalid", "grammar");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXInvalid')", "grammar");
+    }
+
+    function testKeyShortcuts() {
+        debug("\nKeyShortcuts support");
+        node = document.getElementById("keyShortcuts");
+        axNode = accessibilityController.accessibleElementById("keyShortcuts");
+        node.accessibleNode.keyShortcuts = "Ctrl+F";
+        shouldBeEqualToString("node.accessibleNode.keyShortcuts", "Ctrl+F");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXKeyShortcutsValue')", "Ctrl+F");
+    }
+
+    function testLive() {
+        debug("\nLive support");
+        node = document.getElementById("live");
+        axNode = accessibilityController.accessibleElementById("live");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXARIALive')", "");
+        node.accessibleNode.live = "polite";
+        shouldBeEqualToString("node.accessibleNode.live", "polite");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXARIALive')", "polite");
+    }
+
+    function testOrientation() {
+        debug("\nOrientation support");
+        node = document.getElementById("orientation");
+        axNode = accessibilityController.accessibleElementById("orientation");
+        shouldBeEqualToString("axNode.orientation", "AXOrientation: AXHorizontalOrientation");
+        node.accessibleNode.orientation = "vertical";
+        shouldBeEqualToString("node.accessibleNode.orientation", "vertical");
+        shouldBeEqualToString("axNode.orientation", "AXOrientation: AXVerticalOrientation");
+    }
+
+    function testPlaceholder() {
+        debug("\nPlaceholder support");
+        node = document.getElementById("placeholder");
+        axNode = accessibilityController.accessibleElementById("placeholder");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXPlaceholderValue')", "");
+        node.accessibleNode.placeholder = "test placeholder";
+        shouldBeEqualToString("node.accessibleNode.placeholder", "test placeholder");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXPlaceholderValue')", "test placeholder");
+    }
+
+    function testPressed() {
+        debug("\nPressed support");
+        node = document.getElementById("pressed");
+        axNode = accessibilityController.accessibleElementById("pressed");
+        shouldBeFalse("axNode.boolAttributeValue('AXARIAPressedIsPresent')");
+        node.accessibleNode.pressed = "true";
+        shouldBeEqualToString("node.accessibleNode.pressed", "true");
+        shouldBeTrue("axNode.boolAttributeValue('AXARIAPressedIsPresent')");
+    }
+
+    function testRelevant() {
+        debug("\nRelevant support");
+        node = document.getElementById("relevant");
+        axNode = accessibilityController.accessibleElementById("relevant");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXARIARelevant')", "additions text");
+        node.accessibleNode.relevant = "additions";
+        shouldBeEqualToString("node.accessibleNode.relevant", "additions");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXARIARelevant')", "additions");
+    }
+
+    function testRoleDescription() {
+        debug("\nRoleDescription support");
+        node = document.getElementById("roleDescription");
+        axNode = accessibilityController.accessibleElementById("roleDescription");
+        shouldBeEqualToString("axNode.roleDescription", "AXRoleDescription: button");
+        node.accessibleNode.roleDescription = "test button";
+        shouldBeEqualToString("node.accessibleNode.roleDescription", "test button");
+        shouldBeEqualToString("axNode.roleDescription", "AXRoleDescription: test button");
+    }
+
+    function testSort() {
+        debug("\nSort support");
+        node = document.getElementById("sort");
+        axNode = accessibilityController.accessibleElementById("sort");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXSortDirection')", "AXUnknownSortDirection");
+        node.accessibleNode.sort = "ascending";
+        shouldBeEqualToString("node.accessibleNode.sort", "ascending");
+        shouldBeEqualToString("axNode.stringAttributeValue('AXSortDirection')", "AXAscendingSortDirection");
+    }
+
+    function testValueText() {
+        debug("\nValueText support");
+        node = document.getElementById("valueText");
+        axNode = accessibilityController.accessibleElementById("valueText");
+        shouldBeEqualToString("axNode.valueDescription", "AXValueDescription: ");
+        node.accessibleNode.valueText = "Five";
+        shouldBeEqualToString("node.accessibleNode.valueText", "Five");
+        shouldBeEqualToString("axNode.valueDescription", "AXValueDescription: Five");
+    }
+
+</script>
+<script src="../resources/js-test-post.js"></script>
+</body>
+</html>

Source/WebCore/accessibility/AXObjectCache.cpp
@@ -847,7 +847,7 @@ void AXObjectCache::handleLiveRegionCreated(Node* node)
         return;
     
     Element* element = downcast<Element>(node);
-    String liveRegionStatus = element->attributeWithoutSynchronization(aria_liveAttr);
+    String liveRegionStatus = AccessibleNode::effectiveStringValueForElement(*element, AXPropertyName::Live);
     if (liveRegionStatus.isEmpty()) {
         const AtomicString& ariaRole = AccessibleNode::effectiveStringValueForElement(*element, AXPropertyName::Role);
         if (!ariaRole.isEmpty())

Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -145,7 +145,7 @@ void AccessibilityNodeObject::childrenChanged()
         // Sometimes this function can be called many times within a short period of time, leading to posting too many AXLiveRegionChanged
         // notifications. To fix this, we used a timer to make sure we only post one notification for the children changes within a pre-defined
         // time interval.
-        if (parent->supportsARIALiveRegion())
+        if (parent->supportsLiveRegion())
             cache->postLiveRegionChangeNotification(parent);
         
         // If this element is an ARIA text control, notify the AT of changes.
@@ -658,7 +658,7 @@ bool AccessibilityNodeObject::isPressed() const
 
     // If this is an toggle button, check the aria-pressed attribute rather than node()->active()
     if (isToggleButton())
-        return equalLettersIgnoringASCIICase(getAttribute(aria_pressedAttr), "true");
+        return equalLettersIgnoringASCIICase(stringValueForProperty(AXPropertyName::Pressed), "true");
 
     if (!is<Element>(*node))
         return false;
@@ -690,7 +690,7 @@ bool AccessibilityNodeObject::isChecked() const
         break;
     }
     
-    if (validRole && equalLettersIgnoringASCIICase(getAttribute(aria_checkedAttr), "true"))
+    if (validRole && equalLettersIgnoringASCIICase(stringValueForProperty(AXPropertyName::Checked), "true"))
         return true;
 
     return false;
@@ -802,7 +802,7 @@ String AccessibilityNodeObject::valueDescription() const
     if (!isRangeControl())
         return String();
 
-    return getAttribute(aria_valuetextAttr).string();
+    return stringValueForProperty(AXPropertyName::ValueText);
 }
 
 float AccessibilityNodeObject::valueForRange() const

commit 24ce20fa0119ef18932c8b57215195372646baac

AX: [GTK] Regression: accessibility/gtk/aria-keyshortcuts.html fails after r224953 https://bugs.webkit.org/show_bug.cgi?id=179824
Reviewed by Chris Fleizach.

The failure was due to AXPropertyName::KeyShortcuts not being included in
the group of properties whose value is a string. As a result, the new method
to get string values, AccessibleNode::effectiveStringValueForElement(), was
returning an empty String. This was not caught by the shared set of tests
because at the present time, only WebKitGtk supports aria-keyshortcuts.

No new tests needed. The regression was caught by an existing test.

* accessibility/AccessibleNode.cpp:
(WebCore::isPropertyValueString):


Identifier: 195842@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@224970 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/accessibility/AccessibleNode.cpp
@@ -78,6 +78,7 @@ static bool isPropertyValueString(AXPropertyName propertyName)
     case AXPropertyName::Current:
     case AXPropertyName::HasPopUp:
     case AXPropertyName::Invalid:
+    case AXPropertyName::KeyShortcuts:
     case AXPropertyName::Label:
     case AXPropertyName::Live:
     case AXPropertyName::Orientation: