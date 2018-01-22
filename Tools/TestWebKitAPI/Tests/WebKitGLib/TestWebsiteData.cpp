night-codes/WebKit

commit ee2d87e5ee1c1ba5cb6f3e26378a7c143d87caf8

Resource Load Statistics: Implement callback support for removal of WebsiteDataType::ResourceLoadStatistics https://bugs.webkit.org/show_bug.cgi?id=181822 https://bugs.webkit.org/show_bug.cgi?id=175263 https://bugs.webkit.org/show_bug.cgi?id=178536 https://bugs.webkit.org/show_bug.cgi?id=181223 https://bugs.webkit.org/show_bug.cgi?id=181482 <rdar://problem/33491222> <rdar://problem/33805293> <rdar://problem/36332683> <rdar://problem/36549026>
Reviewed by Alex Christensen.

Source/WebKit:

* UIProcess/API/C/WKWebsiteDataStoreRef.cpp:
(WKWebsiteDataStoreStatisticsClearInMemoryAndPersistentStore):
(WKWebsiteDataStoreStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours):
* UIProcess/API/Cocoa/WKWebsiteDataStore.mm:
(-[WKWebsiteDataStore _resourceLoadStatisticsClearInMemoryAndPersistentStore]):
    Calls the new API with no-op completion handler.
(-[WKWebsiteDataStore _resourceLoadStatisticsClearInMemoryAndPersistentStore:]):
    New API with completion handler.
(-[WKWebsiteDataStore _resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:]):
    Calls the new API with no-op completion handler.
(-[WKWebsiteDataStore _resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:completionHandler:]):
    New API with completion handler.
* UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h:
* UIProcess/ResourceLoadStatisticsPersistentStorage.cpp:
(WebKit::ResourceLoadStatisticsPersistentStorage::populateMemoryStoreFromDisk):
* UIProcess/WebResourceLoadStatisticsStore.cpp:
(WebKit::WebResourceLoadStatisticsStore::grandfatherExistingWebsiteData):
(WebKit::WebResourceLoadStatisticsStore::scheduleClearInMemoryAndPersistent):
* UIProcess/WebResourceLoadStatisticsStore.h:
* UIProcess/WebsiteData/WebsiteDataStore.cpp:
(WebKit::WebsiteDataStore::removeData):
(WebKit::WebsiteDataStore::clearResourceLoadStatisticsInWebProcesses):
* UIProcess/WebsiteData/WebsiteDataStore.h:

Tools:

* WebKitTestRunner/InjectedBundle/Bindings/TestRunner.idl:

LayoutTests/http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store-one-hour.html
@@ -30,11 +30,7 @@
         else
             testFailed("Host did not get classified as prevalent resource.");
 
-        testRunner.clearTestRunnerCallbacks();
-        testRunner.installStatisticsDidScanDataRecordsCallback(completeTest);
-        testRunner.installStatisticsDidModifyDataRecordsCallback(completeTest);
-
-        testRunner.statisticsClearInMemoryAndPersistentStoreModifiedSinceHours(1);
+        testRunner.statisticsClearInMemoryAndPersistentStoreModifiedSinceHours(1, completeTest);
     }
 
     function runTestRunnerTest() {
@@ -53,7 +49,6 @@
         testRunner.setStatisticsSubresourceUnderTopFrameOrigin(statisticsUrl, topFrameOrigin3);
 
         testRunner.installStatisticsDidScanDataRecordsCallback(testStep2);
-        testRunner.installStatisticsDidModifyDataRecordsCallback(testStep2);
 
         testRunner.statisticsProcessStatisticsAndDataRecords();
     }

LayoutTests/http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store.html
@@ -19,11 +19,7 @@
         else
             testFailed("Host did not get classified as prevalent resource.");
 
-        testRunner.clearTestRunnerCallbacks();
-        testRunner.installStatisticsDidModifyDataRecordsCallback(completeTest);
-        testRunner.installStatisticsDidScanDataRecordsCallback(completeTest);
-
-        testRunner.statisticsClearInMemoryAndPersistentStore();
+        testRunner.statisticsClearInMemoryAndPersistentStore(completeTest);
     }
 
     function completeTest() {
@@ -53,7 +49,6 @@
         testRunner.setStatisticsSubresourceUnderTopFrameOrigin(statisticsUrl, topFrameOrigin3);
 
         testRunner.installStatisticsDidScanDataRecordsCallback(testStep2);
-        testRunner.installStatisticsDidModifyDataRecordsCallback(testStep2);
 
         testRunner.statisticsProcessStatisticsAndDataRecords();
     }

LayoutTests/http/tests/resourceLoadStatistics/grandfathering.html
@@ -40,10 +40,9 @@
     }
 
     function clearInMemoryAndPersistentStoreAndContinue() {
-        testRunner.installStatisticsDidScanDataRecordsCallback(function() {
+        testRunner.statisticsClearInMemoryAndPersistentStore(function() {
             runTest();
         });
-        testRunner.statisticsClearInMemoryAndPersistentStore();
     }
 
     function runTest() {
@@ -99,6 +98,7 @@
                 break;
             default:
                 testFailed("Should not be reached.");
+                finishTest();
         }
     }
 

LayoutTests/platform/ios/TestExpectations
@@ -3014,6 +3014,9 @@ http/tests/resourceLoadStatistics/add-partitioning-to-redirect.html [ Pass ]
 http/tests/resourceLoadStatistics/add-blocking-to-redirect.html [ Pass ]
 http/tests/resourceLoadStatistics/remove-partitioning-in-redirect.html [ Pass ]
 http/tests/resourceLoadStatistics/remove-blocking-in-redirect.html [ Pass ]
+http/tests/resourceLoadStatistics/grandfathering.html [ Pass ]
+http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store.html [ Pass ]
+http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store-one-hour.html [ Pass ]
 
 webkit.org/b/175273 imported/w3c/web-platform-tests/html/browsers/windows/noreferrer-window-name.html [ Failure ]
 

LayoutTests/platform/mac-wk2/TestExpectations
@@ -808,6 +808,9 @@ webkit.org/b/176122 media/video-controls-drop-and-restore-timeline.html [ Pass F
 [ HighSierra+ ] http/tests/resourceLoadStatistics/add-blocking-to-redirect.html [ Pass ]
 [ HighSierra+ ] http/tests/resourceLoadStatistics/remove-partitioning-in-redirect.html [ Pass ]
 [ HighSierra+ ] http/tests/resourceLoadStatistics/remove-blocking-in-redirect.html [ Pass ]
+[ HighSierra+ ] http/tests/resourceLoadStatistics/grandfathering.html [ Pass ]
+[ HighSierra+ ] http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store.html [ Pass ]
+[ HighSierra+ ] http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store-one-hour.html [ Pass ]
 
 # <rdar://problem/33555759>
 webkit.org/b/177616 [ HighSierra+ ] http/tests/media/video-buffered-range-contains-currentTime.html [ Pass Timeout ]
@@ -824,8 +827,6 @@ webkit.org/b/177214 [ ElCapitan Debug ] imported/w3c/web-platform-tests/FileAPI/
 webkit.org/b/177687 http/tests/inspector/network/resource-sizes-memory-cache.html [ Pass Failure ]
 
 webkit.org/b/178472 http/tests/resourceLoadStatistics/prevalent-resource-handled-keydown.html [ Pass ]
-
-webkit.org/b/178536 http/tests/resourceLoadStatistics/clear-in-memory-and-persistent-store-one-hour.html [ Skip ]
 
 webkit.org/b/176881 plugins/js-from-destroy.html [ Pass Failure ]
 
@@ -865,8 +866,6 @@ webkit.org/b/181392 imported/w3c/web-platform-tests/service-workers/service-work
 webkit.org/b/172879 webrtc/video-unmute.html [ Skip ]
 
 webkit.org/b/181502 swipe/pushstate-with-manual-scrollrestoration.html [ Failure ]
-
-webkit.org/b/181482 http/tests/resourceLoadStatistics/grandfathering.html [ Skip ]
 
 webkit.org/b/181750 imported/w3c/web-platform-tests/service-workers/service-worker/fetch-request-redirect.https.html [ Pass Failure ]
 webkit.org/b/181750 imported/w3c/web-platform-tests/service-workers/service-worker/interfaces-sw.https.html [ Pass Failure ]

Source/WebKit/UIProcess/API/C/WKWebsiteDataStoreRef.cpp
@@ -328,7 +328,7 @@ void WKWebsiteDataStoreStatisticsClearInMemoryAndPersistentStore(WKWebsiteDataSt
     if (!store)
         return;
 
-    store->scheduleClearInMemoryAndPersistent(WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes);
+    store->scheduleClearInMemoryAndPersistent(WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes, []() { });
 }
 
 void WKWebsiteDataStoreStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours(WKWebsiteDataStoreRef dataStoreRef, unsigned hours)
@@ -337,7 +337,7 @@ void WKWebsiteDataStoreStatisticsClearInMemoryAndPersistentStoreModifiedSinceHou
     if (!store)
         return;
 
-    store->scheduleClearInMemoryAndPersistent(WallTime::now() - Seconds::fromHours(hours), WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes);
+    store->scheduleClearInMemoryAndPersistent(WallTime::now() - Seconds::fromHours(hours), WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes, []() { });
 }
 
 void WKWebsiteDataStoreStatisticsClearThroughWebsiteDataRemoval(WKWebsiteDataStoreRef dataStoreRef, void* context, WKWebsiteDataStoreStatisticsClearThroughWebsiteDataRemovalFunction callback)

Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStore.mm
@@ -549,20 +549,38 @@ static Vector<WebKit::WebsiteDataRecord> toWebsiteDataRecords(NSArray *dataRecor
 
 - (void)_resourceLoadStatisticsClearInMemoryAndPersistentStore
 {
-    auto* store = _websiteDataStore->websiteDataStore().resourceLoadStatistics();
-    if (!store)
-        return;
+    [self _resourceLoadStatisticsClearInMemoryAndPersistentStore:^() { }];
+}
 
-    store->scheduleClearInMemoryAndPersistent(WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes);
+- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStore:(void (^)())completionHandler
+{
+    auto* store = _websiteDataStore->websiteDataStore().resourceLoadStatistics();
+    if (!store) {
+        completionHandler();
+        return;
+    }
+
+    store->scheduleClearInMemoryAndPersistent(WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes, [completionHandler = makeBlockPtr(completionHandler)]() {
+        completionHandler();
+    });
 }
 
 - (void)_resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:(unsigned)hours
 {
-    auto* store = _websiteDataStore->websiteDataStore().resourceLoadStatistics();
-    if (!store)
-        return;
+    [self _resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:hours completionHandler:^() { }];
+}
 
-    store->scheduleClearInMemoryAndPersistent(WallTime::now() - Seconds::fromHours(hours), WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes);
+- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:(unsigned)hours completionHandler:(void (^)())completionHandler
+{
+    auto* store = _websiteDataStore->websiteDataStore().resourceLoadStatistics();
+    if (!store) {
+        completionHandler();
+        return;
+    }
+
+    store->scheduleClearInMemoryAndPersistent(WallTime::now() - Seconds::fromHours(hours), WebKit::WebResourceLoadStatisticsStore::ShouldGrandfather::Yes, [completionHandler = makeBlockPtr(completionHandler)]() {
+        completionHandler();
+    });
 }
 
 - (void)_resourceLoadStatisticsResetToConsistentState

Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h
@@ -79,8 +79,10 @@ typedef NS_OPTIONS(NSUInteger, _WKWebsiteDataStoreFetchOptions) {
 - (void)_resourceLoadStatisticsSetShouldClassifyResourcesBeforeDataRecordsRemoval:(BOOL)value WK_API_AVAILABLE(macosx(10.13), ios(11.0));
 - (void)_resourceLoadStatisticsSetNotifyPagesWhenTelemetryWasCaptured:(BOOL)value WK_API_AVAILABLE(macosx(10.13), ios(11.0));
 - (void)_resourceLoadStatisticsSetShouldSubmitTelemetry:(BOOL)value WK_API_AVAILABLE(macosx(10.13), ios(11.0));
-- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStore WK_API_AVAILABLE(macosx(10.13), ios(11.0));
-- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:(unsigned)hours WK_API_AVAILABLE(macosx(10.13), ios(11.0));
+- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStore WK_API_DEPRECATED_WITH_REPLACEMENT("_resourceLoadStatisticsClearInMemoryAndPersistentStore", macosx(10.13, WK_MAC_TBA), ios(11.0, WK_IOS_TBA));
+- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStore:(void (^)())completionHandler WK_API_AVAILABLE(macosx(WK_MAC_TBA), ios(WK_IOS_TBA));
+- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:(unsigned)hours WK_API_DEPRECATED_WITH_REPLACEMENT("_resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours", macosx(10.13, WK_MAC_TBA), ios(11.0, WK_IOS_TBA));
+- (void)_resourceLoadStatisticsClearInMemoryAndPersistentStoreModifiedSinceHours:(unsigned)hours completionHandler:(void (^)())completionHandler WK_API_AVAILABLE(macosx(WK_MAC_TBA), ios(WK_IOS_TBA));
 - (void)_resourceLoadStatisticsResetToConsistentState WK_API_AVAILABLE(macosx(10.13), ios(11.0));
 - (void)_setResourceLoadStatisticsTestingCallback:(nullable void (^)(WKWebsiteDataStore *, NSString *))callback WK_API_AVAILABLE(macosx(10.13), ios(11.0));
 + (void)_allowWebsiteDataRecordsForAllOrigins WK_API_AVAILABLE(macosx(WK_MAC_TBA), ios(WK_IOS_TBA));

commit 3f8222b380bf2da0ffc7caae7cdcc8bf126b09f2

[GTK] Test /webkit/WebKitWebsiteData/resource-load-statistics is failing since r227223 https://bugs.webkit.org/show_bug.cgi?id=181929
Reviewed by Michael Catanzaro.

The test is checking that the resource load stats file is deleted after clearing website data. Since r227223,
the website data completion callback is not called immediately, and a new file is created when the operation
finishes. We need to check that the file was removing after calling clear.

* TestWebKitAPI/Tests/WebKitGLib/TestWebsiteData.cpp:
(fileChangedCallback):
(testWebsiteDataResourceLoadStatistics):

Identifier: 197756@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@227289 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/TestWebKitAPI/Tests/WebKitGLib/TestWebsiteData.cpp
@@ -538,6 +538,12 @@ static void testWebsiteDataCookies(WebsiteDataTest* test, gconstpointer)
     g_assert(!dataList);
 }
 
+static void fileChangedCallback(GFileMonitor*, GFile*, GFile*, GFileMonitorEvent event, bool* fileRemoved)
+{
+    if (event == G_FILE_MONITOR_EVENT_DELETED)
+        *fileRemoved = true;
+}
+
 static void testWebsiteDataResourceLoadStatistics(WebsiteDataTest* test, gconstpointer)
 {
     const char* resourceLoadStatisticsDirectory = webkit_website_data_manager_get_resource_load_statistics_directory(test->m_manager);
@@ -566,9 +572,13 @@ static void testWebsiteDataResourceLoadStatistics(WebsiteDataTest* test, gconstp
 
     // Clear all.
     static const WebKitWebsiteDataTypes cacheAndResourceLoadStatistics = static_cast<WebKitWebsiteDataTypes>(WEBKIT_WEBSITE_DATA_RESOURCE_LOAD_STATISTICS | WEBKIT_WEBSITE_DATA_MEMORY_CACHE | WEBKIT_WEBSITE_DATA_DISK_CACHE);
+    GRefPtr<GFile> file = adoptGRef(g_file_new_for_path(resourceLoadStatisticsLogFile.get()));
+    GRefPtr<GFileMonitor> monitor = adoptGRef(g_file_monitor_file(file.get(), G_FILE_MONITOR_NONE, nullptr, nullptr));
+    g_assert(monitor.get());
+    bool fileRemoved = false;
+    g_signal_connect(monitor.get(), "changed", G_CALLBACK(fileChangedCallback), &fileRemoved);
     test->clear(cacheAndResourceLoadStatistics, 0);
-    g_assert(g_file_test(resourceLoadStatisticsDirectory, G_FILE_TEST_IS_DIR));
-    g_assert(!g_file_test(resourceLoadStatisticsLogFile.get(), G_FILE_TEST_IS_REGULAR));
+    g_assert(fileRemoved);
 
     webkit_website_data_manager_set_resource_load_statistics_enabled(test->m_manager, FALSE);
     g_assert(!webkit_website_data_manager_get_resource_load_statistics_enabled(test->m_manager));