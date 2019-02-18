night-codes/WebKit

commit ee925d9270d00e2b1b41b9c874ca78b1a0513310

Store Ad Click Attribution requests in the network process https://bugs.webkit.org/show_bug.cgi?id=194510 <rdar://problem/47650118>
Reviewed by Alex Christensen and Daniel Bates.

Source/WebCore:

Test: http/tests/adClickAttribution/store-ad-click-attribution.html

This patch adds support functions for validation and storage of
WebCore::AdClickAttribution objects. It also adds WTF::HashTraits so that
WebCore::AdClickAttribution::Source and WebCore::AdClickAttribution::Destination
can be used in a HashMap.

* loader/AdClickAttribution.cpp:
(WebCore::AdClickAttribution::toString const):
* loader/AdClickAttribution.h:
(WebCore::AdClickAttribution::Source::operator== const):
(WebCore::AdClickAttribution::Source::deletedValue):
(WebCore::AdClickAttribution::Source::constructDeletedValue):
(WebCore::AdClickAttribution::Source::deleteValue):
(WebCore::AdClickAttribution::Source::isDeletedValue const):
(WebCore::AdClickAttribution::SourceHash::hash):
(WebCore::AdClickAttribution::SourceHash::equal):
(WebCore::AdClickAttribution::Destination::operator== const):
(WebCore::AdClickAttribution::Destination::matches const):
    This convenience function allows matching of a WTF::URL object.
(WebCore::AdClickAttribution::Destination::deletedValue):
(WebCore::AdClickAttribution::Destination::constructDeletedValue):
(WebCore::AdClickAttribution::Destination::deleteValue):
(WebCore::AdClickAttribution::Destination::isDeletedValue const):
(WebCore::AdClickAttribution::DestinationHash::hash):
(WebCore::AdClickAttribution::DestinationHash::equal):
(WebCore::AdClickAttribution::source const):
(WebCore::AdClickAttribution::destination const):
    Getters added to support mapped storage based on source and destination.
(WTF::HashTraits<WebCore::AdClickAttribution::Source>::emptyValue):
(WTF::HashTraits<WebCore::AdClickAttribution::Source>::constructDeletedValue):
(WTF::HashTraits<WebCore::AdClickAttribution::Source>::isDeletedValue):

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -129,6 +129,7 @@
 #include "WebURLSchemeHandler.h"
 #include "WebUserContentControllerProxy.h"
 #include "WebsiteDataStore.h"
+#include <WebCore/AdClickAttribution.h>
 #include <WebCore/BitmapImage.h>
 #include <WebCore/DeprecatedGlobalSettings.h>
 #include <WebCore/DiagnosticLoggingClient.h>
@@ -4117,8 +4118,13 @@ void WebPageProxy::didFinishDocumentLoadForFrame(uint64_t frameID, uint64_t navi
 
     // FIXME: We should message check that navigationID is not zero here, but it's currently zero for some navigations through the page cache.
     RefPtr<API::Navigation> navigation;
-    if (frame->isMainFrame() && navigationID)
+    if (frame->isMainFrame() && navigationID) {
         navigation = navigationState().navigation(navigationID);
+        if (auto& adClickAttribution = navigation->adClickAttribution()) {
+            if (adClickAttribution->destination().matches(frame->url()))
+                m_process->processPool().sendToNetworkingProcess(Messages::NetworkProcess::StoreAdClickAttribution(m_websiteDataStore->sessionID(), *adClickAttribution));
+        }
+    }
 
     if (frame->isMainFrame())
         m_navigationClient->didFinishDocumentLoad(*this, navigation.get(), m_process->transformHandlesToObjects(userData.object()).get());
@@ -8659,6 +8665,16 @@ void WebPageProxy::removeDataDetectedLinks(CompletionHandler<void(const DataDete
 }
 
 #endif
+
+void WebPageProxy::dumpAdClickAttribution(CompletionHandler<void(const String&)>&& completionHandler)
+{
+    m_process->processPool().dumpAdClickAttribution(m_websiteDataStore->sessionID(), WTFMove(completionHandler));
+}
+
+void WebPageProxy::clearAdClickAttribution(CompletionHandler<void()>&& completionHandler)
+{
+    m_process->processPool().clearAdClickAttribution(m_websiteDataStore->sessionID(), WTFMove(completionHandler));
+}
 
 } // namespace WebKit
 

LayoutTests/TestExpectations
@@ -2054,6 +2054,8 @@ webkit.org/b/177440 imported/w3c/web-platform-tests/html/browsers/origin/relaxin
 webkit.org/b/157068 [ Debug ] imported/w3c/web-platform-tests/fetch/nosniff/importscripts.html [ Pass Crash ]
 webkit.org/b/157068 [ Release ] imported/w3c/web-platform-tests/fetch/nosniff/importscripts.html [ Pass Failure ]
 
+http/tests/adClickAttribution [ Skip ]
+
 # Application Manifest tests
 webkit.org/b/153152 http/tests/security/contentSecurityPolicy/manifest-src-allowed.html [ Skip ]
 webkit.org/b/153152 http/tests/security/contentSecurityPolicy/manifest-src-blocked.html [ Skip ]

LayoutTests/http/tests/adClickAttribution/store-ad-click-attribution-expected.txt
@@ -1,0 +1,8 @@
+Tests storage of ad click attribution.
+
+
+WebCore::AdClickAttribution 1
+Source: 127.0.0.1
+Destination: localhost
+Campaign ID: 3
+No conversion data.

LayoutTests/http/tests/adClickAttribution/store-ad-click-attribution.html
@@ -1,0 +1,47 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ useFlexibleViewport=true, internal:AdClickAttributionEnabled=true ] -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <script src="/js-test-resources/ui-helper.js"></script>
+</head>
+<body onload="setTimeout(runTest, 0)">
+<div id="description">Tests storage of ad click attribution.</div>
+<a id="targetLink" href="http://localhost:8000/adClickAttribution/store-ad-click-attribution.html?stepTwo" adcampaignid="3" addestination="http://localhost:8000">Link</a><br>
+<div id="output"></div>
+<script>
+    if (window.testRunner) {
+        testRunner.waitUntilDone();
+        testRunner.dumpAsText();
+    }
+
+    function activateElement(elementID) {
+        var element = document.getElementById(elementID);
+        var centerX = element.offsetLeft + element.offsetWidth / 2;
+        var centerY = element.offsetTop + element.offsetHeight / 2;
+        UIHelper.activateAt(centerX, centerY).then(
+            function () {
+            },
+            function () {
+                document.getElementById("output").innerText = "FAIL Promise rejected.";
+                testRunner.notifyDone();
+            }
+        );
+    }
+
+    function runTest() {
+        if (window.testRunner) {
+            if (window.location.search === "?stepTwo") {
+                testRunner.dumpAdClickAttribution();
+                document.body.removeChild(document.getElementById("targetLink"));
+                testRunner.notifyDone();
+            } else {
+                activateElement("targetLink");
+            }
+        } else {
+            document.getElementById("output").innerText = "FAIL No testRunner.";
+        }
+    }
+</script>
+</body>
+</html>

LayoutTests/platform/wk2/TestExpectations
@@ -749,6 +749,8 @@ storage/indexeddb/modern/opendatabase-after-storage-crash.html [ Pass ]
 
 fast/forms/call-text-did-change-in-text-field-when-typing.html [ Pass ]
 
+http/tests/adClickAttribution [ Pass ]
+
 ### END OF (5) Progressions, expected successes that are expected failures in WebKit1.
 ########################################
 

Source/WebCore/loader/AdClickAttribution.cpp
@@ -90,4 +90,23 @@ URL AdClickAttribution::referrer() const
     return URL();
 }
 
+String AdClickAttribution::toString() const
+{
+    StringBuilder builder;
+    builder.appendLiteral("Source: ");
+    builder.append(m_source.registrableDomain);
+    builder.appendLiteral("\nDestination: ");
+    builder.append(m_destination.registrableDomain);
+    builder.appendLiteral("\nCampaign ID: ");
+    builder.appendNumber(m_campaign.id);
+    if (m_conversion) {
+        builder.appendLiteral("\nConversion data: ");
+        builder.appendNumber(m_conversion.value().data);
+    } else
+        builder.appendLiteral("\nNo conversion data.");
+    builder.append('\n');
+
+    return builder.toString();
+}
+
 }

Source/WebCore/loader/AdClickAttribution.h
@@ -26,14 +26,11 @@
 #pragma once
 
 #include "PublicSuffix.h"
-#include <wtf/Noncopyable.h>
 #include <wtf/Optional.h>
+#include <wtf/URL.h>
 #include <wtf/WallTime.h>
+#include <wtf/text/StringHash.h>
 #include <wtf/text/WTFString.h>
-
-namespace WTF {
-class URL;
-}
 
 namespace WebCore {
 
@@ -46,6 +43,7 @@ public:
     static constexpr uint32_t MaxEntropy = 64;
 
     struct Campaign {
+        Campaign() = default;
         explicit Campaign(CampaignId id)
             : id { id }
         {
@@ -56,33 +54,148 @@ public:
             return id < MaxEntropy;
         }
         
-        CampaignId id;
+        CampaignId id { 0 };
     };
 
     struct Source {
+        Source() = default;
         explicit Source(const String& host)
 #if ENABLE(PUBLIC_SUFFIX_LIST)
-            : registrableDomain { WebCore::topPrivatelyControlledDomain(host) }
+            : registrableDomain { topPrivatelyControlledDomain(host) }
 #else
             : registrableDomain { emptyString() }
 #endif
         {
+        }
+
+        explicit Source(WTF::HashTableDeletedValueType)
+            : registrableDomain(WTF::HashTableDeletedValue)
+        {
+        }
+
+        bool operator==(const Source& other) const
+        {
+            return registrableDomain == other.registrableDomain;
+        }
+
+        bool matches(const URL& url) const
+        {
+#if ENABLE(PUBLIC_SUFFIX_LIST)
+            return registrableDomain == topPrivatelyControlledDomain(url.host().toString());
+#else
+            return registrableDomain == url.host().toString();
+#endif
+        }
+
+        bool isHashTableDeletedValue() const
+        {
+            return registrableDomain.isHashTableDeletedValue();
+        }
+
+        static Source deletedValue()
+        {
+            return Source { WTF::HashTableDeletedValue };
+        }
+
+        static void constructDeletedValue(Source& source)
+        {
+            new (&source) Source;
+            source = Source::deletedValue();
+        }
+
+        void deleteValue()
+        {
+            registrableDomain = String { WTF::HashTableDeletedValue };
+        }
+
+        bool isDeletedValue() const
+        {
+            return isHashTableDeletedValue();
         }
 
         String registrableDomain;
     };
 
+    struct SourceHash {
+        static unsigned hash(const Source& source)
+        {
+            return source.registrableDomain.hash();
+        }
+        
+        static bool equal(const Source& a, const Source& b)
+        {
+            return a == b;
+        }
+
+        static const bool safeToCompareToEmptyOrDeleted = false;
+    };
+
     struct Destination {
+        Destination() = default;
         explicit Destination(const String& host)
 #if ENABLE(PUBLIC_SUFFIX_LIST)
-            : registrableDomain { WebCore::topPrivatelyControlledDomain(host) }
+            : registrableDomain { topPrivatelyControlledDomain(host) }
 #else
             : registrableDomain { emptyString() }
 #endif
         {
         }
 
+        bool operator==(const Destination& other) const
+        {
+            return registrableDomain == other.registrableDomain;
+        }
+
+        bool matches(const URL& url) const
+        {
+#if ENABLE(PUBLIC_SUFFIX_LIST)
+            return registrableDomain == topPrivatelyControlledDomain(url.host().toString());
+#else
+            return registrableDomain == url.host().toString();
+#endif
+        }
+        
+        bool isHashTableDeletedValue() const
+        {
+            return registrableDomain.isHashTableDeletedValue();
+        }
+
+        static Destination deletedValue()
+        {
+            return Destination { WTF::HashTableDeletedValue };
+        }
+
+        static void constructDeletedValue(Destination& destination)
+        {
+            new (&destination) Destination;
+            destination = Destination::deletedValue();
+        }
+
+        void deleteValue()
+        {
+            registrableDomain = String { WTF::HashTableDeletedValue };
+        }
+
+        bool isDeletedValue() const
+        {
+            return isHashTableDeletedValue();
+        }
+
         String registrableDomain;
+    };
+
+    struct DestinationHash {
+        static unsigned hash(const Destination& destination)
+        {
+            return destination.registrableDomain.hash();
+        }
+        
+        static bool equal(const Destination& a, const Destination& b)
+        {
+            return a == b;
+        }
+
+        static const bool safeToCompareToEmptyOrDeleted = false;
     };
 
     struct Priority {
@@ -113,6 +226,7 @@ public:
         template<class Decoder> static Optional<Conversion> decode(Decoder&);
     };
 
+    AdClickAttribution() = default;
     AdClickAttribution(Campaign campaign, const Source& source, const Destination& destination)
         : m_campaign { campaign }
         , m_source { source }
@@ -124,7 +238,11 @@ public:
     WEBCORE_EXPORT void setConversion(Conversion&&);
     WEBCORE_EXPORT URL url() const;
     WEBCORE_EXPORT URL referrer() const;
+    const Source& source() const { return m_source; };
+    const Destination& destination() const { return m_destination; };
     Optional<WallTime> earliestTimeToSend() const { return m_earliestTimeToSend; };
+
+    WEBCORE_EXPORT String toString() const;
 
     template<class Encoder> void encode(Encoder&) const;
     template<class Decoder> static Optional<AdClickAttribution> decode(Decoder&);
@@ -210,3 +328,25 @@ Optional<AdClickAttribution::Conversion> AdClickAttribution::Conversion::decode(
 }
 
 } // namespace WebCore
+
+namespace WTF {
+template<typename T> struct DefaultHash;
+
+template<> struct DefaultHash<WebCore::AdClickAttribution::Source> {
+    typedef WebCore::AdClickAttribution::SourceHash Hash;
+};
+template<> struct HashTraits<WebCore::AdClickAttribution::Source> : GenericHashTraits<WebCore::AdClickAttribution::Source> {
+    static WebCore::AdClickAttribution::Source emptyValue() { return { }; }
+    static void constructDeletedValue(WebCore::AdClickAttribution::Source& slot) { WebCore::AdClickAttribution::Source::constructDeletedValue(slot); }
+    static bool isDeletedValue(const WebCore::AdClickAttribution::Source& slot) { return slot.isDeletedValue(); }
+};
+
+template<> struct DefaultHash<WebCore::AdClickAttribution::Destination> {
+    typedef WebCore::AdClickAttribution::DestinationHash Hash;
+};
+template<> struct HashTraits<WebCore::AdClickAttribution::Destination> : GenericHashTraits<WebCore::AdClickAttribution::Destination> {
+    static WebCore::AdClickAttribution::Destination emptyValue() { return { }; }
+    static void constructDeletedValue(WebCore::AdClickAttribution::Destination& slot) { WebCore::AdClickAttribution::Destination::constructDeletedValue(slot); }
+    static bool isDeletedValue(const WebCore::AdClickAttribution::Destination& slot) { return slot.isDeletedValue(); }
+};
+}

Source/WebCore/loader/NavigationAction.h
@@ -135,7 +135,7 @@ public:
     LockBackForwardList lockBackForwardList() const { return m_lockBackForwardList; }
     void setLockBackForwardList(LockBackForwardList lockBackForwardList) { m_lockBackForwardList = lockBackForwardList; }
 
-    const Optional<AdClickAttribution>& adClickAttribution() { return m_adClickAttribution; };
+    const Optional<AdClickAttribution>& adClickAttribution() const { return m_adClickAttribution; };
     void setAdClickAttribution(AdClickAttribution&& adClickAttribution) { m_adClickAttribution = adClickAttribution; };
 
 private:

commit 121d3cbe4c882e325dc79e3c2222258b2a823f9b

WebPageProxy::didFinishDocumentLoadForFrame() crashing in WPE and GTK+ ports https://bugs.webkit.org/show_bug.cgi?id=194774
Patch by Claudio Saavedra <csaavedra@igalia.com> on 2019-02-18
Reviewed by Alex Christensen.

* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::didFinishDocumentLoadForFrame):
Null check navigation before using it.
This is a regression from r241451.



Identifier: 209166@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@241735 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -4136,9 +4136,11 @@ void WebPageProxy::didFinishDocumentLoadForFrame(uint64_t frameID, uint64_t navi
     RefPtr<API::Navigation> navigation;
     if (frame->isMainFrame() && navigationID) {
         navigation = navigationState().navigation(navigationID);
-        if (auto& adClickAttribution = navigation->adClickAttribution()) {
-            if (adClickAttribution->destination().matches(frame->url()))
-                m_process->processPool().sendToNetworkingProcess(Messages::NetworkProcess::StoreAdClickAttribution(m_websiteDataStore->sessionID(), *adClickAttribution));
+        if (navigation) {
+            if (auto& adClickAttribution = navigation->adClickAttribution()) {
+                if (adClickAttribution->destination().matches(frame->url()))
+                    m_process->processPool().sendToNetworkingProcess(Messages::NetworkProcess::StoreAdClickAttribution(m_websiteDataStore->sessionID(), *adClickAttribution));
+            }
         }
     }
 