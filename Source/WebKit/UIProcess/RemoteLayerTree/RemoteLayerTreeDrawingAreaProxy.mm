night-codes/WebKit

commit 4c5ab46145b191e01088c2f74fc3256665bb99a2

[iOS] element.focus() sometimes fails to reveal the focused element when it becomes editable dynamically https://bugs.webkit.org/show_bug.cgi?id=197188
Reviewed by Wenson Hsieh.

Source/WebCore:

The bug was caused by the scroll-to-reveal code triggered by Element::updateFocusAppearance updating
the scroll position via scrolling tree update in a layer tree commit which happens after
_zoomToRevealFocusedElement in WKContentView had already scrolled the frame view.

To fix this problem, we need to defer the editor state update until the layer commit (see r244494),
and update the scrolling tree before invoking WebPageProxy::editorStateChanged which brings up
the keyboard and scroll-to-reveal the caret in the UI process side.

We also avoid revealing the focus for the second time via Document::scheduleScrollToFocusedElement
in Element::updateFocusAppearance as this timer based scrolling also happens after we had already
revealed the caret in _zoomToRevealFocusedElement. This is a bit hacky but works for most cases since
we wouldn't bring up a keyboard if the focused element was not editable anyway.

Test: editing/selection/ios/scrolling-to-focused-element-inside-iframe.html

* dom/Element.cpp:
(WebCore::Element::updateFocusAppearance): Avoid scheduling a timer based reavel of the focused element
when we're already revealing the element via selection change.

Source/WebKit:

Commit the scroll tree update before revealing the keyboard via editor state update.

* UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm:
(WebKit::RemoteLayerTreeDrawingAreaProxy::commitLayerTree):

LayoutTests:

Added a regression test.

* editing/selection/ios/scrolling-to-focused-element-inside-iframe-expected.txt: Added.
* editing/selection/ios/scrolling-to-focused-element-inside-iframe.html: Added.


Identifier: 211406@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@244546 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm
@@ -199,9 +199,6 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTree(const RemoteLayerTreeTrans
     m_transactionIDForPendingCACommit = layerTreeTransaction.transactionID();
     m_activityStateChangeID = layerTreeTransaction.activityStateChangeID();
 
-    if (layerTreeTransaction.hasEditorState())
-        m_webPageProxy.editorStateChanged(layerTreeTransaction.editorState());
-
     if (m_remoteLayerTreeHost->updateLayerTree(layerTreeTransaction)) {
         if (layerTreeTransaction.transactionID() >= m_transactionIDForUnhidingContent)
             m_webPageProxy.setRemoteLayerTreeRootNode(m_remoteLayerTreeHost->rootNode());
@@ -254,6 +251,9 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTree(const RemoteLayerTreeTrans
     m_didUpdateMessageState = NeedsDidUpdate;
     didRefreshDisplay();
 #endif
+
+    if (layerTreeTransaction.hasEditorState())
+        m_webPageProxy.editorStateChanged(layerTreeTransaction.editorState());
 
     if (auto milestones = layerTreeTransaction.newlyReachedPaintingMilestones())
         m_webPageProxy.didReachLayoutMilestone(milestones);

LayoutTests/editing/selection/ios/scrolling-to-focused-element-inside-iframe-expected.txt
@@ -1,0 +1,8 @@
+
+
+--------
+Frame: '<!--frame1-->'
+--------
+click here This tests focusing an element inside an iframe at the bottom of the page.
+To manually test, tap on "click here" below with docked software keyboard.
+PASS

LayoutTests/editing/selection/ios/scrolling-to-focused-element-inside-iframe.html
@@ -1,0 +1,57 @@
+<!DOCTYPE html><!-- webkit-test-runner [ useFlexibleViewport=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+</head>
+<body>
+<style>
+html, body { margin: 0; padding: 0; width: 100%; height: 100%; }
+iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+</style>
+<script src="../../../resources/ui-helper.js"></script>
+<script>
+
+const frame = document.createElement('iframe');
+document.body.appendChild(frame);
+frame.contentDocument.body.innerHTML = `
+<style>
+html, body { margin: 0; padding: 0; width: 100%; height: 100%; }
+body { background: #6cf; }
+#description { position: absolute; top: 50%; padding: 10px; }
+#container { position: absolute; top: 90%; left: 20%; width: 60%; padding: 10px; font-size: 20px; text-align: center; height: 200px; border: solid 1px #ccc; }
+</style>
+<div id="container" onclick="this.contentEditable = true; this.focus();">click here</div>
+<p id="description">
+    This tests focusing an element inside an iframe at the bottom of the page.<br>
+    To manually test, tap on "click here" below with docked software keyboard.<br>
+    <span id="result"></span>
+</p>
+`;
+
+let keyboardHeight = 250;
+
+function checkScrollTop() {
+    frame.contentWindow.result.textContent = visualViewport.pageTop > keyboardHeight ? 'PASS' : `FAIL - ${visualViewport.offsetTop}px`;
+}
+
+async function runTest() {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+    testRunner.dumpChildFramesAsText();
+    await UIHelper.setHardwareKeyboardAttached(false);
+    await UIHelper.activateAndWaitForInputSessionAt(frame.offsetWidth / 2, frame.offsetHeight - 5);
+    await UIHelper.ensurePresentationUpdate();
+    const rect = await UIHelper.inputViewBounds();
+    keyboardHeight = rect.height;
+    checkScrollTop();
+    testRunner.notifyDone();
+}
+
+if (window.testRunner)
+    runTest();
+else
+    frame.contentWindow.container.addEventListener('focus', () => setTimeout(checkScrollTop, 500));
+
+</script>
+</body>
+</html>

Source/WebCore/dom/Element.cpp
@@ -2785,6 +2785,7 @@ void Element::updateFocusAppearance(SelectionRestorationMode, SelectionRevealMod
         if (frame->selection().shouldChangeSelection(newSelection)) {
             frame->selection().setSelection(newSelection, FrameSelection::defaultSetSelectionOptions(), Element::defaultFocusTextStateChangeIntent());
             frame->selection().revealSelection(revealMode);
+            return;
         }
     }
 

commit 081ffbe811d177676a72bfba0ab9c6d0b1330fe5

REGRESSION: Layout test editing/selection/ios/selection-after-changing-text-with-callout-menu.html is failing https://bugs.webkit.org/show_bug.cgi?id=197532 <rdar://problem/50177144>
Reviewed by Ryosuke Niwa.

Fixes layout tests that began failing after r244546. See below for details.

* UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm:
(WebKit::RemoteLayerTreeDrawingAreaProxy::commitLayerTree):

Partially reverts a change in r244546, after which we commit the layer tree and scroll before updating the
page's editor state. The purpose of this change was to ensure that UI process-side element focus scrolling logic
would not conflict with web-process-driven scrolling logic.

Instead, we split the existing logic in WebPageProxy::editorStateChanged into two pieces: one that updates the
editor state (by setting m_editorState), and a second that dispatches updates to PageClient when the first
editor state is received after focusing an element. During a layer tree commit in the UI process, we first
update the editor state, then commit the layer tree and apply scroll position changes, and finally scroll to
reveal the focused element if necessary.

When an editor state is delivered to the UI process in an out-of-band update (i.e. not in a layer tree commit),
simply dispatch the initial editor state for a focused element immediately.

* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::scheduleFullEditorStateUpdate):

Add an IPC message to schedule an editor state update in the next remote layer tree commit. See below for more
details.

(WebKit::WebPageProxy::editorStateChanged):
(WebKit::WebPageProxy::dispatchDidReceiveEditorStateAfterFocus):
* UIProcess/WebPageProxy.h:
* UIProcess/gtk/WebPageProxyGtk.cpp:
(WebKit::WebPageProxy::updateEditorState):
(WebKit::WebPageProxy::editorStateChanged): Deleted.
* UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView willFinishIgnoringCalloutBarFadeAfterPerformingAction]):

Source/WebKit/UIProcess/RemoteLayerTree/RemoteLayerTreeDrawingAreaProxy.mm
@@ -199,6 +199,9 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTree(const RemoteLayerTreeTrans
     m_transactionIDForPendingCACommit = layerTreeTransaction.transactionID();
     m_activityStateChangeID = layerTreeTransaction.activityStateChangeID();
 
+    if (layerTreeTransaction.hasEditorState())
+        m_webPageProxy.updateEditorState(layerTreeTransaction.editorState());
+
     if (m_remoteLayerTreeHost->updateLayerTree(layerTreeTransaction)) {
         if (layerTreeTransaction.transactionID() >= m_transactionIDForUnhidingContent)
             m_webPageProxy.setRemoteLayerTreeRootNode(m_remoteLayerTreeHost->rootNode());
@@ -253,7 +256,7 @@ void RemoteLayerTreeDrawingAreaProxy::commitLayerTree(const RemoteLayerTreeTrans
 #endif
 
     if (layerTreeTransaction.hasEditorState())
-        m_webPageProxy.editorStateChanged(layerTreeTransaction.editorState());
+        m_webPageProxy.dispatchDidReceiveEditorStateAfterFocus();
 
     if (auto milestones = layerTreeTransaction.newlyReachedPaintingMilestones())
         m_webPageProxy.didReachLayoutMilestone(milestones);

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -2006,6 +2006,14 @@ void WebPageProxy::setMaintainsInactiveSelection(bool newValue)
     m_maintainsInactiveSelection = newValue;
 }
 
+void WebPageProxy::scheduleFullEditorStateUpdate()
+{
+    if (!hasRunningProcess())
+        return;
+
+    m_process->send(Messages::WebPage::ScheduleFullEditorStateUpdate(), m_pageID);
+}
+
 void WebPageProxy::executeEditCommand(const String& commandName, const String& argument, WTF::Function<void(CallbackBase::Error)>&& callbackFunction)
 {
     if (!hasRunningProcess()) {
@@ -6502,6 +6510,20 @@ void WebPageProxy::editingRangeCallback(const EditingRange& range, CallbackID ca
 
     callback->performCallbackWithReturnValue(range);
 }
+
+void WebPageProxy::editorStateChanged(const EditorState& editorState)
+{
+    updateEditorState(editorState);
+    dispatchDidReceiveEditorStateAfterFocus();
+}
+
+#if !PLATFORM(IOS_FAMILY)
+
+void WebPageProxy::dispatchDidReceiveEditorStateAfterFocus()
+{
+}
+
+#endif
 
 #if ENABLE(APPLICATION_MANIFEST)
 void WebPageProxy::applicationManifestCallback(const Optional<WebCore::ApplicationManifest>& manifestOrNull, CallbackID callbackID)

Source/WebKit/UIProcess/WebPageProxy.h
@@ -1423,6 +1423,9 @@ public:
     void createSandboxExtensionsIfNeeded(const Vector<String>& files, SandboxExtension::Handle& fileReadHandle, SandboxExtension::HandleArray& fileUploadHandles);
 #endif
     void editorStateChanged(const EditorState&);
+    void updateEditorState(const EditorState&);
+    void scheduleFullEditorStateUpdate();
+    void dispatchDidReceiveEditorStateAfterFocus();
 
 #if PLATFORM(COCOA)
     void touchBarMenuDataRemoved();

Source/WebKit/UIProcess/gtk/WebPageProxyGtk.cpp
@@ -79,7 +79,7 @@ void WebsiteDataStore::platformRemoveRecentSearches(WallTime oldestTimeToRemove)
     notImplemented();
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     m_editorState = editorState;
     

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -3920,6 +3920,7 @@ static void selectionChangedWithTouch(WKContentView *view, const WebCore::IntPoi
 - (void)willFinishIgnoringCalloutBarFadeAfterPerformingAction
 {
     _ignoreSelectionCommandFadeCount++;
+    _page->scheduleFullEditorStateUpdate();
     _page->callAfterNextPresentationUpdate([weakSelf = WeakObjCPtr<WKContentView>(self)] (auto) {
         if (auto strongSelf = weakSelf.get())
             strongSelf->_ignoreSelectionCommandFadeCount--;

Source/WebKit/UIProcess/ios/WebPageProxyIOS.mm
@@ -430,6 +430,11 @@ bool WebPageProxy::updateLayoutViewportParameters(const WebKit::RemoteLayerTreeT
 void WebPageProxy::layerTreeCommitComplete()
 {
     pageClient().layerTreeCommitComplete();
+
+    if (m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement && !m_editorState.isMissingPostLayoutData) {
+        pageClient().didReceiveEditorStateUpdateAfterFocus();
+        m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
+    }
 }
 
 void WebPageProxy::selectWithGesture(const WebCore::IntPoint point, WebCore::TextGranularity granularity, uint32_t gestureType, uint32_t gestureState, bool isInteractingWithFocusedElement, WTF::Function<void(const WebCore::IntPoint&, uint32_t, uint32_t, uint32_t, CallbackBase::Error)>&& callbackFunction)
@@ -1102,7 +1107,7 @@ void WebPageProxy::contentSizeCategoryDidChange(const String& contentSizeCategor
     process().send(Messages::WebPage::ContentSizeCategoryDidChange(contentSizeCategory), m_pageID);
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     bool couldChangeSecureInputState = m_editorState.isInPasswordField != editorState.isInPasswordField || m_editorState.selectionIsNone;
     
@@ -1119,11 +1124,15 @@ void WebPageProxy::editorStateChanged(const EditorState& editorState)
     // even during composition to support phrase boundary gesture.
     pageClient().selectionDidChange();
     updateFontAttributesAfterEditorStateChange();
+}
 
-    if (m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement && !m_editorState.isMissingPostLayoutData) {
-        pageClient().didReceiveEditorStateUpdateAfterFocus();
-        m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
-    }
+void WebPageProxy::dispatchDidReceiveEditorStateAfterFocus()
+{
+    if (!m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement || m_editorState.isMissingPostLayoutData)
+        return;
+
+    pageClient().didReceiveEditorStateUpdateAfterFocus();
+    m_waitingForPostLayoutEditorStateUpdateAfterFocusingElement = false;
 }
 
 void WebPageProxy::showValidationMessage(const IntRect& anchorClientRect, const String& message)

Source/WebKit/UIProcess/mac/WebPageProxyMac.mm
@@ -607,7 +607,7 @@ bool WebPageProxy::appleMailLinesClampEnabled()
     return MacApplication::isAppleMail();
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     bool couldChangeSecureInputState = m_editorState.isInPasswordField != editorState.isInPasswordField || m_editorState.selectionIsNone;
     

Source/WebKit/UIProcess/win/WebPageProxyWin.cpp
@@ -60,7 +60,7 @@ void WebPageProxy::loadRecentSearches(const String& name, CompletionHandler<void
     completionHandler(WTFMove(searchItems));
 }
 
-void WebPageProxy::editorStateChanged(const EditorState& editorState)
+void WebPageProxy::updateEditorState(const EditorState& editorState)
 {
     m_editorState = editorState;
 }

Source/WebKit/UIProcess/wpe/WebPageProxyWPE.cpp
@@ -64,7 +64,7 @@ void WebsiteDataStore::platformRemoveRecentSearches(WallTime)
     notImplemented();
 }
 
-void WebPageProxy::editorStateChanged(const EditorState&)
+void WebPageProxy::updateEditorState(const EditorState&)
 {
     notImplemented();
 }

Source/WebKit/WebProcess/WebPage/WebPage.h
@@ -1183,6 +1183,7 @@ public:
     void setRemoteObjectRegistry(RemoteObjectRegistry&);
 
     void updateIntrinsicContentSizeIfNeeded(const WebCore::IntSize&);
+    void scheduleFullEditorStateUpdate();
 
 private:
     WebPage(uint64_t pageID, WebPageCreationParameters&&);
@@ -1199,7 +1200,6 @@ private:
     void platformDetach();
     void platformEditorState(WebCore::Frame&, EditorState& result, IncludePostLayoutDataHint) const;
     void sendEditorStateUpdate();
-    void scheduleFullEditorStateUpdate();
 
 #if PLATFORM(COCOA)
     void sendTouchBarMenuDataAddedUpdate(WebCore::HTMLMenuElement&);

Source/WebKit/WebProcess/WebPage/WebPage.messages.in
@@ -202,6 +202,8 @@ messages -> WebPage LegacyReceiver {
     RunJavaScriptInFrame(uint64_t frameID, String script, bool forceUserGesture, WebKit::CallbackID callbackID)
     ForceRepaint(WebKit::CallbackID callbackID)
 
+    ScheduleFullEditorStateUpdate()
+
 #if PLATFORM(COCOA)
     # Dictionary support.
     PerformDictionaryLookupOfCurrentSelection()