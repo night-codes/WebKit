night-codes/WebKit

commit fd7f860ddca2f1e6043a43632b081ddaf134da10

[GTK][WPE] Add API to configure and enable resource load statistics https://bugs.webkit.org/show_bug.cgi?id=177943
Reviewed by Michael Catanzaro.

Source/WebKit:

* UIProcess/API/glib/WebKitWebsiteData.cpp:
(recordContainsSupportedDataTypes):
(toWebKitWebsiteDataTypes):
* UIProcess/API/glib/WebKitWebsiteDataManager.cpp:
(webkitWebsiteDataManagerGetProperty):
(webkitWebsiteDataManagerSetProperty):
(webkitWebsiteDataManagerConstructed):
(webkit_website_data_manager_class_init):
(webkitWebsiteDataManagerGetDataStore):
(webkit_website_data_manager_get_itp_directory):
(webkit_website_data_manager_set_itp_enabled):
(webkit_website_data_manager_get_itp_enabled):
(toWebsiteDataTypes):
* UIProcess/API/gtk/WebKitWebsiteData.h:
* UIProcess/API/gtk/WebKitWebsiteDataManager.h:
* UIProcess/API/gtk/docs/webkit2gtk-4.0-sections.txt:
* UIProcess/API/wpe/WebKitWebsiteData.h:
* UIProcess/API/wpe/WebKitWebsiteDataManager.h:
* UIProcess/API/wpe/docs/wpe-1.0-sections.txt:
* UIProcess/glib/WebsiteDataStoreGLib.cpp:
(WebKit::WebsiteDataStore::defaultResourceLoadStatisticsDirectory):

Tools:

Add --enable-itp command line version to MiniBrowser and unit tests to check the new API.

* MiniBrowser/gtk/main.c:
(gotWebsiteDataCallback):
(activate):
* MiniBrowser/wpe/main.cpp:
(main):
* TestWebKitAPI/Tests/WebKitGLib/TestWebsiteData.cpp:
(serverCallback):
(testWebsiteDataConfiguration):
(testWebsiteDataEphemeral):
(testWebsiteDataITP):
(beforeAll):
* TestWebKitAPI/glib/TestExpectations.json:
* TestWebKitAPI/glib/WebKitGLib/TestMain.h:
(Test::Test):
* TestWebKitAPI/glib/WebKitGLib/WebViewTest.cpp:
(directoryChangedCallback):
(WebViewTest::waitUntilFileChanged):
* TestWebKitAPI/glib/WebKitGLib/WebViewTest.h:

Identifier: 226022@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263085 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/API/glib/WebKitWebsiteDataManager.cpp
@@ -86,6 +86,7 @@ enum {
     PROP_INDEXEDDB_DIRECTORY,
     PROP_WEBSQL_DIRECTORY,
     PROP_HSTS_CACHE_DIRECTORY,
+    PROP_ITP_DIRECTORY,
     PROP_IS_EPHEMERAL
 };
 
@@ -104,6 +105,7 @@ struct _WebKitWebsiteDataManagerPrivate {
     GUniquePtr<char> indexedDBDirectory;
     GUniquePtr<char> webSQLDirectory;
     GUniquePtr<char> hstsCacheDirectory;
+    GUniquePtr<char> itpDirectory;
 
     GRefPtr<WebKitCookieManager> cookieManager;
     Vector<WebProcessPool*> processPools;
@@ -142,6 +144,9 @@ static void webkitWebsiteDataManagerGetProperty(GObject* object, guint propID, G
     case PROP_HSTS_CACHE_DIRECTORY:
         g_value_set_string(value, webkit_website_data_manager_get_hsts_cache_directory(manager));
         break;
+    case PROP_ITP_DIRECTORY:
+        g_value_set_string(value, webkit_website_data_manager_get_itp_directory(manager));
+        break;
     case PROP_IS_EPHEMERAL:
         g_value_set_boolean(value, webkit_website_data_manager_is_ephemeral(manager));
         break;
@@ -179,6 +184,9 @@ static void webkitWebsiteDataManagerSetProperty(GObject* object, guint propID, c
     case PROP_HSTS_CACHE_DIRECTORY:
         manager->priv->hstsCacheDirectory.reset(g_value_dup_string(value));
         break;
+    case PROP_ITP_DIRECTORY:
+        manager->priv->itpDirectory.reset(g_value_dup_string(value));
+        break;
     case PROP_IS_EPHEMERAL:
         if (g_value_get_boolean(value))
             manager->priv->websiteDataStore = WebKit::WebsiteDataStore::createNonPersistent();
@@ -200,6 +208,8 @@ static void webkitWebsiteDataManagerConstructed(GObject* object)
             priv->indexedDBDirectory.reset(g_build_filename(priv->baseDataDirectory.get(), "databases", "indexeddb", nullptr));
         if (!priv->webSQLDirectory)
             priv->webSQLDirectory.reset(g_build_filename(priv->baseDataDirectory.get(), "databases", nullptr));
+        if (!priv->itpDirectory)
+            priv->itpDirectory.reset(g_build_filename(priv->baseDataDirectory.get(), "itp", nullptr));
     }
 
     if (priv->baseCacheDirectory) {
@@ -361,6 +371,23 @@ static void webkit_website_data_manager_class_init(WebKitWebsiteDataManagerClass
             static_cast<GParamFlags>(WEBKIT_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY)));
 
     /**
+     * WebKitWebsiteDataManager:itp-directory:
+     *
+     * The directory where Intelligent Tracking Prevention (ITP) data will be stored.
+     *
+     * Since: 2.30
+     */
+    g_object_class_install_property(
+        gObjectClass,
+        PROP_ITP_DIRECTORY,
+        g_param_spec_string(
+            "itp-directory",
+            _("ITP Direcory"),
+            _("The directory where Intelligent Tracking Prevention data will be stored"),
+            nullptr,
+            static_cast<GParamFlags>(WEBKIT_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY)));
+
+    /**
      * WebKitWebsiteDataManager:is-ephemeral:
      *
      * Whether the #WebKitWebsiteDataManager is ephemeral. An ephemeral #WebKitWebsiteDataManager
@@ -398,6 +425,8 @@ WebKit::WebsiteDataStore& webkitWebsiteDataManagerGetDataStore(WebKitWebsiteData
             WebKit::WebsiteDataStore::defaultWebSQLDatabaseDirectory() : FileSystem::stringFromFileSystemRepresentation(priv->webSQLDirectory.get()));
         configuration->setHSTSStorageDirectory(!priv->hstsCacheDirectory ?
             WebKit::WebsiteDataStore::defaultHSTSDirectory() : FileSystem::stringFromFileSystemRepresentation(priv->hstsCacheDirectory.get()));
+        configuration->setResourceLoadStatisticsDirectory(!priv->itpDirectory ?
+            WebKit::WebsiteDataStore::defaultResourceLoadStatisticsDirectory() : FileSystem::stringFromFileSystemRepresentation(priv->itpDirectory.get()));
         configuration->setMediaKeysStorageDirectory(WebKit::WebsiteDataStore::defaultMediaKeysStorageDirectory());
         priv->websiteDataStore = WebKit::WebsiteDataStore::create(WTFMove(configuration), PAL::SessionID::defaultSessionID());
     }
@@ -660,6 +689,29 @@ const gchar* webkit_website_data_manager_get_hsts_cache_directory(WebKitWebsiteD
 }
 
 /**
+ * webkit_website_data_manager_get_itp_directory:
+ * @manager: a #WebKitWebsiteDataManager
+ *
+ * Get the #WebKitWebsiteDataManager:itp-directory property.
+ *
+ * Returns: (allow-none): the directory where Intelligent Tracking Prevention data is stored or %NULL if @manager is ephemeral.
+ *
+ * Since: 2.30
+ */
+const gchar* webkit_website_data_manager_get_itp_directory(WebKitWebsiteDataManager* manager)
+{
+    g_return_val_if_fail(WEBKIT_IS_WEBSITE_DATA_MANAGER(manager), nullptr);
+
+    WebKitWebsiteDataManagerPrivate* priv = manager->priv;
+    if (priv->websiteDataStore && !priv->websiteDataStore->isPersistent())
+        return nullptr;
+
+    if (!priv->itpDirectory)
+        priv->itpDirectory.reset(g_strdup(WebKit::WebsiteDataStore::defaultResourceLoadStatisticsDirectory().utf8().data()));
+    return priv->itpDirectory.get();
+}
+
+/**
  * webkit_website_data_manager_get_cookie_manager:
  * @manager: a #WebKitWebsiteDataManager
  *
@@ -677,6 +729,42 @@ WebKitCookieManager* webkit_website_data_manager_get_cookie_manager(WebKitWebsit
         manager->priv->cookieManager = adoptGRef(webkitCookieManagerCreate(manager));
 
     return manager->priv->cookieManager.get();
+}
+
+/**
+ * webkit_website_data_manager_set_itp_enabled:
+ * @manager: a #WebKitWebsiteDataManager
+ * @enabled: value to set
+ *
+ * Enable or disable Intelligent Tracking Prevention (ITP). When ITP is enabled resource load statistics
+ * are collected and used to decide whether to allow or block third-party cookies and prevent user tracking.
+ * Note that when ITP is enabled the resources for which cookies are allowed will follow the accept policy
+ * set with webkit_cookie_manager_set_accept_policy().
+ *
+ * Since: 2.30
+ */
+void webkit_website_data_manager_set_itp_enabled(WebKitWebsiteDataManager* manager, gboolean enabled)
+{
+    g_return_if_fail(WEBKIT_IS_WEBSITE_DATA_MANAGER(manager));
+
+    webkitWebsiteDataManagerGetDataStore(manager).setResourceLoadStatisticsEnabled(enabled);
+}
+
+/**
+ * webkit_website_data_manager_get_itp_enabled:
+ * @manager: a #WebKitWebsiteDataManager
+ *
+ * Get whether Intelligent Tracking Prevention (ITP) is enabled or not.
+ *
+ * Returns: %TRUE if ITP is enabled, or %FALSE otherwise.
+ *
+ * Since: 2.30
+ */
+gboolean webkit_website_data_manager_get_itp_enabled(WebKitWebsiteDataManager* manager)
+{
+    g_return_val_if_fail(WEBKIT_IS_WEBSITE_DATA_MANAGER(manager), FALSE);
+
+    return webkitWebsiteDataManagerGetDataStore(manager).resourceLoadStatisticsEnabled();
 }
 
 static OptionSet<WebsiteDataType> toWebsiteDataTypes(WebKitWebsiteDataTypes types)
@@ -706,6 +794,8 @@ static OptionSet<WebsiteDataType> toWebsiteDataTypes(WebKitWebsiteDataTypes type
         returnValue.add(WebsiteDataType::Cookies);
     if (types & WEBKIT_WEBSITE_DATA_DEVICE_ID_HASH_SALT)
         returnValue.add(WebsiteDataType::DeviceIdHashSalt);
+    if (types & WEBKIT_WEBSITE_DATA_ITP)
+        returnValue.add(WebsiteDataType::ResourceLoadStatistics);
     return returnValue;
 }
 

Source/WebKit/UIProcess/API/glib/WebKitWebsiteData.cpp
@@ -78,7 +78,8 @@ static bool recordContainsSupportedDataTypes(const WebsiteDataRecord& record)
         WebsiteDataType::PlugInData,
 #endif
         WebsiteDataType::Cookies,
-        WebsiteDataType::DeviceIdHashSalt
+        WebsiteDataType::DeviceIdHashSalt,
+        WebsiteDataType::ResourceLoadStatistics
     });
 }
 
@@ -109,6 +110,8 @@ static WebKitWebsiteDataTypes toWebKitWebsiteDataTypes(OptionSet<WebsiteDataType
         returnValue |= WEBKIT_WEBSITE_DATA_COOKIES;
     if (types.contains(WebsiteDataType::DeviceIdHashSalt))
         returnValue |= WEBKIT_WEBSITE_DATA_DEVICE_ID_HASH_SALT;
+    if (types.contains(WebsiteDataType::ResourceLoadStatistics))
+        returnValue |= WEBKIT_WEBSITE_DATA_ITP;
     return static_cast<WebKitWebsiteDataTypes>(returnValue);
 }
 

Source/WebKit/UIProcess/API/gtk/WebKitWebsiteData.h
@@ -46,6 +46,7 @@ typedef struct _WebKitWebsiteData WebKitWebsiteData;
  * @WEBKIT_WEBSITE_DATA_COOKIES: Cookies.
  * @WEBKIT_WEBSITE_DATA_DEVICE_ID_HASH_SALT: Hash salt used to generate the device ids used by webpages. Since 2.24
  * @WEBKIT_WEBSITE_DATA_HSTS_CACHE: HSTS cache. Since 2.26
+ * @WEBKIT_WEBSITE_DATA_ITP: Intelligent Tracking Prevention data. Since 2.30.
  * @WEBKIT_WEBSITE_DATA_ALL: All types.
  *
  * Enum values with flags representing types of Website data.
@@ -64,7 +65,8 @@ typedef enum {
     WEBKIT_WEBSITE_DATA_COOKIES                   = 1 << 8,
     WEBKIT_WEBSITE_DATA_DEVICE_ID_HASH_SALT       = 1 << 9,
     WEBKIT_WEBSITE_DATA_HSTS_CACHE                = 1 << 10,
-    WEBKIT_WEBSITE_DATA_ALL                       = (1 << 11) - 1
+    WEBKIT_WEBSITE_DATA_ITP                       = 1 << 11,
+    WEBKIT_WEBSITE_DATA_ALL                       = (1 << 12) - 1
 } WebKitWebsiteDataTypes;
 
 WEBKIT_API GType

Source/WebKit/UIProcess/API/gtk/WebKitWebsiteDataManager.h
@@ -93,8 +93,18 @@ webkit_website_data_manager_get_websql_directory                      (WebKitWeb
 WEBKIT_API const gchar *
 webkit_website_data_manager_get_hsts_cache_directory                  (WebKitWebsiteDataManager *manager);
 
+WEBKIT_API const gchar *
+webkit_website_data_manager_get_itp_directory                         (WebKitWebsiteDataManager *manager);
+
 WEBKIT_API WebKitCookieManager *
 webkit_website_data_manager_get_cookie_manager                        (WebKitWebsiteDataManager *manager);
+
+WEBKIT_API void
+webkit_website_data_manager_set_itp_enabled                           (WebKitWebsiteDataManager *manager,
+                                                                       gboolean                  enabled);
+
+WEBKIT_API gboolean
+webkit_website_data_manager_get_itp_enabled                           (WebKitWebsiteDataManager *manager);
 
 WEBKIT_API void
 webkit_website_data_manager_fetch                                     (WebKitWebsiteDataManager *manager,

Source/WebKit/UIProcess/API/wpe/WebKitWebsiteData.h
@@ -46,6 +46,7 @@ typedef struct _WebKitWebsiteData WebKitWebsiteData;
  * @WEBKIT_WEBSITE_DATA_COOKIES: Cookies.
  * @WEBKIT_WEBSITE_DATA_DEVICE_ID_HASH_SALT: Hash salt used to generate the device ids used by webpages. Since 2.24
  * @WEBKIT_WEBSITE_DATA_HSTS_CACHE: HSTS cache. Since 2.26
+ * @WEBKIT_WEBSITE_DATA_ITP: Intelligent Tracking Prevention data. Since 2.30.
  * @WEBKIT_WEBSITE_DATA_ALL: All types.
  *
  * Enum values with flags representing types of Website data.
@@ -64,7 +65,8 @@ typedef enum {
     WEBKIT_WEBSITE_DATA_COOKIES                   = 1 << 8,
     WEBKIT_WEBSITE_DATA_DEVICE_ID_HASH_SALT       = 1 << 9,
     WEBKIT_WEBSITE_DATA_HSTS_CACHE                = 1 << 10,
-    WEBKIT_WEBSITE_DATA_ALL                       = (1 << 11) - 1
+    WEBKIT_WEBSITE_DATA_ITP                       = 1 << 11,
+    WEBKIT_WEBSITE_DATA_ALL                       = (1 << 12) - 1
 } WebKitWebsiteDataTypes;
 
 WEBKIT_API GType

Source/WebKit/UIProcess/API/wpe/WebKitWebsiteDataManager.h
@@ -93,8 +93,18 @@ webkit_website_data_manager_get_websql_directory                      (WebKitWeb
 WEBKIT_API const gchar *
 webkit_website_data_manager_get_hsts_cache_directory                  (WebKitWebsiteDataManager *manager);
 
+WEBKIT_API const gchar *
+webkit_website_data_manager_get_itp_directory                         (WebKitWebsiteDataManager *manager);
+
 WEBKIT_API WebKitCookieManager *
 webkit_website_data_manager_get_cookie_manager                        (WebKitWebsiteDataManager *manager);
+
+WEBKIT_API void
+webkit_website_data_manager_set_itp_enabled                           (WebKitWebsiteDataManager *manager,
+                                                                       gboolean                  enabled);
+
+WEBKIT_API gboolean
+webkit_website_data_manager_get_itp_enabled                           (WebKitWebsiteDataManager *manager);
 
 WEBKIT_API void
 webkit_website_data_manager_fetch                                     (WebKitWebsiteDataManager *manager,

Source/WebKit/UIProcess/glib/WebsiteDataStoreGLib.cpp
@@ -91,7 +91,7 @@ WTF::String WebsiteDataStore::defaultHSTSDirectory()
 
 WTF::String WebsiteDataStore::defaultResourceLoadStatisticsDirectory()
 {
-    return websiteDataDirectoryFileSystemRepresentation(BASE_DIRECTORY G_DIR_SEPARATOR_S "resourceloadstatistics");
+    return websiteDataDirectoryFileSystemRepresentation(BASE_DIRECTORY G_DIR_SEPARATOR_S "itp");
 }
 
 WTF::String WebsiteDataStore::cacheDirectoryFileSystemRepresentation(const WTF::String& directoryName)

Tools/MiniBrowser/gtk/main.c
@@ -53,6 +53,7 @@ static const char *cookiesFile;
 static const char *cookiesPolicy;
 static const char *proxy;
 static gboolean darkMode;
+static gboolean enableITP;
 static gboolean printVersion;
 
 typedef enum {
@@ -120,6 +121,7 @@ static const GOptionEntry commandLineOptions[] =
     { "ignore-host", 0, 0, G_OPTION_ARG_STRING_ARRAY, &ignoreHosts, "Set proxy ignore hosts", "HOSTS" },
     { "ignore-tls-errors", 0, 0, G_OPTION_ARG_NONE, &ignoreTLSErrors, "Ignore TLS errors", NULL },
     { "content-filter", 0, 0, G_OPTION_ARG_FILENAME, &contentFilter, "JSON with content filtering rules", "FILE" },
+    { "enable-itp", 0, 0, G_OPTION_ARG_NONE, &enableITP, "Enable Intelligent Tracking Prevention (ITP)", NULL },
     { "version", 'v', 0, G_OPTION_ARG_NONE, &printVersion, "Print the WebKitGTK version", NULL },
     { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &uriArguments, 0, "[URL…]" },
     { 0, 0, 0, 0, 0, 0, 0 }
@@ -422,6 +424,7 @@ static void gotWebsiteDataCallback(WebKitWebsiteDataManager *manager, GAsyncResu
     aboutDataFillTable(result, dataRequest, dataList, "Plugins Data", WEBKIT_WEBSITE_DATA_PLUGIN_DATA, NULL, pageID);
     aboutDataFillTable(result, dataRequest, dataList, "Offline Web Applications Cache", WEBKIT_WEBSITE_DATA_OFFLINE_APPLICATION_CACHE, webkit_website_data_manager_get_offline_application_cache_directory(manager), pageID);
     aboutDataFillTable(result, dataRequest, dataList, "HSTS Cache", WEBKIT_WEBSITE_DATA_HSTS_CACHE, webkit_website_data_manager_get_hsts_cache_directory(manager), pageID);
+    aboutDataFillTable(result, dataRequest, dataList, "ITP data", WEBKIT_WEBSITE_DATA_ITP, webkit_website_data_manager_get_itp_directory(manager), pageID);
 
     result = g_string_append(result, "</body></html>");
     gsize streamLength = result->len;
@@ -531,6 +534,7 @@ static void startup(GApplication *application)
 static void activate(GApplication *application, WebKitSettings *webkitSettings)
 {
     WebKitWebsiteDataManager *manager = (privateMode || automationMode) ? webkit_website_data_manager_new_ephemeral() : webkit_website_data_manager_new(NULL);
+    webkit_website_data_manager_set_itp_enabled(manager, enableITP);
     WebKitWebContext *webContext = g_object_new(WEBKIT_TYPE_WEB_CONTEXT, "website-data-manager", manager, "process-swap-on-cross-site-navigation-enabled", TRUE,
 #if !GTK_CHECK_VERSION(3, 98, 0)
         "use-system-appearance-for-scrollbars", FALSE,

commit 6fa0104a9d25c0362e049c69692baae4fe590de6

Unreviewed. Fix a typo introduced in r263085
Patch by Yuri Chornoivan <yurchor@ukr.net> on 2020-06-18

* UIProcess/API/glib/WebKitWebsiteDataManager.cpp:
(webkit_website_data_manager_class_init):

Identifier: 226124@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@263202 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/API/glib/WebKitWebsiteDataManager.cpp
@@ -382,7 +382,7 @@ static void webkit_website_data_manager_class_init(WebKitWebsiteDataManagerClass
         PROP_ITP_DIRECTORY,
         g_param_spec_string(
             "itp-directory",
-            _("ITP Direcory"),
+            _("ITP Directory"),
             _("The directory where Intelligent Tracking Prevention data will be stored"),
             nullptr,
             static_cast<GParamFlags>(WEBKIT_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY)));