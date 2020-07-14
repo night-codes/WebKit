night-codes/WebKit

commit 0e96116448225a2d3665eec8e4f374ba8cbdb3fd

Use correct encoding when converting a WTF::URL to CFURLRef https://bugs.webkit.org/show_bug.cgi?id=212486
Patch by Alex Christensen <achristensen@webkit.org> on 2020-05-29
Reviewed by Darin Adler.

Source/WebKit:

* Shared/API/c/cf/WKURLCF.mm:
(WKURLCopyCFURL):
* Shared/cf/ArgumentCodersCF.cpp:
(IPC::decode):

Source/WTF:

* wtf/cf/CFURLExtras.cpp:
(WTF::createCFURLFromBuffer):
* wtf/cf/CFURLExtras.h:
* wtf/cf/URLCF.cpp:
(WTF::URL::createCFURL const):
* wtf/cocoa/URLCocoa.mm:
(WTF::URL::createCFURL const):

Tools:

* TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm:
(TestWebKitAPI::TEST):

Identifier: 225389@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@262341 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/cf/URLCF.cpp
@@ -52,10 +52,10 @@ RetainPtr<CFURLRef> URL::createCFURL() const
 {
     RetainPtr<CFURLRef> cfURL;
     if (LIKELY(m_string.is8Bit()))
-        cfURL = WTF::createCFURLFromBuffer(reinterpret_cast<const char*>(m_string.characters8()), m_string.length());
+        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(m_string.characters8()), m_string.length(), kCFStringEncodingISOLatin1, nullptr, true));
     else {
         CString utf8 = m_string.utf8();
-        cfURL = WTF::createCFURLFromBuffer(utf8.data(), utf8.length());
+        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(utf8.data()), utf8.length(), kCFStringEncodingUTF8, nullptr, true));
     }
 
     if (protocolIsInHTTPFamily() && !isCFURLSameOrigin(cfURL.get(), *this))

Source/WTF/wtf/cocoa/URLCocoa.mm
@@ -70,10 +70,10 @@ RetainPtr<CFURLRef> URL::createCFURL() const
 
     RetainPtr<CFURLRef> cfURL;
     if (LIKELY(m_string.is8Bit()))
-        cfURL = WTF::createCFURLFromBuffer(reinterpret_cast<const char*>(m_string.characters8()), m_string.length());
+        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(m_string.characters8()), m_string.length(), kCFStringEncodingISOLatin1, nullptr, true));
     else {
         CString utf8 = m_string.utf8();
-        cfURL = WTF::createCFURLFromBuffer(utf8.data(), utf8.length());
+        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(utf8.data()), utf8.length(), kCFStringEncodingUTF8, nullptr, true));
     }
 
     if (protocolIsInHTTPFamily() && !WTF::isCFURLSameOrigin(cfURL.get(), *this))

Tools/TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm
@@ -207,6 +207,24 @@ TEST(WTF_URLExtras, URLExtras_ParsingError)
 
     WTF::URL url2(URL(), utf16String(u"http://\u2267\u222E\uFE63"));
     EXPECT_STREQ([[url2 absoluteString] UTF8String], "http://%E2%89%A7%E2%88%AE%EF%B9%A3");
+
+    std::array<UChar, 3> utf16 { 0xC2, 0xB6, 0x00 };
+    WTF::URL url3(URL(), String(utf16.data()));
+    EXPECT_FALSE(url3.string().is8Bit());
+    EXPECT_FALSE(url3.isValid());
+    EXPECT_STREQ([[url3 absoluteString] UTF8String], "%C3%82%C2%B6");
+    
+    std::array<LChar, 3> latin1 { 0xC2, 0xB6, 0x00 };
+    WTF::URL url4(URL(), String(latin1.data()));
+    EXPECT_FALSE(url4.isValid());
+    EXPECT_TRUE(url4.string().is8Bit());
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MAX_ALLOWED < 101500
+    // CFURLCreateAbsoluteURLWithBytes has incorrect behavior on Mojave
+    // See https://bugs.webkit.org/show_bug.cgi?id=212486#c6
+    EXPECT_STREQ([[url4 absoluteString] UTF8String], "%C2%B6");
+#else
+    EXPECT_STREQ([[url4 absoluteString] UTF8String], "%C3%82%C2%B6");
+#endif
 }
 
 TEST(WTF_URLExtras, URLExtras_Nil)

Source/WTF/wtf/cf/CFURLExtras.cpp
@@ -31,18 +31,6 @@
 
 namespace WTF {
 
-RetainPtr<CFURLRef> createCFURLFromBuffer(const char* data, size_t size, CFURLRef baseURL)
-{
-    // NOTE: We use UTF-8 here since this encoding is used when computing strings when returning URL components
-    // (e.g calls to NSURL -path). However, this function is not tolerant of illegal UTF-8 sequences, which
-    // could either be a malformed string or bytes in a different encoding, like Shift-JIS, so we fall back
-    // onto using ISO Latin-1 in those cases.
-    RetainPtr<CFURLRef> result = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(data), size, kCFStringEncodingUTF8, baseURL, true));
-    if (!result)
-        result = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(data), size, kCFStringEncodingISOLatin1, baseURL, true));
-    return result;
-}
-
 void getURLBytes(CFURLRef url, URLCharBuffer& result)
 {
     CFIndex bytesLength = CFURLGetBytes(url, nullptr, 0);

Source/WTF/wtf/cf/CFURLExtras.h
@@ -34,7 +34,6 @@ namespace WTF {
 class URL;
 typedef Vector<char, 512> URLCharBuffer;
 
-WTF_EXPORT_PRIVATE RetainPtr<CFURLRef> createCFURLFromBuffer(const char*, size_t, CFURLRef baseURL = nullptr);
 WTF_EXPORT_PRIVATE void getURLBytes(CFURLRef, URLCharBuffer&);
 WTF_EXPORT_PRIVATE void getURLBytes(CFURLRef, CString&);
 

Source/WebKit/Shared/API/c/cf/WKURLCF.mm
@@ -65,5 +65,5 @@ CFURLRef WKURLCopyCFURL(CFAllocatorRef allocatorRef, WKURLRef URLRef)
     // UTF-8 which uses less memory and is what WebKit clients might expect.
 
     CString buffer = WebKit::toImpl(URLRef)->string().utf8();
-    return WTF::createCFURLFromBuffer(buffer.data(), buffer.length(), 0).leakRef();
+    return CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(buffer.data()), buffer.length(), kCFStringEncodingUTF8, nullptr, true);
 }

Source/WebKit/Shared/cf/ArgumentCodersCF.cpp
@@ -639,7 +639,7 @@ bool decode(Decoder& decoder, RetainPtr<CFURLRef>& result)
     }
 #endif
 
-    result = WTF::createCFURLFromBuffer(reinterpret_cast<const char*>(urlBytes.data()), urlBytes.size(), baseURL.get());
+    result = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(urlBytes.data()), urlBytes.size(), kCFStringEncodingASCII, baseURL.get(), true));
     return result;
 }
 

commit 92959d952f21a4211500ec3317053f02876ef6ac

REGRESSION(r262341) URL::createCFURL should produce a CFURL that uses UTF-8 to decode its percent-encoded sequences https://bugs.webkit.org/show_bug.cgi?id=214314 <rdar://problem/65079249>
Patch by Alex Christensen <achristensen@webkit.org> on 2020-07-14
Reviewed by Darin Adler.

Source/WTF:

r262341 made it so we usually pass kCFStringEncodingISOLatin1 into CFURLCreateAbsoluteURLWithBytes when creating a CFURLRef from a WTF::URL.
This is correct for the interpretation of the bytes to create a CFStringRef, and that is a change we want to make.
The encoding, however, is also stored with the CFURL and used later when interpreting percent-encoded sequences.
We want to use kCFStringEncodingUTF8 to make the interpretation of percent-encoded sequences the same as it used to be.
To avoid making a separate CString most of the time, use characters8() only for ASCII-only URLs, where UTF-8 and Latin1 are the same.
For all other URLs, we have to make a CString containing the UTF-8 representation of the string to get the percent-encoded sequences interpreted the same.

* wtf/cf/URLCF.cpp:
(WTF::URL::createCFURL const):
* wtf/cocoa/URLCocoa.mm:
(WTF::URL::createCFURL const):

Tools:

* TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm:
Add a direct test and update the Mojave test expectations for the test introduced in r262341 with the change that introduced this regression.
* TestWebKitAPI/Tests/WebKitCocoa/LoadInvalidURLRequest.mm:
Update a test from r263475 which fixed another regression from r262341.
The test verified that nothing crashed or timed out, and that is still the case even with this intentional change in behavior.

Identifier: 227136@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264382 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/cf/URLCF.cpp
@@ -51,8 +51,8 @@ URL::URL(CFURLRef url)
 RetainPtr<CFURLRef> URL::createCFURL() const
 {
     RetainPtr<CFURLRef> cfURL;
-    if (LIKELY(m_string.is8Bit()))
-        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(m_string.characters8()), m_string.length(), kCFStringEncodingISOLatin1, nullptr, true));
+    if (LIKELY(m_string.is8Bit() && m_string.isAllASCII()))
+        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(m_string.characters8()), m_string.length(), kCFStringEncodingUTF8, nullptr, true));
     else {
         CString utf8 = m_string.utf8();
         cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(utf8.data()), utf8.length(), kCFStringEncodingUTF8, nullptr, true));

Source/WTF/wtf/cocoa/URLCocoa.mm
@@ -69,8 +69,8 @@ RetainPtr<CFURLRef> URL::createCFURL() const
     }
 
     RetainPtr<CFURLRef> cfURL;
-    if (LIKELY(m_string.is8Bit()))
-        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(m_string.characters8()), m_string.length(), kCFStringEncodingISOLatin1, nullptr, true));
+    if (LIKELY(m_string.is8Bit() && m_string.isAllASCII()))
+        cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(m_string.characters8()), m_string.length(), kCFStringEncodingUTF8, nullptr, true));
     else {
         CString utf8 = m_string.utf8();
         cfURL = adoptCF(CFURLCreateAbsoluteURLWithBytes(nullptr, reinterpret_cast<const UInt8*>(utf8.data()), utf8.length(), kCFStringEncodingUTF8, nullptr, true));

Tools/TestWebKitAPI/Tests/WTF/cocoa/URLExtras.mm
@@ -218,13 +218,13 @@ TEST(WTF_URLExtras, URLExtras_ParsingError)
     WTF::URL url4(URL(), String(latin1.data()));
     EXPECT_FALSE(url4.isValid());
     EXPECT_TRUE(url4.string().is8Bit());
-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MAX_ALLOWED < 101500
-    // CFURLCreateAbsoluteURLWithBytes has incorrect behavior on Mojave
-    // See https://bugs.webkit.org/show_bug.cgi?id=212486#c6
-    EXPECT_STREQ([[url4 absoluteString] UTF8String], "%C2%B6");
-#else
     EXPECT_STREQ([[url4 absoluteString] UTF8String], "%C3%82%C2%B6");
-#endif
+
+    char buffer[100];
+    memset(buffer, 0, sizeof(buffer));
+    WTF::URL url5(URL(), "file:///A%C3%A7%C3%A3o.html"_s);
+    CFURLGetFileSystemRepresentation(url5.createCFURL().get(), false, reinterpret_cast<UInt8*>(buffer), sizeof(buffer));
+    EXPECT_STREQ(buffer, "/Ação.html");
 }
 
 TEST(WTF_URLExtras, URLExtras_Nil)

Tools/TestWebKitAPI/Tests/WebKitCocoa/LoadInvalidURLRequest.mm
@@ -93,7 +93,7 @@ TEST(WebKit, LoadInvalidURLRequestNonASCII)
     delegate.get().didFailProvisionalNavigation = ^(WKWebView *, WKNavigation *, NSError *error) {
         EXPECT_WK_STREQ(error.domain, @"WebKitErrorDomain");
         EXPECT_EQ(error.code, WebKitErrorCannotShowURL);
-        EXPECT_WK_STREQ([error.userInfo[@"NSErrorFailingURLKey"] absoluteString], "http://%E2%80%80");
+        EXPECT_WK_STREQ([error.userInfo[@"NSErrorFailingURLKey"] absoluteString], "http://%C3%A2%C2%80%C2%80");
         done = true;
     };
     auto webView = adoptNS([WKWebView new]);